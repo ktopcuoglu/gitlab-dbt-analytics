c510f3ea2bfc3ad26c198fd2765bf4d708d506b8
Michael Walker <mwalker@gitlab.com>
2020-08-21 16:21:08 +0000

    Revert "Merge branch..."
    This reverts merge request !3388

--- transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
+++ transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
@@ -3,12 +3,7 @@
     sha2(
         TRIM(
             LOWER(
-                {{column|lower}} || 
-                ENCRYPT_RAW(
-                  to_binary('{{ get_salt(column|lower) }}', 'utf-8'), 
-                  to_binary('{{ env_var("SALT_PASSWORD") }}', 'HEX'), 
-                  to_binary('416C736F4E637265FFFFFFAB', 'HEX')
-                )['ciphertext']::VARCHAR 
+                {{column|lower}} || ENCRYPT('{{ get_salt(column|lower) }}', '{{ get_salt(column|lower) }}')::VARCHAR 
             )
         )
     ) AS {{column|lower}}_hash,
