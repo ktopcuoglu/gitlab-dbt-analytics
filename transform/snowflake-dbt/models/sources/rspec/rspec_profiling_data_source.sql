bf548e3a8c01170194cb1414284607c5352103db
parmstrong <parmstrong@gitlab.com>
2020-08-25 20:03:40 +0200

    Corrected formatting

--- transform/snowflake-dbt/models/sources/rspec/rspec_profiling_data_source.sql
+++ transform/snowflake-dbt/models/sources/rspec/rspec_profiling_data_source.sql
@@ -3,26 +3,21 @@ WITH source AS (
     SELECT *
     FROM {{ source('rspec', 'profiling_data') }}
 
-),
-
-renamed AS (
+), renamed AS (
 
     SELECT
-
-
-      commit				    ,
-      commit_time			    ,
-      total_time			    ,
-      number_of_tests		    ,
-      time_per_single_test      ,
-      total_queries		        ,
-      total_query_time	        ,
-      total_requests		    ,
-      _UPDATED_AT      as updated_at
+      commit                   AS commit,
+      commit_time              AS commit_time,
+      total_time               AS total_time,
+      number_of_tests          AS number_of_tests,
+      time_per_single_test     AS time_per_single_test,
+      total_queries            AS total_queries,
+      total_query_time         AS total_query_time,
+      total_requests           AS total_requests,
+      _UPDATED_AT              AS updated_at
     FROM source
 
 )
 
 SELECT *
 FROM renamed
-
