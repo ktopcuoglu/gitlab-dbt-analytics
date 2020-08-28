4ca8a70f604ad756d516b93b4b8de8a4be815fc9
Michael Walker <mwalker@gitlab.com>
2020-08-28 15:51:13 -0500

    add stages yaml model information into schema.yml

--- transform/snowflake-dbt/models/sources/gitlab_data_yaml/stages_yaml_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_data_yaml/stages_yaml_source.sql
@@ -16,17 +16,19 @@ WITH source AS (
 ), intermediate_stage AS (
 
     SELECT
-      data_by_row['pm']::VARCHAR                             AS project_manager,
-      data_by_row['display_name']::VARCHAR                   AS stage_display_name,
-      data_by_row['established']::NUMBER                     AS year_established,
-      data_by_row['lifecycle']::NUMBER                       AS lifecycle,
-      data_by_row['horizon']::VARCHAR                        AS horizon,
-      data_by_row['contributions']::NUMBER                   AS contributions,
-      data_by_row['usage_driver_score']::NUMBER              AS usage_driver_score,
-      data_by_row['sam_driver_score']::NUMBER                AS sam_driver_score,
+      rank,
       data_by_row['related']::ARRAY                          AS related_stages,
+      snapshot_date,
+      data_by_row['pm']::VARCHAR                             AS stage_product_manager,
+      data_by_row['display_name']::VARCHAR                   AS stage_display_name,
+      data_by_row['established']::NUMBER                     AS stage_year_established,
+      data_by_row['lifecycle']::NUMBER                       AS stage_lifecycle,
+      data_by_row['horizon']::VARCHAR                        AS stage_horizon,
+      data_by_row['contributions']::NUMBER                   AS stage_contributions,
+      data_by_row['usage_driver_score']::NUMBER              AS stage_usage_driver_score,
+      data_by_row['sam_driver_score']::NUMBER                AS stage_sam_driver_score,
       data_by_row['stage_development_spend_percent']::NUMBER AS stage_development_spend_percent,
-      data_by_row['groups']::ARRAY                            AS stage_groups
+      data_by_row['groups']::ARRAY                           AS stage_groups
     FROM intermediate
       
 )
