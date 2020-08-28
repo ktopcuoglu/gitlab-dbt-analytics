834eab2303473448e21ed5b14f189ea911c95bb2
parmstrong <parmstrong@gitlab.com>
2020-08-28 14:22:43 +0200

    Testing a different approach to reading

--- extract/postgres_pipeline/postgres_pipeline/utils.py
+++ extract/postgres_pipeline/postgres_pipeline/utils.py
@@ -187,43 +187,38 @@ def chunk_and_upload(
     """
 
     rows_uploaded = 0
-    results_generator = read_sql_tmpfile(query, source_engine)
-    logging.info(results_generator.head(5))
+    chunk_df = read_sql_tmpfile(query, source_engine)
+    logging.info(chunk_df.head(5))
+    logging.info(len(chunk_df))
 
 
     upload_file_name = f"{target_table}_CHUNK.tsv.gz"
 
     backfilled_rows = 0
 
-    for idx, chunk_df in enumerate(results_generator):
-        # If the table doesn't exist, it needs to send the first chunk to the dataframe_uploader
-        if backfill:
-            rows_to_seed = 10000
-            seed_table(
-                advanced_metadata,
-                chunk_df,
-                target_engine,
-                target_table,
-                rows_to_seed=rows_to_seed,
-            )
-            backfilled_rows += chunk_df[:rows_to_seed].shape[0]
-            chunk_df = chunk_df.iloc[rows_to_seed:]
-        row_count = chunk_df.shape[0]
-        rows_uploaded += row_count
-        if not backfill or row_count > 0:
-            upload_to_gcs(
-                advanced_metadata, chunk_df, upload_file_name + "." + str(idx)
-            )
-        backfill = False
-    if rows_uploaded > 0:
-        trigger_snowflake_upload(
+    #rows_to_seed = 10000
+    #seed_table(
+    #        advanced_metadata,
+    #        chunk_df,
+    #        target_engine,
+    #        target_table,
+    #        rows_to_seed=rows_to_seed,
+    #)
+
+    upload_to_gcs(
+            advanced_metadata, chunk_df, upload_file_name + "." + str(idx)
+    )
+
+    trigger_snowflake_upload(
             target_engine, target_table, upload_file_name + "[.]\\\\d*", purge=True
-        )
+    )
     logging.info(
-        f"Uploaded {rows_uploaded + backfilled_rows} total rows to table {target_table}."
+            f"Uploaded {rows_uploaded + backfilled_rows} total rows to table {target_table}."
     )
     target_engine.dispose()
     source_engine.dispose()
+
+
 
 
 def range_generator(
