f37000545948a41d3550e08af97364e0ff4e240a
Taylor Murphy <tmurphy@gitlab.com>
2020-09-02 16:10:25 -0500

    Add subscription new record test

---
+++ transform/snowflake-dbt/tests/sources/zuora/rowcount/zuora_subscription_source_new_records_per_day.sql
@@ -0,0 +1,13 @@
+{{ config({
+    "tags": ["tdf"]
+    })
+}}
+
+{{ source_new_rows_per_day(
+    'zuora', 
+    'subscription',
+    'createddate',
+    50,
+    200,
+    "date_trunc('day',createddate) >= '2020-01-01'"
+) }}
