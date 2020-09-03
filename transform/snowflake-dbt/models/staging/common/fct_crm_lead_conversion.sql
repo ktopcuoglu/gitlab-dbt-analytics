981014bfd7754ba3a376c865eac534050e3e18d8
jjstark <jstark@gitlab.com>
2020-09-03 11:05:35 -0700

    add contacts

---
+++ transform/snowflake-dbt/models/staging/common/fct_crm_lead_conversion.sql
@@ -0,0 +1,34 @@
+WITH sfdc_lead AS(
+
+  SELECT *
+  FROM {{ ref('sfdc_lead') }}
+
+), sfdc_lead_history_source AS(
+
+  SELECT *
+  FROM {{ ref('sfdc_lead_history_source') }}
+
+), lead_conversion_event AS (
+
+  SELECT
+    
+    {{ dbt_utils.surrogate_key(['lead_history_id','field_modified_at']) }}               AS event_id,
+    sfdc_lead_history_source.field_modified_at                                           AS event_timestamp,
+    sfdc_lead_history_source.lead_history_id                                             AS sfdc_record_id,
+    'lead history'                                                                       AS sfdc_record,
+    {{ dbt_utils.surrogate_key(['COALESCE(converted_contact_id, sfdc_lead.lead_id)']) }} AS crm_person_id,
+    sfdc_lead_history_source.lead_id                                                     AS lead_id,
+    sfdc_lead_history_source.created_by_id                                               AS crm_user_id,
+    sfdc_lead.converted_contact_id                                                       AS contact_id,
+    sfdc_lead.converted_account_id                                                       AS account_id,
+    sfdc_lead.converted_opportunity_id                                                   AS opportunity_id,
+    'lead conversion'                                                                    AS event_name
+
+  FROM sfdc_lead_history_source
+  INNER JOIN sfdc_lead
+    ON sfdc_lead.lead_id = sfdc_lead_history_source.lead_id
+
+)
+
+SELECT *
+FROM lead_conversion_event
---
+++ transform/snowflake-dbt/models/staging/common/fct_crm_marketing_qualification.sql
@@ -0,0 +1,45 @@
+WITH sfdc_lead AS(
+
+  SELECT *
+  FROM {{ ref('sfdc_lead') }}
+
+), sfdc_contact AS (
+
+	SELECT *
+	FROM {{ ref('sfdc_contact')}}
+), marketing_qualification_events AS(
+
+SELECT
+
+    {{ dbt_utils.surrogate_key(['lead_id','marketo_qualified_lead_date::timestamp']) }} AS event_id,
+    marketo_qualified_lead_date::timestamp                                              AS event_timestamp,
+    lead_id                                                                             AS sfdc_record_id,
+    'lead'                                                                              AS sfdc_record,
+    {{ dbt_utils.surrogate_key(['COALESCE(converted_contact_id, lead_id)']) }}          AS crm_person_id,
+    converted_contact_id                                                                AS contact_id,
+    converted_account_id                                                                AS account_id,
+    'marketing qualification'                                                           AS event_name
+  
+  FROM sfdc_lead
+  WHERE marketo_qualified_lead_date IS NOT NULL
+
+  UNION 
+
+  SELECT
+
+    {{ dbt_utils.surrogate_key(['contact_id','marketo_qualified_lead_date::timestamp']) }} AS event_id,
+    marketo_qualified_lead_date::timestamp                                                 AS event_timestamp,
+    contact_id                                                                             AS sfdc_record_id,
+    'contact'                                                                              AS sfdc_record,
+    {{ dbt_utils.surrogate_key(['contact_id']) }}                                          AS crm_person_id,
+    contact_id                                                                             AS contact_id,
+    account_id                                                                             AS account_id,
+    'marketing qualification'                                                              AS event_name
+     
+  FROM sfdc_contact
+  WHERE marketo_qualified_lead_date IS NOT NULL
+
+)
+
+SELECT *
+FROM marketing_qualification_events
