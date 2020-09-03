b4ca1a966f1d759377c698e81fcb7342218d4766
jjstark <jstark@gitlab.com>
2020-09-03 11:42:32 -0700

    add staging layer for lead history

--- transform/snowflake-dbt/models/staging/common/fct_crm_lead_conversion.sql
+++ transform/snowflake-dbt/models/staging/common/fct_crm_lead_conversion.sql
@@ -13,12 +13,12 @@ WITH sfdc_lead AS(
   SELECT
     
     {{ dbt_utils.surrogate_key(['lead_history_id','field_modified_at']) }}               AS event_id,
-    sfdc_lead_history.field_modified_at                                           AS event_timestamp,
-    sfdc_lead_history.lead_history_id                                             AS sfdc_record_id,
+    sfdc_lead_history.field_modified_at                                                  AS event_timestamp,
+    sfdc_lead_history.lead_history_id                                                    AS sfdc_record_id,
     'lead history'                                                                       AS sfdc_record,
     {{ dbt_utils.surrogate_key(['COALESCE(converted_contact_id, sfdc_lead.lead_id)']) }} AS crm_person_id,
-    sfdc_lead_history.lead_id                                                     AS lead_id,
-    sfdc_lead_history.created_by_id                                               AS crm_user_id,
+    sfdc_lead_history.lead_id                                                            AS lead_id,
+    sfdc_lead_history.created_by_id                                                      AS crm_user_id,
     sfdc_lead.converted_contact_id                                                       AS contact_id,
     sfdc_lead.converted_account_id                                                       AS account_id,
     sfdc_lead.converted_opportunity_id                                                   AS opportunity_id,
