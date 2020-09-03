d570af3458b9239205221197e721c6734990c2ef
parmstrong <parmstrong@gitlab.com>
2020-09-03 16:58:41 +0200

    Corrected tests, finalized macro.

--- transform/snowflake-dbt/macros/tests/source_golden_records_unchanged.sql
+++ transform/snowflake-dbt/macros/tests/source_golden_records_unchanged.sql
@@ -1,8 +1,10 @@
 {% macro source_golden_records_unchanged(golden_record_model, hashed_model, join_column) %}
 
+
 WITH sheetload_data AS (
 
     SELECT
+
         {{ hash_sensitive_columns(golden_record_model)}}
     FROM {{ ref(golden_record_model) }}
 
@@ -12,10 +14,30 @@ WITH sheetload_data AS (
         *
     FROM {{ ref(hashed_model) }}
 
+),
+check_data AS (
+
+{% set meta_columns = get_meta_columns(golden_record_model, "sensitive") %}
+
+
+    SELECT
+        sheetload.account_id,
+         SUM(
+        {%- for column in meta_columns %}
+        CASE WHEN
+            sheetload.{{ column }}_hash = hashed.{{ column }}_hash THEN 0 ELSE 1
+        END +
+        {% endfor %}
+        -- Terminate the last +
+        0 )
+        AS num_rows
+    FROM sheetload_data sheetload
+    LEFT JOIN hashed_data hashed ON hashed.{{ join_column }} = sheetload.{{ join_column }}
+    GROUP BY sheetload.account_id
 )
 
-SELECT COUNT(*)
-FROM sheetload_data sheetload
-JOIN hashed_data hashed ON hashed.{{ join_column }} = sheetload.{{ join_column }}
+    SELECT *
+    FROM check_data
+    WHERE num_rows > 0
 
 {% endmacro %}
