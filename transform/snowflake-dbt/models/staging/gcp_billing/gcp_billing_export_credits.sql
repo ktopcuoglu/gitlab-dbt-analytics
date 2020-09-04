7ac210dc1338b001f5e1a91171fcc4b190ae785d
davis_townsend <dtownsend@gitlab.com>
2020-09-04 13:16:11 -0500

    fix view surrogate keys

--- transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_credits.sql
+++ transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_credits.sql
@@ -11,7 +11,7 @@ WITH source AS (
 )
 
 SELECT
-{{ dbt_utils.surrogate_key(['primary_key', 'credit_description','credit_name'] ) }} AS credit_pk,
+{{ dbt_utils.surrogate_key(['source_primary_key', 'credit_description','credit_name'] ) }} AS credit_pk,
 source.primary_key                                                                  AS source_primary_key,
 credits_flat.value:name::VARCHAR                                                    AS credit_description,
 credits_flat.value:amount::FLOAT                                                    AS credit_amount,
--- transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_resource_labels.sql
+++ transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_resource_labels.sql
@@ -11,7 +11,7 @@ WITH source AS (
 )
 
 SELECT
-{{ dbt_utils.surrogate_key(['primary_key', 'resource_label_key','resource_label_value'] ) }}          AS resource_label_pk,
+{{ dbt_utils.surrogate_key(['source_primary_key', 'resource_label_key','resource_label_value'] ) }}          AS resource_label_pk,
 source.primary_key                                                                                    AS source_primary_key,
 labels_flat.value:key::VARCHAR                                                                        AS resource_label_key,
 labels_flat.value:value::VARCHAR                                                                      AS resource_label_value,
