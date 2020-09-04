d102d9ed95a07088cf93edfadcc684cd3c6ffeb2
davis_townsend <dtownsend@gitlab.com>
2020-09-04 13:34:24 -0500

    remove erroneous commas

--- transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_system_labels.sql
+++ transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_system_labels.sql
@@ -14,6 +14,6 @@ SELECT
 {{ dbt_utils.surrogate_key(['source_primary_key', 'system_label_key','system_label_value'] ) }}            AS system_label_pk,
 source.primary_key                                                                                  AS source_primary_key,
 system_labels_flat.value:key::VARCHAR                                                               AS system_label_key,
-system_labels_flat.value:value::VARCHAR                                                             AS system_label_value,
+system_labels_flat.value:value::VARCHAR                                                             AS system_label_value
 FROM source,
 lateral flatten(input=> system_labels) system_labels_flat
