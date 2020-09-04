0c7c0865a8d90368358ce746518056ff8b8ff8a0
davis_townsend <dtownsend@gitlab.com>
2020-09-04 10:46:04 -0500

    add labels schema

--- transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_credits.sql
+++ transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_credits.sql
@@ -6,9 +6,9 @@ WITH source AS (
 )
 
 SELECT
-{{ dbt_utils.surrogate_key(['primary_key', 'credit_description','credit_name'] ) }} AS credit_pk,
+{{ dbt_utils.surrogate_key(['primary_key', 'credit_description','credit_name'] ) }} AS credits_pk,
 source.primary_key                                                                  AS source_primary_key,
-credits_flat.value:name                                                             AS credit_description,
-credits_flat.value:amount                                                           AS credit_amount,
+credits_flat.value:name::VARCHAR                                                    AS credit_description,
+credits_flat.value:amount::FLOAT                                                    AS credit_amount,
 FROM source,
 lateral flatten(input=> credits) credits_flat
---
+++ transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_labels.sql
@@ -0,0 +1,14 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('gcp_billing_export_source') }}
+
+)
+
+SELECT
+{{ dbt_utils.surrogate_key(['primary_key', 'label_key','label_value'] ) }} AS labels_pk,
+source.primary_key                                                                  AS source_primary_key,
+labels_flat.value:key::VARCHAR                                                      AS label_key,
+labels_flat.value:value::VARCHAR                                                    AS label_value,
+FROM source,
+lateral flatten(input=> labels) labels_flat
