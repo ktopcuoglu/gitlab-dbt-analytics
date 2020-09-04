0fe6e445d7128851c7dab7d2d600144481c19f21
msendal <msendal@gitlab.com>
2020-09-04 19:28:25 +0200

    add extra fields to dim_dates

--- transform/snowflake-dbt/models/staging/common/dim_dates.sql
+++ transform/snowflake-dbt/models/staging/common/dim_dates.sql
@@ -4,6 +4,10 @@ WITH dates AS (
 )
 
 SELECT
-  TO_NUMBER(TO_CHAR(date_actual,'YYYYMMDD'),'99999999') AS date_id,
-  *
+  TO_NUMBER(TO_CHAR(date_actual,'YYYYMMDD'),'99999999')                           AS date_id,
+  *,
+  DATE_TRUNC('month', last_day_of_fiscal_quarter)                                 AS last_month_of_fiscal_quarter,
+  IFF(DATE_TRUNC('month', last_day_of_fiscal_quarter) = date_actual, True, False) AS is_first_day_of_last_month_of_fiscal_quarter,
+  DATE_TRUNC('month', last_day_of_fiscal_year)                                    AS last_month_of_fiscal_year,
+  IFF(DATE_TRUNC('month', last_day_of_fiscal_year) = date_actual, True, False)    AS is_first_day_of_last_month_of_fiscal_quarter
 FROM dates
