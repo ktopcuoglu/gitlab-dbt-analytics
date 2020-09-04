842c1464e7bb4f4e78b776731223e5ce515eefa1
davis_townsend <dtownsend@gitlab.com>
2020-09-04 10:58:15 -0500

    add project/system labels and add view specification

---
+++ transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_project_labels.sql
@@ -0,0 +1,17 @@
+{{ config({
+    "materialized": "view"
+    })
+}}
+
+    SELECT *
+    FROM {{ ref('gcp_billing_export_source') }}
+
+)
+
+SELECT
+{{ dbt_utils.surrogate_key(['primary_key', 'project_label_key','project_label_value'] ) }}          AS label_pk,
+source.primary_key                                                                                  AS source_primary_key,
+project_labels_flat.value:key::VARCHAR                                                              AS project_label_key,
+project_labels_flat.value:value::VARCHAR                                                            AS project_label_value,
+FROM source,
+lateral flatten(input=> project_labels) project_labels_flat
---
+++ transform/snowflake-dbt/models/staging/gcp_billing/gcp_billing_export_system_labels.sql
@@ -0,0 +1,19 @@
+{{ config({
+    "materialized": "view"
+    })
+}}
+
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('gcp_billing_export_source') }}
+
+)
+
+SELECT
+{{ dbt_utils.surrogate_key(['primary_key', 'system_label_key','system_label_value'] ) }}            AS label_pk,
+source.primary_key                                                                                  AS source_primary_key,
+system_labels_flat.value:key::VARCHAR                                                               AS system_label_key,
+system_labels_flat.value:value::VARCHAR                                                             AS system_label_value,
+FROM source,
+lateral flatten(input=> system_labels) system_labels_flat
