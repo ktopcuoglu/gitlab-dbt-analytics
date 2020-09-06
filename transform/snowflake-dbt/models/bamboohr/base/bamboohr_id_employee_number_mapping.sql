e2beaccd7289ac059bda92a2c889a90de56e545e
Parul Luthra <pluthra@gitlab.com>
2020-09-06 09:02:53 -0700

    adding in country and age cohort

--- transform/snowflake-dbt/models/bamboohr/base/bamboohr_id_employee_number_mapping.sql
+++ transform/snowflake-dbt/models/bamboohr/base/bamboohr_id_employee_number_mapping.sql
@@ -30,13 +30,41 @@ WITH source AS (
     FROM source,
     LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext['employees']), outer => true) d
 
+), final AS (
+
+    SELECT
+      employee_number,
+      employee_id,
+      first_name,
+      last_name,
+      hire_date,
+      termination_date,
+      CASE WHEN age BETWEEN 18 AND 24 THEN '18-24'
+          WHEN age BETWEEN 25 AND 29  THEN '25-29'
+          WHEN age BETWEEN 30 AND 34  THEN '30-34'
+          WHEN age BETWEEN 35 AND 39  THEN '35-39'
+          WHEN age BETWEEN 40 AND 44  THEN '40-44'
+          WHEN age BETWEEN 44 AND 49  THEN '44-49'
+          WHEN age BETWEEN 50 AND 54  THEN '50-54'
+          WHEN age BETWEEN 55 AND 59  THEN '55-59'
+          WHEN age>= 60               THEN '60+'
+          WHEN age IS NULL            THEN 'Unreported'
+          WHEN age = -1               THEN 'Unreported'
+          ELSE NULL END               AS age_cohort,
+      country,
+      ethnicity,
+      gender,  
+      nationality,
+      region,
+      greenhouse_candidate_id
+    FROM intermediate
+    WHERE hire_date IS NOT NULL
+        AND (LOWER(first_name) NOT LIKE '%greenhouse test%'
+            and LOWER(last_name) NOT LIKE '%test profile%'
+            and LOWER(last_name) != 'test-gitlab')
+        AND employee_id  NOT IN (42039, 42043)
+
 )
 
-
 SELECT *
-FROM intermediate
-WHERE hire_date IS NOT NULL
-    AND (LOWER(first_name) NOT LIKE '%greenhouse test%'
-         and LOWER(last_name) NOT LIKE '%test profile%'
-         and LOWER(last_name) != 'test-gitlab')
-    AND employee_id  NOT IN (42039, 42043)
+FROM final
