90cc82bcc2698b3c80fb076cbd2a3633f7c73bb8
Justin Stark <jstark@gitlab.com>
2020-09-08 23:09:23 +0000

    Update sfdc_opportunity_contact_role_source.sql

--- transform/snowflake-dbt/models/sources/sfdc/sfdc_opportunity_contact_role_source.sql
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_opportunity_contact_role_source.sql
@@ -11,16 +11,15 @@ WITH source AS (
 
       id               AS opportunity_contact_role_id,
       contactid        AS contact_id,
-	  opportunityid    AS opportunity_id,
+      opportunityid    AS opportunity_id,
       createdbyid      AS created_by_id,
       lastmodifiedbyid AS last_modified_by_id, 
 
-	  -- info
-	  role             AS contact_role,
+      -- info
+      role             AS contact_role,
       isprimary        AS is_primary_contact,
-	  createddate      AS created_date,
-	  lastmodifieddate AS last_modified_date
-
+      createddate      AS created_date,
+      lastmodifieddate AS last_modified_date
 
     FROM source
 )
