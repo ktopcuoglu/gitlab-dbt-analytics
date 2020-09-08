c2812f14c564debc9eb4ad28315a0f94a044b512
msendal <msendal@gitlab.com>
2020-09-08 18:09:13 +0200

    align AS

--- transform/snowflake-dbt/models/sources/date/date_details_source.sql
+++ transform/snowflake-dbt/models/sources/date/date_details_source.sql
@@ -87,7 +87,7 @@ WITH date_spine AS (
         ELSE NULL END)::VARCHAR                                                               AS holiday_desc,
       (CASE WHEN HOLIDAY_DESC IS NULL THEN 0
         ELSE 1 END)::BOOLEAN                                                                  AS is_holiday,
-        DATE_TRUNC('month', last_day_of_fiscal_quarter)                                       AS last_month_of_fiscal_quarter,
+      DATE_TRUNC('month', last_day_of_fiscal_quarter)                                         AS last_month_of_fiscal_quarter,
       IFF(DATE_TRUNC('month', last_day_of_fiscal_quarter) = date_actual, True, False)         AS is_first_day_of_last_month_of_fiscal_quarter,
       DATE_TRUNC('month', last_day_of_fiscal_year)                                            AS last_month_of_fiscal_year,
       IFF(DATE_TRUNC('month', last_day_of_fiscal_year) = date_actual, True, False)            AS is_first_day_of_last_month_of_fiscal_year
