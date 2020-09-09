2feec4a4e344dc60295bbf822103bea374822d95
jjstark <jstark@gitlab.com>
2020-09-09 10:50:17 -0700

    add SAO fact

--- transform/snowflake-dbt/models/staging/common/fct_crm_sales_accepted_opportunity.sql
+++ transform/snowflake-dbt/models/staging/common/fct_crm_sales_accepted_opportunity.sql
@@ -8,7 +8,7 @@ WITH sfdc_opportunity AS (
   SELECT
   
     created_date,
-	opportunity_contact_role_id,
+    opportunity_contact_role_id,
     opportunity_id,
     contact_id,
     ROW_NUMBER() OVER (PARTITION BY opportunity_id ORDER BY created_date ASC) AS row_num
@@ -20,12 +20,12 @@ WITH sfdc_opportunity AS (
   SELECT
   
     {{ dbt_utils.surrogate_key(['sfdc_opportunity.opportunity_id','sales_accepted_date']) }} AS event_id,
-	sfdc_opportunity.sales_accepted_date                                                     AS sales_accepted_date,
+    sfdc_opportunity.sales_accepted_date                                                     AS sales_accepted_date,
     {{ dbt_utils.surrogate_key(['first_contact_roles.contact_id']) }}                        AS crm_person_id,
-	first_contact_roles.contact_id                                                           AS contact_id,
+    first_contact_roles.contact_id                                                           AS contact_id,
     sfdc_opportunity.account_id                                                              AS account_id,
     sfdc_opportunity.opportunity_id                                                          AS opportunity_id,
-	first_contact_roles.opportunity_contact_role_id                                          AS opportunity_contact_role_id
+    first_contact_roles.opportunity_contact_role_id                                          AS opportunity_contact_role_id
 
   FROM sfdc_opportunity
   INNER JOIN first_contact_roles 
