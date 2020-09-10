cd92e396a3f103e67f693b8307f30eefbc9a8d4b
Taylor Murphy <tmurphy@gitlab.com>
2020-09-10 15:19:39 -0500

    New macros

---
+++ transform/snowflake-dbt/macros/tests/rowcount/model_rowcount.sql
@@ -0,0 +1,22 @@
+{% macro source_rowcount(model_name, count, where_clause=None) %}
+
+WITH source as (
+
+    SELECT *
+    FROM {{ ref(model_name) }}
+
+), counts AS (
+
+    SELECT count(*) as row_count
+    FROM source
+    {% if where_clause != None %}
+    WHERE {{ where_clause }}
+    {% endif %}
+
+)
+
+SELECT row_count
+FROM counts
+WHERE row_count < {{ count }}
+
+{% endmacro %}
