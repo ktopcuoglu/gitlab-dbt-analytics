771a0c957ef0eeb058b032cd085c47d138ea4b72
mpeychet <mpeychet@gitlab.com>
2020-09-11 14:42:30 +0200

    dev source and staging model

---
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_dev_section_metrics_source.sql
@@ -0,0 +1,20 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('sheetload', 'dev_section_metrics') }}
+
+), renamed AS (
+
+    SELECT 
+      section::VARCHAR                AS section,
+      metrics_path::VARCHAR           AS metrics_path,
+      stage::VARCHAR                  AS stage_name
+      smau::BOOLEAN                   AS is_smau,
+      clean_metrics_name::VARCHAR     AS clean_metrics_name,
+      periscope_metrics_name::VARCHAR AS periscope_metrics_name
+    FROM source 
+
+)
+
+SELECT *
+FROM renamed
