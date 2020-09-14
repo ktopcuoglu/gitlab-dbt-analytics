c6c007d44ede511176d5be336bbf17d773a9b3e0
parmstrong <parmstrong@gitlab.com>
2020-09-14 16:00:05 +0200

    Updated TDF checks

--- transform/snowflake-dbt/tests/sources/zuora/golden_records/zuora_account_name.sql
+++ transform/snowflake-dbt/tests/sources/zuora/golden_records/zuora_account_name.sql
@@ -1,1 +1,1 @@
-{{ source_golden_records_unchanged('zuora_golden_records','zuora_account_source', 'account_id', 'account_name') }}
+{{ source_golden_records_unchanged('zuora_account_golden_records','zuora_account_source', 'account_id', 'account_name') }}
