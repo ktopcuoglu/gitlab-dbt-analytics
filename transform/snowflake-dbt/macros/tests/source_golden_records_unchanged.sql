8810aa668a4f718e6311d73dd677b2addd36864f
parmstrong <parmstrong@gitlab.com>
2020-09-15 15:41:54 +0200

    Revert "Changed to natural join and IFF"
    This reverts commit 7d81753a

--- transform/snowflake-dbt/macros/tests/source_golden_records_unchanged.sql
+++ transform/snowflake-dbt/macros/tests/source_golden_records_unchanged.sql
@@ -2,19 +2,27 @@
 
 {% set golden_record_model = source_model + '_golden_records' %}
 
-set (golden_record_count) = (
-    SELECT COUNT(*)
-    FROM {{ ref(golden_record_model) }}
-)
+{% set meta_columns = get_meta_columns(golden_record_model, "sensitive") %}
 
-set (joined_count) = (
+WITH check_data AS (
 
     SELECT
-      COUNT(*)
+
+    SUM(
+    {%- for column in meta_columns %}
+        CASE WHEN IFNULL(golden_records.{{ column }}, '') = IFNULL(golden_records.{{ column }}, '') THEN 0 ELSE 1 END
+            {%- if not loop.last %}
+                +
+            {% endif %}
+        {% endfor %}
+        ) AS is_correct
     FROM {{ ref(golden_record_model) }} golden_records
-    NATURAL JOIN {{ ref(source_model) }}
+    JOIN {{ ref(source_model) }} ON
+    {%- for column in meta_columns %}
+        source_model.{{ column }} = golden_records.{{ column  }} AND
+    {% endfor %}
 )
-
-SELECT IFF($joined_count = $golden_record_count, NULL, 1)
-
+    SELECT *
+    FROM check_data
+    WHERE is_correct > 1
 {% endmacro %}
