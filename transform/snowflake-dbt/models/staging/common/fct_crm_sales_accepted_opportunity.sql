b00ff44c538f5084a0f565d6c5f24cae2365b9af
jjstark <jstark@gitlab.com>
2020-09-15 13:23:28 -0700

    add filters

--- transform/snowflake-dbt/models/staging/common/fct_crm_sales_accepted_opportunity.sql
+++ transform/snowflake-dbt/models/staging/common/fct_crm_sales_accepted_opportunity.sql
@@ -33,7 +33,11 @@ WITH sfdc_opportunity AS (
 	  AND row_num = 1
   WHERE sfdc_opportunity.sales_accepted_date IS NOT NULL
     AND is_edu_oss = 0
-	AND order_type = '1. New - First Order'
+	AND stage_name != '10-Duplicate'
+	AND sales_qualified_source IN (
+                                 'SDR Generated'
+								 , 'BDR Generated'
+	                             )
 )
 
 SELECT *
