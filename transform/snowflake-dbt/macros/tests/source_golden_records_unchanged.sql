cd150244500ea5da2a2f1c8a9a92a291ac8e42c8
parmstrong <parmstrong@gitlab.com>
2020-09-15 16:23:33 +0200

    Syntax improvements.

--- transform/snowflake-dbt/macros/tests/source_golden_records_unchanged.sql
+++ transform/snowflake-dbt/macros/tests/source_golden_records_unchanged.sql
@@ -1,21 +1,19 @@
 {% macro source_golden_records_unchanged(source_model) %}
 
 {% set golden_record_model = source_model + '_golden_records' %}
-
 {% set meta_columns = get_meta_columns(golden_record_model) %}
 
 WITH check_data AS (
 
     SELECT
-
-    SUM(
-    {%- for column in meta_columns %}
-        CASE WHEN IFNULL(golden_records.{{ column }}, '') = IFNULL(golden_records.{{ column }}, '') THEN 0 ELSE 1 END
-            {%- if not loop.last %}
-                +
-            {% endif %}
-        {% endfor %}
-        ) AS is_correct
+      SUM(
+      {%- for column in meta_columns %}
+          CASE WHEN IFNULL(golden_records.{{ column }}, '') = IFNULL(golden_records.{{ column }}, '') THEN 0 ELSE 1 END
+              {%- if not loop.last %}
+                  +
+              {% endif %}
+          {% endfor %}
+          ) AS is_incorrect
     FROM {{ ref(golden_record_model) }} golden_records
     JOIN {{ ref(source_model) }} source_model ON
     {%- for column in meta_columns %}
@@ -25,7 +23,10 @@ WITH check_data AS (
         {% endif %}
     {% endfor %}
 )
-    SELECT *
-    FROM check_data
-    WHERE is_correct > 1
+
+
+SELECT *
+FROM check_data
+WHERE is_incorrect > 1
+
 {% endmacro %}
