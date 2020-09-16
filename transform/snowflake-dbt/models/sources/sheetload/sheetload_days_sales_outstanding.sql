996ab3ac56b32b7a992e32fd64605d566309eb9b
iweeks <iweeks@gitlab.com>
2020-09-16 18:12:01 -0400

    fix formatting

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_days_sales_outstanding.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_days_sales_outstanding.sql
@@ -6,18 +6,18 @@ WITH source AS (
 ), renamed AS (
 
   SELECT
-    period::DATE                            AS period,
-    total_touch_billings::NUMBER            AS total_touch_billings,
-    total_beginning_ar::NUMBER              AS total_beginning_ar,
-    total_ar_at_end_of_period::NUMBER       AS total_ar_at_end_of_period,
-    nbr_of_days_in_period::NUMBER           AS nbr_of_days_in_period,
-    dso::NUMBER                             AS dso,
-    collections_based_on_formula::NUMBER    AS collections_based_on_formula,
-    total_current_ar_close::NUMBER          AS total_current_ar_close,
-    p_d_ar::NUMBER                          AS p_d_ar,
-    collection_effectiveness_index::NUMBER  AS collection_effectiveness_index,
-    dso_trend::NUMBER                       AS dso_trend,
-    cei_trend::NUMBER                       AS cei_trend
+    period::DATE                                                        AS period,
+    regexp_replace(total_touch_billings, '[,]','')::NUMBER              AS total_touch_billings,
+    regexp_replace(total_beginning_ar, '[,]','')::NUMBER                AS total_beginning_ar,
+    regexp_replace(total_ar_at_end_of_period, '[,]','')::NUMBER         AS total_ar_at_end_of_period,
+    nbr_of_days_in_period::NUMBER                                       AS nbr_of_days_in_period,
+    dso::NUMBER                                                         AS dso,
+    regexp_replace(collections_based_on_formula, '[,]','')::NUMBER      AS collections_based_on_formula,
+    regexp_replace(total_current_ar_close, '[,]','')::NUMBER            AS total_current_ar_close,
+    regexp_replace(p_d_ar, '[,]','')::NUMBER                            AS p_d_ar,
+    collection_effectiveness_index::NUMBER                              AS collection_effectiveness_index,
+    dso_trend::NUMBER                                                   AS dso_trend,
+    cei_trend::NUMBER                                                   AS cei_trend
   FROM source
 
 )
