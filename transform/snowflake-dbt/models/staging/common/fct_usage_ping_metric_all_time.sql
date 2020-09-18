19fd9b7dd3719554a525764f775638f272373309
Michael Walker <mwalker@gitlab.com>
2020-09-18 12:01:31 -0500

    part of metric_all_time

--- transform/snowflake-dbt/models/staging/common/fct_usage_ping_metric_all_time.sql
+++ transform/snowflake-dbt/models/staging/common/fct_usage_ping_metric_all_time.sql
@@ -23,7 +23,7 @@ WITH usage_data AS (
     FROM unpacked_stages,
       lateral flatten(input => unpacked_stages.stage_activity_count_json) data
 
-), renamed AS (
+), renamed_activity_by_stage AS (
 
     SELECT
       id          AS usage_ping_id,
@@ -32,6 +32,33 @@ WITH usage_data AS (
       metric_name,
       metric_value
     FROM unpacked_metric_names
+
+), stats_used_mappings AS (
+    
+    SELECT
+      full_metrics_path,
+      stage
+    FROM ref('test_metrics_renaming')
+
+), stats_used AS (
+
+    SELECT
+      usage_data.*,
+      f.key                     AS metric_name,
+      f.value                   AS metric_value
+    FROM usage_data,
+      lateral flatten(input => usage_data.stats_used) f
+
+), joined_stats_info AS (
+
+    SELECT
+      id            AS usage_ping_id,
+      recorded_at   AS recorded_at,
+      stage_name,
+      metric_name,
+      metric_value
+    FROM stats_used
+    INNER JOIN 
 
 )
 
