f2d7ba5f2a111760a7ebf46e7c859be392fabbe2
davis_townsend <dtownsend@gitlab.com>
2020-09-18 14:19:06 -0500

    move qualify back to grouped

--- transform/snowflake-dbt/models/sources/gcp_billing/gcp_billing_export_source.sql
+++ transform/snowflake-dbt/models/sources/gcp_billing/gcp_billing_export_source.sql
@@ -78,7 +78,6 @@ WITH source AS (
 
   FROM source,
   TABLE(FLATTEN(source.jsontext)) flatten_export
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY primary_key ORDER BY uploaded_at DESC) = 1
 
 ), grouped AS (
     SELECT
@@ -114,6 +113,7 @@ WITH source AS (
       SUM(usage_amount_in_pricing_units)                                AS usage_amount_in_pricing_units
       FROM flattened
       {{ dbt_utils.group_by(n=27) }}
+      QUALIFY ROW_NUMBER() OVER (PARTITION BY primary_key ORDER BY uploaded_at DESC) = 1
 
 )
 
