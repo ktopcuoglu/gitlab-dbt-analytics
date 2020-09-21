b90b48e5d7a9ec701c0f68ddf0aa3651c6340e9c
mpeychet <mpeychet@gitlab.com>
2020-09-21 16:38:23 +0200

    push first version

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_monthly_stage_active_users.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_monthly_stage_active_users.sql
@@ -1,3 +1,9 @@
+{{ config({
+    "materialized": "incremental",
+    "unique_key": "mau_id"
+    })
+}}
+
 WITH date_details AS (
   
     SELECT *
@@ -9,7 +15,12 @@ WITH date_details AS (
   
     SELECT *
     FROM {{ ref('gitlab_dotcom_usage_data_events') }}
-  
+    {% if is_incremental() %}
+
+      WHERE DATE_TRUNC('month', event_date) >= (SELECT DATEADD('days', -1, MAX(smau_month) FROM {{this}}))
+
+    {% endif %}
+      
 )
 
 , gitlab_subscriptions AS (
@@ -26,7 +37,8 @@ WITH date_details AS (
 )
 
 SELECT 
-  DATEADD('month', -1, date_day)                  AS smau_month,
+  {{ dbt_utils.surrogate_key(['namespace_id', 'user_id', 'event_name', 'smau_month']) }} AS mau_id,
+  DATEADD('month', -1, date_day)                                                         AS smau_month,
   
   -- ids 
   gitlab_dotcom_usage_data_events.user_id,
@@ -38,12 +50,12 @@ SELECT
     WHEN gitlab_subscriptions.is_trial
       THEN 'trial'
     ELSE COALESCE(gitlab_subscriptions.plan_id, 34)::VARCHAR
-  END                                                         AS plan_id_at_smau_month_end,
+  END                                                                                    AS plan_id_at_smau_month_end,
   CASE
     WHEN gitlab_subscriptions.is_trial
       THEN 'trial'
     ELSE COALESCE(plans.plan_name, 'free')
-  END                                                         AS plan_name_at_smau_month_end,
+  END                                                                                    AS plan_name_at_smau_month_end,
   
   -- event data
   event_name,
@@ -51,11 +63,11 @@ SELECT
   is_representative_of_stage,
   
   --metadata
-  DATEDIFF('day', user_created_at, date_day)      AS days_since_user_creation,
-  DATEDIFF('day', namespace_created_at, date_day) AS days_since_namespace_creation,
+  DATEDIFF('day', user_created_at, date_day)                                             AS days_since_user_creation,
+  DATEDIFF('day', namespace_created_at, date_day)                                        AS days_since_namespace_creation,
   
-  COUNT(*)                                        AS event_count,
-  COUNT(DISTINCT TO_DATE(event_created_at))       AS event_day_count
+  COUNT(*)                                                                               AS event_count,
+  COUNT(DISTINCT TO_DATE(event_created_at))                                              AS event_day_count
 FROM date_details
 INNER JOIN gitlab_dotcom_usage_data_events 
   ON gitlab_dotcom_usage_data_events.event_created_at BETWEEN DATEADD('day', -28, date_details.date_day) AND date_day 
