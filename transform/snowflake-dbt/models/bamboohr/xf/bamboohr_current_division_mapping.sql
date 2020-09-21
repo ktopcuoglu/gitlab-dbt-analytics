d67a77e231203c5a59cf6a4068c0d71a1b71e9bb
Parul Luthra <pluthra@gitlab.com>
2020-09-21 04:47:57 +0000

    update to use ref

--- transform/snowflake-dbt/models/bamboohr/xf/bamboohr_current_division_mapping.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/bamboohr_current_division_mapping.sql
@@ -1,18 +1,18 @@
 WITH current_division_department_mapping AS (
 
     SELECT *
-    FROM "ANALYTICS"."ANALYTICS"."COST_CENTER_DIVISION_DEPARTMENT_MAPPING_CURRENT"
-  
+    FROM {{ ref ('cost_center_division_department_mapping_current') }}
+
 ), bamboo_mapping AS (
 
     SELECT *
-    FROM "ANALYTICS"."PLUTHRA_SCRATCH_SENSITIVE"."BAMBOOHR_ID_EMPLOYEE_NUMBER_MAPPING"
+    FROM {{ ref ('bamboohr_id_employee_number_mapping') }}
 
 ), job_info AS (
 
     SELECT *
-    FROM "ANALYTICS"."ANALYTICS_SENSITIVE"."BAMBOOHR_JOB_INFO"
-  
+    FROM {{ ref ('bamboohr_job_info') }}
+
 ), mapped_division AS (
 
     SELECT 
@@ -23,7 +23,7 @@ WITH current_division_department_mapping AS (
       bamboo_mapping.termination_date   
     FROM job_info 
     LEFT JOIN current_division_department_mapping
-        ON current_division_department_mapping.department = job_info.department    
+      ON current_division_department_mapping.department = job_info.department    
     LEFT JOIN bamboo_mapping
       ON bamboo_mapping.employee_id = job_info.employee_id
     
@@ -33,7 +33,7 @@ SELECT
   job_id,
   employee_id,
   effective_date,
-  IFF(termination_date IS NOT NULL AND effective_end_date IS NULL, termination_date, effective_end_date) AS effective_end_Date,
+  IFF(termination_date IS NOT NULL AND effective_end_date IS NULL, termination_date, effective_end_date) AS effective_end_date,
   division_mapped_current,
   termination_Date
 FROM mapped_division
