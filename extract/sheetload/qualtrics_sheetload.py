35a6087e857d6a3342c7770b0565f25c1066af59
msendal <msendal@gitlab.com>
2020-09-21 16:16:16 +0200

    format file

--- extract/sheetload/qualtrics_sheetload.py
+++ extract/sheetload/qualtrics_sheetload.py
@@ -70,6 +70,25 @@ def push_contacts_to_qualtrics(
     return final_status
 
 
+def get_metadata(file, google_sheet_client):
+    maximum_backoff_sec = 300
+    n = 0
+    while maximum_backoff_sec > (2 ** n):
+        try:
+            file_name = file.title
+            tab = file.sheet1.title
+            return file_name, tab
+        except APIError as gspread_error:
+            if gspread_error.response.status_code == 429:
+                google_sheet_client.wait_exponential_backoff(n)
+                n = n + 1
+            else:
+                raise
+    else:
+        error(f"Max retries exceeded, giving up on {file}")
+        return
+
+
 def process_qualtrics_file(
     file,
     is_test,
@@ -78,28 +97,10 @@ def process_qualtrics_file(
     qualtrics_client,
     qualtrics_mailing_lists,
 ):
-    maximum_backoff_sec = 300
-    n = 0
-    should_file_be_processed = False
-    while maximum_backoff_sec > (2 ** n):
-        try:
-            file_name = file.title
+    file_name, tab = get_metadata(file, google_sheet_client)
+    if not should_file_be_processed(file_name, tab, qualtrics_mailing_lists):
+        return
 
-            tab = file.sheet1.title
-            should_file_be_processed = should_file_be_processed(
-                file_name, tab, qualtrics_mailing_lists
-            )
-        except APIError as gspread_error:
-            if gspread_error.response.status_code == 429:
-                google_sheet_client.wait_exponential_backoff(n)
-                n = n + 1
-            else:
-                raise
-    else:
-        error(f"Max retries exceeded, giving up on {file_name}")
-        return
-    if not should_file_be_processed:
-        return
     dataframe = google_sheet_client.load_google_sheet(None, file_name, tab)
     if list(dataframe.columns.values)[0].lower() != "id":
         warning(f"{file.title}: First column did not match expected name of id")
