5016b4ffb2ed528e3c4f78b1a5d27010b7298e66
Jean Del Rosario <jpeguero@gitlab.com>
2020-09-23 16:37:45 -0400

    Add tests and documentation;

--- transform/snowflake-dbt/models/staging/common/dim_subscriptions.sql
+++ transform/snowflake-dbt/models/staging/common/dim_subscriptions.sql
@@ -44,7 +44,7 @@ WITH zuora_subscription AS (
     zuora_subscription.renewal_term_period_type,
     zuora_subscription.subscription_start_date                                AS subscription_start_date,
     zuora_subscription.subscription_end_date                                  AS subscription_end_date,
-    IFF(zuora_subscription.created_by_id = '2c92a0fd55822b4d015593ac264767f2',
+    IFF(zuora_subscription.created_by_id = '2c92a0fd55822b4d015593ac264767f2', -- All Self-Service / Web direct subscriptions are identified by that created_by_id
       'Self-Service', 'Sales-Assisted')                                       AS subscription_sales_type,
     DATE_TRUNC('month', zuora_subscription.subscription_start_date)           AS subscription_start_month,
     DATE_TRUNC('month', zuora_subscription.subscription_end_date)             AS subscription_end_month
