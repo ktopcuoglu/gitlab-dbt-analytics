ae3bc7c0581eb5f7fb8f13588cf5a2d189dc389d
davis_townsend <dtownsend@gitlab.com>
2020-09-23 10:58:57 -0500

    remove qualify

--- transform/snowflake-dbt/models/sources/gcp_billing/gcp_billing_export_source.sql
+++ transform/snowflake-dbt/models/sources/gcp_billing/gcp_billing_export_source.sql
@@ -115,7 +115,7 @@ WITH source AS (
       SUM(usage_amount_in_pricing_units)                                AS usage_amount_in_pricing_units
       FROM flattened
       {{ dbt_utils.group_by(n=26) }}
-      QUALIFY ROW_NUMBER() OVER (PARTITION BY primary_key ORDER BY uploaded_at DESC) = 1
+      --UALIFY ROW_NUMBER() OVER (PARTITION BY primary_key ORDER BY uploaded_at DESC) = 1
 
 )
 
