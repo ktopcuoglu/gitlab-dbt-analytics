944ef234a322d8e5de3a34ff9179c9943df5fc69
Jean Del Rosario <jpeguero@gitlab.com>
2020-09-27 22:06:03 -0400

    Name selected columns in ci_minutes_ui_replication model

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_ci_minutes_ui_namespace_replication.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_ci_minutes_ui_namespace_replication.sql
@@ -1,18 +1,33 @@
 WITH project_snapshot_monthly AS (
   
-    SELECT *
+    SELECT
+      snapshot_month,
+      project_id,
+      namespace_id,
+      visibility_level,
+      shared_runners_enabled
     FROM {{ ref('gitlab_dotcom_project_historical_monthly') }}
     WHERE snapshot_month >= '2020-07-01'
 
 ), namespace_lineage_monthly AS (
 
-    SELECT *
+    SELECT
+      snapshot_month,
+      namespace_id,
+      parent_id,
+      upstream_lineage,
+      ultimate_parent_id,
+      namespace_is_internal
     FROM {{ ref('gitlab_dotcom_namespace_lineage_historical_monthly') }}
     WHERE snapshot_month >= '2020-07-01'
 
 ), namespace_statistic_monthly AS (
   
-    SELECT *
+    SELECT
+      snapshot_month,
+      namespace_id,
+      shared_runners_seconds,
+      shared_runners_seconds_last_reset
     FROM {{ ref('gitlab_dotcom_namespace_statistics_historical_monthly') }}
     WHERE snapshot_month >= '2020-07-01'
 
@@ -29,7 +44,16 @@ WITH project_snapshot_monthly AS (
 
 ), namespace_snapshots_monthly AS (
 
-    SELECT *
+    SELECT
+      snapshot_month,
+      namespace_id,
+      plan_id,
+      parent_id,
+      owner_id,
+      namespace_type,
+      visibility_level,
+      shared_runners_minutes_limit,
+      extra_shared_runners_minutes_limit
     FROM {{ ref('gitlab_dotcom_namespace_historical_monthly') }}
     WHERE snapshot_month >= '2020-07-01'
 
