0a1f75a80fc84e9740ae5e22383dc616da94c545
jjstark <jstark@gitlab.com>
2020-09-29 08:57:15 -0700

    add sales rep dim

--- transform/snowflake-dbt/models/staging/common/dim_crm_sales_rep.sql
+++ transform/snowflake-dbt/models/staging/common/dim_crm_sales_rep.sql
@@ -13,14 +13,14 @@ WITH sfdc_users AS (
   SELECT 
 
     sfdc_users.user_id     AS crm_sales_rep_id,
-		sfdc_users.name        AS rep_name,
-		sfdc_users.title       AS title,
+    sfdc_users.name        AS rep_name,
+    sfdc_users.title       AS title,
     sfdc_users.department, -- department
-		sfdc_users.team,       -- team
+    sfdc_users.team,       -- team
     sfdc_users.manager_id, -- manager_id
-		sfdc_users.is_active,  -- is_active
-		sfdc_users.start_date, -- start_date
-		sfdc_user_roles.name   AS user_role_name
+    sfdc_users.is_active,  -- is_active
+    sfdc_users.start_date, -- start_date
+    sfdc_user_roles.name   AS user_role_name
 	
 	FROM sfdc_users
 	LEFT JOIN sfdc_user_roles
