40ce69940b33170b5986a25477648b8c9515d6a8
Lily Mai <lmai@gitlab.com>
2020-09-29 22:19:40 +0000

    add new csv reference file, new macro file, and new sql file for new dbt model

---
+++ transform/snowflake-dbt/models/bamboohr/xf/bamboohr_engineering_division.sql
@@ -0,0 +1,54 @@
+WITH bamboohr_employee_directory AS (
+
+    SELECT *
+    FROM {{ ref ('employee_directory_analysis') }}
+
+), bamboohr_engineering_division_mapping AS (
+
+    SELECT *
+    FROM {{ ref ('bamboohr_engineering_division_mapping') }}
+
+), development_department_employees AS (
+
+    SELECT
+      DISTINCT date_actual                                                  AS date_employed,
+      employee_id,
+      full_name,
+      lower(job_title)                                                      AS job_title,
+      lower(TRIM(value::string))                                            AS jobtitle_speciality,
+      reports_to,
+      layers,
+      department,
+      work_email
+    FROM bamboohr_employee_directory,
+    lateral flatten(input=>split(COALESCE(replace(jobtitle_speciality,'&',','),''), ','))
+    WHERE division = 'Engineering'
+      AND date_actual >= '2020-01-01'
+
+), reporting_structure AS (
+    
+    SELECT 
+      DISTINCT development_department_employees.date_employed,
+      development_department_employees.employee_id,
+      development_department_employees.full_name,
+      development_department_employees.job_title,
+      bamboohr_engineering_division_mapping.sub_department,
+      development_department_employees.jobtitle_speciality AS group_,
+      CASE 
+        WHEN development_department_employees.employee_id IN (41965,41996,41453,41482,41974,41487,42029,40914,41954) OR development_department_employees.job_title LIKE '%backend%' THEN 'backend'
+        WHEN development_department_employees.job_title LIKE '%fullstack%' THEN 'fullstack'
+        WHEN development_department_employees.job_title LIKE '%frontend%' THEN 'frontend'
+        ELSE null END AS technology_group,
+      CONCATENATE(bamboohr_engineering_division_mapping.sub_department,'_',development_department_employees.jobtitle_speciality,'_',technology_group) AS team_member_attributes,
+      development_department_employees.department,
+      development_department_employees.work_email,
+      development_department_employees.reports_to
+    FROM development_department_employees
+    LEFT JOIN bamboohr_engineering_division_mapping
+    ON development_department_employees.jobtitle_speciality = bamboohr_engineering_division_mapping.jobtitle_speciality
+
+)
+
+SELECT *,
+{{ engineering_division_mapping('sub_department','group_','technology_group') }}                AS team_name
+FROM reporting_structure
