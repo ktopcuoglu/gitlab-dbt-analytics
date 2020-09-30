d860b65f0d48b421f20be438ca7a4ace3a205d48
msendal <msendal@gitlab.com>
2020-09-30 20:02:14 +0200

    correct ref

--- transform/snowflake-dbt/models/staging/common/fct_mrr_snapshots.sql
+++ transform/snowflake-dbt/models/staging/common/fct_mrr_snapshots.sql
@@ -89,7 +89,7 @@ WITH dim_dates AS (
       zuora_rate_plan_charge_spined.snapshot_id,
       zuora_account_spined.account_id                           AS billing_account_id,
       zuora_account_spined.crm_id                               AS crm_account_id,
-      zuora_account_spined.subscription_id,
+      zuora_subscription_spined.subscription_id,
       zuora_rate_plan_charge_spined.product_rate_plan_charge_id AS product_details_id,
       zuora_rate_plan_charge_spined.mrr,
       zuora_rate_plan_charge_spined.delta_tcv,
