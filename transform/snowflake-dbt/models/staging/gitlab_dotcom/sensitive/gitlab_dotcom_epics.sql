5ea01a5f618ec95ef9c964b0c4512aff8292a92e
Taylor Murphy <tmurphy@gitlab.com>
2020-09-30 11:47:02 -0500

    Update gitlab_dotcom_epics intermediate view

---
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/sensitive/gitlab_dotcom_epics.sql
@@ -0,0 +1,15 @@
+{{ config({
+    "schema": "sensitive",
+    "materialized": "view"
+    })
+}}
+
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_epics_source') }}
+
+)
+
+SELECT *
+FROM source
