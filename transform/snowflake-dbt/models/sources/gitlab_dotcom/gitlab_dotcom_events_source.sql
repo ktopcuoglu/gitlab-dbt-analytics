948701e80e006700e783215d3fb86fe12ed6a37b
Taylor Murphy <tmurphy@gitlab.com>
2020-09-30 11:57:20 -0500

    More events updates

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_events_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_events_source.sql
@@ -1,7 +1,6 @@
 {{ config({
     "materialized": "incremental",
-    "unique_key": "event_id",
-    "schema": "analytics"
+    "unique_key": "event_id"
     })
 }}
 
@@ -17,26 +16,6 @@ WITH source AS (
       {% endif %}
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
-), projects AS (
-
-    SELECT *
-    FROM {{ ref('gitlab_dotcom_projects_xf') }}
-
-), gitlab_subscriptions AS (
-
-    SELECT *
-    FROM {{ ref('gitlab_dotcom_gitlab_subscriptions_snapshots_namespace_id_base') }}
-
-), plans AS (
-
-    SELECT *
-    FROM {{ ref('gitlab_dotcom_plans') }}
-
-), users AS (
-    
-    SELECT *
-    FROM {{ ref('gitlab_dotcom_users') }}
-  
 ), renamed AS (
 
     SELECT
@@ -52,37 +31,8 @@ WITH source AS (
 
     FROM source
 
-), joined AS (
-
-    SELECT
-      renamed.*,
-      projects.ultimate_parent_id,
-      CASE
-        WHEN gitlab_subscriptions.is_trial
-          THEN 'trial'
-        ELSE COALESCE(gitlab_subscriptions.plan_id, 34)::VARCHAR
-      END                                 AS plan_id_at_event_date,
-      CASE
-        WHEN gitlab_subscriptions.is_trial
-          THEN 'trial'
-        ELSE COALESCE(plans.plan_name, 'free')
-      END                                 AS plan_name_at_event_date,
-      COALESCE(plans.plan_is_paid, FALSE) AS plan_was_paid_at_event_date,
-      users.created_at                    AS user_created_at
-    FROM renamed
-      LEFT JOIN projects
-        ON renamed.project_id = projects.project_id
-      LEFT JOIN gitlab_subscriptions
-        ON projects.ultimate_parent_id = gitlab_subscriptions.namespace_id
-        AND renamed.created_at BETWEEN gitlab_subscriptions.valid_from
-        AND {{ coalesce_to_infinity("gitlab_subscriptions.valid_to") }}
-      LEFT JOIN plans
-        ON gitlab_subscriptions.plan_id = plans.plan_id
-      LEFT JOIN users
-        ON renamed.author_id = users.user_id
-
 )
 
 SELECT *
-FROM joined
+FROM renamed
 ORDER BY updated_at
