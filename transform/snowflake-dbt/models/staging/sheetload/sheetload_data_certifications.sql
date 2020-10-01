560d3993e37391efd06c47c960917ae99532cc61
Parul Luthra <pluthra@gitlab.com>
2020-10-01 19:05:25 +0000

    Update transform/snowflake-dbt/models/staging/sheetload/sheetload_data_certifications.sql

--- transform/snowflake-dbt/models/staging/sheetload/sheetload_data_certifications.sql
+++ transform/snowflake-dbt/models/staging/sheetload/sheetload_data_certifications.sql
@@ -7,13 +7,5 @@
     relations=[ref('sheetload_cert_product_geo_dashboard_source'),
                ref('sheetload_cert_product_geo_sql_source'),
                ref('sheetload_cert_customer_segmentation_dashboard_source'),
-               ref('sheetload_cert_customer_segmentation_sql_source'),
-               ref('sheetload_communication_certificate'),
-               ref('sheetload_ic_collaboration_competency'),
-               ref('sheetload_ic_dib_comptency'),
-               ref('sheetload_ic_efficiency_competency'),
-               ref('sheetload_ic_iteration_competency'),
-               ref('sheetload_ic_results_competency'),
-               ref('sheetload_ic_transparency_competency'),
-               ref('sheetload_compensation_certificate')]
+               ref('sheetload_cert_customer_segmentation_sql_source')
 ) }}
