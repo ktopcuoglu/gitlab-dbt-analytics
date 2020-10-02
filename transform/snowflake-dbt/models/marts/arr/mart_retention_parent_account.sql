9bca666a4c8ad26a0ef272a58af7299b3b3105c3
msendal <msendal@gitlab.com>
2020-10-02 16:01:48 +0200

    initial commit

---
+++ transform/snowflake-dbt/models/marts/arr/mart_retention_parent_account.sql
@@ -0,0 +1,57 @@
+WITH dim_crm_accounts AS (
+
+  SELECT *
+  FROM {{ ref('dim_crm_accounts') }}
+
+), fct_mrr AS (
+
+  SELECT *
+  FROM {{ ref('fct_mrr') }}
+
+), parent_account_mrrs AS (
+
+  SELECT
+    dim_crm_accounts.ultimate_parent_account_id,
+    fct_mrr.mrr_month,
+    dateadd('year', 1, mrr_month) AS retention_month,
+    SUM(mrr)                      AS mrr_total
+  FROM fct_mrr
+  LEFT JOIN dim_crm_accounts
+    ON dim_billing_accounts.crm_account_id = fct_mrr.crm_account_id
+  GROUP BY 1, 2, 3
+
+), retention_subs AS (
+
+    SELECT
+      current_mrr.ultimate_parent_account_id,
+      current_mrr.mrr_month     AS original_mrr_month,
+      current_mrr.retention_month,
+      current_mrr.mrr           AS original_mrr,
+      future_mrr.mrr            AS retention_mrr
+    FROM parent_account_mrrs AS current_mrr
+    LEFT JOIN parent_account_mrrs AS future_mrr
+      ON current_mrr.ultimate_parent_account_id = future_mrr.ultimate_parent_account_id
+        AND current_mrr.retention_month = future_mrr.mrr_month
+
+), final AS (
+
+    SELECT
+      retention_subs.ultimate_parent_account_id,
+      dim_crm_accounts.crm_account_name,
+      retention_mrr,
+      coalesce(retention_mrr, 0) AS net_retention_mrr,
+      CASE WHEN net_retention_mrr > 0
+        THEN least(net_retention_mrr, original_mrr)
+        ELSE 0 END               AS gross_retention_mrr,
+      retention_month,
+      original_mrr_month,
+      original_mrr,
+      {{ churn_type('original_mrr', 'net_retention_mrr') }}
+    FROM retention_subs
+    LEFT JOIN dim_crm_accounts
+      ON dim_crm_accounts.crm_account_id = retention_subs.ultimate_parent_account_id
+
+)
+
+SELECT *
+FROM final
