fbb697dd602c8de7b1592533139136351c991aed
Parul Luthra <pluthra@gitlab.com>
2020-10-05 00:32:27 +0000

    Update sheetload_data_certifications.sql

--- transform/snowflake-dbt/models/staging/sheetload/sheetload_data_certifications.sql
+++ transform/snowflake-dbt/models/staging/sheetload/sheetload_data_certifications.sql
@@ -3,9 +3,13 @@
     })
 }}
 
-{{ dbt_utils.union_relations(
-    relations=[ref('sheetload_cert_product_geo_dashboard_source'),
-               ref('sheetload_cert_product_geo_sql_source'),
-               ref('sheetload_cert_customer_segmentation_dashboard_source'),
-               ref('sheetload_cert_customer_segmentation_sql_source')]
-) }}
+-- {{ dbt_utils.union_relations(
+--     relations=[ref('sheetload_cert_product_geo_dashboard_source'),
+--                ref('sheetload_cert_product_geo_sql_source'),
+--                ref('sheetload_cert_customer_segmentation_dashboard_source'),
+--                ref('sheetload_cert_customer_segmentation_sql_source')]
+-- ) }}
+
+SELECT *
+FROM {{ ref('sheetload_cert_product_geo_dashboard_source') }}
+
