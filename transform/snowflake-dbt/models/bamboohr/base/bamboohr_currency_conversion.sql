1698098804d40f0fceb899903cf3f42a9a982ef2
Parul Luthra <pluthra@gitlab.com>
2020-10-06 10:46:28 -0700

    update base models to fewer ctes

--- transform/snowflake-dbt/models/bamboohr/base/bamboohr_currency_conversion.sql
+++ transform/snowflake-dbt/models/bamboohr/base/bamboohr_currency_conversion.sql
@@ -1,8 +1,7 @@
-{# {{ config({
-    "schema": "sensitive",
+{{ config({
     "materialized": "ephemeral"
     })
-}} #}
+}}
 
 WITH source AS (
 
@@ -11,58 +10,48 @@ WITH source AS (
     ORDER BY uploaded_at DESC
     LIMIT 1
 
-), intermediate AS (
-
-    SELECT 
-      d.value AS data_by_row,
-      uploaded_at
-    FROM source,
-    LATERAL FLATTEN(INPUT => parse_json(jsontext), OUTER => true) d
-
-), unnest_again AS (
-
-    SELECT 
-      d.value AS data_by_row,
-      uploaded_at
-    FROM intermediate,
-    LATERAL FLATTEN(INPUT => parse_json(data_by_row), OUTER => true) d  
-
 ), renamed AS (
 
-    SELECT 
-      data_by_row['id']::NUMBER                                      AS conversion_id,
-      data_by_row['employeeId']::NUMBER                              AS employee_id,
-      data_by_row['customConversionEffectiveDate']::DATE             AS effective_date,
-      data_by_row['customCurrencyConversionFactor']::VARCHAR           AS currency_conversion_factor,
-      data_by_row['customLocalAnnualSalary']::VARCHAR                AS local_annual_salary,
-      data_by_row['customUSDAnnualSalary']::VARCHAR                  AS usd_annual_salary,
-      uploaded_at
-    FROM unnest_again
-  
-), cleaned AS (
-
-    SELECT 
-      renamed.*,
-      TO_NUMBER(SPLIT_PART(local_annual_salary,' ',1)) AS local_annual_salary_amount,
-      SPLIT_PART(local_annual_salary,' ',2)            AS local_currency_code,  
-      TO_NUMBER(SPLIT_PART(usd_annual_salary,' ',1))   AS usd_annual_salary_amount,
-      SPLIT_PART(usd_annual_salary,' ',2)              AS usd_currency_code
-    FROM renamed
+    SELECT
+      e.value['id']::NUMBER                       AS target_earnings_update_id,
+      e.value['employeeId']::NUMBER               AS employee_id,
+      e.value['customDate']::DATE                 AS effective_date,
+      e.value['customType']::VARCHAR              AS compensation_type,
+      e.value['customAnnualAmountLocal']::VARCHAR AS annual_amount_local,
+      e.value['customAnnualAmountUSD']::VARCHAR   AS annual_amount_usd,
+      e.value['customOTELocal']::VARCHAR          AS ote_local,
+      e.value['customOTEUSD']::VARCHAR            AS ote_usd,
+      e.value['customType']::VARCHAR              AS ote_type,
+      e.value['customVariablePay']::VARCHAR       AS variable_pay
+    FROM source,
+    LATERAL FLATTEN(INPUT => parse_json(jsontext), OUTER => true) initial_unnest,
+    LATERAL FLATTEN(INPUT => parse_json(initial_unnest.value), OUTER => true) e
 
 ), final AS (
 
     SELECT 
-      conversion_id,
+      target_earnings_update_id,
       employee_id,
       effective_date,
-      currency_conversion_factor,
-      local_annual_salary_amount,
-      local_currency_code,
-      usd_annual_salary_amount
-    FROM cleaned 
+      variable_pay,
+      compensation_type,
+      REGEXP_REPLACE(annual_amount_local, '[a-z/-/A-z/#/*]', '')::DECIMAL   AS annual_amount_local_value,
+      REGEXP_REPLACE(annual_amount_local, '[0-9/-/#/./*]', '')              AS annual_local_currency_code,
+      REGEXP_REPLACE(annual_amount_usd, '[a-z/-/A-z/#/*]', '')::DECIMAL     AS annual_amount_usd_value,
+      REGEXP_REPLACE(ote_local, '[a-z/-/A-z/#/*]', '')::DECIMAL             AS ote_local_amount,
+      REGEXP_REPLACE(ote_local, '[0-9/-/#/./*]', '')                        AS ote_local_currency_code,
+      REGEXP_REPLACE(ote_usd, '[a-z/-/A-z/#/*]', '')::DECIMAL               AS ote_usd,
+      ote_type
+    FROM renamed
 
-) 
+), final AS (
 
-SELECT *,
-  LAG(usd_annual_salary_amount) OVER (PARTITION BY employee_id ORDER BY conversion_id) AS prior_bamboohr_annual_salary
-FROM final 
+    SELECT *,
+      LAG(COALESCE(annual_amount_usd_value,0)) OVER (PARTITION BY employee_id ORDER BY target_earnings_update_id)  AS prior_annual_amount_usd,
+      annual_amount_usd_value - prior_annual_amount_usd AS change_in_annual_amount_usd
+    FROM final
+
+)
+
+SELECT *
+FROM final
