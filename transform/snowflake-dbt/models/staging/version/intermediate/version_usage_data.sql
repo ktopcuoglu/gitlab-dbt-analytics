7d27730d6382cdea1d58eb07655896103977e11f
mpeychet <mpeychet@gitlab.com>
2020-10-06 17:53:12 +0200

    cleaned edition

--- transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
+++ transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
@@ -6,12 +6,13 @@
 WITH usage_data AS (
 
     SELECT
-      *,
-      REGEXP_REPLACE(NULLIF(version, ''), '[^0-9.]+')     AS cleaned_version,
-      IFF(version ILIKE '%-pre', True, False)             AS version_is_prerelease,
-      SPLIT_PART(cleaned_version, '.', 1)::NUMBER         AS major_version,
-      SPLIT_PART(cleaned_version, '.', 2)::NUMBER         AS minor_version,
-      major_version || '.' || minor_version               AS major_minor_version
+      {{ dbt_utils.star(from=ref('version_usage_data_source'), except=['EDITION']) }},
+      IFF(license_expires_at >= created_at OR license_expires_at IS NULL, edition, 'EE Free') AS edition,
+      REGEXP_REPLACE(NULLIF(version, ''), '[^0-9.]+')                                         AS cleaned_version,
+      IFF(version ILIKE '%-pre', True, False)                                                 AS version_is_prerelease,
+      SPLIT_PART(cleaned_version, '.', 1)::NUMBER                                             AS major_version,
+      SPLIT_PART(cleaned_version, '.', 2)::NUMBER                                             AS minor_version,
+      major_version || '.' || minor_version                                                   AS major_minor_version
     FROM {{ ref('version_usage_data_source') }}
     WHERE uuid IS NOT NULL
       AND version NOT LIKE ('%VERSION%') -- Messy data that's not worth parsing.
