21a5b3a4343352a0367d6bb0b5361192c831c3a9
parmstrong <parmstrong@gitlab.com>
2020-10-07 10:35:19 +0200

    Exclude circular record

--- transform/snowflake-dbt/models/staging/zuora/intermediate/zuora_subscription.sql
+++ transform/snowflake-dbt/models/staging/zuora/intermediate/zuora_subscription.sql
@@ -12,3 +12,5 @@ FROM source
 WHERE is_deleted = FALSE
   AND exclude_from_analysis IN ('False', '')
   AND account_id NOT IN ({{ zuora_excluded_accounts() }})
+  -- See issue https://gitlab.com/gitlab-data/analytics/-/issues/6518
+  AND subscription_id NOT IN ('2c92a00774ddaf190174de37f0eb147d')
