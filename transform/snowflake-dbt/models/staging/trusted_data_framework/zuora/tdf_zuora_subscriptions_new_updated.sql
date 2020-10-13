e7468542ad9fbd4a45959f3b0587e97f58dc613b
parmstrong <parmstrong@gitlab.com>
2020-10-13 19:23:08 +0200

    Correct formatting

--- transform/snowflake-dbt/models/staging/trusted_data_framework/zuora/tdf_zuora_subscriptions_new_updated.sql
+++ transform/snowflake-dbt/models/staging/trusted_data_framework/zuora/tdf_zuora_subscriptions_new_updated.sql
@@ -3,23 +3,28 @@ WITH zuora_subscriptions_new AS (
     SELECT *
     FROM {{ ref('zuora_subscriptions_new') }}
 
-),
- zuora_subscriptions_updated AS (
+), zuora_subscriptions_updated AS (
 
     SELECT *
     FROM {{ ref('zuora_subscriptions_updated') }}
 
-),
-dim_dates AS (
+), dim_dates AS (
 
-  SELECT *
-  FROM {{ ref('dim_dates') }}
+    SELECT *
+    FROM {{ ref('dim_dates') }}
+
+), final AS (
+
+    SELECT
+      dates.date_day            AS date_day,
+      new_records.rowcount      AS new_records,
+      updated_records.rowcount  AS updated_records
+    FROM dim_dates dates
+    LEFT JOIN zuora_subscriptions_new new_records ON new_records.date_day = dates.date_day
+    LEFT JOIN zuora_subscriptions_updated updated_records ON updated_records.date_day = dates.date_day
+    WHERE (new_records.rowcount > 0 OR updated_records.rowcount > 0)
 
 )
 
-
-SELECT dates.date_day, new_records.rowcount as new_records, updated_records.rowcount as updated_records
-  FROM dim_dates dates
-LEFT JOIN zuora_subscriptions_new new_records on new_records.date_day = dates.date_day
-LEFT JOIN zuora_subscriptions_updated updated_records on updated_records.date_day = dates.date_day
-WHERE (new_records.rowcount > 0 or updated_records.rowcount > 0)
+SELECT *
+FROM final
