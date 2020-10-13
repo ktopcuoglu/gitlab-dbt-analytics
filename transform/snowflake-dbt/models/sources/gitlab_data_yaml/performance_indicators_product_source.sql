8c3d657940c218b10f5adfee57450a1a5d0f86b4
Parul Luthra <pluthra@gitlab.com>
2020-10-13 11:08:26 -0700

    update all base tables to use macro

--- transform/snowflake-dbt/models/sources/gitlab_data_yaml/performance_indicators_product_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_data_yaml/performance_indicators_product_source.sql
@@ -1,35 +1,7 @@
-WITH source AS (
-
-    SELECT *,
-      RANK() OVER (PARTITION BY DATE_TRUNC('day', uploaded_at) ORDER BY uploaded_at DESC) AS rank
-    FROM {{ source('gitlab_data_yaml', 'product_pi') }}
-
-), intermediate AS (
-
-    SELECT
-      d.value                                 AS data_by_row,
-      date_trunc('day', uploaded_at)::date    AS snapshot_date,
-      rank
-    FROM source,
-    LATERAL FLATTEN(INPUT => parse_json(jsontext), OUTER => TRUE) d
-
-), intermediate_stage AS (
-
-    SELECT 
-      data_by_row['definition']::VARCHAR                   AS pi_definition,
-      data_by_row['is_key']::BOOLEAN                       AS is_key,
-      data_by_row['is_primary']::BOOLEAN                   AS is_primary,
-      data_by_row['name']::VARCHAR                         AS pi_name,
-      data_by_row['org']::VARCHAR                          AS org_name,
-      data_by_row['public']::BOOLEAN                       AS is_public,
-      data_by_row['sisense_data'] IS NOT NULL              AS is_embedded,
-      data_by_row['target']::VARCHAR                       AS pi_target,
-      data_by_row['telemetry_type']::VARCHAR               AS telemetry_type,
-      snapshot_date,
-      rank
-    FROM intermediate
-      
-)
+{{ create_pi_source_table(
+    source=source('gitlab_data_yaml', 'product_pi')
+    )
+}}
 
 SELECT *
 FROM intermediate_stage
