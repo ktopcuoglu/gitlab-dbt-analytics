77b1d1aebdaaaaaeae09eff7d403fc8ff8033c26
Michael Walker <mwalker@gitlab.com>
2020-10-14 11:51:13 -0500

    add more metrics to pull for lcp

--- transform/snowflake-dbt/models/sources/engineering/engineering_lcp_source.sql
+++ transform/snowflake-dbt/models/sources/engineering/engineering_lcp_source.sql
@@ -15,7 +15,7 @@ WITH source AS (
 ), data_points_flushed_out AS (
 
     SELECT
-      SPLIT(metric_name, '.')[12]::VARCHAR AS metric_name,
+      metric_name,
       data_by_row.value[0]::FLOAT          AS metric_value,
       data_by_row.value[1]::TIMESTAMP      AS metric_reported_at
     FROM metric_per_row,
