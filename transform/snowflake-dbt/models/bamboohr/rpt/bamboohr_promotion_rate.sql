c907329491433d2cbaaf03e7c62d168c2121fa36
Parul Luthra <pluthra@gitlab.com>
2020-10-15 09:52:13 -0700

    formatting

--- transform/snowflake-dbt/models/bamboohr/rpt/bamboohr_promotion_rate.sql
+++ transform/snowflake-dbt/models/bamboohr/rpt/bamboohr_promotion_rate.sql
@@ -1,7 +1,7 @@
 WITH promotions AS (
 
     SELECT *
-    FROM "ANALYTICS"."ANALYTICS_SENSITIVE"."BAMBOOHR_PROMOTIONS_XF"
+    FROM {{ ref ('bamboohr_promotions_xf') }}
     WHERE promotion_month BETWEEN DATEADD(month,-12, DATE_TRUNC(month, CURRENT_DATE())) AND
           DATEADD(month,-1, DATE_TRUNC(month, CURRENT_DATE()))
 
@@ -17,7 +17,7 @@ WITH promotions AS (
       department,
       headcount_end, 
       rolling_12_month_promotions
-    FROM "ANALYTICS"."ANALYTICS"."BAMBOOHR_RPT_HEADCOUNT_AGGREGATION"
+    FROM {{ ref ('bamboohr_rpt_headcount_aggregation') }}
     WHERE breakout_type in ('department_breakout','kpi_breakout','division_breakout')
       AND eeoc_field_name = 'no_eeoc'
       AND month_date = DATEADD(month,-1, DATE_TRUNC(month, CURRENT_DATE()))
@@ -27,7 +27,7 @@ WITH promotions AS (
     SELECT 
       DISTINCT division_mapped_current, 
       department_modified
-    FROM "ANALYTICS"."ANALYTICS_STAGING"."BAMBOOHR_JOB_INFO_CURRENT_DIVISION_BASE"
+    FROM {{ ref ('bamboohr_job_info_current_division_base') }}
 
 ), marketing_headcount_excluding_sales_development AS (
 
@@ -57,20 +57,20 @@ WITH promotions AS (
 ), division AS (
 
     SELECT
-      'division_breakout'                                                                       AS breakout_type,
-      division                                                                                  AS division_department,
-      AVG(promotions.percent_change_in_comp)                                                    AS average_increase,
-      MEDIAN(promotions.percent_change_in_comp)                                                 AS median_increase
+      'division_breakout'                                AS breakout_type,
+      division                                           AS division_department,
+      AVG(promotions.percent_change_in_comp)             AS average_increase,
+      MEDIAN(promotions.percent_change_in_comp)          AS median_increase
     FROM promotions
     GROUP BY 1,2
 
     UNION ALL
 
     SELECT
-      'division_breakout'                                                                       AS breakout_type,
-      'Marketing - Excluding Sales Development'                                                 AS division_department,
-      AVG(percent_change_in_comp)                                                               AS average_increase,
-      MEDIAN(percent_change_in_comp)                                                            AS median_increase
+      'division_breakout'                                AS breakout_type,
+      'Marketing - Excluding Sales Development'          AS division_department,
+      AVG(percent_change_in_comp)                        AS average_increase,
+      MEDIAN(percent_change_in_comp)                     AS median_increase
     FROM promotions
     WHERE department != 'Sales Development'
       AND division = 'Marketing'
@@ -79,8 +79,8 @@ WITH promotions AS (
 ), company AS (
 
     SELECT
-      'company_breakout'                                                                        AS breakout_type,
-      'Company Overall - Including Sales Development'                                           AS division_department,
+      'company_breakout'                                  AS breakout_type,
+      'Company Overall - Including Sales Development'     AS division_department,
       AVG(percent_change_in_comp) AS average_increase,
       MEDIAN(percent_change_in_comp) AS median_incrase
     FROM promotions
@@ -89,8 +89,8 @@ WITH promotions AS (
     UNION ALL 
 
     SELECT
-      'company_breakout'                                                                        AS breakout_type,
-      'Company - Excluding Sales Development'                                                   AS division_department,
+      'company_breakout'                                  AS breakout_type,
+      'Company - Excluding Sales Development'             AS division_department,
       AVG(percent_change_in_comp) AS average_increase,
       MEDIAN(percent_change_in_comp) AS median_incrase
     FROM promotions
@@ -100,8 +100,8 @@ WITH promotions AS (
 ), department AS (
 
     SELECT
-      'department_breakout'                                                                     AS breakout_type,
-      department                                                                                AS division_department,
+      'department_breakout'                               AS breakout_type,
+      department                                          AS division_department,
       AVG(percent_change_in_comp) AS average_increase,
       MEDIAN(percent_change_in_comp) AS median_incrase
     FROM promotions
@@ -126,21 +126,21 @@ WITH promotions AS (
 ), final AS (
 
     SELECT
-    unioned.*,
-    COALESCE(bamboohr_headcount_aggregated.headcount_end,
-            marketing_headcount_excluding_sales_development.headcount_end,
-            company_headcount_excluding_sales_development.headcount_end)                 AS headcount_end,
-    COALESCE(bamboohr_headcount_aggregated.rolling_12_month_promotions,
-            marketing_headcount_excluding_sales_development.rolling_12_month_promotions,
-            company_headcount_excluding_sales_development.rolling_12_month_promotions)   AS rolling_12_month_promotions 
+      unioned.*,
+      COALESCE(bamboohr_headcount_aggregated.headcount_end,
+               marketing_headcount_excluding_sales_development.headcount_end,
+               company_headcount_excluding_sales_development.headcount_end)                 AS headcount_end,
+      COALESCE(bamboohr_headcount_aggregated.rolling_12_month_promotions,
+               marketing_headcount_excluding_sales_development.rolling_12_month_promotions,
+               company_headcount_excluding_sales_development.rolling_12_month_promotions)   AS rolling_12_month_promotions 
     FROM unioned
     LEFT JOIN bamboohr_headcount_aggregated
-    ON bamboohr_headcount_aggregated.breakout_type = unioned.breakout_type
-    AND bamboohr_headcount_aggregated.division_department = unioned.division_department
+      ON bamboohr_headcount_aggregated.breakout_type = unioned.breakout_type
+      AND bamboohr_headcount_aggregated.division_department = unioned.division_department
     LEFT JOIN marketing_headcount_excluding_sales_development
-    ON marketing_headcount_excluding_sales_development.division_department = unioned.division_department
+      ON marketing_headcount_excluding_sales_development.division_department = unioned.division_department
     LEFT JOIN company_headcount_excluding_sales_development
-    ON company_headcount_excluding_sales_development.division_department = unioned.division_department
+      ON company_headcount_excluding_sales_development.division_department = unioned.division_department
 
 )
 
