390a4e4c5491ab9d69cbc764016509695321935d
Kathleen Tam <ktam@gitlab.com>
2020-10-16 17:05:39 +0000

    Add new data model for access in sisense customers_db_trial_histories

---
+++ transform/snowflake-dbt/models/staging/customers/xf/customers_db_trial_histories.sql
@@ -0,0 +1,10 @@
+WITH customers_db_trial_histories_source AS (
+  
+    SELECT * 
+    FROM {{ ref('customers_db_trial_histories_source') }}
+  
+)
+
+SELECT * 
+FROM customers_db_trial_histories_source
+
