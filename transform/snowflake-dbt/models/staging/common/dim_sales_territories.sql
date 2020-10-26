9665b2c1164f246f3a7b9fba554c44d4b1104845
parmstrong <parmstrong@gitlab.com>
2020-10-26 10:00:16 +0200

    Added Audit & configs

--- transform/snowflake-dbt/models/staging/common/dim_sales_territories.sql
+++ transform/snowflake-dbt/models/staging/common/dim_sales_territories.sql
@@ -1,1 +1,15 @@
+{{config({
+    "materialized": "table",
+    "schema": "common"
+  })
+}}
+
 {{ generate_single_field_dimension ('sfdc_account_source', 'tsp_territory', 'dim_sales_territory_id', 'tsp_territory', 'sales_territory_name') }}
+
+{{ dbt_audit(
+    cte_ref="unioned",
+    created_by="@paul_armstrong",
+    updated_by="@paul_armstrong",
+    created_date="2020-10-26",
+    updated_date="2020-10-26"
+) }}
