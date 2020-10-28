7729e58836a679b856faa78a5875ca7f93426b4f
mpeychet <mpeychet@gitlab.com>
2020-10-28 16:58:10 +0100

    finally passing

--- transform/snowflake-dbt/models/marts/usage_ping/mart_poc_monthly_usage_data.sql
+++ transform/snowflake-dbt/models/marts/usage_ping/mart_poc_monthly_usage_data.sql
@@ -14,17 +14,16 @@ WITH monthly_usage_data AS (
     SELECT 
       created_month,
       clean_metrics_name,
-      uuid,
-      product_tier,
+      instance_id,
+      ping_id,
       group_name,
       stage_name,
       section_name, 
       is_smau,
       is_gmau,
-      ping_source,
       MAX(monthly_metric_value) AS monthly_metric_value
-    FROM monthly_usage_datap
-    GROUP BY 1,2,3,4,5,6,7,8,9,10
+    FROM monthly_usage_data
+    GROUP BY 1,2,3,4,5,6,7,8,9
     
 )
 
@@ -42,5 +41,5 @@ SELECT
   SUM(monthly_metric_value) AS monthly_metric_value_sum
 FROM monthly_usage_data_agg
 INNER JOIN fct_usage_ping_payloads
-  ON monthly_usage_data.ping_id = fct_usage_ping_payloads.usage_ping_id
+  ON monthly_usage_data_agg.ping_id = fct_usage_ping_payloads.usage_ping_id
 GROUP BY 1,2,3,4,5,6,7,8,9,10
