94af628b0c769ff2cfaecd3f0d8cf4ce9c9ded69
parmstrong <parmstrong@gitlab.com>
2020-10-29 11:35:12 +0200

    Pretty sure this is the major issue with the reformat

--- extract/keyhole_twitter/src/execute.py
+++ extract/keyhole_twitter/src/execute.py
@@ -18,7 +18,8 @@ valid_years = range(datetime.now().year - 15, datetime.now().year + 1)
 year = datetime.now().year
 
 
-def recursive_parse_dict(dict_to_parse: dict, path: str = ""):
+def recursive_parse_dict(dict_to_parse: dict,
+                         path: str = ""):
     """
         Parses dicts of dicts, specifically for the keyhole extract.
         Written as a recursive function so new data endpoints can be added without any changes required.
@@ -64,7 +65,8 @@ def get_twitter_impressions_data(endpoint: str) -> pd.DataFrame:
     return output_df
 
 
-def write_csv_data(file_name: str, data: pd.DataFrame):
+def write_csv_data(file_name: str,
+                   data: pd.DataFrame):
     """
         Just here to return filenames, probably not needed but consistent with our other extracts
     """
