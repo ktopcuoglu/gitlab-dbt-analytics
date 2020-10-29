942542efeedb9d44aae20d7d56b4786f1d86c61a
Lily Mai <lmai@gitlab.com>
2020-10-29 16:55:12 +0000

    update CTE names and fix join formatting

--- transform/snowflake-dbt/models/bamboohr/xf/bamboohr_engineering_division.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/bamboohr_engineering_division.sql
@@ -1,4 +1,4 @@
-WITH bamboohr_employee_directory AS (
+WITH employees AS (
 
     SELECT *
     FROM {{ ref ('employee_directory_analysis') }}
@@ -8,7 +8,7 @@ WITH bamboohr_employee_directory AS (
     SELECT *
     FROM {{ ref ('bamboohr_engineering_division_mapping') }}
 
-), development_department_employees AS (
+), engineering_employees AS (
 
     SELECT
       DISTINCT date_actual                                                  AS date_employed,
@@ -20,41 +20,41 @@ WITH bamboohr_employee_directory AS (
       layers,
       department,
       work_email
-    FROM bamboohr_employee_directory,
+    FROM employees,
     LATERAL FLATTEN(INPUT=>SPLIT(COALESCE(REPLACE(jobtitle_speciality,'&',','),''), ','))
     WHERE division = 'Engineering'
       AND date_actual >= '2020-01-01'
 
-), engineering_team_member_attributes AS (
+), engineering_employee_attributes AS (
     
     SELECT 
-      DISTINCT development_department_employees.date_employed,
-      development_department_employees.employee_id,
-      development_department_employees.full_name,
-      development_department_employees.job_title,
+      DISTINCT engineering_employees.date_employed,
+      engineering_employees.employee_id,
+      engineering_employees.full_name,
+      engineering_employees.job_title,
       bamboohr_engineering_division_mapping.sub_department,
-      development_department_employees.jobtitle_speciality,
+      engineering_employees.jobtitle_speciality,
       CASE 
-        WHEN development_department_employees.employee_id IN (41965,41996,41453,41482,41974,41487,42029,40914,41954) OR development_department_employees.job_title LIKE '%backend%' THEN 'backend'
-        WHEN development_department_employees.job_title LIKE '%fullstack%' THEN 'fullstack'
-        WHEN development_department_employees.job_title LIKE '%frontend%' THEN 'frontend'
+        WHEN engineering_employees.employee_id IN (41965,41996,41453,41482,41974,41487,42029,40914,41954) OR engineering_employees.job_title LIKE '%backend%' THEN 'backend'
+        WHEN engineering_employees.job_title LIKE '%fullstack%' THEN 'fullstack'
+        WHEN engineering_employees.job_title LIKE '%frontend%' THEN 'frontend'
         ELSE NULL END AS technology_group,
-      development_department_employees.department,
-      development_department_employees.work_email,
-      development_department_employees.reports_to
-    FROM development_department_employees
+      engineering_employees.department,
+      engineering_employees.work_email,
+      engineering_employees.reports_to
+    FROM engineering_employees
     LEFT JOIN bamboohr_engineering_division_mapping
-      ON development_department_employees.jobtitle_speciality = bamboohr_engineering_division_mapping.jobtitle_speciality
+      ON bamboohr_engineering_division_mapping.jobtitle_speciality = engineering_employees.jobtitle_speciality 
 
 ), reporting_structure AS (
 
-    SELECT engineering_team_member_attributes.*,
+    SELECT engineering_employee_attributes.*,
       bamboohr_engineering_division_mapping.team_name
-    FROM engineering_team_member_attributes
+    FROM engineering_employee_attributes
     LEFT JOIN bamboohr_engineering_division_mapping
-      ON engineering_team_member_attributes.jobtitle_speciality = bamboohr_engineering_division_mapping.jobtitle_speciality
-      ON engineering_team_member_attributes.sub_department = bamboohr_engineering_division_mapping.sub_department
-      ON engineering_team_member_attributes.technology_group = bamboohr_engineering_division_mapping.technology_group
+      ON bamboohr_engineering_division_mapping.jobtitle_speciality = engineering_employee_attributes.jobtitle_speciality
+      ON bamboohr_engineering_division_mapping.sub_department = engineering_employee_attributes.sub_department
+      ON bamboohr_engineering_division_mapping.technology_group = engineering_employee_attributes.technology_group
  
 )
 
