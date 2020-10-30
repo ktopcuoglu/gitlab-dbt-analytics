7616d70c33af21670908fbca2c9b54a0b6eb0eac
Parul Luthra <pluthra@gitlab.com>
2020-10-30 09:55:59 -0700

    update ref name

--- transform/snowflake-dbt/models/staging/gitlab_data_yaml/base/categories_yaml_acquisitions.sql
+++ transform/snowflake-dbt/models/staging/gitlab_data_yaml/base/categories_yaml_acquisitions.sql
@@ -1,7 +1,7 @@
 WITH source AS (
     
     SELECT * 
-    FROM {{ ref('categories_yaml_source') }}
+    FROM {{ ref('categories_yaml_acquisitions_source') }}
     QUALIFY ROW_NUMBER() OVER (PARTITION BY category_name, category_stage, acquisition_key, acquisition_name, acquisition_end_date 
                                ORDER BY acquisition_start_date)=1
 
