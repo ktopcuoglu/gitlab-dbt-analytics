1ee501adc54a29a04517240c2575331668e03d03
Lily Mai <lmai@gitlab.com>
2020-10-30 20:04:56 +0000

    update SQL and mapping csv file

--- transform/snowflake-dbt/models/bamboohr/xf/bamboohr_engineering_division.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/bamboohr_engineering_division.sql
@@ -60,7 +60,7 @@ WITH employees AS (
     LEFT JOIN bamboohr_engineering_division_mapping
       ON bamboohr_engineering_division_mapping.jobtitle_speciality = engineering_employee_attributes.jobtitle_speciality
       AND bamboohr_engineering_division_mapping.sub_department = engineering_employee_attributes.sub_department
-      AND bamboohr_engineering_division_mapping.technology_group = engineering_employee_attributes.technology_group
+      AND (bamboohr_engineering_division_mapping.technology_group = engineering_employee_attributes.technology_group OR bamboohr_engineering_division_mapping.technology_group = '')
  
 )
 
