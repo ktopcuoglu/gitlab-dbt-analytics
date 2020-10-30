d13671d843efaea6c340680e06aaf8136edc66e5
parmstrong <parmstrong@gitlab.com>
2020-10-30 19:38:05 +0200

    Changed to using a prep table

--- transform/snowflake-dbt/models/staging/sfdc/prep/prep_sfdc_account.sql
+++ transform/snowflake-dbt/models/staging/sfdc/prep/prep_sfdc_account.sql
@@ -10,10 +10,10 @@ SELECT
   TRIM(SPLIT_PART(tsp_sub_region, '-', 1))                                            AS tsp_sub_region_clean,
   TRIM(SPLIT_PART(tsp_region, '-', 1))                                                AS tsp_region_clean,
   TRIM(SPLIT_PART(REPLACE(tsp_area,'Mid - Atlantic', 'Mid Atlantic'), '-', 1))        AS tsp_area_clean,
-  UPPER(TRIM(tsp_territory))                                                          AS tsp_territory,
+  TRIM(SPLIT_PART(tsp_territory, '-1', 1))                                            AS tsp_territory_clean,
   account_id                                                                          AS crm_account_id,
   MAX(tsp_area_clean) OVER (Partition by UPPER(TRIM(tsp_area_clean)))                 AS dim_area_name_source,
   MAX(tsp_region_clean) OVER (Partition by UPPER(TRIM(tsp_region_clean)))             AS dim_region_name_source,
-  MAX(tsp_sub_region_clean) OVER (Partition by UPPER(TRIM(tsp_sub_region_clean)))     AS dim_sub_region_name_source
+  MAX(tsp_sub_region_clean) OVER (Partition by UPPER(TRIM(tsp_sub_region_clean)))     AS dim_sub_region_name_source,
+  MAX(tsp_territory_clean) OVER (Partition by UPPER(TRIM(tsp_territory_clean)))       AS dim_sales_territory_source
 FROM source
-GROUP BY account_id
