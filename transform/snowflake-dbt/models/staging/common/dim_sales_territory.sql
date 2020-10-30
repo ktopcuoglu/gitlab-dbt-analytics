359afb824e35f810b5686a46188229305d7935d6
parmstrong <parmstrong@gitlab.com>
2020-10-30 19:38:28 +0200

    Simplify generation based off of prep table

--- transform/snowflake-dbt/models/staging/common/dim_sales_territory.sql
+++ transform/snowflake-dbt/models/staging/common/dim_sales_territory.sql
@@ -6,10 +6,7 @@
 
 {{ generate_single_field_dimension (
     model_name="prep_sfdc_account",
-    id_column="tsp_territory",
-    id_column_name="dim_sales_territory_id",
-    dimension_column="tsp_territory",
-    dimension_column_name="sales_territory_name"
+    dimension_column="dim_sales_territory_source",
 ) }}
 
 
