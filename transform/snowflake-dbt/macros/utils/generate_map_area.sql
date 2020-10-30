db82e610e1db8cffdc9b9afd36bb95799824d835
parmstrong <parmstrong@gitlab.com>
2020-10-30 14:52:50 +0200

    Revert "Put original column name back in"
    This reverts commit 60f3d9e8

--- transform/snowflake-dbt/macros/utils/generate_map_area.sql
+++ transform/snowflake-dbt/macros/utils/generate_map_area.sql
@@ -11,10 +11,9 @@ WITH source_data AS (
 ), map_data AS (
 
     SELECT DISTINCT
-          {{  dimension_column }}                               AS {{  dimension_column }},
-          UPPER(TRIM({{  dimension_column }}))                  AS {{   dedup_column_name }},
-          {{ dbt_utils.surrogate_key([dedup_column_name]) }}    AS {{ id_column_name }},
-          MAX({{  dimension_column }}) OVER (Partition by {{  id_column_name }}) AS {{ dimension_column_name }}
+          UPPER(TRIM({{  dimension_column }}))        as {{   dedup_column_name }},
+          {{ dbt_utils.surrogate_key([dedup_column_name]) }}  AS {{ id_column_name }},
+          MAX({{  dimension_column }}) OVER (Partition by {{  id_column_name }}) as {{ dimension_column_name }}
     FROM source_data
 )
 {%- endmacro -%}
