891762283d4da37f772ab7c2c0e0151c406ca2f8
mpeychet <mpeychet@gitlab.com>
2020-11-05 10:52:58 +0100

    remodel

--- transform/snowflake-dbt/models/marts/usage_ping/mart_usage_ping_counters_statistics.sql
+++ transform/snowflake-dbt/models/marts/usage_ping/mart_usage_ping_counters_statistics.sql
@@ -1,33 +1,33 @@
 WITH flattened_usage_data AS (
   
     SELECT DISTINCT
-      f.path                           AS ping_name, 
-      IFF(edition='CE', edition, 'EE') AS edition,
-      SPLIT_PART(ping_name, '.', 1)    AS main_json_name,
-      SPLIT_PART(ping_name, '.', -1)   AS feature_name,
+      f.path                                                  AS ping_name, 
+      IFF(edition='CE', edition, 'EE')                        AS edition,
+      SPLIT_PART(ping_name, '.', 1)                           AS main_json_name,
+      SPLIT_PART(ping_name, '.', -1)                          AS feature_name,
       FIRST_VALUE(major_minor_version) OVER (
-        PARTITION BY full_ping_name 
+        PARTITION BY ping_name 
         ORDER BY  major_version ASC, minor_version ASC
-      )                                AS first_version_with_counter,
+      )                                                       AS first_version_with_counter,
       MIN(major_version) OVER (
-        PARTITION BY full_ping_name
-      )                                as firt_major_version_with_counter,
+        PARTITION BY ping_name
+      )                                                       AS firt_major_version_with_counter,
       FIRST_VALUE(minor_version) OVER (
-        PARTITION BY full_ping_name 
+        PARTITION BY ping_name 
         ORDER BY major_version ASC, minor_version ASC
-      )                                AS firt_minor_version_with_counter,
+      )                                                       AS firt_minor_version_with_counter,
       LAST_VALUE(major_minor_version) OVER (
-        PARTITION BY full_ping_name 
+        PARTITION BY ping_name 
         ORDER BY major_version ASC, minor_version ASC
-      )                                AS last_version_with_counter,
+      )                                                       AS last_version_with_counter,
       MAX(major_version) OVER (
-        PARTITION BY full_ping_name
-      )                                as last_major_version_with_counter,
+        PARTITION BY ping_name
+      )                                                       AS last_major_version_with_counter,
       LAST_VALUE(minor_version) OVER (
         PARTITION BY full_ping_name 
         ORDER BY major_version ASC, minor_version ASC
-      )                                AS last_minor_version_with_counter,
-      COUNT(DISTINCT id) OVER (PARTITION BY full_ping_name) AS count_pings,
+      )                                                       AS last_minor_version_with_counter,
+      COUNT(DISTINCT id) OVER (PARTITION BY full_ping_name)   AS count_pings,
       COUNT(DISTINCT uuid) OVER (PARTITION BY full_ping_name) AS count_instances
     FROM {{ ref('version_usage_data') }},
       lateral flatten(input => version_usage_data.raw_usage_data_payload, recursive => True) f
