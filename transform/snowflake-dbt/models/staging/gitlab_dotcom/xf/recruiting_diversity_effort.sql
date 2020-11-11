a83f44bda744c2beee77de5086b66da208e17ab8
Kathleen Tam <ktam@gitlab.com>
2020-11-11 19:49:37 +0000

    Update AS statement spacing to align throughout sql file

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/recruiting_diversity_effort.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/recruiting_diversity_effort.sql
@@ -18,7 +18,7 @@ WITH issues AS (
 
     SELECT
      issue_id,
-     ARRAY_AGG(LOWER(user_name)) WITHIN GROUP (ORDER BY user_name ASC) AS assignee
+     ARRAY_AGG(LOWER(user_name)) WITHIN GROUP (ORDER BY user_name ASC)  AS assignee
     FROM assignee
     LEFT JOIN users
       ON assignee.user_id = users.user_id 
@@ -30,14 +30,14 @@ WITH issues AS (
       issues.issue_title,
       issues.issue_iid,
       issues.issue_created_at,
-      DATE_TRUNC(week,issue_created_at)                             AS issue_created_week,
+      DATE_TRUNC(week,issue_created_at)                                 AS issue_created_week,
       issues.issue_closed_at,
-      DATE_TRUNC(week,issues.issue_closed_at)                       AS issue_closed_week,
-      IFF(issue_closed_at IS NOT NULL,1,0)                          AS is_issue_closed,
-      issues.state                                                  AS issue_state,
+      DATE_TRUNC(week,issues.issue_closed_at)                           AS issue_closed_week,
+      IFF(issue_closed_at IS NOT NULL,1,0)                              AS is_issue_closed,
+      issues.state                                                      AS issue_state,
       agg_assignee.assignee,
       issues.issue_description,
-      SPLIT_PART(issue_description, '**Weekly Check-In Table**',2)  AS issue_description_split,
+      SPLIT_PART(issue_description, '**Weekly Check-In Table**',2)      AS issue_description_split,
       CASE
         WHEN CONTAINS(issue_description, '[x] Yes, Diversity Sourcing methods were used'::VARCHAR) = TRUE
           THEN 'Used Diversity Strings'
@@ -46,7 +46,7 @@ WITH issues AS (
         WHEN CONTAINS(issue_description, '[x] Not Actively Sourcing'::VARCHAR) = TRUE
           THEN 'Not Actively Sourcing'
         ELSE 'No Answer' 
-      END                                                           AS issue_answer
+      END                                                               AS issue_answer
     FROM issues
     LEFT JOIN agg_assignee 
       ON agg_assignee.issue_id = issues.issue_id
@@ -63,7 +63,7 @@ WITH issues AS (
 ), cleaned AS (
 
     SELECT *,
-      LEFT(TRIM(value),10)                                          AS week_of,
+      LEFT(TRIM(value),10)                                              AS week_of,
       CASE
         WHEN CONTAINS(value,'[x] Yes, Diversity sourcing was used')
           THEN 'Yes'
@@ -72,7 +72,7 @@ WITH issues AS (
         WHEN CONTAINS(value,'[x] No, Did not use')
           THEN 'No'
         ELSE issue_answer 
-      END                                                           AS used_diversity_string
+      END                                                               AS used_diversity_string
     FROM split_issue
 
 ), final AS (
