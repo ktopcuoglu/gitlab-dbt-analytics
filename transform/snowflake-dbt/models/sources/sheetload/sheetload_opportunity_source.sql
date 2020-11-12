437e52714b3bd25fd2273b6651e176e1e48ac0a3
parmstrong <parmstrong@gitlab.com>
2020-11-12 14:50:18 +0200

    Corrected column name

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_opportunity_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_opportunity_source.sql
@@ -7,9 +7,9 @@ WITH source AS (
 
 SELECT
   "Concat"::VARCHAR                       AS fields_concatenated,
-  "KPI_Name"::VARCHAR                       AS kpi_name,
-  "Opportunity_Source"::VARCHAR             AS opportunity_source,
-  "Target"::VARCHAR                         AS target,
-  "Percent_Curve"::VARCHAR                  AS percent_curve,
-  "_updated_at"::FLOAT                      AS last_updated_at
+  "KPI_Name"::VARCHAR                     AS kpi_name,
+  "Opportunity_Source"::VARCHAR           AS opportunity_source,
+  "Target"::VARCHAR                       AS target,
+  "Percent_Curve"::VARCHAR                AS percent_curve,
+  "_UPDATED_AT"::FLOAT                    AS last_updated_at
 FROM source
