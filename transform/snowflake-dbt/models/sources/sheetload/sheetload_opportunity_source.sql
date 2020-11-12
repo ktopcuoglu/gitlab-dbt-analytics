f14c77f9b90e6d1ebeca55934bf2e642afaec8dd
parmstrong <parmstrong@gitlab.com>
2020-11-12 14:40:51 +0200

    Removed incorrect column

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_opportunity_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_opportunity_source.sql
@@ -11,6 +11,5 @@ SELECT
   "Opportunity_Source"::VARCHAR             AS opportunity_source,
   "Target"::VARCHAR                         AS target,
   "Percent_Curve"::VARCHAR                  AS percent_curve,
-  "application_exercise_complete"::DATE     AS application_exercise_compled_date,
   "_updated_at"::FLOAT                      AS last_updated_at
 FROM source
