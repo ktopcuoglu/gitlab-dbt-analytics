1c5846ac29927877cd987237730c2863727e21cd
Parul Luthra <pluthra@gitlab.com>
2020-11-12 23:39:26 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/macros/bamboohr/bamboohr_division_grouping.sql
+++ transform/snowflake-dbt/macros/bamboohr/bamboohr_division_grouping.sql
@@ -1,6 +1,6 @@
 {%- macro bamboohr_division_grouping(division) -%}
 
-     CASE WHEN {{division}} IN ('Engineering','Meltano') THEN 'Engineering/Meltano'
+     CASE WHEN {{division}} IN ('Engineering', 'Meltano') THEN 'Engineering/Meltano'
            WHEN {{division}} IN ('CEO','People Group') THEN 'People Group/CEO'
            ELSE {{division}} END  
 
