d9e7c2d8ae73e78cb56f6a801f9ecc6bfa8f890f
parmstrong <parmstrong@gitlab.com>
2020-11-13 11:11:52 +0200

    Corrected more dtypes

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_segment_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_segment_source.sql
@@ -6,10 +6,10 @@ WITH source AS (
 )
 
 SELECT
-  "Concat"::VARCHAR                       AS fields_concatenated,
+  "Concat"::VARCHAR                         AS fields_concatenated,
   "KPI_Name"::VARCHAR                       AS kpi_name,
   "Sales_Segment"::VARCHAR                  AS sales_segment,
-  "Target"::FLOAT                           AS target,
+  "Target"::VARCHAR                         AS target,
   "Percent_Curve"::VARCHAR                  AS percent_curve,
   "_UPDATED_AT"::FLOAT                      AS last_updated_at
 FROM source
--- transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_territory_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_territory_source.sql
@@ -8,7 +8,7 @@ WITH source AS (
 SELECT
   "KPI_Name"::VARCHAR                       AS kpi_name,
   "Sales_Territory"::VARCHAR                AS sales_territory,
-  "Target"::FLOAT                           AS target,
+  "Target"::VARCHAR                         AS target,
   "Percent_Curve"::VARCHAR                  AS percent_curve,
   "_UPDATED_AT"::FLOAT                      AS last_updated_at
 FROM source
