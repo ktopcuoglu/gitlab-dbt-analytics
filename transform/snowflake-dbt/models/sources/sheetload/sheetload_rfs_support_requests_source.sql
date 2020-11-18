31390808f4db925c9d91b617556f5284ce935c5f
Kathleen Tam <ktam@gitlab.com>
2020-11-18 16:25:29 +0000

    Update spacing

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_rfs_support_requests_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_rfs_support_requests_source.sql
@@ -6,18 +6,17 @@ WITH source AS (
 ), final AS (
     
     SELECT 
-        NULLIF(customer_prospect_name, '')::VARCHAR                       AS customer_prospect_name,
-        NULLIF(request_type, '')::VARCHAR                                 AS request_type,
-        NULLIF(market_industry_vertical, '')::VARCHAR                     AS market_industry_vertical,
-        NULLIF(customer_prospect_size, '')::VARCHAR                       AS customer_prospect_size,
-        NULLIF(sfdc_link, '')::VARCHAR                                    AS sfdc_link,
-        NULLIF(iacv_impact, '')::VARCHAR                                  AS iacv_impact,
-        NULLIF(product_host, '')::VARCHAR                                 AS product_host,
-        NULLIF(due_date, '')::VARCHAR                                   
- AS due_date,
-        NULLIF(other, '')::VARCHAR                                        AS other,
-        NULLIF(requestor_name, '')::VARCHAR                               AS requestor_name,
-        NULLIF(additional_gitlab_team_members, '')::VARCHAR               AS additional_gitlab_team_members
+      NULLIF(customer_prospect_name, '')::VARCHAR                       AS customer_prospect_name,
+      NULLIF(request_type, '')::VARCHAR                                 AS request_type,
+      NULLIF(market_industry_vertical, '')::VARCHAR                     AS market_industry_vertical,
+      NULLIF(customer_prospect_size, '')::VARCHAR                       AS customer_prospect_size,
+      NULLIF(sfdc_link, '')::VARCHAR                                    AS sfdc_link,
+      NULLIF(iacv_impact, '')::VARCHAR                                  AS iacv_impact,
+      NULLIF(product_host, '')::VARCHAR                                 AS product_host,
+      NULLIF(due_date, '')::VARCHAR                                     AS due_date,
+      NULLIF(other, '')::VARCHAR                                        AS other,
+      NULLIF(requestor_name, '')::VARCHAR                               AS requestor_name,
+      NULLIF(additional_gitlab_team_members, '')::VARCHAR               AS additional_gitlab_team_members
     FROM source
 
 ) 
