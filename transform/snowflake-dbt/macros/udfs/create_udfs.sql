5cde048fe8729c8afbfb865b199b92a36553fe6c
Taylor Murphy <tmurphy@gitlab.com>
2020-11-19 16:47:08 -0600

    Fix base udf macro

--- transform/snowflake-dbt/macros/udfs/create_udfs.sql
+++ transform/snowflake-dbt/macros/udfs/create_udfs.sql
@@ -1,13 +1,16 @@
 {% macro create_udfs() %}
 
 {%- set production_targets = production_targets() -%}
+{%- set db_analytics = env_var("SNOWFLAKE_TRANSFORM_DATABASE") -%}
+{%- set db_prep = env_var("SNOWFLAKE_PREP_DATABASE") -%}
+{%- set db_prod = env_var("SNOWFLAKE_PROD_DATABASE") -%}
+{%- set production_databases = [db_analytics, db_prep, db_prod] -%}
 
 {%- if target.name in production_targets -%}
 
-  create schema if not exists "ANALYTICS".{{target.schema}};
-  create schema if not exists "PREP".{{target.schema}};
-  create schema if not exists "PROD".{{target.schema}};
-
+    {% for db in production_databases %}
+        create schema if not exists "{{ db | trim }}".{{target.schema}};
+    {% endfor %}
     {{sfdc_id_15_to_18()}}
     {{regexp_substr_to_array()}}
     {{crc32()}}
@@ -15,10 +18,9 @@
 {%- else -%}
     
     {# Need to create analytics for gitlab_dotcom models #}
-    create schema if not exists "{{ target.database | trim }}_ANALYTICS".{{target.schema}};
-    create schema if not exists "{{ target.database | trim }}_PREP".{{target.schema}};
-    create schema if not exists "{{ target.database | trim }}_PROD".{{target.schema}};
-
+    {% for db in production_databases %}
+        create schema if not exists "{{ target.database | trim }}_{{ db | trim}}".{{target.schema}};
+    {% endfor %}
     {{sfdc_id_15_to_18()}}
     {{regexp_substr_to_array()}}
     {{crc32()}}
