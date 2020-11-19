bacc303944a5adbd4379aaa159488340fee9a111
parmstrong <parmstrong@gitlab.com>
2020-11-19 20:44:54 +0200

    Removed broken sheets, renamed account_region to region

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_funnel_targets_region_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_funnel_targets_region_source.sql
@@ -1,14 +1,14 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ source('sheetload', 'sales_funnel_targets_purchase_channel') }}
+    FROM {{ source('sheetload', 'sales_funnel_targets_region') }}
 
 ), renamed AS (
 
     SELECT
       "Concat"::VARCHAR                                     AS fields_concatenated,
+      "Account_Region"::VARCHAR                             AS account_region,
       "KPI_Name"::VARCHAR                                   AS kpi_name,
-      "Purchase_Channel"::VARCHAR                           AS purchase_channel,
       "Target"::VARCHAR                                     AS target,
       "Percent_Curve"::VARCHAR                              AS percent_curve,
       TO_TIMESTAMP(TO_NUMERIC("_UPDATED_AT"))::TIMESTAMP    AS last_updated_at
