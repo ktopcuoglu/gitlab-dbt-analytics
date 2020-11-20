0ab21a857ef3932f31036103acbea6f3b11bd242
parmstrong <parmstrong@gitlab.com>
2020-11-20 11:22:04 +0200

    Corrected column dtype and name

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_funnel_targets_dates_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_funnel_targets_dates_source.sql
@@ -8,7 +8,7 @@ WITH source AS (
     SELECT
       "Concat"::VARCHAR                                     AS fields_concatenated,
       "KPI_Name"::VARCHAR                                   AS kpi_name,
-      "Date"::DATE                                          AS sales_funnel_target_date,
+      "Date"::VARCHAR                                       AS sales_funnel_target_quarter_date,
       "Target"::VARCHAR                                     AS target,
       "Percent_Curve"::VARCHAR                              AS percent_curve,
       TO_TIMESTAMP(TO_NUMERIC("_UPDATED_AT"))::TIMESTAMP    AS last_updated_at
