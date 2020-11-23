b0f47575907491b69086ce7f6bbd25ad5ca6ad1d
Kathleen Tam <ktam@gitlab.com>
2020-11-23 19:18:40 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/staging/sheetload/hire_plan_xf.sql
+++ transform/snowflake-dbt/models/staging/sheetload/hire_plan_xf.sql
@@ -27,7 +27,7 @@ WITH source AS (
       department_modified,
       division_mapped_current AS division
     FROM {{ ref ('bamboohr_job_info_current_division_base') }}   
-    WHERE DEPARTMENT IS NOT NULL
+    WHERE department IS NOT NULL
       
 ), all_company AS (
 
