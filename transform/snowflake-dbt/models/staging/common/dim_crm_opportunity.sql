a6cae1ea507dd5549a3265273a27decf9aa6def4
Taylor Murphy <tmurphy@gitlab.com>
2020-11-24 07:06:10 -0600

    Scoot in the aliasing

--- transform/snowflake-dbt/models/staging/common/dim_crm_opportunity.sql
+++ transform/snowflake-dbt/models/staging/common/dim_crm_opportunity.sql
@@ -17,10 +17,10 @@ WITH sfdc_opportunity AS (
 
     SELECT
       -- keys
-      sfdc_opportunity.account_id                                                                 AS dim_crm_account_id,
-      sfdc_opportunity.opportunity_id                                                             AS dim_crm_opportunity_id,
+      sfdc_opportunity.account_id                       AS dim_crm_account_id,
+      sfdc_opportunity.opportunity_id                   AS dim_crm_opportunity_id,
       sfdc_opportunity.opportunity_name,
-      sfdc_opportunity.owner_id                                                                   AS dim_crm_sales_rep_id,
+      sfdc_opportunity.owner_id                         AS dim_crm_sales_rep_id,
 
       -- logistical information
       sfdc_opportunity.business_type,
@@ -29,14 +29,14 @@ WITH sfdc_opportunity AS (
       sfdc_opportunity.merged_opportunity_id,
       sfdc_opportunity.net_new_source_categories,
       sfdc_opportunity.account_owner_team_stamped,
-      sfdc_opportunity.primary_campaign_source_id                                                 AS primary_campaign_source_id,
+      sfdc_opportunity.primary_campaign_source_id       AS primary_campaign_source_id,
       sfdc_opportunity.sales_accepted_date,
       sfdc_opportunity.sales_path,
       sfdc_opportunity.sales_type,
       sfdc_opportunity.source_buckets,
       sfdc_opportunity.stage_name,
-      sfdc_opportunity_stage.is_active                                                             AS stage_is_active,
-      sfdc_opportunity_stage.is_closed                                                             AS stage_is_closed,
+      sfdc_opportunity_stage.is_active                  AS stage_is_active,
+      sfdc_opportunity_stage.is_closed                  AS stage_is_closed,
       sfdc_opportunity.technical_evaluation_date,
       sfdc_opportunity.deal_path,
 
@@ -54,10 +54,10 @@ WITH sfdc_opportunity AS (
           OR sfdc_opportunity.pushed_count > 0)
           THEN TRUE
           ELSE FALSE
-      END                                                                                         AS is_risky,
+      END                                               AS is_risky,
       sfdc_opportunity.is_swing_deal,
       sfdc_opportunity.is_edu_oss,
-      sfdc_opportunity_stage.is_won                                                               AS is_won,
+      sfdc_opportunity_stage.is_won                     AS is_won,
       sfdc_opportunity.nrv,
       sfdc_opportunity.probability,
       sfdc_opportunity.reason_for_loss,
@@ -65,12 +65,12 @@ WITH sfdc_opportunity AS (
       CASE
         WHEN sfdc_opportunity.sales_qualified_source = 'BDR Generated' THEN 'SDR Generated'
         ELSE sfdc_opportunity.sales_qualified_source
-      END                                                                                         AS sales_qualified_source,
+      END                                               AS sales_qualified_source,
       sfdc_opportunity.solutions_to_be_replaced,
       sfdc_opportunity.is_web_portal_purchase,
       sfdc_opportunity.partner_initiated_opportunity,
       sfdc_opportunity.user_segment,
-      sfdc_opportunity.order_type_stamped                                                         AS order_type,
+      sfdc_opportunity.order_type_stamped               AS order_type,
 
       -- ************************************
       -- channel reporting
