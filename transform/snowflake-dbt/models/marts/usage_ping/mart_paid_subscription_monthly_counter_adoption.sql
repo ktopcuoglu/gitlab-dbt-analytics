8dda4a5116a751cfcf00866479b231d17cc1c7ba
mpeychet <mpeychet@gitlab.com>
2020-12-01 17:26:30 +0100

    change the hard coded table

--- transform/snowflake-dbt/models/marts/usage_ping/mart_paid_subscription_monthly_counter_adoption.sql
+++ transform/snowflake-dbt/models/marts/usage_ping/mart_paid_subscription_monthly_counter_adoption.sql
@@ -36,6 +36,11 @@ WITH self_managed_active_subscriptions AS (
     SELECT *
     FROM {{ ref('fct_usage_ping_payloads') }}
   
+), gitlab_release_schedule AS (
+  
+    SELECT *
+    FROM {{ ref('gitlab_release_schedule')}}  
+
 ), transformed AS (
   
     SELECT 
@@ -110,7 +115,7 @@ WITH self_managed_active_subscriptions AS (
       COUNT(DISTINCT subscription_name_slugify)                         AS major_minor_version_subscriptions,
       major_minor_version_subscriptions /  MAX(total_subscrption_count) AS pct_major_minor_version_subscriptions
     FROM paid_subscriptions_monthly_usage_ping_optin
-    INNER JOIN analytics_staging.gitlab_release_schedule AS gitlab_releases
+    INNER JOIN gitlab_release_schedule AS gitlab_releases
       ON paid_subscriptions_monthly_usage_ping_optin.latest_major_minor_version = gitlab_releases.major_minor_version
     LEFT JOIN agg_total_subscriptions AS agg ON paid_subscriptions_monthly_usage_ping_optin.reporting_month = agg.agg_month
     {{ dbt_utils.group_by(n=4) }}
