875d33b5ad9d6c6c4132caab15028f91b2944b38
mpeychet <mpeychet@gitlab.com>
2020-12-01 12:25:36 +0100

    working versioni

--- transform/snowflake-dbt/models/marts/usage_ping/mart_monthly_product_usage.sql
+++ transform/snowflake-dbt/models/marts/usage_ping/mart_monthly_product_usage.sql
@@ -35,12 +35,31 @@ WITH dim_billing_accounts AS (
     SELECT *
     FROM {{ ref('dim_subscriptions') }}
     WHERE subscription_name_slugify <> zuora_renewal_subscription_name_slugify[0]::TEXT
+      OR zuora_renewal_subscription_name_slugify IS NULL
       --OR subscription_end_date < DATE_TRUNC('month', current_date)
 
-), fct_mrr AS (
+),  zuora_subscription_snapshots AS (
+
+  /**
+  This partition handles duplicates and hard deletes by taking only
+    the latest subscription version snapshot
+   */
+
+  SELECT
+    rank() OVER (
+      PARTITION BY subscription_name
+      ORDER BY DBT_VALID_FROM DESC) AS rank,
+    subscription_id,
+    subscription_name
+  FROM {{ ref('zuora_subscription_snapshots_source') }}
+  WHERE subscription_status NOT IN ('Draft', 'Expired')
+    AND CURRENT_TIMESTAMP()::TIMESTAMP_TZ >= dbt_valid_from
+    AND {{ coalesce_to_infinity('dbt_valid_to') }} > current_timestamp()::TIMESTAMP_TZ
+
+), fct_invoice_items AS (
 
     SELECT *
-    FROM {{ ref('fct_mrr')}}
+    FROM {{ ref('fct_invoice_items')}}
 
 ), fct_monthly_usage_data AS (
 
@@ -64,28 +83,43 @@ WITH dim_billing_accounts AS (
     SELECT DISTINCT
       license_id,
       dim_licenses.license_md5,
-      subscription_source.subscription_id AS original_linked_subscription_id,
+      subscription_source.subscription_id              AS original_linked_subscription_id,
       subscription_source.account_id,
       subscription_source.subscription_name_slugify,
-      dim_subscriptions.subscription_id       AS latest_active_subscription_id,
+      dim_subscriptions.subscription_id                AS latest_active_subscription_id,
       dim_subscriptions.subscription_start_date,
       dim_subscriptions.subscription_end_date,
       dim_subscriptions.subscription_start_month,
       dim_subscriptions.subscription_end_month,
-      dim_billing_accounts.billing_account_id
+      dim_billing_accounts.billing_account_id,
+      fct_invoice_items.effective_end_month,
+      fct_invoice_items.effective_start_month,
+      IFF(MAX(arr) > 0, TRUE, FALSE)                            AS is_paid,
+      ARRAY_AGG(DISTINCT product_category)
+        WITHIN GROUP (ORDER BY product_category ASC)            AS product_category_array,
+      ARRAY_AGG(DISTINCT product_rate_plan_name)
+        WITHIN GROUP (ORDER BY product_rate_plan_name ASC)      AS product_rate_plan_name_array,
+      ARRAY_AGG(DISTINCT product_rate_plan_name ILIKE ANY ('%edeu%', '%oss%%'))
+        WITHIN GROUP (ORDER BY product_rate_plan_name ILIKE ANY ('%edeu%', '%oss%%') ASC)      AS product_rate_plan_name_is_edu_oss_array
     FROM dim_licenses
     INNER JOIN subscription_source
       ON dim_licenses.subscription_id = subscription_source.subscription_id
-    LEFT JOIN dim_subscriptions
+    INNER JOIN dim_subscriptions
       ON subscription_source.subscription_name_slugify = dim_subscriptions.subscription_name_slugify
-    INNER JOIN fct_mrr
-      ON dim_subscriptions.subscription_id = fct_mrr.subscription_id
+    INNER JOIN zuora_subscription_snapshots
+      ON zuora_subscription_snapshots.subscription_id = dim_subscriptions.subscription_id
+      AND zuora_subscription_snapshots.rank = 1
+    LEFT JOIN fct_invoice_items
+      ON zuora_subscription_snapshots.subscription_id = fct_invoice_items.dim_subscription_id
     INNER JOIN dim_product_details
-      ON dim_product_details.product_details_id = fct_mrr.product_details_id
+      ON dim_product_details.product_details_id = fct_invoice_items.dim_product_details_id
+        AND delivery = 'Self-Managed'
+        AND product_rate_plan_name NOT IN ('Premium - 1 Year - Eval')
     INNER JOIN dim_billing_accounts
       ON dim_subscriptions.billing_account_id = dim_billing_accounts.billing_account_id
     INNER JOIN dim_crm_accounts
       ON dim_billing_accounts.crm_account_id = dim_crm_accounts.crm_account_id
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13
 
 ), joined AS (
 
@@ -104,6 +138,10 @@ WITH dim_billing_accounts AS (
       license_subscriptions.latest_active_subscription_id,
       license_subscriptions.subscription_name_slugify,
       license_subscriptions.billing_account_id,
+      license_subscriptions.product_category_array,
+      license_subscriptions.product_rate_plan_name_array,
+      COALESCE(is_paid, FALSE) AS is_paid,
+      COALESCE(ARRAY_CONTAINS('True'::VARIANT, license_subscriptions.product_rate_plan_name_array), FALSE) AS is_edu_oss,
       fct_usage_ping_payloads.delivery,
       fct_usage_ping_payloads.edition,
       fct_usage_ping_payloads.product_tier,
@@ -125,7 +163,8 @@ WITH dim_billing_accounts AS (
         AND fct_usage_ping_payloads.source_ip_hash = dim_hosts.source_ip_hash
         AND fct_usage_ping_payloads.uuid = dim_hosts.instance_id
     LEFT JOIN license_subscriptions
-      ON fct_usage_ping_payloads.license_md5 = license_subscriptions.license_md5      
+      ON fct_usage_ping_payloads.license_md5 = license_subscriptions.license_md5 
+        AND created_month >= license_subscriptions.effective_start_month AND created_month < license_subscriptions.effective_end_month
 
 ), renamed AS (
 
@@ -145,7 +184,7 @@ WITH dim_billing_accounts AS (
       location_id,
 
       -- metadata usage ping
-      --delivery,
+      delivery,
       edition,
       product_tier,
       main_edition_product_tier,
@@ -167,7 +206,10 @@ WITH dim_billing_accounts AS (
       --license_plan,
       --license_trial   AS is_trial,
       --license_user_count,
-
+      product_category_array,
+      product_rate_plan_name_array,
+      is_paid,
+      is_edu,
       --created_at,
       --recorded_at
 
@@ -180,8 +222,8 @@ WITH dim_billing_accounts AS (
 
 {{ dbt_audit(
     cte_ref="renamed",
-    created_by="@derekatwood",
-    updated_by="@msendal",
-    created_date="2020-08-17",
-    updated_date="2020-10-26"
+    created_by="@mpeychet",
+    updated_by="@mpeychet",
+    created_date="2020-12-01",
+    updated_date="2020-12-01"
 ) }}
