06a60046dceb63e472ac623c1468870c8c75f360
Michael Walker <mwalker@gitlab.com>
2020-12-07 12:25:10 -0600

    fix dbt project and sensor definitions for datasiren

--- transform/snowflake-dbt/models/sources/datasiren/transform_db_email_column_name_sensor.sql
+++ transform/snowflake-dbt/models/sources/datasiren/transform_db_email_column_name_sensor.sql
@@ -3,4 +3,4 @@
     })
 }}
 
-{{ snowflake__find_columns_with_names_like('email_column_name_sensor', '%email%', env_var('SNOWFLAKE_TRANSFORM_DATABASE')) }}
+{{ datasiren.snowflake__find_columns_with_names_like('email_column_name_sensor', '%email%', env_var('SNOWFLAKE_TRANSFORM_DATABASE')) }}
