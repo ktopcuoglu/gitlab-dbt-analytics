458e28ff1d67478b079a477576fc8bb46e061a46
Michael Walker <mwalker@gitlab.com>
2020-12-07 12:27:07 -0600

    fix first datasiren sensor

--- transform/snowflake-dbt/models/sources/datasiren/transform_db_email_column_name_sensor.sql
+++ transform/snowflake-dbt/models/sources/datasiren/transform_db_email_column_name_sensor.sql
@@ -3,4 +3,4 @@
     })
 }}
 
-{{ datasiren.snowflake__find_columns_with_names_like('email_column_name_sensor', '%email%', env_var('SNOWFLAKE_TRANSFORM_DATABASE')) }}
+{{ datasiren.snowflake__email_column_name_sensor(env_var('SNOWFLAKE_TRANSFORM_DATABASE')) }}
