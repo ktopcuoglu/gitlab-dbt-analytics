867d6fa4eed5bd246e8fb41c996fb4ce173c62a7
Michael Walker <mwalker@gitlab.com>
2020-12-07 12:12:28 -0600

    reflect model based changes to datasiren

---
+++ transform/snowflake-dbt/models/sources/datasiren/transform_db_email_column_name_sensor.sql
@@ -0,0 +1,6 @@
+{{ config({
+        "materialized": "incremental"
+    })
+}}
+
+{{ snowflake__find_columns_with_names_like('email_column_name_sensor', '%email%', env_var('SNOWFLAKE_TRANSFORM_DATABASE')) }}
