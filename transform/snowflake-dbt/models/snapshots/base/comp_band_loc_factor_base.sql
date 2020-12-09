6984920965946f16bddcf0e8b03bd2f305e69788
Parul Luthra <pluthra@gitlab.com>
2020-12-09 01:10:09 +0000

    Cleaning up where merge conflict wasn't resolved

--- transform/snowflake-dbt/models/snapshots/base/comp_band_loc_factor_base.sql
+++ transform/snowflake-dbt/models/snapshots/base/comp_band_loc_factor_base.sql
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
 {{ config({
     "schema": "temporary"
     })
 }}
 
 WITH source AS (
-=======
-with source as (
->>>>>>> master
 
     SELECT *
     FROM {{ source('snapshots', 'sheetload_employee_location_factor_snapshots') }}
     WHERE "Employee_ID" != 'Not In Comp Calc'
       AND "Employee_ID" NOT IN ('$72,124','S1453')
 
-<<<<<<< HEAD
 ), renamed AS (
 
     SELECT
@@ -59,9 +54,7 @@ with source as (
   FROM deduplicated
   GROUP BY 1, 2, 3
 
-=======
->>>>>>> master
 )
 
 SELECT *
-FROM source
+FROM final
