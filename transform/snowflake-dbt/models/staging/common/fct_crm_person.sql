1a8f9564dd8870e20fc3c19939bcd0139ccc983d
mcooperDD <michelle.cooper@decisivedata.net>
2020-12-08 16:53:02 -0800

    create prep view for crm_person and update references in fact and dimension models to pull from prep view

--- transform/snowflake-dbt/models/staging/common/fct_crm_person.sql
+++ transform/snowflake-dbt/models/staging/common/fct_crm_person.sql
@@ -8,28 +8,21 @@ WITH account_dims_mapping AS (
   SELECT *
   FROM {{ ref('map_crm_account') }}
 
-), biz_touchpoints AS (
+), crm_person AS (
 
-    SELECT *
-    FROM {{ ref('sfdc_bizible_touchpoint_source') }}
-    WHERE bizible_touchpoint_position LIKE '%FT%'
-     AND is_deleted = 'FALSE'
+  SELECT
 
-), biz_person AS (
+    crm_person_id,
+    sfdc_record_id,
+    bizible_person_id,
+    bizible_touchpoint_position,
+    bizible_marketing_channel_path,
+    bizible_touchpoint_date,
+    crm_account_id,
+    crm_sales_rep_id,
+    person_score
 
-    SELECT *
-    FROM {{ ref('sfdc_bizible_person_source') }}
-    WHERE is_deleted = 'FALSE'
-
-), biz_person_with_touchpoints AS (
-
-    SELECT
-      biz_touchpoints.*,
-      biz_person.bizible_contact_id,
-      biz_person.bizible_lead_id
-    FROM biz_touchpoints
-    JOIN biz_person
-      ON biz_touchpoints.bizible_person_id = biz_person.person_id
+    FROM {{ref('prep_crm_person')}}
 
 ), geo_region AS (
 
@@ -56,6 +49,11 @@ WITH account_dims_mapping AS (
     SELECT *
     FROM {{ ref('map_marketing_channel') }}
 
+), sales_segment AS (
+
+      SELECT *
+      FROM {{ ref('dim_sales_segment') }}
+
 ), sales_territory AS (
 
     SELECT *
@@ -72,48 +70,6 @@ WITH account_dims_mapping AS (
     SELECT *
     FROM {{ ref('sfdc_lead_source') }}
     WHERE is_deleted = 'FALSE'
-
-), sales_segment AS (
-
-    SELECT *
-    FROM {{ ref('dim_sales_segment') }}
-
-), crm_person_with_touchpoints AS (
-
-    SELECT
-
-      {{ dbt_utils.surrogate_key(['contact_id']) }}                         AS crm_person_id,
-      contact_id                                                          AS sfdc_record_id,
-      bizible_person_id                                                   AS bizible_person_id,
-      bizible_touchpoint_position,
-      bizible_marketing_channel_path,
-      bizible_touchpoint_date,
-      account_id                                                          AS crm_account_id,
-      owner_id                                                            AS crm_sales_rep_id,
-      person_score
-
-    FROM sfdc_contacts
-    LEFT JOIN biz_person_with_touchpoints
-      on sfdc_contacts.contact_id = biz_person_with_touchpoints.bizible_contact_id
-
-    UNION
-
-    SELECT
-
-      {{ dbt_utils.surrogate_key(['lead_id']) }}                            AS crm_person_id,
-      lead_id                                                             AS sfdc_record_id,
-      bizible_person_id                                                   AS bizible_person_id,
-      bizible_touchpoint_position,
-      bizible_marketing_channel_path,
-      bizible_touchpoint_date,
-      lean_data_matched_account                                           AS crm_account_id,
-      owner_id                                                            AS crm_sales_rep_id,
-      person_score
-
-    FROM sfdc_leads
-    LEFT JOIN biz_person_with_touchpoints
-      on sfdc_leads.lead_id = biz_person_with_touchpoints.bizible_lead_id
-    WHERE is_converted = 'FALSE'
 
 ) , marketing_qualified_leads AS(
 
@@ -136,11 +92,11 @@ WITH account_dims_mapping AS (
 
     SELECT
 
-      {{ dbt_utils.surrogate_key(['contact_id','marketo_qualified_lead_date::timestamp']) }}    AS event_id,
+      {{ dbt_utils.surrogate_key(['contact_id','marketo_qualified_lead_date::timestamp']) }}  AS event_id,
       marketo_qualified_lead_date::timestamp                                                  AS event_timestamp,
       contact_id                                                                              AS sfdc_record_id,
       'contact'                                                                               AS sfdc_record,
-      {{ dbt_utils.surrogate_key(['contact_id']) }}                                             AS crm_person_id,
+      {{ dbt_utils.surrogate_key(['contact_id']) }}                                           AS crm_person_id,
       contact_id                                                                              AS contact_id,
       account_id                                                                              AS account_id,
       owner_id                                                                                AS crm_sales_rep_id,
@@ -179,14 +135,14 @@ WITH account_dims_mapping AS (
 
     SELECT
     -- ids
-      crm_person_with_touchpoints.crm_person_id                                                                           AS dim_crm_person_id,
-      crm_person_with_touchpoints.sfdc_record_id                                                                          AS sfdc_record_id,
-      crm_person_with_touchpoints.bizible_person_id                                                                       AS bizible_person_id,
+      crm_person.crm_person_id                                                                           AS dim_crm_person_id,
+      crm_person.sfdc_record_id                                                                          AS sfdc_record_id,
+      crm_person.bizible_person_id                                                                       AS bizible_person_id,
 
      -- common dimension keys
 
-      crm_person_with_touchpoints.crm_sales_rep_id                                                                        AS dim_crm_sales_rep_id,
-      crm_person_with_touchpoints.crm_account_id                                                                          AS dim_crm_account_id,
+      crm_person.crm_sales_rep_id                                                                        AS dim_crm_sales_rep_id,
+      crm_person.crm_account_id                                                                          AS dim_crm_account_id,
       COALESCE(account_dims_mapping.dim_sales_segment_name_id, sales_segment.dim_sales_segment_name_id, MD5(-1))          AS dim_sales_segment_id,
       COALESCE(account_dims_mapping.dim_geo_region_name_id, geo_region.dim_geo_region_name_id, MD5(-1))                   AS dim_geo_region_id,
       COALESCE(account_dims_mapping.dim_geo_sub_region_name_id, geo_sub_region.dim_geo_sub_region_name_id, MD5(-1))       AS dim_geo_sub_region_id,
@@ -222,18 +178,18 @@ WITH account_dims_mapping AS (
 
      -- additive fields
 
-      crm_person_with_touchpoints.person_score                                                                            AS person_score,
+      crm_person.person_score                                                                            AS person_score,
       mqls.mql_count                                                                                                      AS mql_count
 
-    FROM crm_person_with_touchpoints
+    FROM crm_person
     LEFT JOIN sfdc_leads
-      ON crm_person_with_touchpoints.sfdc_record_id = sfdc_leads.lead_id
+      ON crm_person.sfdc_record_id = sfdc_leads.lead_id
     LEFT JOIN sfdc_contacts
-      ON crm_person_with_touchpoints.sfdc_record_id = sfdc_contacts.contact_id
+      ON crm_person.sfdc_record_id = sfdc_contacts.contact_id
     LEFT JOIN mqls
-      ON crm_person_with_touchpoints.crm_person_id = mqls.crm_person_id
+      ON crm_person.crm_person_id = mqls.crm_person_id
     LEFT JOIN account_dims_mapping
-      ON crm_person_with_touchpoints.crm_account_id = account_dims_mapping.crm_account_id
+      ON crm_person.crm_account_id = account_dims_mapping.crm_account_id
     LEFT JOIN sales_segment
       ON sfdc_leads.sales_segmentation = sales_segment.dim_sales_segment_name
     LEFT JOIN geo_region
@@ -245,7 +201,7 @@ WITH account_dims_mapping AS (
     LEFT JOIN industry
       ON COALESCE(sfdc_contacts.industry, sfdc_leads.industry) = industry.dim_industry_name
     LEFT JOIN marketing_channel_mapping
-      ON crm_person_with_touchpoints.bizible_marketing_channel_path = marketing_channel_mapping.bizible_marketing_channel_path
+      ON crm_person.bizible_marketing_channel_path = marketing_channel_mapping.bizible_marketing_channel_path
     LEFT JOIN marketing_channel
       ON marketing_channel_mapping.marketing_channel_name = marketing_channel.marketing_channel_name
 
@@ -255,5 +211,5 @@ WITH account_dims_mapping AS (
     created_by="@mcooperDD",
     updated_by="@iweeks",
     created_date="2020-12-01",
-    updated_date="2020-12-07"
+    updated_date="2020-12-08"
 ) }}
--- transform/snowflake-dbt/models/staging/mapping/prep/prep.md
+++ transform/snowflake-dbt/models/staging/mapping/prep/prep.md
@@ -1,6 +1,12 @@
 {% docs prep_sfdc_account %}
 
-SFDC Account Prep table, used to clean and dedupe fields from a common source for use in further downstream dimensions. 
+SFDC Account Prep table, used to clean and dedupe fields from a common source for use in further downstream dimensions.
 Cleaning operations vary across columns, depending on the nature of the source data. See discussion in [MR](https://gitlab.com/gitlab-data/analytics/-/merge_requests/3782) for further details
+
+{% enddocs %}
+
+{% docs prep_crm_person%}
+
+This table creates a singular source for crm_person logic to be used in dim_crm_person and fct_crm_person. It combines unconverted leads and contacts, and maps both to a bizible_person_id.
 
 {% enddocs %}
