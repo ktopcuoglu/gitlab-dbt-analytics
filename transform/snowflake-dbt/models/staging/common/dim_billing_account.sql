29f2fada519ec8560fe968da888889242bd9f598
mcooperDD <michelle.cooper@decisivedata.net>
2020-12-09 09:40:07 -0800

    update column names for dim_crm_account_id and dim_billing_account_id

--- transform/snowflake-dbt/models/staging/common/dim_billing_account.sql
+++ transform/snowflake-dbt/models/staging/common/dim_billing_account.sql
@@ -27,8 +27,8 @@ WITH map_merged_crm_accounts AS (
 ), filtered AS (
 
     SELECT
-      zuora_account.account_id                              AS billing_account_id,
-      map_merged_crm_accounts.dim_crm_account_id            AS crm_account_id,
+      zuora_account.account_id                              AS dim_billing_account_id,
+      map_merged_crm_accounts.dim_crm_account_id            AS dim_crm_account_id,
       zuora_account.account_number                          AS billing_account_number,
       zuora_account.account_name                            AS billing_account_name,
       zuora_account.status                                  AS account_status,
