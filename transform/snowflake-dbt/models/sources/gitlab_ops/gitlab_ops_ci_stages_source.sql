67c59676162c314cda1a91cdfb8020964ef7b62b
Kathleen Tam <ktam@gitlab.com>
2020-12-10 20:45:27 +0000

    reformat spacing for AS in ci_stages_source

--- transform/snowflake-dbt/models/sources/gitlab_ops/gitlab_ops_ci_stages_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_ops/gitlab_ops_ci_stages_source.sql
@@ -20,15 +20,15 @@ WITH source AS (
 ), renamed AS (
   
   SELECT
-    id::NUMBER           AS ci_stage_id,
-    project_id::NUMBER   AS project_id,
-    pipeline_id::NUMBER  AS pipeline_id,
+    id::NUMBER            AS ci_stage_id,
+    project_id::NUMBER    AS project_id,
+    pipeline_id::NUMBER   AS pipeline_id,
     created_at::TIMESTAMP AS created_at,
     updated_at::TIMESTAMP AS updated_at,
     name::VARCHAR         AS ci_stage_name,
-    status::NUMBER       AS ci_stage_status,
-    lock_version::NUMBER AS lock_version,
-    position::NUMBER     AS position
+    status::NUMBER        AS ci_stage_status,
+    lock_version::NUMBER  AS lock_version,
+    position::NUMBER      AS position
   FROM source
 
 )
