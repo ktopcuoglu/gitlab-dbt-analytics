2edc5de2081eeb1b537e320759f38030b9985579
Taylor Murphy <tmurphy@gitlab.com>
2020-12-11 14:10:00 -0600

    Updates for testing

--- extract/gitlab_feature_flags_yaml/upload.py
+++ extract/gitlab_feature_flags_yaml/upload.py
@@ -1,10 +1,12 @@
 import json
 import logging
-import glob
+import re
 import sys
 import yaml
-from subprocess import run
+import os
+from os.path import join, getsize, dirname
 from os import environ as env
+from subprocess import run
 
 from gitlabdata.orchestration_utils import (
     snowflake_engine_factory,
@@ -20,14 +22,16 @@ if __name__ == "__main__":
     snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
 
     command = (
-        "rm -rf gitlab && "
+        "cd /usr/local/ && "
         "mkdir -p gitlab && "
         "cd gitlab && "
-        "git sparse-checkout init --cone && "
-        "git sparse-checkout set config/feature_flags ee/config/feature_flags && "
+        "git init && "
         "git remote add origin https://gitlab.com/gitlab-org/gitlab.git && "
-        "git fetch origin --depth=1 --shallow && "
-        "git checkout master"
+        "git checkout -b master && "
+        "git config core.sparsecheckout true && "
+        "echo config/feature_flags/ >> .git/info/sparse-checkout && "
+        "echo ee/config/feature_flags/ >> .git/info/sparse-checkout && "
+        "git pull origin master"
     )
 
     logging.info("Cloning GitLab...")
@@ -39,15 +43,19 @@ if __name__ == "__main__":
 
     all_ffs = []
 
-    for flag_path in glob.glob("gitlab/*/config/feature_flags/**/*.yaml", recursive=True):
-        with open(flag_path) as file:
-            ff = yaml.load(file, Loader=yaml.SafeLoader)
-            if "name" in ff and "type" in ff:
-                all_ffs.append(ff)
+    for root, dirs, files in os.walk("/usr/local/gitlab/"):
+        for yaml_file in files:
+            full_dir = os.path.join(root, yaml_file)
+            if re.search("yaml|yml", full_dir):
+                # print(full_dir)
+                with open(full_dir) as file:
+                    ff = yaml.load(file, Loader=yaml.SafeLoader)
+                    if "name" in ff and "type" in ff:
+                        all_ffs.append(ff)
 
     logging.info("Writing JSON...")
 
-    with open('all_feature_flags.json', 'w') as outfile:
+    with open("all_feature_flags.json", "w") as outfile:
         json.dump(all_ffs, outfile)
 
     snowflake_stage_load_copy_remove(
