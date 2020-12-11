dfb8c8fcd4f630026c6979695d70e9c00bdb8a19
mcooperDD <michelle.cooper@decisivedata.net>
2020-12-11 15:50:05 -0800

    add user hierarchy columns to dim_crm_sales_representative

--- transform/snowflake-dbt/models/staging/common/dim_crm_sales_representative.sql
+++ transform/snowflake-dbt/models/staging/common/dim_crm_sales_representative.sql
@@ -6,63 +6,34 @@
 WITH sfdc_users AS (
 
     SELECT *
-    FROM {{ ref('sfdc_users')}}
+    FROM {{ ref('sfdc_users_source')}}
 
 ), sfdc_user_roles AS (
 
     SELECT *
     FROM {{ ref('sfdc_user_roles')}}
 
-), joined AS (
+), final_users AS (
 
     SELECT
-
-      sfdc_users.user_id              AS crm_sales_rep_id,
+      sfdc_users.user_id              AS dim_crm_sales_rep_id,
       sfdc_users.name                 AS rep_name,
-      sfdc_users.title                AS title,
-      sfdc_users.department,          -- department
-      sfdc_users.team,                -- team
-      sfdc_users.manager_id,          -- manager_id
-      sfdc_users.is_active,           -- is_active
-      sfdc_users.start_date,          -- start_date
-      sfdc_users.user_role_id,        -- user_role_id
+      sfdc_users.title,
+      sfdc_users.department,
+      sfdc_users.team,
+      sfdc_users.manager_id,
+      sfdc_users.is_active,
+      sfdc_users.start_date,
+      sfdc_users.user_role_id,
       sfdc_user_roles.name            AS user_role_name,
-      sfdc_user_roles.parentroleid    AS parent_role_id
-
+      sfdc_users.user_segment,
+      sfdc_users.user_geo,
+      sfdc_users.user_region,
+      sfdc_users.user_area
     FROM sfdc_users
     LEFT JOIN sfdc_user_roles
       ON sfdc_users.user_role_id = sfdc_user_roles.id
 
-), sales_rep_hierarchy AS (
-
-    SELECT
-      id,
-      sys_connect_by_path(name, ' :: ')   AS path,
-      level
-    FROM sfdc_user_roles
-    START WITH name = 'CRO'
-    CONNECT BY PRIOR id = parentroleid
-
-), final_users AS (
-
-    SELECT
-      crm_sales_rep_id,
-      rep_name,
-      title,
-      department,
-      team,
-      manager_id,
-      is_active,
-      start_date,
-      user_role_name,
-      SPLIT_PART(sales_rep_hierarchy.path, '::', 2)::VARCHAR AS parent_role_1,
-      SPLIT_PART(sales_rep_hierarchy.path, '::', 3)::VARCHAR AS parent_role_2,
-      SPLIT_PART(sales_rep_hierarchy.path, '::', 4)::VARCHAR AS parent_role_3,
-      SPLIT_PART(sales_rep_hierarchy.path, '::', 5)::VARCHAR AS parent_role_4,
-      SPLIT_PART(sales_rep_hierarchy.path, '::', 6)::VARCHAR AS parent_role_5
-    FROM joined
-    LEFT JOIN sales_rep_hierarchy
-      ON joined.user_role_id = sales_rep_hierarchy.id
 )
 
 {{ dbt_audit(
