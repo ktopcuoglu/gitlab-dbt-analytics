cd3f0b2fec6844d5422ec36dceb2bb79eed81ac6
Michael Walker <mwalker@gitlab.com>
2020-12-14 11:12:25 -0600

    move version db scripts, make more generic

--- extract/version_db/do_create_tables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-declare TABLES_TO_EXPORT=("conversational_development_indices" "fortune_companies" "hosts" "licences" "raw_usage_data" "usage_data" "users" "version_checks" "versions");
-for table in "${TABLES_TO_EXPORT[@]}"
-do
-	.  ./sql/$table.sql.sh
-	export table_name=$table
-	python3 /home/mwalker/repos/analytics/extract/version_db/parse_bash_sql.py
-done
