ee0f5f2b18ed46cc03fd5f7b6c41cf16f2193577
Michael Walker <mwalker@gitlab.com>
2020-12-16 16:14:09 -0600

    scan less columns at a time for social security detector

--- transform/snowflake-dbt/models/sources/datasiren/datasiren_source_db_social_security_number_sensor.sql
+++ transform/snowflake-dbt/models/sources/datasiren/datasiren_source_db_social_security_number_sensor.sql
@@ -3,4 +3,4 @@
     })
 }}
 
-{{ datasiren.search_sample_columns_samples_for_pattern(this.identifier, env_var('SNOWFLAKE_LOAD_DATABASE'), 'TEXT', 25, 25, '.*\\d\\d\\d-\\d\\d-\\d\\d\\d\\d.*') }}
+{{ datasiren.search_sample_columns_samples_for_pattern(this.identifier, env_var('SNOWFLAKE_LOAD_DATABASE'), 'TEXT', '100 rows', '25', '.*\\d\\d\\d-\\d\\d-\\d\\d\\d\\d.*') }}
