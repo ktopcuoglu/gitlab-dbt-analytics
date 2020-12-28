0ac6fe54192df0ab2a79a8752d87f26e4e23fbe6
Parul Luthra <pluthra@gitlab.com>
2020-12-28 16:34:07 -0500

    adding fct_employee_work_email model

---
+++ transform/snowflake-dbt/models/legacy/bamboohr/xf/fct_employee_work_email.sql
@@ -0,0 +1,31 @@
+WITH bamboohr_directory AS (
+
+    SELECT *
+    FROM {{ ref ('bamboohr_directory_source') }}
+
+), intermediate AS (
+
+    SELECT
+      employee_id,
+      work_email,
+      DATE_TRUNC(day, uploaded_at) AS uploaded_at
+    FROM bamboohr_directory
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY employee_id, work_email ORDER BY uploaded_at) = 1       
+
+), final AS (
+
+    SELECT
+      employee_id,
+      full_name,
+      work_email,
+      uploaded_at                                                                     AS valid_from_date,
+      IFF(max_uploaded_date< CURRENT_DATE(), 
+        max_uploaded_date, 
+        COALESCE(LEAD(DATEADD(day,-1,uploaded_at)) OVER (PARTITION BY employee_id ORDER BY uploaded_at),
+                {{max_date_in_bamboo_analyses()}}))         AS valid_to_date,
+      DENSE_RANK() OVER (PARTITION BY employee_id ORDER BY valid_from_date DESC)      AS rank_email_desc
+    FROM intermediate
+)
+
+SELECT *
+FROM final
