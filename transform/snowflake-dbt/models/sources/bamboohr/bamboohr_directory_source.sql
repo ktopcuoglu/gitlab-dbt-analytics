7fd343de58efce90f2a3e42d3be1e296ee18d09c
Parul Luthra <pluthra@gitlab.com>
2020-12-28 16:11:55 -0500

    updating bamboohr_directory_source to remove limit 1 record and add qualify rn = 1 to employee_directory

--- transform/snowflake-dbt/models/sources/bamboohr/bamboohr_directory_source.sql
+++ transform/snowflake-dbt/models/sources/bamboohr/bamboohr_directory_source.sql
@@ -1,28 +1,34 @@
 WITH source AS (
 
-			SELECT *
-	    FROM {{ source('bamboohr', 'directory') }}
-	    ORDER BY uploaded_at DESC
-	    LIMIT 1
-
+    SELECT *
+	FROM {{ source('bamboohr', 'directory') }}
+	
 ), intermediate AS (
 
-      SELECT d.value as data_by_row
-      FROM source,
-      LATERAL FLATTEN(INPUT => parse_json(jsontext), outer => true) d
+    SELECT 
+      d.value                                   AS data_by_row, 
+      uploaded_at
+    FROM source,
+    LATERAL FLATTEN(INPUT => parse_json(jsontext), outer => true) d
 
 ), renamed AS (
 
     SELECT
-        data_by_row['id']::NUMBER 						AS employee_id,
-				data_by_row['displayName']::varchar 	AS full_name,
-        data_by_row['jobTitle']::varchar 			AS job_title,
-				data_by_row['supervisor']::varchar 		AS supervisor,
-				data_by_row['workEmail']::varchar			AS work_email
+      data_by_row['id']::NUMBER 				AS employee_id,
+	  data_by_row['displayName']::varchar 	    AS full_name,
+      data_by_row['jobTitle']::varchar 			AS job_title,
+	  data_by_row['supervisor']::varchar 		AS supervisor,
+	  data_by_row['workEmail']::varchar			AS work_email,
+      uploaded_at
     FROM intermediate
+
+), final AS (
+
+    SELECT *
+    FROM renamed
+    WHERE work_email != 't2test@gitlab.com'
 
 )
 
 SELECT *
-FROM renamed
-WHERE work_email != 't2test@gitlab.com'
+FROM final
