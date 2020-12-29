c5e63cd9763537efe7b571dee1d0fd7a6e59b3c3
Kathleen Tam <ktam@gitlab.com>
2020-12-29 16:58:26 +0000

    Add in user_name column

--- transform/snowflake-dbt/models/common/dim_gitlab_ops_gitlab_emails.sql
+++ transform/snowflake-dbt/models/common/dim_gitlab_ops_gitlab_emails.sql
@@ -7,6 +7,7 @@ WITH gitlab_ops_users_xf AS (
 
     SELECT 
       user_id, 
+      user_name                                                AS gitlab_ops_user_name,
       notification_email, 
       SPLIT_PART(notification_email,'@', 0)                    AS email_handle, 
       COUNT(notification_email) OVER (PARTITION BY user_id)    AS number_of_emails 
@@ -21,7 +22,7 @@ WITH gitlab_ops_users_xf AS (
       AND email_handle NOT LIKE '%xxx%'   -- removes accounts with more than three xs
       AND email_handle NOT LIKE '%gitlab%'-- removes accounts that have the word gitlab
       AND email_handle NOT LIKE '%test%'  -- removes accounts with more than three xs
-    GROUP BY 1,2,3
+    GROUP BY 1,2,3,4
 
 )
 
