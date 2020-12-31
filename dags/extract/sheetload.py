9a4c49ea380ca9ca2e094150f4b70325aa2f2815
Michael Walker <mwalker@gitlab.com>
2020-12-31 10:56:00 -0600

    run dependencies of dependencies for sheetload dbt run

--- dags/extract/sheetload.py
+++ dags/extract/sheetload.py
@@ -114,7 +114,7 @@ for sheet in sheets:
 dbt_sheetload_cmd = f"""
     export snowflake_load_database="RAW" &&
     {dbt_install_deps_and_seed_nosha_cmd} &&
-    dbt run --profiles-dir profile --target prod --models source:sheetload+; ret=$?;
+    dbt run --profiles-dir profile --target prod --models @source:sheetload; ret=$?;
     python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
 """
 dbt_sheetload = KubernetesPodOperator(
