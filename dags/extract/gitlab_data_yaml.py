2ae496113818ff89ce845e2236d204f9b90bac4e
Taylor Murphy <tmurphy@gitlab.com>
2021-01-04 09:50:43 -0600

    Add token

--- dags/extract/gitlab_data_yaml.py
+++ dags/extract/gitlab_data_yaml.py
@@ -62,6 +62,7 @@ data_yaml_extract = KubernetesPodOperator(
         SNOWFLAKE_LOAD_USER,
         SNOWFLAKE_LOAD_WAREHOUSE,
         SNOWFLAKE_LOAD_PASSWORD,
+        GITLAB_ANALYTICS_PRIVATE_TOKEN,
     ],
     affinity=get_affinity(False),
     tolerations=get_toleration(False),
