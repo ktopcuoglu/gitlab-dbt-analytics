c875c36b998dcebfff59fd96493291e2dd6c59f2
Parul Luthra <pluthra@gitlab.com>
2021-01-06 20:19:25 -0500

    update greenhouse models that were using bamboohr_id_employee_mapping_source

--- transform/snowflake-dbt/models/legacy/greenhouse/xf/greenhouse_hires.sql
+++ transform/snowflake-dbt/models/legacy/greenhouse/xf/greenhouse_hires.sql
@@ -26,7 +26,7 @@ WITH applications AS (
  ), bamboohr_mapping AS (
  
     SELECT *
-    FROM {{ ref ('bamboohr_id_employee_number_mapping_source') }}
+    FROM {{ ref ('bamboohr_id_employee_number_mapping') }}
    
 ), bamboo_hires AS (
 
