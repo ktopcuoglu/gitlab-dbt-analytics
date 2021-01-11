3324e3f61473d08c0c8b4974e5e9d507ab2a193e
Parul Luthra <pluthra@gitlab.com>
2021-01-11 14:32:51 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_user_custom_attributes_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_user_custom_attributes_source.sql
@@ -7,7 +7,7 @@
 WITH source AS (
 
   SELECT *
-  FROM {{ source('gitlab_com') }}
+FROM {{ source('gitlab_db_user_custom_attributes') }}
   WHERE created_at IS NOT NULL
   QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
   
