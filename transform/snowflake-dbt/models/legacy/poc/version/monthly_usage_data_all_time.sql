0c504a0cfd7e0a081c85e70b666eabe47ec24df0
mpeychet <mpeychet@gitlab.com>
2021-01-11 18:47:09 +0100

    remove one column

--- transform/snowflake-dbt/models/legacy/poc/version/monthly_usage_data_all_time.sql
+++ transform/snowflake-dbt/models/legacy/poc/version/monthly_usage_data_all_time.sql
@@ -41,7 +41,6 @@ WITH data AS (
 
 SELECT
   {{ dbt_utils.surrogate_key(['instance_id', 'host_id', 'created_month', 'metrics_path']) }} AS primary_key,
-  ping_id,
   instance_id,
   host_id,
   created_month,
