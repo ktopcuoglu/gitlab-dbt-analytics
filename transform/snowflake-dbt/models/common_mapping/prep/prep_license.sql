0e8c399b94424873646e60cbb9cd7a5553162461
Sushma  Nalamaru <snalamaru@gitlab.com>
2021-01-11 15:30:19 +0000

    Remove Alias names for tables in renamed CTE

--- transform/snowflake-dbt/models/common_mapping/prep/prep_license.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_license.sql
@@ -16,33 +16,33 @@ WITH licenses AS (
 ), renamed AS (
 
     SELECT
-      license_id                      AS dim_license_id,
+      license_id                                                       AS dim_license_id,
       license_md5,
-      zuora_subscription_id           AS dim_subscription_id,
-      zuora_subscription_name         AS subscription_name,
-      i.original_id                   AS dim_subscription_id_original,
-      i.previous_subscription_id      AS dim_subscription_id_previous,
-      users_count                     AS license_user_count,
-      plan_code                       AS license_plan,
-      t.dim_product_tier_id,
+      zuora_subscription_id                                            AS dim_subscription_id,
+      zuora_subscription_name                                          AS subscription_name,
+      original_subscription.original_id                                AS dim_subscription_id_original,
+      original_subscription.previous_subscription_id                   AS dim_subscription_id_previous,
+      users_count                                                      AS license_user_count,
+      plan_code                                                        AS license_plan,
+      tiers.dim_product_tier_id,
       is_trial,
       IFF(
           LOWER(email) LIKE '%@gitlab.com' AND LOWER(company) LIKE '%gitlab%',
           TRUE, FALSE
-         )                            AS is_internal,
-      company                         AS company,
-      starts_at::DATE                 AS license_start_date,
-      license_expires_at::DATE        AS license_expire_date,
+         )                                                             AS is_internal,
+      company                                                          AS company,
+      starts_at::DATE                                                  AS license_start_date,
+      license_expires_at::DATE                                         AS license_expire_date,
       created_at,
       updated_at
 
-    FROM licenses l
-    LEFT JOIN tiers t 
-       ON lower(t.product_tier_name) = (CASE WHEN l.plan_code IS NULL 
-                                          OR l.plan_code = '' THEN 'core' 
-                                        ELSE l.plan_code END)
-    LEFT JOIN original_subscription i 
-       ON l.zuora_subscription_id = i.subscription_id 
+    FROM licenses
+    LEFT JOIN tiers
+       ON lower(tiers.product_tier_name) = (CASE WHEN licenses.plan_code IS NULL 
+                                             OR licenses.plan_code = '' THEN 'core' 
+                                            ELSE licenses.plan_code END)
+    LEFT JOIN original_subscription
+       ON licenses.zuora_subscription_id = original_subscription.subscription_id 
 
 )
 
