11c5fad70922fa2be89a0fdfe12c97ae090d4cc5
Parul Luthra <pluthra@gitlab.com>
2021-01-12 15:23:08 +0000

    adding source model

---
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_user_custom_attributes.sql
@@ -0,0 +1,34 @@
+{{ config({
+    "materialized": "incremental",
+    "unique_key": "user_id"
+    })
+}}
+
+WITH source AS (
+
+  SELECT *
+  FROM {{ source('gitlab_com','user_custom_attributes') }}
+  WHERE created_at IS NOT NULL
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+  
+    {% if is_incremental() %}
+
+    AND updated_at >= (SELECT MAX(updated_at) FROM {{this}})
+
+    {% endif %}
+
+), renamed AS (
+  
+  SELECT
+    user_id::NUMBER       AS user_id,
+    created_at::TIMESTAMP AS created_at,
+    updated_at::TIMESTAMP AS updated_at,
+    key::VARCHAR          AS user_custom_key,
+    value::VARCHAR        AS user_custom_value,
+  FROM source
+
+)
+
+SELECT *
+FROM renamed
+ORDER BY updated_at
