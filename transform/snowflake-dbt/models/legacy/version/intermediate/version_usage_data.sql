d6ba12fd82a8b255ae969c7235e35411dc25b2e6
mpeychet <mpeychet@gitlab.com>
2021-01-13 12:41:52 +0100

    add is_trial field

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_data.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_data.sql
@@ -57,6 +57,8 @@ WITH source AS (
       major_version,
       minor_version,
       major_minor_version,
+      -- license_trial_ends_on is only in raw payload
+      AS_DATE(raw_usage_data.raw_usage_data_payload:license_trial_ends_on)                  AS license_trial_ends_on,
       COALESCE(raw_usage_data.raw_usage_data_payload, raw_usage_data_payload_reconstructed) AS raw_usage_data_payload
     FROM usage_data
     LEFT JOIN raw_usage_data
