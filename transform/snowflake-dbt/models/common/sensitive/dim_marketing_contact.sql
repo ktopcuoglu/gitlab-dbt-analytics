81e17ba648f84fa6932a1c1a7900f5cdb6d063f8
rmistry <rheanna.mistry@decisivedata.net>
2021-01-20 08:39:22 -0800

    formatting updates to dim_marketing_contact

--- transform/snowflake-dbt/models/common/sensitive/dim_marketing_contact.sql
+++ transform/snowflake-dbt/models/common/sensitive/dim_marketing_contact.sql
@@ -38,10 +38,10 @@ WITH sfdc_lead AS (
 ), sfdc AS (
     SELECT
       sfdc_lead.lead_id,
-      CASE WHEN crm_person.sfdc_record_type = 'contact' THEN sfdc_contact.contact_email ELSE sfdc_lead.lead_email END AS "email_address",
+      CASE WHEN crm_person.sfdc_record_type = 'contact' THEN sfdc_contact.contact_email ELSE sfdc_lead.lead_email END        AS "email_address",
       crm_person.dim_crm_person_id AS "crm_person_id",
       crm_person.sfdc_record_type AS "sfdc_lead_contact",
-      CASE WHEN "sfdc_lead_contact" = 'contact' THEN sfdc_contact.contact_first_name ELSE	sfdc_lead.lead_first_name END AS "first_name",
+      CASE WHEN "sfdc_lead_contact" = 'contact' THEN sfdc_contact.contact_first_name ELSE	sfdc_lead.lead_first_name END    AS "first_name",
       CASE WHEN "sfdc_lead_contact" = 'contact' AND sfdc_contact.contact_last_name  = '[[unknown]]' THEN NULL
            WHEN "sfdc_lead_contact" = 'contact' AND sfdc_contact.contact_last_name  <> '[[unknown]]' THEN sfdc_contact.contact_last_name
            WHEN "sfdc_lead_contact" = 'lead' AND sfdc_lead.lead_last_name = '[[unknown]]' THEN NULL
@@ -49,13 +49,13 @@ WITH sfdc_lead AS (
       CASE WHEN "sfdc_lead_contact" = 'contact' AND sfdc_account.account_name = '[[unknown]]' THEN NULL
            WHEN "sfdc_lead_contact" = 'contact' AND sfdc_account.account_name <> '[[unknown]]' THEN sfdc_account.account_name
            WHEN "sfdc_lead_contact" = 'lead' AND sfdc_lead.company =  '[[unknown]]' THEN NULL
-           WHEN "sfdc_lead_contact" = 'lead' AND sfdc_lead.company <>  '[[unknown]]' THEN sfdc_lead.company END AS "company_name",
-      crm_person.title AS "job_title",
-      sales_segment.sales_segment_name AS "sales_segment",
-      CASE WHEN "sfdc_lead_contact" = 'contact' THEN sfdc_contact.mailing_country ELSE sfdc_lead.country END AS "country",
-      CASE WHEN "sfdc_lead_contact" = 'contact' THEN sfdc_contact.created_date ELSE sfdc_lead.created_date  END AS "created_date",
-      crm_person.has_opted_out_email AS "opted_out_salesforce",
-      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC)) AS "record_number"
+           WHEN "sfdc_lead_contact" = 'lead' AND sfdc_lead.company <>  '[[unknown]]' THEN sfdc_lead.company END              AS "company_name",
+      crm_person.title                                                                                                       AS "job_title",
+      sales_segment.sales_segment_name                                                                                       AS "sales_segment",
+      CASE WHEN "sfdc_lead_contact" = 'contact' THEN sfdc_contact.mailing_country ELSE sfdc_lead.country END                 AS "country",
+      CASE WHEN "sfdc_lead_contact" = 'contact' THEN sfdc_contact.created_date ELSE sfdc_lead.created_date END               AS "created_date",
+      crm_person.has_opted_out_email                                                                                         AS "opted_out_salesforce",
+      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC))                                        AS "record_number"
 
     FROM crm_person
     LEFT JOIN sfdc_contact 
@@ -74,19 +74,19 @@ WITH sfdc_lead AS (
 
 ), gitlab_dotcom AS (
     SELECT
-      notification_email AS "email_address",
-      user_id AS "user_id",
-      SPLIT_PART(users_name,' ',1) AS "first_name",
-      SPLIT_PART(users_name,' ',2) AS "last_name",
-      user_name AS "user_name",
-      organization AS "company_name",
-      role AS "job_title",
-      created_at AS "created_date",
-      confirmed_at AS "confirmed_date",
-      state AS "active_state",
-      last_sign_in_at AS "last_login_date",
-      is_email_opted_in AS "email_opted_in",
-      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC)) AS "record_number"
+      notification_email                                                                                                    AS "email_address",
+      user_id                                                                                                               AS "user_id",
+      SPLIT_PART(users_name,' ',1)                                                                                          AS "first_name",
+      SPLIT_PART(users_name,' ',2)                                                                                          AS "last_name",
+      user_name                                                                                                             AS "user_name",
+      organization                                                                                                          AS "company_name",
+      role                                                                                                                  AS "job_title",
+      created_at                                                                                                            AS "created_date",
+      confirmed_at                                                                                                          AS "confirmed_date",
+      state                                                                                                                 AS "active_state",
+      last_sign_in_at                                                                                                       AS "last_login_date",
+      is_email_opted_in                                                                                                     AS "email_opted_in",
+      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC))                                       AS "record_number"
     FROM gitlab_users
     WHERE "email_address" IS NOT NULL 
       AND "email_address" <> ''
@@ -94,17 +94,17 @@ WITH sfdc_lead AS (
     
 ), customer_db AS (
     SELECT
-      customer_email AS "email_address",
-      customer_id AS "customer_id",
-      customer_first_name AS "first_name",
-      customer_last_name AS "last_name",
-      company AS "company_name",
-      country AS "country",
-      customer_created_at AS "created_date",
-      confirmed_at AS "confirmed_date",
-      company_size AS "market_segment",
-      last_sign_in_at AS "last_login_date",
-      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC)) AS "record_number"
+      customer_email                                                                                                        AS "email_address",
+      customer_id                                                                                                           AS "customer_id",
+      customer_first_name                                                                                                   AS "first_name",
+      customer_last_name                                                                                                    AS "last_name",
+      company                                                                                                               AS "company_name",
+      country                                                                                                               AS "country",
+      customer_created_at                                                                                                   AS "created_date",
+      confirmed_at                                                                                                          AS "confirmed_date",
+      company_size                                                                                                          AS "market_segment",
+      last_sign_in_at                                                                                                       AS "last_login_date",
+      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC))                                       AS "record_number"
     FROM customer_db_source
     WHERE "email_address" IS NOT NULL 
       AND "email_address" <> ''
@@ -113,15 +113,15 @@ WITH sfdc_lead AS (
 ),
     zuora AS (
     SELECT
-      contact_source.work_email AS "email_address",
-      contact_source.contact_id AS "contact_id",
-      contact_source.first_name AS "first_name",
-      contact_source.last_name AS "last_name",
-      zuora_account_source.account_name AS "company_name",
-      contact_source.country AS "country",
-      contact_source.created_date AS "created_date",
-      CASE WHEN contact_source.is_deleted = TRUE THEN 'Inactive' ELSE 'Active' END AS "active_state",
-      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC)) AS "record_number"
+      contact_source.work_email                                                                                             AS "email_address",
+      contact_source.contact_id                                                                                             AS "contact_id",
+      contact_source.first_name                                                                                             AS "first_name",
+      contact_source.last_name                                                                                              AS "last_name",
+      zuora_account_source.account_name                                                                                     AS "company_name",
+      contact_source.country                                                                                                AS "country",
+      contact_source.created_date                                                                                           AS "created_date",
+      CASE WHEN contact_source.is_deleted = TRUE THEN 'Inactive' ELSE 'Active' END                                          AS "active_state",
+      (ROW_NUMBER() OVER (PARTITION BY "email_address" ORDER BY "created_date" DESC))                                       AS "record_number"
     FROM prep.zuora.zuora_contact_source contact_source
     JOIN zuora_account_source 
       ON zuora_account_source.account_id = contact_source.account_id
@@ -151,32 +151,32 @@ WITH sfdc_lead AS (
 
     SELECT
       emails."email_address",
-      COALESCE(zuora."first_name", sfdc."first_name", customer_db."first_name", gitlab_dotcom."first_name") AS "first_name",
-      COALESCE(zuora."last_name", sfdc."last_name", customer_db."last_name", gitlab_dotcom."last_name") AS "last_name",
-      gitlab_dotcom."user_name" AS "gitlab_user_name",
-      COALESCE(zuora."company_name",  sfdc."company_name", customer_db."company_name", gitlab_dotcom."company_name") AS "company_name",
-      COALESCE(sfdc."job_title", gitlab_dotcom."job_title") AS "job_title",
-      COALESCE(zuora."country", sfdc."country", customer_db."country") AS "country",
-      sfdc."sales_segment" AS "sfdc_parent_sales_segment",
-      CASE WHEN sfdc."email_address" IS NOT NULL THEN TRUE ELSE FALSE END AS "is_sfdc_lead_contact",
+      COALESCE(zuora."first_name", sfdc."first_name", customer_db."first_name", gitlab_dotcom."first_name")                AS "first_name",
+      COALESCE(zuora."last_name", sfdc."last_name", customer_db."last_name", gitlab_dotcom."last_name")                    AS "last_name",
+      gitlab_dotcom."user_name"                                                                                            AS "gitlab_user_name",
+      COALESCE(zuora."company_name",  sfdc."company_name", customer_db."company_name", gitlab_dotcom."company_name")       AS "company_name",
+      COALESCE(sfdc."job_title", gitlab_dotcom."job_title")                                                                AS "job_title",
+      COALESCE(zuora."country", sfdc."country", customer_db."country")                                                     AS "country",
+      sfdc."sales_segment"                                                                                                 AS "sfdc_parent_sales_segment",
+      CASE WHEN sfdc."email_address" IS NOT NULL THEN TRUE ELSE FALSE END                                                  AS "is_sfdc_lead_contact",
       sfdc."sfdc_lead_contact",
-      sfdc."created_date" AS "sfdc_created_date",
-      sfdc."opted_out_salesforce" AS "is_sfdc_opted_out",
-      CASE WHEN gitlab_dotcom."email_address" IS NOT NULL THEN TRUE ELSE FALSE END AS "is_gitlab.com_user",
-      gitlab_dotcom."user_id" AS "gitlab.com_user_id",
-      gitlab_dotcom."created_date" AS "gitlab.com_created_date",
-      gitlab_dotcom."confirmed_date" AS "gitlab.com_confirmed_date",
-      gitlab_dotcom."active_state" AS "gitlab.com_active_state",
-      gitlab_dotcom."last_login_date" AS "gitlab.com_last_login_date",
-      gitlab_dotcom."email_opted_in" AS "gitlab.com_email_opted_in",
-      CASE WHEN customer_db."email_address" IS NOT NULL THEN TRUE ELSE FALSE END AS "is_customer_db_user",
-      customer_db."customer_id" AS "customer_db_customer_id",
-      customer_db."created_date" AS "customer_db_created_date",
-      customer_db."confirmed_date" AS "customer_db_confirmed_date",
-      CASE WHEN zuora."email_address" IS NOT NULL THEN TRUE ELSE FALSE END AS "is_zuora_billing_contact",
-      zuora."contact_id" AS "zuora_contact_id",
-      zuora."created_date" AS "zuora_created_date",
-      zuora."active_state" AS "zuora_active_state"
+      sfdc."created_date"                                                                                                  AS "sfdc_created_date",
+      sfdc."opted_out_salesforce"                                                                                          AS "is_sfdc_opted_out",
+      CASE WHEN gitlab_dotcom."email_address" IS NOT NULL THEN TRUE ELSE FALSE END                                         AS "is_gitlab.com_user",
+      gitlab_dotcom."user_id"                                                                                              AS "gitlab.com_user_id",
+      gitlab_dotcom."created_date"                                                                                         AS "gitlab.com_created_date",
+      gitlab_dotcom."confirmed_date"                                                                                       AS "gitlab.com_confirmed_date",
+      gitlab_dotcom."active_state"                                                                                         AS "gitlab.com_active_state",
+      gitlab_dotcom."last_login_date"                                                                                      AS "gitlab.com_last_login_date",
+      gitlab_dotcom."email_opted_in"                                                                                       AS "gitlab.com_email_opted_in",
+      CASE WHEN customer_db."email_address" IS NOT NULL THEN TRUE ELSE FALSE END                                           AS "is_customer_db_user",
+      customer_db."customer_id"                                                                                            AS "customer_db_customer_id",
+      customer_db."created_date"                                                                                           AS "customer_db_created_date",
+      customer_db."confirmed_date"                                                                                         AS "customer_db_confirmed_date",
+      CASE WHEN zuora."email_address" IS NOT NULL THEN TRUE ELSE FALSE END                                                 AS "is_zuora_billing_contact",
+      zuora."contact_id"                                                                                                   AS "zuora_contact_id",
+      zuora."created_date"                                                                                                 AS "zuora_created_date",
+      zuora."active_state"                                                                                                 AS "zuora_active_state"
 
     FROM emails
     LEFT JOIN sfdc 
