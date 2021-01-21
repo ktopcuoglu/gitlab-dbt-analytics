11452e87e78b78a129d077611771b94cae7c7c2c
Jean Del Rosario <jpeguero@gitlab.com>
2021-01-21 12:03:53 -0400

    add lacking comma to dim_crm_opp

--- transform/snowflake-dbt/models/common/dim_crm_opportunity.sql
+++ transform/snowflake-dbt/models/common/dim_crm_opportunity.sql
@@ -67,7 +67,7 @@ WITH sfdc_opportunity AS (
       sfdc_opportunity.opportunity_health,
       sfdc_opportunity.risk_type,
       sfdc_opportunity.risk_reasons,
-      sfdc_opportunity.tam_notes
+      sfdc_opportunity.tam_notes,
 
       -- ************************************
       -- channel reporting
