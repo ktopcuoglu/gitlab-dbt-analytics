1f5e2a9a9f44af31b54aedb7ceb0a43441f16cdd
Parul Luthra <pluthra@gitlab.com>
2021-01-26 10:49:35 -0500

    updating greenhouse hires to use employee_directory to eliminate multiple ctes, and accounting for true hire as appears in employee_directory_int with employment status

--- transform/snowflake-dbt/models/legacy/greenhouse/xf/greenhouse_hires.sql
+++ transform/snowflake-dbt/models/legacy/greenhouse/xf/greenhouse_hires.sql
@@ -21,57 +21,32 @@ WITH applications AS (
     SELECT *
     FROM  {{ ref ('greenhouse_opening_custom_fields') }}
 
-), initial_hire_date AS (
- 
-    SELECT *
-    FROM {{ ref ('bamboohr_employment_status_xf') }}
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY employee_id ORDER BY valid_from_date) = 1
-
-), rehire_date AS (
- 
-    SELECT *
-    FROM {{ ref ('bamboohr_employment_status_xf') }}
-    WHERE is_rehire = 'True'
-
- ), bamboohr_mapping AS (
- 
-    SELECT *
-    FROM {{ ref ('bamboohr_id_employee_number_mapping') }}
-   
 ), bamboo_hires AS (
-
-    SELECT 
-      bamboohr_mapping.employee_id,
-      bamboohr_mapping.greenhouse_candidate_id,
-      CONCAT(bamboohr_mapping.first_name, ' ', bamboohr_mapping.last_name)      AS employee_name,
-      COALESCE(initial_hire_date.valid_from_date, bamboohr_mapping.hire_date)   AS initial_hire_date,
-      bamboohr_mapping.region,
-      rehire_date.valid_from_date                                               AS rehire_date
-    FROM bamboohr_mapping
-    LEFT JOIN initial_hire_date 
-      ON initial_hire_date.employee_id = bamboohr_mapping.employee_id
-    LEFT JOIN rehire_date 
-      ON rehire_date.employee_id = bamboohr_mapping.employee_id
-
+ 
+    SELECT *
+    FROM "PLUTHRA_PREP"."SENSITIVE"."EMPLOYEE_DIRECTORY"
+   
 ), division_department AS (
 
     SELECT *
-    FROM {{ ref ('employee_directory_intermediate') }}
+    FROM "PLUTHRA_PREP"."SENSITIVE"."EMPLOYEE_DIRECTORY_INTERMEDIATE"   
     
 ), joined AS (
 
     SELECT 
+      openings.job_id,
       applications.application_id,  
       applications.candidate_id, 
       bamboo_hires.employee_id,
-      bamboo_hires.employee_name,
+      bamboo_hires.full_name AS employee_name,
       offers.start_date                                             AS candidate_target_hire_date, 
       applications.applied_at, 
-      bamboo_hires.region,
       applications.greenhouse_candidate_row_number,
       IFF(applications.greenhouse_candidate_row_number = 1 
-            AND applied_at < initial_hire_date, 
-              initial_hire_date, candidate_target_hire_date)        AS hire_date_mod,
+            AND applied_at < bamboo_hires.hire_date, 
+              bamboo_hires.hire_date, candidate_target_hire_date)        AS hire_date_mod,
+      is_hire_date,
+      is_rehire_date,
       CASE WHEN greenhouse_candidate_row_number = 1 
             THEN 'hire'
            WHEN offers.start_date = bamboo_hires.rehire_date
@@ -81,7 +56,8 @@ WITH applications AS (
            ELSE NULL END                                            AS hire_type,
       greenhouse_opening_custom_fields.job_opening_type,
       division_department.division,
-      division_department.department
+      division_department.department,
+      division_department.employment_status
     FROM applications
     LEFT JOIN offers
       ON offers.application_id = applications.application_id
@@ -94,23 +70,24 @@ WITH applications AS (
     LEFT JOIN division_department
       ON division_department.employee_id = bamboo_hires.employee_id
       AND division_department.date_actual =  IFF(applications.greenhouse_candidate_row_number = 1 
-            AND applied_at < initial_hire_date, 
-              initial_hire_date, offers.start_date)
+            AND applied_at < bamboo_hires.hire_date, 
+              bamboo_hires.hire_date, offers.start_date)
 
 ), final AS (    
 
     SELECT 
-      {{ dbt_utils.surrogate_key(['application_id', 'candidate_id',]) }}  AS unique_key,
+      job_id,
       application_id,
       candidate_id,
       employee_id,
       employee_name,
-      region,
       greenhouse_candidate_row_number,
       hire_date_mod,
       hire_type,
       job_opening_type,
-      IFF(employee_id IS NOT NULL,TRUE,FALSE)                             AS hired_in_bamboohr
+      IFF(employment_status IS NOT NULL ,TRUE,FALSE)                             AS hired_in_bamboohr,
+      division,
+      department
     FROM joined 
 
 )
