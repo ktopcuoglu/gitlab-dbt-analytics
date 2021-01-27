6f0036b7d6f7036ab328dc83f0c60fbbef1b6392
Michael Walker <mwalker@gitlab.com>
2021-01-27 15:18:32 -0600

    fix datasiren names

--- transform/snowflake-dbt/models/sources/datasiren/email_sensors/prod_legacy_email_value_sensor.sql
+++
@@ -1,1 +0,0 @@
-{{ datasiren.search_sample_columns_samples_for_pattern(this.identifier, env_var('SNOWFLAKE_PROD_DATABASE'), 'TEXT', '100', '100', '^\w+@{1}[a-zA-Z_]+?\.{1}[a-zA-Z]{2,3}$', 'legacy') }}
