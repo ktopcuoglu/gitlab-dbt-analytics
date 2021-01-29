f02229e784b78defa10aefb40e078a8c271e6c8e
Melia Vilain <mvilain@gitlab.com>
2021-01-29 04:11:49 +0000

    Type can only be "new" if it's row number 1. Otherwise, it's expansion.

--- transform/snowflake-dbt/macros/marts/arr/type_of_arr_change.sql
+++ transform/snowflake-dbt/macros/marts/arr/type_of_arr_change.sql
@@ -1,13 +1,13 @@
 {%- macro type_of_arr_change(arr, previous_arr) -%}
 
    CASE
-     WHEN {{ previous_arr }} = 0 AND {{ arr }} > 0
+     WHEN {{ row_number }} = 1
        THEN 'New'
      WHEN {{ arr }} = 0 AND {{ previous_arr }} > 0
        THEN 'Churn'
      WHEN {{ arr }} < {{ previous_arr }} AND {{ arr }} > 0
        THEN 'Contraction'
-     WHEN {{ arr }} > {{ previous_arr }}
+     WHEN {{ arr }} > {{ previous_arr }} AND {{ row_number }} > 1
        THEN 'Expansion'
      WHEN {{ arr }} = {{ previous_arr }}
        THEN 'No Impact'
