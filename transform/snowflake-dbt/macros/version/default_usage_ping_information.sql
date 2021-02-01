8e545f718eff47107dbf3417a086d966cb2bff27
Kathleen Tam <ktam@gitlab.com>
2021-02-01 16:54:33 +0000

    Update from dot notation to bracket notation for 30 metrics

--- transform/snowflake-dbt/macros/version/default_usage_ping_information.sql
+++ transform/snowflake-dbt/macros/version/default_usage_ping_information.sql
@@ -21,14 +21,14 @@
     edition, 
     main_edition, 
     raw_usage_data_payload['hostname']::VARCHAR                                                             AS hostname, 
-    raw_usage_data_payload['host_id']::VARCHAR                                                              AS host_id, 
+    raw_usage_data_payload['host_id']::NUMBER(38,0)                                                         AS host_id, 
     raw_usage_data_payload['installation_type']::VARCHAR                                                    AS installation_type, 
     is_internal, 
     is_staging,    
 
     -- instance user statistics 
-    raw_usage_data_payload['instance_user_count']::VARCHAR                                                  AS instance_user_count, 
-    raw_usage_data_payload['historical_max_users']::VARCHAR                                                 AS historical_max_users, 
+    raw_usage_data_payload['instance_user_count']::NUMBER(38,0)                                             AS instance_user_count, 
+    raw_usage_data_payload['historical_max_users']::NUMBER(38,0)                                            AS historical_max_users, 
     raw_usage_data_payload['license_md5']::VARCHAR                                                          AS license_md5,
 
     
