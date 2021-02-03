90c82db25413b19bcc4fae2fee7d66c4ae6f9cf3
Michael Walker <mwalker@gitlab.com>
2021-02-03 14:51:18 -0600

    swap analytics repo http reference out to use ssh

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -7,7 +7,7 @@ from typing import List
 from airflow.contrib.kubernetes.pod import Resources
 from airflow.operators.slack_operator import SlackAPIPostOperator
 
-REPO = "https://gitlab.com/gitlab-data/analytics.git"
+REPO = "git@gitlab.com:gitlab-data/analytics.git"
 DATA_IMAGE = "registry.gitlab.com/gitlab-data/data-image/data-image:v0.0.13"
 DBT_IMAGE = "registry.gitlab.com/gitlab-data/data-image/dbt-image:v0.0.13"
 PERMIFROST_IMAGE = "registry.gitlab.com/gitlab-data/permifrost:v0.8.0"
