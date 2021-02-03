c9e3a39847666c7d528dfc3debd29dc0910515e7
Michael Walker <mwalker@gitlab.com>
2021-02-03 19:05:09 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/sources/license/license_db_licenses_source.sql
+++ transform/snowflake-dbt/models/sources/license/license_db_licenses_source.sql
@@ -15,7 +15,7 @@ WITH source AS (
       CASE 
         WHEN expires_at IS NULL                               THEN NULL::TIMESTAMP
         WHEN SPLIT_PART(expires_at, '-', 1)::NUMBER > 9999    THEN '9999-12-30 00:00:00.000 +00'::TIMESTAMP
-        ELSE expires_at::TIMESTAMP END             AS expires_at,
+        ELSE expires_at::TIMESTAMP END             AS license_expires_at,
       plan_name::VARCHAR                           AS plan_name,
       starts_at::TIMESTAMP                         AS starts_at,
       NULLIF(zuora_subscription_name, '')::VARCHAR AS zuora_subscription_name,
