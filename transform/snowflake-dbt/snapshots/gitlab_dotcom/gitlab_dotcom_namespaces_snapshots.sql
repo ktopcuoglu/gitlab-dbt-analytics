177a3ad78cd92786a5a0de93ecfebb47ef7252e0
Taylor Murphy <tmurphy@gitlab.com>
2021-02-04 13:22:52 -0600

    Update gitlab.com snapshot definitions

--- transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_namespaces_snapshots.sql
+++ transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_namespaces_snapshots.sql
@@ -8,18 +8,8 @@
         )
     }}
     
-    WITH source as (
-
-    	SELECT 
-        *, 
-        ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) AS namespaces_rank_in_key
-      
-      FROM {{ source('gitlab_dotcom', 'namespaces') }}
-
-    )
-    
-    SELECT *
-    FROM source
-    WHERE namespaces_rank_in_key = 1
+    SELECT 
+    FROM {{ source('gitlab_dotcom', 'namespaces') }}
+    QUALIFY (ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1)
     
 {% endsnapshot %}
