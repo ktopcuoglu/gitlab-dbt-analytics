20d7f68efac66a2eac89f410bad35166f55edc13
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2021-02-04 21:49:08 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_namespaces_snapshots.sql
+++ transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_namespaces_snapshots.sql
@@ -8,7 +8,7 @@
         )
     }}
     
-    SELECT 
+    SELECT * 
     FROM {{ source('gitlab_dotcom', 'namespaces') }}
     QUALIFY (ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1)
     
