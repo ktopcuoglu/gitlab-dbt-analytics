d3c2a02cad94a1b37f83c2fe63b1dd5ad8785a32
mpeychet <mpeychet@gitlab.com>
2021-02-08 13:00:32 +0100

    change to upload

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_members_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_members_dedupe_source.sql
@@ -1,4 +1,4 @@
 
   SELECT *
   FROM {{ source('gitlab_dotcom', 'members') }}
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_user_preferences_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_user_preferences_dedupe_source.sql
@@ -1,4 +1,4 @@
 
   SELECT *
   FROM {{ source('gitlab_dotcom', 'user_preferences') }}
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY updated_at DESC) = 1
