cf545bde4720a82eb7447fffb06a605f0d019c31
Paul Armstrong <parmstrong@gitlab.com>
2021-02-09 21:52:16 +0200

    Format correction

--- transform/snowflake-dbt/models/common/dim_subscription.sql
+++ transform/snowflake-dbt/models/common/dim_subscription.sql
@@ -42,7 +42,8 @@ WITH subscription AS (
     subscription_lineage.cohort_quarter,
     subscription_lineage.cohort_year
   FROM subscription
-  LEFT JOIN subscription_lineage ON subscription_lineage.dim_subscription_id = subscription.dim_subscription_id
+  LEFT JOIN subscription_lineage
+    ON subscription_lineage.dim_subscription_id = subscription.dim_subscription_id
 
 )
 
