1d0ae0433250b0d785ade702bf08bb474d0bae8a
mpeychet <mpeychet@gitlab.com>
2021-02-09 12:02:52 +0100

    change this to this

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_members_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_members_dedupe_source.sql
@@ -9,8 +9,7 @@
   FROM {{ source('gitlab_dotcom', 'members') }}
   {% if is_incremental() %}
 
-  WHERE created_at >= (SELECT MAX(created_at) FROM {this})
+  WHERE created_at >= (SELECT MAX(created_at) FROM {{this}})
 
   {% endif %}
   QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1
-  
--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_namespaces_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_namespaces_dedupe_source.sql
@@ -9,7 +9,7 @@
   FROM {{ source('gitlab_dotcom', 'namespaces') }}
   {% if is_incremental() %}
 
-  WHERE updated_at >= (SELECT MAX(updated_at) FROM {this})
+  WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
 
   {% endif %}
   QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
