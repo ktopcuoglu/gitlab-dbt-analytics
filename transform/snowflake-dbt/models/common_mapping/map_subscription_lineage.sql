005de5abe7fb355704e6945c61c8f43a0b1c249c
Paul Armstrong <parmstrong@gitlab.com>
2021-02-11 20:14:56 +0200

    More formatting corrections, added dbt_audit values for the mapping table

--- transform/snowflake-dbt/models/common_mapping/map_subscription_lineage.sql
+++ transform/snowflake-dbt/models/common_mapping/map_subscription_lineage.sql
@@ -3,28 +3,38 @@ with zuora_subscription_intermediate as (
     SELECT *
     FROM {{ ref ('prep_subscription_lineage_intermediate')}}
 
-), zuora_subscription_lineage as (
+), zuora_subscription_lineage AS (
 
     SELECT *
     FROM {{ ref ('prep_subscription_lineage')}}
 
-), zuora_subscription_parentage as (
+), zuora_subscription_parentage AS (
 
     SELECT *
     FROM {{ ref ('prep_subscription_lineage_parentage_finish')}}
 
+), final AS (
+
+    SELECT  zuora_subscription_intermediate.subscription_id AS dim_subscription_id,
+            zuora_subscription_lineage.lineage,
+            coalesce(zuora_subscription_parentage.ultimate_parent_sub,zuora_subscription_intermediate.subscription_name_slugify) AS oldest_subscription_in_cohort,
+            coalesce(zuora_subscription_parentage.cohort_month, zuora_subscription_intermediate.subscription_month) AS cohort_month,
+            coalesce(zuora_subscription_parentage.cohort_quarter,zuora_subscription_intermediate.subscription_quarter) AS cohort_quarter,
+            coalesce(zuora_subscription_parentage.cohort_year, zuora_subscription_intermediate.subscription_year) AS cohort_year
+    FROM zuora_subscription_intermediate
+    LEFT JOIN zuora_subscription_lineage
+    ON zuora_subscription_intermediate.subscription_name_slugify =
+        zuora_subscription_lineage.subscription_name_slugify
+    LEFT JOIN zuora_subscription_parentage
+    ON zuora_subscription_intermediate.subscription_name_slugify =
+        zuora_subscription_parentage.child_sub
+
 )
 
-SELECT  zuora_subscription_intermediate.subscription_id as dim_subscription_id,
-        zuora_subscription_lineage.lineage,
-        coalesce(zuora_subscription_parentage.ultimate_parent_sub,zuora_subscription_intermediate.subscription_name_slugify) AS oldest_subscription_in_cohort,
-        coalesce(zuora_subscription_parentage.cohort_month, zuora_subscription_intermediate.subscription_month) as cohort_month,
-        coalesce(zuora_subscription_parentage.cohort_quarter,zuora_subscription_intermediate.subscription_quarter) as cohort_quarter,
-        coalesce(zuora_subscription_parentage.cohort_year, zuora_subscription_intermediate.subscription_year) as cohort_year
-FROM zuora_subscription_intermediate
-LEFT JOIN zuora_subscription_lineage
-ON zuora_subscription_intermediate.subscription_name_slugify =
-    zuora_subscription_lineage.subscription_name_slugify
-LEFT JOIN zuora_subscription_parentage
-ON zuora_subscription_intermediate.subscription_name_slugify =
-	zuora_subscription_parentage.child_sub
+{{ dbt_audit(
+    cte_ref="final",
+    created_by="@paul_armstrong",
+    updated_by="@paul_armstrong",
+    created_date="2021-02-11",
+    updated_date="2021-02-11"
+) }}
