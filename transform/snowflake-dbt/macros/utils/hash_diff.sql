6d85cc5acfaae523b8c38ca8ea2fdbdf4546e402
jjstark <jstark@gitlab.com>
2021-02-12 08:51:27 -0800

    add indentation

--- transform/snowflake-dbt/macros/utils/hash_diff.sql
+++ transform/snowflake-dbt/macros/utils/hash_diff.sql
@@ -1,42 +1,42 @@
 {% macro hash_diff(cte_ref, return_cte, columns) %}
 
-, hashing AS (
+  , hashing AS (
 
-  SELECT *,
-    
-    {{ dbt_utils.surrogate_key( columns ) }} as prev_hash
+    SELECT *,
   
-  FROM {{ cte_ref }}
+      {{ dbt_utils.surrogate_key( columns ) }} as prev_hash
 
-), {{ return_cte }} as (
- 
-  {%- set columns = adapter.get_columns_in_relation(this) -%}
-  
-  {%- set column_names = [] -%}
-  
-  {%- for column in columns -%}
-	
-    {%- set _ = column_names.append(column.name) -%}
-  
-  {% endfor %}
+    FROM {{ cte_ref }}
 
-    {% if 'LAST_CHANGED' in column_names %}
-	
-      SELECT hashing.*,
-        CASE
-          WHEN hashing.prev_hash = t.prev_hash THEN last_changed
-          ELSE CURRENT_TIMESTAMP()
-        END AS last_changed
-      FROM hashing
-      LEFT JOIN {{ this }} as t on t.prev_hash = hashing.prev_hash
+  ), {{ return_cte }} as (
 
-    {% else %}
+    {%- set columns = adapter.get_columns_in_relation(this) -%}
+
+    {%- set column_names = [] -%}
+  
+    {%- for column in columns -%}
+
+      {%- set _ = column_names.append(column.name) -%}
+
+    {% endfor %}
+
+      {% if 'LAST_CHANGED' in column_names %}
+
+        SELECT hashing.*,
+          CASE
+            WHEN hashing.prev_hash = t.prev_hash THEN last_changed
+            ELSE CURRENT_TIMESTAMP()
+          END AS last_changed
+        FROM hashing
+        LEFT JOIN {{ this }} as t on t.prev_hash = hashing.prev_hash
+
+      {% else %}
         
-      SELECT *,
-      CURRENT_TIMESTAMP() AS last_changed
-      FROM hashing
+        SELECT *,
+        CURRENT_TIMESTAMP() AS last_changed
+        FROM hashing
 
-    {% endif %}	
+      {% endif %}	
 
-)
+  )
 {% endmacro %}
