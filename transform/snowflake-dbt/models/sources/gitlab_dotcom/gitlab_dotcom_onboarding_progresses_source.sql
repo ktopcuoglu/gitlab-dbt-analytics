7bab2c3c6d2ead69acd8d89fb0b60fcb6d3b1515
Michael Walker <mwalker@gitlab.com>
2021-02-22 14:00:39 -0600

    dedupe onboarding progresses

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_onboarding_progresses_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_onboarding_progresses_source.sql
@@ -2,6 +2,7 @@ WITH source AS (
 
     SELECT *
     FROM {{ source('gitlab_dotcom', 'onboarding_progresses') }}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
 ), renamed AS (
 
