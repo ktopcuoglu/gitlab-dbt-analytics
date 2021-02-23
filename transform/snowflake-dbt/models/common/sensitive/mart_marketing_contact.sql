fceec560306704c28b227ff93f1f228b20051ffc
Trevor Romero <trevor@decisivedata.net>
2021-02-22 17:27:39 -0800

    adding "No Role" for contacts that have no role

--- transform/snowflake-dbt/models/common/sensitive/mart_marketing_contact.sql
+++ transform/snowflake-dbt/models/common/sensitive/mart_marketing_contact.sql
@@ -219,8 +219,8 @@ WITH marketing_contact AS (
         ELSE FALSE 
       END                                                                                        AS is_self_managed_ultimate_tier,
       ARRAY_AGG(
-                DISTINCT marketing_contact_order.marketing_contact_role || ': ' || 
-                  IFNULL(saas_product_tier,'') || IFNULL(self_managed_product_tier,'') 
+                DISTINCT IFNULL(marketing_contact_order.marketing_contact_role || ': ' || 
+                  IFNULL(marketing_contact_order.saas_product_tier,'') || IFNULL(marketing_contact_order.self_managed_product_tier,''), 'No Role') 
                )                                                                                 AS role_tier_text
     FROM marketing_contact
     LEFT JOIN  marketing_contact_order
