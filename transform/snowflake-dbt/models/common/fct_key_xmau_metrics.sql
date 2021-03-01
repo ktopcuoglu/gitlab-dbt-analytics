d41ef51508b9293ff08071ad39c803e2518a6dcb
Kathleen Tam <ktam@gitlab.com>
2021-03-01 22:21:27 +0000

    Add in a table that a macro can easily query for the key xmau metrics

---
+++ transform/snowflake-dbt/models/common/fct_key_xmau_metrics.sql
@@ -0,0 +1,35 @@
+{{ simple_cte([
+    ('sheetload_usage_ping_metrics_sections','usage_ping_metrics')
+]) }}
+
+, final AS (
+
+    SELECT 
+      section_name, 
+      stage_name, 
+      group_name, 
+      metrics_path,
+      'raw_usage_data_payload::' || REPLACE(metrics_path,'.','::') AS full_metrics_path,                              
+      clean_metrics_name, 
+      periscope_metrics_name, 
+      is_umau, 
+      is_smau, 
+      is_gmau, 
+      is_paid_gmau 
+      time_period 
+    FROM usage_ping_metrics
+    WHERE 
+      is_smau = TRUE OR 
+      is_gmau = TRUE or 
+      is_umau = TRUE OR 
+      is_paid_GMAU = TRUE 
+
+) 
+
+{{ dbt_audit(
+    cte_ref="final",
+    created_by="@kathleentam",
+    updated_by="@kathleentam",
+    created_date="2021-03-01",
+    updated_date="2021-03-01"
+) }}
