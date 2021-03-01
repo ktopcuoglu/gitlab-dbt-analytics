fcf72c3646bdc5556772bdfd752d3afc17defbc3
Kathleen Tam <ktam@gitlab.com>
2021-03-01 22:38:54 +0000

    Add a fact model that represents all the usage ping metric names

---
+++ transform/snowflake-dbt/models/common/fct_all_usage_ping_metric_names.sql
@@ -0,0 +1,45 @@
+{{ 
+    config({
+        "materialized": "incremental",
+        "unique_key": "full_metrics_path"
+    })
+}}
+
+WITH prep_usage_ping AS (
+  
+    SELECT * 
+    FROM {{ ref('prep_usage_ping') }}
+    
+), usage_ping_full_name AS (
+
+        SELECT DISTINCT 
+        TRIM(LOWER(f.path))                                                     AS metrics_path,
+        REPLACE(ping_name, '.','_')                                             AS metrics_path_column_name,
+        'raw_usage_data_payload::' || REPLACE(ping_name, '.','::')              AS full_metrics_path,
+        SPLIT_PART(ping_name, '.', 1)                                           AS main_json_name, 
+        SPLIT_PART(ping_name, '.', -1)                                          AS feature_name
+        FROM prep_usage_ping,
+        lateral flatten(input => prep_usage_ping.raw_usage_data_payload, recursive => True) f
+
+        {% if is_incremental() %}
+    
+            WHERE ping_created_at_date > (SELECT MAX(ping_created_at_date) FROM {{ this }})
+    
+        {% endif %}
+
+), final AS (
+
+    SELECT * 
+    FROM usage_ping_full_name
+    WHERE feature_name NOT IN ('source_ip')
+
+)
+
+{{ dbt_audit(
+    cte_ref="final",
+    created_by="@kathleentam",
+    updated_by="@kathleentam",
+    created_date="2021-03-01",
+    updated_date="2021-03-01"
+) }}
+
