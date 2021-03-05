bdb8bbf462e7117bde639119c70372d3b8afa80d
Parul Luthra <pluthra@gitlab.com>
2021-03-05 14:48:39 -0500

    adding model, updating roles.yml and schema.yml and updating bamboohr_rpt_headcount_aggregation to add in last month flag

--- transform/snowflake-dbt/models/legacy/bamboohr/sensitive/bamboohr_separations.sql
+++ transform/snowflake-dbt/models/legacy/bamboohr/sensitive/bamboohr_separations.sql
@@ -1,15 +1,22 @@
-WITH separations AS (
+WITH dim_date AS (
+
+    SELECT *
+    FROM {{ ref('dim_date') }}
+
+), separations AS (
 
     SELECT
       employee_number,
       employee_id,
       hire_date,
       date_actual                       AS separation_date, 
-      DATE_TRUNC(month, date_actual)    AS separated_month, 
-      division_mapped_Current as division, department_modified as department, job_title
+      DATE_TRUNC(month, date_actual)    AS separation_month, 
+      division_mapped_current           AS division, 
+      department_modified               AS department,
+      job_title                         AS job_title
     FROM {{ ref('employee_directory_intermediate') }}
     WHERE is_termination_date = TRUE
-      AND date_Actual>='2020-02-01'
+      AND date_actual>='2020-02-01'
 
 ), separation_type AS (
 
@@ -25,12 +32,15 @@ WITH separations AS (
 ), final AS (
 
     SELECT 
+      dim_date.fiscal_year,
       separations.*,
       termination_type,
       eeoc.gender,
       eeoc.ethnicity,
       eeoc.region
     FROM separations
+    LEFT JOIN dim_date
+      ON separations.separation_date = dim_date.date_actual
     LEFT JOIN separation_type 
       ON separations.employee_id = separation_type.employee_id
       AND separations.separation_date = separation_type.effective_Date
