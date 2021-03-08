cbcecb61c3f3fd63b93ab93cf6bcd9dffbf7a541
Paul Armstrong <parmstrong@gitlab.com>
2021-03-08 21:22:51 +0200

    Corrected spacing

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_ci_runners_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_ci_runners_source.sql
@@ -8,24 +8,24 @@ WITH source AS (
 
   SELECT
     id::NUMBER                                     AS runner_id,
-    created_at::TIMESTAMP                           AS created_at,
-    updated_at::TIMESTAMP                           AS updated_at,
-    description::VARCHAR                            AS description,
-    contacted_at::TIMESTAMP                         AS contacted_at,
-    active::BOOLEAN                                 AS is_active,
-    name::VARCHAR                                   AS runner_name,
-    version::VARCHAR                                AS version,
-    revision::VARCHAR                               AS revision,
-    platform::VARCHAR                               AS platform,
-    architecture::VARCHAR                           AS architecture,
-    run_untagged::BOOLEAN                           AS is_untagged,
-    locked::BOOLEAN                                 AS is_locked,
+    created_at::TIMESTAMP                          AS created_at,
+    updated_at::TIMESTAMP                          AS updated_at,
+    description::VARCHAR                           AS description,
+    contacted_at::TIMESTAMP                        AS contacted_at,
+    active::BOOLEAN                                AS is_active,
+    name::VARCHAR                                  AS runner_name,
+    version::VARCHAR                               AS version,
+    revision::VARCHAR                              AS revision,
+    platform::VARCHAR                              AS platform,
+    architecture::VARCHAR                          AS architecture,
+    run_untagged::BOOLEAN                          AS is_untagged,
+    locked::BOOLEAN                                AS is_locked,
     access_level::NUMBER                           AS access_level,
-    ip_address::VARCHAR                             AS ip_address,
+    ip_address::VARCHAR                            AS ip_address,
     maximum_timeout::NUMBER                        AS maximum_timeout,
     runner_type::NUMBER                            AS runner_type,
-    public_projects_minutes_cost_factor::FLOAT      AS public_projects_minutes_cost_factor,
-    private_projects_minutes_cost_factor::FLOAT     AS private_projects_minutes_cost_factor
+    public_projects_minutes_cost_factor::FLOAT     AS public_projects_minutes_cost_factor,
+    private_projects_minutes_cost_factor::FLOAT    AS private_projects_minutes_cost_factor
   FROM source
 
 )
