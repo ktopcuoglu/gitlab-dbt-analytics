b97083786c7d2ccccae3ad7ce6938318fcd2dc1f
jjstark <jstark@gitlab.com>
2021-03-09 10:18:06 -0800

    style

--- transform/snowflake-dbt/models/marts/pumps/pump_hash_marketing_contact.sql
+++ transform/snowflake-dbt/models/marts/pumps/pump_hash_marketing_contact.sql
@@ -1,4 +1,4 @@
-select 
+SELECT 
   SHA2(email_address) || '@example.com' AS email_address,
   {{ dbt_utils.star(from=ref('pump_marketing_contact'), except=["EMAIL_ADDRESS"]) }}
-from {{ref('pump_marketing_contact')}}
+FROM {{ref('pump_marketing_contact')}}
