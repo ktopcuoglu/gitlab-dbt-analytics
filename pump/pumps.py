e8df89f71f73909fe6f9f41990f9ec80b1e58569
jjstark <jstark@gitlab.com>
2021-03-09 07:47:09 -0800

    commenting

--- pump/pumps.py
+++ pump/pumps.py
@@ -53,8 +53,7 @@ def get_copy_command(
 # output for testing
 for pump_models in pumps:
 
-    print(pump_models)
-    print(pump_models["sensitive"])
+    print(pump_models) # for audit
 
     out = get_copy_command(
         **pump_models,
