3e878a28400799ba248b72d3b5167e65154ac0dc
Ian Schweickart <ian.schweickart@decisivedata.net>
2021-03-11 17:39:58 -0800

    fixed metric_path references

--- transform/snowflake-dbt/models/common_mapping/prep/prep_usage_ping_metric_name.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_usage_ping_metric_name.sql
@@ -12,11 +12,11 @@
 
     SELECT DISTINCT 
       TRIM(LOWER(flattened_payload.path))                                           AS metric_path,
-      REPLACE(metrics_path, '.','_')                                                AS metric_path_column_name,
-      'raw_usage_data_payload::' || REPLACE(metrics_path, '.','::')                 AS full_metric_path,
-      'raw_usage_data_payload[''' || REPLACE(metrics_path, '.', '''][''') || ''']'  AS full_metric_path_sql,
-      SPLIT_PART(metrics_path, '.', 1)                                              AS main_json_name, 
-      SPLIT_PART(metrics_path, '.', -1)                                             AS feature_name
+      REPLACE(metric_path, '.','_')                                                 AS metric_path_column_name,
+      'raw_usage_data_payload::' || REPLACE(metric_path, '.','::')                  AS full_metric_path,
+      'raw_usage_data_payload[''' || REPLACE(metric_path, '.', '''][''') || ''']'   AS full_metric_path_sql,
+      SPLIT_PART(metric_path, '.', 1)                                               AS main_json_name, 
+      SPLIT_PART(metric_path, '.', -1)                                              AS feature_name
     FROM prep_usage_ping,
     LATERAL FLATTEN(INPUT => prep_usage_ping.raw_usage_data_payload, RECURSIVE => TRUE) flattened_payload
 
