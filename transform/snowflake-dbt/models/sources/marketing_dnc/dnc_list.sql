a17c7abdf9e03f8435afca5e552aed23c8a4cfd3
jjstark <jstark@gitlab.com>
2021-03-16 08:52:22 -0700

    add marketing dnc source

---
+++ transform/snowflake-dbt/models/sources/marketing_dnc/dnc_list.sql
@@ -0,0 +1,22 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('marketing_dnc', 'dnc_list') }}
+
+), renamed AS (
+
+    SELECT
+      email_address::VARCHAR      AS email_address,
+      is_role::BOOLEAN            AS is_role,
+      is_disposable::BOOLEAN      AS is_disposable,
+      did_you_mean::VARCHAR       AS did_you_mean,
+      result::VARCHAR             AS result,
+      reason::VARCHAR             AS reason,
+      risk::VARCHAR               AS risk,
+      root_email_address::VARCHAR AS root_email_address
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
