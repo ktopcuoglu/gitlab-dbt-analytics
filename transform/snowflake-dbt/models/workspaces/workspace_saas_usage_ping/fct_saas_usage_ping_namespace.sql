6f9d12b7b518d749b7054fe8599cdb000869b12d
mpeychet <mpeychet@gitlab.com>
2021-03-22 16:51:45 +0100

    add 2 models in workspaces

---
+++ transform/snowflake-dbt/models/workspaces/workspace_saas_usage_ping/fct_saas_usage_ping_namespace.sql
@@ -0,0 +1,31 @@
+
+{{ simple_cte([
+    ('saas_usage_ping_namespace', 'saas_usage_ping_namespace'),
+    ('dim_date', 'dim_date')
+]) }}
+
+, transformed AS (
+
+    SELECT 
+      saas_usage_ping_gitlab_dotcom_namespace_id,
+      namespace_ultimate_parent_id                           AS dim_namespace_id,
+      ping_name                                              AS ping_name, --potentially renamed
+      IFF(ping_date < '2021-03-31', _uploaded_at, ping_date) AS ping_date, --curently wrong date input in the airflow run
+      counter_value
+    FROM saas_usage_ping_namespace
+    WHERE error = 'Success'
+
+), joined AS (
+
+    SELECT
+      saas_usage_ping_gitlab_dotcom_namespace_id,
+      dim_namespace_id,
+      ping_name,
+      ping_date
+    FROM transformed
+    INNER JOIN dim_date ON ping_date = date_day
+
+)
+
+SELECT *
+FROM joined
