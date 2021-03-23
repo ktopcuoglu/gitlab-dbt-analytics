51b44038fea9ec75ff3557d844bd7946c834331e
Mathieu Peychet <mpeychet@gitlab.com>
2021-03-23 12:24:14 +0000

    convert uploaded at to date

--- transform/snowflake-dbt/models/workspaces/workspace_saas_usage_ping/fct_saas_usage_ping_namespace.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_saas_usage_ping/fct_saas_usage_ping_namespace.sql
@@ -8,9 +8,9 @@
 
     SELECT 
       saas_usage_ping_gitlab_dotcom_namespace_id,
-      namespace_ultimate_parent_id                           AS dim_namespace_id,
-      ping_name                                              AS ping_name, --potentially renamed
-      IFF(ping_date < '2021-03-31', _uploaded_at, ping_date) AS ping_date, --currently wrong date input in the airflow run
+      namespace_ultimate_parent_id                                    AS dim_namespace_id,
+      ping_name                                                       AS ping_name, --potentially renamed
+      IFF(ping_date < '2021-03-31', TO_DATE(_uploaded_at), ping_date) AS ping_date, --currently wrong date input in the airflow run
       counter_value
     FROM saas_usage_ping_namespace
     WHERE error = 'Success'
