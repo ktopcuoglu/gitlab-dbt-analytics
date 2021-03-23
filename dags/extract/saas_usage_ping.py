f5f31978d9f4f56075f7baea004258c744a38613
Michael Walker <mwalker@gitlab.com>
2021-03-23 16:47:16 -0500

    run black formatting

--- dags/extract/saas_usage_ping.py
+++ dags/extract/saas_usage_ping.py
@@ -61,9 +61,7 @@ default_args = {
 
 # Create the DAG
 #  Sunday at 0900 UTC
-dag = DAG(
-    "saas_usage_ping", default_args=default_args, schedule_interval="0 9 * * 0"
-)
+dag = DAG("saas_usage_ping", default_args=default_args, schedule_interval="0 9 * * 0")
 
 # Instance Level Usage Ping
 instance_cmd = f"""
