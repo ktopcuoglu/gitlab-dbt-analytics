9ce2085289000054a5270ac68e801214cade85ff
mpeychet <mpeychet@gitlab.com>
2021-03-24 09:57:58 +0100

    add column

--- transform/snowflake-dbt/models/workspaces/workspace_saas_usage_ping/fct_saas_usage_ping_namespace.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_saas_usage_ping/fct_saas_usage_ping_namespace.sql
@@ -10,7 +10,8 @@
       saas_usage_ping_gitlab_dotcom_namespace_id,
       namespace_ultimate_parent_id                                    AS dim_namespace_id,
       ping_name                                                       AS ping_name, --potentially renamed
-      IFF(ping_date < '2021-03-31', TO_DATE(_uploaded_at), ping_date) AS ping_date, --currently wrong date input in the airflow run
+      ping_date, --currently wrong date input in the airflow run
+      TO_DATE(_uploaded_at)                                           AS run_date,
       counter_value
     FROM saas_usage_ping_namespace
     WHERE error = 'Success'
