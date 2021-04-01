e5976494253e8f674768c9aadabecc4cc3f62c7b
Alper Akgun <aakgun@gitlab.com>
2021-04-01 10:57:20 +0300

    Remove created_at & updated_at

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_user_details_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_user_details_source.sql
@@ -7,14 +7,12 @@ WITH source AS (
 
     SELECT
       user_id::NUMBER                     AS user_id,
-      job_title::VARCHAR         AS issue_notes_filter,
-      other_role::VARCHAR AS merge_request_notes_filter
-      created_at::TIMESTAMP               AS created_at,
-      updated_at::TIMESTAMP               AS updated_at
+      job_title::VARCHAR                  AS issue_notes_filter,
+      other_role::VARCHAR                 AS merge_request_notes_filter
     FROM source
     
 )
 
 SELECT  *
 FROM renamed
-ORDER BY updated_at
+ORDER by user_id
