8fa5cd2db40657f65c20153a55c813e75776dae7
Jean Del Rosario <jpeguero@gitlab.com>
2021-04-05 07:46:11 -0400

    Set sheetload for alliance and partner targets

---
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_funnel_partner_alliance_targets_matrix_source.sql
@@ -0,0 +1,22 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('sheetload', 'sheetload_sales_funnel_partner_alliance_targets_matrix') }}
+
+), renamed AS (
+
+    SELECT
+      kpi_name::VARCHAR                                   AS kpi_name,
+      month::VARCHAR                                      AS month,
+      partner_engagement_type::VARCHAR                    AS partner_engagement_type,
+      alliance_partner::VARCHAR                           AS alliance_partner,
+      order_type::VARCHAR                                 AS alliance_partner,
+      area:VARCHAR                                        AS area,
+      REPLACE(allocated_target, ',', '')::FLOAT           AS allocated_target,
+      TO_TIMESTAMP(TO_NUMERIC("_UPDATED_AT"))::TIMESTAMP  AS last_updated_at
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
