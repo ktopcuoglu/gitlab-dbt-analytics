bb6a75c9de6ca5e32fae2c2f38dc4a2f462a123a
Justin Stark <jstark@gitlab.com>
2021-04-05 17:57:10 +0000

    Update transform/snowflake-dbt/models/sources/datasiren/datasiren_source_db_social_security_number_sensor.sql

--- transform/snowflake-dbt/models/sources/datasiren/datasiren_source_db_social_security_number_sensor.sql
+++ transform/snowflake-dbt/models/sources/datasiren/datasiren_source_db_social_security_number_sensor.sql
@@ -1,1 +1,5 @@
+{{ config(
+  enabled=false
+) }}
+
 {{ datasiren.search_sample_columns_samples_for_pattern(this.identifier, env_var('SNOWFLAKE_LOAD_DATABASE'), 'TEXT', '100', '25', '^(.*\\D)*((\\d{3}-\\d{2}-\\d{4})|(\\d{9}))(\\D.*)*$') }}
