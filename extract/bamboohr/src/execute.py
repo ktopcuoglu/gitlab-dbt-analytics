28d9092190e53e3735cc1b2cc824c01e6a67d4b7
Michael Walker <mwalker@gitlab.com>
2021-04-06 15:45:36 -0500

    remove bamboo extraction record count testing

--- extract/bamboohr/src/execute.py
+++ extract/bamboohr/src/execute.py
@@ -35,28 +35,6 @@ def get_snowflake_latest_entry_count(table_name, snowflake_engine, field_name):
     return query_executor(snowflake_engine, query)[0][0]
 
 
-def test_extraction(
-    data, snowflake_table, snowflake_engine, tables_to_skip, field_name="JSONTEXT"
-):
-    if snowflake_table.upper() in tables_to_skip:
-        return None
-    elif snowflake_table.upper().split(".")[-1] in tables_to_skip:
-        return None
-    count_extracted = len(data)
-    snowflake_latest_count = get_snowflake_latest_entry_count(
-        snowflake_table, snowflake_engine, field_name
-    )
-    snowflake_latest_count = float(snowflake_latest_count or 0)
-    snowflake_difference_count = count_extracted - snowflake_latest_count
-    if abs(snowflake_difference_count) > (
-        ALLOWED_DATA_CHANGE_PER_EXTRACT * float(snowflake_latest_count)
-    ):
-        logging.error(
-            f"Row difference was {snowflake_difference_count} for {snowflake_table}."
-        )
-        sys.exit(1)
-
-
 if __name__ == "__main__":
 
     logging.basicConfig(stream=sys.stdout, level=20)
@@ -67,13 +45,6 @@ if __name__ == "__main__":
 
     config_dict = env.copy()
     snowflake_load_database = config_dict["SNOWFLAKE_LOAD_DATABASE"]
-    tables_to_skip_test_list = []
-    if "BAMBOOHR_SKIP_TEST" in config_dict:
-        tables_to_skip_test_str = config_dict["BAMBOOHR_SKIP_TEST"]
-        if tables_to_skip_test_str:
-            tables_to_skip_test_list = [
-                table_name.upper() for table_name in tables_to_skip_test_str.split(",")
-            ]
 
     snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
 
@@ -87,12 +58,6 @@ if __name__ == "__main__":
     with open("directory.json", "w") as outfile:
         json.dump(employees, outfile)
 
-    test_extraction(
-        employees,
-        f"{snowflake_load_database}.bamboohr.directory",
-        snowflake_engine,
-        tables_to_skip_test_list,
-    )
 
     snowflake_stage_load_copy_remove(
         "directory.json",
@@ -121,13 +86,6 @@ if __name__ == "__main__":
         with open(f"{key}.json", "w") as outfile:
             json.dump(data, outfile)
 
-        test_extraction(
-            data,
-            f"{snowflake_load_database}.bamboohr.{key}",
-            snowflake_engine,
-            tables_to_skip_test_list,
-        )
-
         snowflake_stage_load_copy_remove(
             f"{key}.json",
             f"{snowflake_load_database}.bamboohr.bamboohr_load",
@@ -147,14 +105,6 @@ if __name__ == "__main__":
 
         record_counts[key] = len(data["employees"])
 
-        test_extraction(
-            data["employees"],
-            f"{snowflake_load_database}.bamboohr.{key}",
-            snowflake_engine,
-            tables_to_skip_test_list,
-            field_name="JSONTEXT:employees",
-        )
-
         snowflake_stage_load_copy_remove(
             f"{key}.json",
             f"{snowflake_load_database}.bamboohr.bamboohr_load",
@@ -173,13 +123,6 @@ if __name__ == "__main__":
             json.dump(data, outfile)
 
         record_counts[key] = len(data)
-
-        test_extraction(
-            data,
-            f"{snowflake_load_database}.bamboohr.{key}",
-            snowflake_engine,
-            tables_to_skip_test_list,
-        )
 
         snowflake_stage_load_copy_remove(
             f"{key}.json",
