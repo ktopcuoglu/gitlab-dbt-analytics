4ba9edbd32e94a88b37836e11950f9a5e151fecc
Ved Prakash <vprakash@gitlab.com>
2021-04-19 14:28:13 +0530

    added fix for handling internal error

--- extract/sheetload/qualtrics_sheetload.py
+++ extract/sheetload/qualtrics_sheetload.py
@@ -70,8 +70,14 @@ def push_contacts_to_qualtrics(
     return final_status
 
 
-def get_metadata(file, google_sheet_client):
-    maximum_backoff_sec = 300
+def get_metadata(
+    file,
+    google_sheet_client,
+    maximum_backoff_sec: int = 300,
+) -> gspread.metadata:
+    """
+    Returns the google sheet name and file name
+    """
     n = 0
     while maximum_backoff_sec > (2 ** n):
         try:
@@ -79,10 +85,7 @@ def get_metadata(file, google_sheet_client):
             tab = file.sheet1.title
             return file_name, tab
         except APIError as gspread_error:
-            if (
-                gspread_error.response.status_code == 429
-                or gspread_error.response.status_code == 503
-            ):
+            if gspread_error.response.status_code in (429, 500, 502, 503):
                 google_sheet_client.wait_exponential_backoff(n)
                 n = n + 1
             else:
