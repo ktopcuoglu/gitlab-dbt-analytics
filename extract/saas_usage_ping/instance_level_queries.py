bbce71fb2369c8ac1c86f84edb2e889035cdc944
Michael Walker <mwalker@gitlab.com>
2021-04-19 17:58:10 -0500

    unify quotation marks in instance level query transformation

--- extract/saas_usage_ping/instance_level_queries.py
+++ extract/saas_usage_ping/instance_level_queries.py
@@ -17,36 +17,36 @@ from sqlparse.utils import imt
 ### Read and transform JSON file
 
 def sql_queries_dict(json_file):
-    ''' 
+    """ 
     function that transforms the sql-export.json file into a Python dict with only SQL batch counters
-    '''
+    """
     with open(json_file) as f:
         data = json.load(f)
 
-    full_payload_dict = flatten(data, reducer=make_reducer(delimiter='.'))
+    full_payload_dict = flatten(data, reducer=make_reducer(delimiter="."))
 
     sql_queries_dict  = {}
 
     for key, value in full_payload_dict.items():
        # Check if key is even then add pair to new dictionary
-       if isinstance(value, str) and value.startswith('SELECT'):
+       if isinstance(value, str) and value.startswith("SELECT"):
            sql_queries_dict[key] = value
     
     return sql_queries_dict
 
 
 def add_counter_name_as_column(sql_metrics_name, sql_query):
-    '''
+    """
     step needed to add the first 2 columns:
       - counter_name
       - run_day
     
     this needs a specific row of a specific dataframe, I think this could be changed to a SQL query for more convenience
     
-    a query like that SELECT COUNT(issues.id) FROM issues will be changed to SELECT 'counts.issues', COUNT(issues.id), TO_DATE(CURRENT_DATE)
+    a query like that SELECT COUNT(issues.id) FROM issues will be changed to SELECT "counts.issues", COUNT(issues.id), TO_DATE(CURRENT_DATE)
     
     needed for version 1 and 2
-    '''
+    """
 
     # removing extra " to have an easier query to parse
     sql_query = sql_query.replace('"', "")
@@ -62,14 +62,14 @@ def add_counter_name_as_column(sql_metrics_name, sql_query):
     for index, token in enumerate(token_list):
 
         # identify if it is a select statement
-        if token.is_keyword and str(token) == 'SELECT':
+        if token.is_keyword and str(token) == "SELECT":
             # set the select_index
             select_index = index
             break
 
     from_index = 0
     for index, token in enumerate(token_list):
-        if token.is_keyword and str(token) == 'FROM':
+        if token.is_keyword and str(token) == "FROM":
             from_index = index
             break
     token_list_with_counter_name = token_list[:]
@@ -84,19 +84,19 @@ def add_counter_name_as_column(sql_metrics_name, sql_query):
     enhanced_query_list = [str(token) for token in token_list_with_counter_name]
 
     # recreate from the list the SQL query
-    enhanced_query = ''.join(enhanced_query_list)
+    enhanced_query = "".join(enhanced_query_list)
     
     return enhanced_query
 
 
 def rename_query_tables(sql_query):
-    '''
+    """
     function to rename the table based on a new regex
-    '''
+    """
     
     ### comprehensive list of all the keywords that are followed by a table name
     keywords_to_look_at = [            
-                'FROM',
+                "FROM",
                 "JOIN",
     ]
 
@@ -111,29 +111,29 @@ def rename_query_tables(sql_query):
     # I find this for loop very confusing... there might be better ways to do it for sure
     for index in range(len(tokens)):
         token = tokens[index]
-        if any(token_word in keywords_to_look_at for token_word in str(token).split(' ')):
+        if any(token_word in keywords_to_look_at for keyword in str(token).split(" ")):
             i = 1
             # Whitespaces are considered as tokens and should be skipped
             while tokens[index + i].ttype is Whitespace:
                 i += 1
                 
             next_token = tokens[index + i]
-            if not str(next_token).startswith('prep') and not str(next_token).startswith('prod'):
+            if not str(next_token).startswith("prep") and not str(next_token).startswith("prod"):
 
                 # insert, token list to string list, create the SQL query, reparse it
                 # there is FOR sure a beter way to do that
                 token_string_list[index + i] = "prep.gitlab_dotcom.gitlab_dotcom_" + str(next_token) + "_dedupe_source AS "
 
-    return ''.join(token_string_list)
+    return "".join(token_string_list)
         
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ## Files imported
 
     ## counter queries (sql+redis): this is generated manually by the product intelligence team
     ## available here
 
-    json_file_path = './usage_ping_instance_queries.json'
+    json_file_path = "./usage_ping_instance_queries.json"
 
     sql_queries_dictionary = sql_queries_dict(json_file_path)
 
