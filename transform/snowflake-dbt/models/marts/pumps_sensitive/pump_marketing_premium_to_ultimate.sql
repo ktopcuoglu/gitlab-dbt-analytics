d535a803c36c719030660bd90ec22823277503dc
jjstark <jstark@gitlab.com>
2021-04-28 13:34:57 -0700

    add docs

--- transform/snowflake-dbt/models/marts/pumps_sensitive/pump_marketing_premium_to_ultimate.sql
+++ transform/snowflake-dbt/models/marts/pumps_sensitive/pump_marketing_premium_to_ultimate.sql
@@ -1,10 +1,10 @@
 SELECT *
 FROM  {{ ref('pump_marketing_contact') }}
 WHERE (
-	individual_namespace_is_saas_premium_tier
+  individual_namespace_is_saas_premium_tier
   OR group_owner_of_saas_premium_tier
   OR group_member_of_saas_premium_tier
   OR responsible_for_group_saas_premium_tier
-	)
+  )
 AND gitlab_dotcom_email_opted_in = true
 OR is_self_managed_premium_tier
