bd53402c3a5b8ce674b9b3faa7d02a4eda559de4
iweeks <iweeks@gitlab.com>
2021-04-28 18:03:17 -0400

    update fct charge

--- transform/snowflake-dbt/models/common_mapping/prep/prep_recurring_charge_non_amortized.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_recurring_charge_non_amortized.sql
@@ -19,22 +19,27 @@ WITH zuora_rate_plan AS (
       zuora_rate_plan_charge.rate_plan_charge_number,
       zuora_rate_plan.rate_plan_name                                    AS rate_plan_name,
       zuora_rate_plan_charge.rate_plan_charge_name,
-      zuora_rate_plan_charge.effective_start_month,
-      zuora_rate_plan_charge.effective_end_month,
       {{ get_date_id('zuora_rate_plan_charge.effective_start_date') }}  AS effective_start_date_id,
       {{ get_date_id('zuora_rate_plan_charge.effective_end_date') }}    AS effective_end_date_id,
+      zuora_rate_plan_charge.effective_start_date,
+      zuora_rate_plan_charge.effective_end_date,
       {{ get_date_id('zuora_rate_plan_charge.effective_start_month') }} AS effective_start_month_id,
       {{ get_date_id('zuora_rate_plan_charge.effective_end_month') }}   AS effective_end_month_id,
+      zuora_rate_plan_charge.effective_start_month,
+      zuora_rate_plan_charge.effective_end_month,
       zuora_rate_plan_charge.unit_of_measure,
       zuora_rate_plan_charge.quantity,
       zuora_rate_plan_charge.mrr,
-      zuora_rate_plan_charge.delta_tcv,
       zuora_rate_plan_charge.delta_mrc,
+      zuora_rate_plan_charge.mrr * 12                                   AS arr,
+      zuora_rate_plan_charge.delta_mrc * 12                             AS delta_arc,
+      zuora_rate_plan_charge.tcv,
+      zuora_rate_plan_charge.delta_tcv,
       zuora_rate_plan_charge.discount_level,
       zuora_rate_plan_charge.segment                                    AS rate_plan_charge_segment,
       zuora_rate_plan_charge.version                                    AS rate_plan_charge_version,
       zuora_rate_plan_charge.charge_type,
-      zuora_rate_plan.amendement_type
+      zuora_rate_plan.amendement_type                                   AS rate_plan_charge_amendement_type
     FROM zuora_rate_plan
     INNER JOIN zuora_rate_plan_charge
       ON zuora_rate_plan.rate_plan_id = zuora_rate_plan_charge.rate_plan_id
