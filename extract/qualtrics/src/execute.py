8cf3306f350b2bf99d9521b9846cf1de8d4c06c1
Michael Walker <mwalker@gitlab.com>
2021-04-30 17:36:22 -0500

    fix chunk size on distributions from qualtrics again

--- extract/qualtrics/src/execute.py
+++ extract/qualtrics/src/execute.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         all_distributions = all_distributions + current_distributions
         if current_distributions:
 
-            for chunk in chunk_list(current_distributions, 10):
+            for chunk in chunk_list(current_distributions, 4):
                 with open("distributions.json", "w") as out_file:
                     json.dump(current_distributions, out_file)
 
