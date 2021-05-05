0fb8d1a9ef8ccdca2a2d493e88b3936bde015eb9
Michael Walker <mwalker@gitlab.com>
2021-05-05 08:10:47 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/sources/gitlab_data_yaml/release_managers_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_data_yaml/release_managers_source.sql
@@ -20,8 +20,8 @@ WITH source AS (
       SPLIT_PART(major_minor_version, '.', 1)                   AS major_version,
       SPLIT_PART(major_minor_version, '.', 2)                   AS minor_version,
       TRY_TO_DATE(data_by_row['date']::TEXT, 'MMMM DDnd, YYYY') AS release_date,
-      data_by_row['manager_americas'][0]::TEXT                  AS release_manager_americas,
-      data_by_row['manager_apac_emea'][0]::TEXT                 AS release_manager_emea,
+      data_by_row['manager_americas'][0]::VARCHAR               AS release_manager_americas,
+      data_by_row['manager_apac_emea'][0]::VARCHAR              AS release_manager_emea,
       rank,
       snapshot_date
     FROM intermediate
