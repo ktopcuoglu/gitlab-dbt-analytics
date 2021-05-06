9ea4b392441a751c6ea0d49241767155a40be483
Ved Prakash <vprakash@gitlab.com>
2021-05-06 19:10:36 +0530

    Added loading pipeline

---
+++ dags/extract/zuora_revenue_load_extract.py
@@ -0,0 +1,98 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow_utils import (
+    DATA_IMAGE,
+    clone_and_setup_extraction_cmd,
+    gitlab_defaults,
+    slack_failed_task,
+)
+from kube_secrets import (
+    GCP_SERVICE_CREDS,
+    ZUORA_REVENUE_GCS_NAME,
+    ZUORA_REVENUE_API_URL,
+    ZUORA_REVENUE_AUTH_CODE,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+)
+from kubernetes_helpers import get_affinity, get_toleration
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+GIT_BRANCH = env["GIT_BRANCH"]
+pod_env_vars = {
+    "SNOWFLAKE_LOAD_DATABASE": "RAW"
+    if GIT_BRANCH == "master"
+    else f"{GIT_BRANCH.upper()}_RAW",
+    "CI_PROJECT_DIR": "/analytics",
+}
+
+# Default arguments for the DAG
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+    "sla": timedelta(hours=24),
+    "sla_miss_callback": slack_failed_task,
+    "start_date": datetime(2019, 1, 1),
+    "dagrun_timeout": timedelta(hours=6),
+}
+
+airflow_home = env["AIRFLOW_HOME"]
+
+# Get all the table name for which tasks for loading needs to be created
+with open(f"{airflow_home}/analytics/extract/zuora_revenue/zuora_revenue_table_name.yml", "r") as file:
+    try:
+        stream = safe_load(file)
+    except YAMLError as exc:
+        print(exc)
+
+    table_name_list = [
+        "{table_name}".format(table_name=tab)
+        for sheet in stream["table_info"]
+        for tab in sheet["table_name"]
+    ]
+
+runs = []
+
+# Create the DAG  with one load happening at once
+dag = DAG(
+    "zuora_revenue_load_extract", default_args=default_args, schedule_interval="0 10 * * 0",concurrency=1,
+)
+
+for table_name in table_name_list:
+    #Set the command for the container
+    container_cmd = f"""
+        {clone_and_setup_extraction_cmd} &&
+        python3 zuora_revenue/zuora_extract_load.py zuora_load --bucket $ZUORA_REVENUE_BUCKET_NAME --schema_name zuora_revenue --table_name {table_name}
+        """
+    # Task 1
+    zuora_revenue_load_run = KubernetesPodOperator(
+        **gitlab_defaults,
+        image=DATA_IMAGE,
+        task_id=f"{table_name}-load",
+        name=f"{table_name}-load",
+        secrets=[
+            ZUORA_REVENUE_GCS_NAME,
+            GCP_SERVICE_CREDS,
+            SNOWFLAKE_ACCOUNT,
+            SNOWFLAKE_LOAD_ROLE,
+            SNOWFLAKE_LOAD_USER,
+            SNOWFLAKE_LOAD_WAREHOUSE,
+            SNOWFLAKE_LOAD_PASSWORD,
+            ],
+        env_vars=pod_env_vars,
+        affinity=get_affinity(False),
+        tolerations=get_toleration(False),
+        arguments=[container_cmd],
+        dag=dag,
+        )
+    runs.append(sheetload_run)
--- extract/zuora_revenue/src/zuora_extract_load.py
+++ extract/zuora_revenue/src/zuora_extract_load.py
@@ -1,10 +1,25 @@
-import logging
 import sys
 import time
+from logging import error, info, basicConfig, getLogger, warning
 from os import environ as env
 from fire import Fire
+from yaml import load, safe_load, YAMLError
+
+import pandas as pd
 
 from api import ZuoraRevProAPI
+from google.cloud import storage
+from google.oauth2 import service_account
+from sqlalchemy.engine.base import Engine
+
+
+from sheetload.sheetload import gcs_loader
+from sheetload_dataframe_utils import dw_uploader
+from gitlabdata.orchestration_utils import (
+    snowflake_engine_factory,
+    query_executor,
+)
+
 
 zuora_revenue_bi_entity_table_list = [
     {
@@ -99,6 +114,7 @@ config_dict = env.copy()
 def zuora_revenue_extract(
     config_dict: dict, zuora_revenue_bi_entity_table_list: list
 ) -> None:
+    logging.basicConfig(level=20, filename="loging_file.log")
 
     logging.info("1st Step to get the authentication URL and header informtation.")
     headers = {
@@ -136,12 +152,61 @@ def zuora_revenue_extract(
         )
 
 
-def zuora_revenue_load()->None:
-    print("zuora_revenue_load")
+def zuora_revenue_load(
+    bucket: str,
+    schema: str,
+    table_name: str,
+    gapi_keyfile: str = None,
+    conn_dict: Dict[str, str] = None,
+    compression: str = None,
+) -> None:
+    # Set some vars
+    chunksize = 15000
+    chunk_iter = 0
+    engine = snowflake_engine_factory(conn_dict or env, "LOADER", schema)
+
+    # Get the gcloud storage client and authenticate
+    scope = ["https://www.googleapis.com/auth/cloud-platform"]
+    keyfile = load(gapi_keyfile or env["GCP_SERVICE_CREDS"])
+    credentials = service_account.Credentials.from_service_account_info(keyfile)
+    scoped_credentials = credentials.with_scopes(scope)
+    storage_client = storage.Client(credentials=scoped_credentials)
+    bucket_obj = storage_client.get_bucket(bucket)
+
+    # Download the file and then pass it in chunks to dw_uploader
+    blobs = bucket_obj.list_blobs(prefix=f"RAW_DB/{table_name}")
+    for blob in blobs:
+        if blob.name.endswith("csv"):
+            print(blob.name)
+            blob = bucket_obj.blob(blob.name)
+            blob.download_to_filename(f"{table_name}.csv")
+
+            try:
+                sheet_df = pd.read_csv(
+                    f"{table_name}.csv",
+                    engine="c",
+                    low_memory=False,
+                    compression=compression,
+                    chunksize=chunksize,
+                )
+            except FileNotFoundError:
+                info("File {} not found.".format(f"{table_name}.csv"))
+
+            for chunk in sheet_df:
+                chunk[chunk.columns] = chunk[chunk.columns].astype("str")
+                dw_uploader(engine=engine, table=table, data=chunk, chunk=chunk_iter)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=20, filename="loging_file.log")
+    basicConfig(stream=sys.stdout, level=20)
+    getLogger("snowflake.connector.cursor").disabled = True
     # Copy all environment variables to dict.
     config_dict = env.copy()
-    Fire({"zuora_extract": zuora_revenue_extract(config_dict,zuora_revenue_bi_entity_table_list), "zuora_load": zuora_revenue_load})
+    Fire(
+        {
+            "zuora_extract": zuora_revenue_extract(
+                config_dict, zuora_revenue_bi_entity_table_list
+            ),
+            "zuora_load": zuora_revenue_load,
+        }
+    )
