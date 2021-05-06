272de3de88534a86ef63a8dcc88886030bbeb243
Mathieu Peychet <mpeychet@gitlab.com>
2021-05-06 10:16:35 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/sources/version/version_usage_data_source.sql
+++ transform/snowflake-dbt/models/sources/version/version_usage_data_source.sql
@@ -21,7 +21,7 @@ WITH source AS (
       *,
       OBJECT_CONSTRUCT(
         {% for column in columns %}  
-          '{{ column.column | lower }}', {{ column.column | lower }}
+          '{{ column.name | lower }}', {{ column.column | lower }}
           {% if not loop.last %}
             ,
           {% endif %}
