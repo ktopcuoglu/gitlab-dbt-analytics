5541598d3d8dac3d856af471bd14c27bdd0b0d15
Jean Del Rosario <jpeguero@gitlab.com>
2021-05-13 17:19:56 -0400

    add rpt non senstitive marketing_contact model

---
+++ transform/snowflake-dbt/models/rpt/marketing/rpt_marketing_contact.sql
@@ -0,0 +1,14 @@
+WITH mart_marketing_contact AS (
+    SELECT
+      {{ dbt_utils.star(from=ref('mart_marketing_contact'), except=['EMAIL_ADDRESS', 'FIRST_NAME', 'LAST_NAME', 'GITLAB_USER_NAME', 'GITLAB_DOTCOM_USER_ID']) }}
+    FROM {{ ref('mart_marketing_contact') }}
+
+)
+
+{{ dbt_audit(
+    cte_ref="mart_marketing_contact",
+    created_by="@jpeguero",
+    updated_by="@jpeguero",
+    created_date="2021-05-13",
+    updated_date="2021-05-13"
+) }}
