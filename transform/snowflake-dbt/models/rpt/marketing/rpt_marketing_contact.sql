12deab0b7e565cb796dc5ffad5a7dd1b612f5213
Jean Del Rosario <jpeguero@gitlab.com>
2021-05-14 12:07:10 -0400

    delete duplicate columns in rpt_marketing_contact model

--- transform/snowflake-dbt/models/rpt/marketing/rpt_marketing_contact.sql
+++ transform/snowflake-dbt/models/rpt/marketing/rpt_marketing_contact.sql
@@ -1,6 +1,8 @@
 WITH mart_marketing_contact AS (
+  
     SELECT
-      {{ dbt_utils.star(from=ref('mart_marketing_contact'), except=['EMAIL_ADDRESS', 'FIRST_NAME', 'LAST_NAME', 'GITLAB_USER_NAME', 'GITLAB_DOTCOM_USER_ID']) }}
+      {{ dbt_utils.star(from=ref('mart_marketing_contact'), except=['EMAIL_ADDRESS', 'FIRST_NAME', 'LAST_NAME', 'GITLAB_USER_NAME', 'GITLAB_DOTCOM_USER_ID',
+      'CREATED_BY', 'UPDATED_BY', 'CREATED_DATE', 'UPDATED_DATE']) }}
     FROM {{ ref('mart_marketing_contact') }}
 
 )
