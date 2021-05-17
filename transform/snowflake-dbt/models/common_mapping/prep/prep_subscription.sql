6491cc9e583320de68d17607cd73658b5e9ec861
iweeks <iweeks@gitlab.com>
2021-05-17 17:12:25 -0400

    update logic

--- transform/snowflake-dbt/models/common_mapping/prep/prep_subscription.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_subscription.sql
@@ -25,8 +25,18 @@ WITH date_details AS (
 ), renewal_subscriptions AS (
 
     SELECT DISTINCT
+      sub_1.subscription_id,
       sub_1.subscription_name,
       sub_1.zuora_renewal_subscription_name,
+      CASE
+        WHEN sub_1.current_term >= 24 THEN TRUE
+        WHEN sub_1.zuora_renewal_subscription_name != '' THEN TRUE
+        ELSE FALSE
+      END                                                                       AS is_myb,
+      CASE
+        WHEN sub_1.zuora_renewal_subscription_name != '' THEN TRUE
+        ELSE FALSE
+      END                                                                       AS is_myb_with_multi_subs,
       DATE_TRUNC('month',sub_2.subscription_end_date::DATE)                     AS myb_renewal_month,
       RANK() OVER (PARTITION BY sub_1.subscription_name
                    ORDER BY sub_1.zuora_renewal_subscription_name, sub_2.subscription_end_date )
@@ -34,7 +44,6 @@ WITH date_details AS (
     FROM zuora_subscription sub_1
     INNER JOIN zuora_subscription sub_2
       ON sub_1.zuora_renewal_subscription_name = sub_2.subscription_name
-    WHERE sub_1.zuora_renewal_subscription_name != ''
     QUALIFY rank = 1
 
 ), joined AS (
@@ -55,15 +64,8 @@ WITH date_details AS (
       zuora_subscription.auto_renew                                             AS is_auto_renew,
       zuora_subscription.zuora_renewal_subscription_name,
       zuora_subscription.zuora_renewal_subscription_name_slugify,
-      CASE
-        WHEN zuora_subscription.current_term >= 24 THEN TRUE
-        WHEN zuora_subscription.subscription_name IN (SELECT DISTINCT subscription_name FROM renewal_subscriptions) THEN TRUE
-        ELSE FALSE
-      END                                                                       AS is_myb,
-      CASE
-        WHEN zuora_subscription.subscription_name IN (SELECT DISTINCT subscription_name FROM renewal_subscriptions) THEN TRUE
-        ELSE FALSE
-      END                                                                       AS is_myb_with_multi_subs,
+      renewal_subscriptions.is_myb,
+      renewal_subscriptions.is_myb_with_multi_subs,
       zuora_subscription.current_term,
       zuora_subscription.renewal_term,
       zuora_subscription.renewal_term_period_type,
@@ -77,6 +79,7 @@ WITH date_details AS (
       DATE_TRUNC('month', zuora_subscription.subscription_start_date::DATE)     AS subscription_start_month,
       DATE_TRUNC('month', zuora_subscription.subscription_end_date::DATE)       AS subscription_end_month,
       date_details.fiscal_year                                                  AS subscription_end_fiscal_year,
+      date_details.fiscal_quarter_name_fy                                       AS subscription_end_fiscal_quarter_name_fy,
       zuora_subscription.term_start_date::DATE                                  AS term_start_date,
       zuora_subscription.term_end_date::DATE                                    AS term_end_date,
       DATE_TRUNC('month', zuora_subscription.term_start_date::DATE)             AS term_start_month,
@@ -95,7 +98,7 @@ WITH date_details AS (
     LEFT JOIN map_merged_crm_account
       ON zuora_account.crm_id = map_merged_crm_account.sfdc_account_id
     LEFT JOIN renewal_subscriptions
-      ON zuora_subscription.subscription_name = renewal_subscriptions.subscription_name
+      ON zuora_subscription.subscription_id = renewal_subscriptions.subscription_id
     LEFT JOIN date_details
       ON zuora_subscription.subscription_end_date::DATE = date_details.date_day
 
