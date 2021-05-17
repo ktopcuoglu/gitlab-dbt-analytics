4117c5accb6b7db3dd5822593eca0bc91eb1778b
jjstark <jstark@gitlab.com>
2021-05-17 16:57:03 -0700

    style

--- transform/snowflake-dbt/models/sources/dbt/dbt_model_source.sql
+++ transform/snowflake-dbt/models/sources/dbt/dbt_model_source.sql
@@ -8,36 +8,32 @@ WITH source AS (
 
 ), nodes AS (
 
-  SELECT 
-
-    d.value                                         AS data_by_row,
-    jsontext['metadata']['dbt_version']::VARCHAR          AS dbt_version,
-    jsontext['metadata']['dbt_schema_version']::VARCHAR   AS schema_version,
-    jsontext['metadata']['generated_at']::TIMESTAMP_NTZ   AS generated_at,
-    uploaded_at
-  
-  FROM source
-  INNER JOIN LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext['nodes']), outer => true) d
+    SELECT 
+      d.value                                         AS data_by_row,
+      jsontext['metadata']['dbt_version']::VARCHAR          AS dbt_version,
+      jsontext['metadata']['dbt_schema_version']::VARCHAR   AS schema_version,
+      jsontext['metadata']['generated_at']::TIMESTAMP_NTZ   AS generated_at,
+      uploaded_at
+    FROM source
+    INNER JOIN LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext['nodes']), outer => true) d
 
 ), parsed AS (
 
-  SELECT
-
-    data_by_row['unique_id']::VARCHAR     AS unique_id,  
-    data_by_row['name']::VARCHAR          AS name,
-    data_by_row['alias']::VARCHAR         AS alias,
-    data_by_row['database']::VARCHAR      AS database_name,
-    data_by_row['schema']::VARCHAR        AS schema_name,
-    data_by_row['package_name']::VARCHAR  AS package_name,
-    data_by_row['tags']::ARRAY            AS tags,
-    data_by_row['refs']::ARRAY            AS referrences,
-    dbt_version,
-    schema_version,
-    generated_at,
-    uploaded_at
-
-  FROM nodes
-  WHERE data_by_row['resource_type']::VARCHAR ='model'
+    SELECT
+      data_by_row['unique_id']::VARCHAR     AS unique_id,  
+      data_by_row['name']::VARCHAR          AS name,
+      data_by_row['alias']::VARCHAR         AS alias,
+      data_by_row['database']::VARCHAR      AS database_name,
+      data_by_row['schema']::VARCHAR        AS schema_name,
+      data_by_row['package_name']::VARCHAR  AS package_name,
+      data_by_row['tags']::ARRAY            AS tags,
+      data_by_row['refs']::ARRAY            AS referrences,
+      dbt_version,
+      schema_version,
+      generated_at,
+      uploaded_at
+    FROM nodes
+    WHERE data_by_row['resource_type']::VARCHAR ='model'
 
 )
 
--- transform/snowflake-dbt/models/sources/dbt/dbt_test_source.sql
+++ transform/snowflake-dbt/models/sources/dbt/dbt_test_source.sql
@@ -8,37 +8,33 @@ WITH source AS (
 
 ), nodes AS (
 
-  SELECT 
-
-    d.value                                         AS data_by_row,
-    jsontext['metadata']['dbt_version']::VARCHAR          AS dbt_version,
-    jsontext['metadata']['dbt_schema_version']::VARCHAR   AS schema_version,
-    jsontext['metadata']['generated_at']::TIMESTAMP_NTZ   AS generated_at,
-    uploaded_at
-  
-  FROM source
-  INNER JOIN LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext['nodes']), outer => true) d
+    SELECT 
+      d.value                                         AS data_by_row,
+      jsontext['metadata']['dbt_version']::VARCHAR          AS dbt_version,
+      jsontext['metadata']['dbt_schema_version']::VARCHAR   AS schema_version,
+      jsontext['metadata']['generated_at']::TIMESTAMP_NTZ   AS generated_at,
+      uploaded_at
+    FROM source
+    INNER JOIN LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext['nodes']), outer => true) d
 
 ), parsed AS (
 
-  SELECT
-
-    data_by_row['unique_id']::VARCHAR                                   AS unique_id,  
-    data_by_row['name']::VARCHAR                                        AS name,
-    data_by_row['alias']::VARCHAR                                       AS alias,
-    IFNULL(data_by_row['test_metadata']['name']::VARCHAR, 'custom')     AS test_type,
-    data_by_row['package_name']::VARCHAR                                AS package_name,
-    data_by_row['tags']::ARRAY                                          AS tags,
-    LOWER(data_by_row['config']['severity']::VARCHAR)                   AS severity,
-    data_by_row['refs']::ARRAY                                          AS referrences,
-    data_by_row['depends_on']                                           AS depends_on,
-    dbt_version,
-    schema_version,
-    generated_at,
-    uploaded_at
-
-  FROM nodes
-  WHERE data_by_row['resource_type']::VARCHAR ='test'
+    SELECT
+      data_by_row['unique_id']::VARCHAR                                   AS unique_id,  
+      data_by_row['name']::VARCHAR                                        AS name,
+      data_by_row['alias']::VARCHAR                                       AS alias,
+      IFNULL(data_by_row['test_metadata']['name']::VARCHAR, 'custom')     AS test_type,
+      data_by_row['package_name']::VARCHAR                                AS package_name,
+      data_by_row['tags']::ARRAY                                          AS tags,
+      LOWER(data_by_row['config']['severity']::VARCHAR)                   AS severity,
+      data_by_row['refs']::ARRAY                                          AS referrences,
+      data_by_row['depends_on']                                           AS depends_on,
+      dbt_version,
+      schema_version,
+      generated_at,
+      uploaded_at
+    FROM nodes
+    WHERE data_by_row['resource_type']::VARCHAR ='test'
 
 )
 
