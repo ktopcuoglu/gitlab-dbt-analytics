eed93be95c76151e4e8c9d622a4f5ab3135cd015
Michael Walker <mwalker@gitlab.com>
2021-05-17 16:15:34 -0500

    add namespace id to qualtrics contacts uploaded as part of mailing lists

--- transform/snowflake-dbt/models/legacy/qualtrics/qualtrics_api_formatted_contacts.sql
+++ transform/snowflake-dbt/models/legacy/qualtrics/qualtrics_api_formatted_contacts.sql
@@ -17,19 +17,20 @@ WITH users AS (
 ), renamed AS (
 
     SELECT
-      users.user_id                                          AS user_id,
-      IFNULL(first_name, SPLIT_PART(users_name, ' ', 1))     AS first_name,
+      users.user_id                                                                    AS user_id,
+      IFNULL(first_name, SPLIT_PART(users_name, ' ', 1))                               AS first_name,
       LTRIM(IFNULL(last_name,  LTRIM(users_name, SPLIT_PART(users_name, ' ', 1))))     AS last_name,
-      IFNULL(notification_email, public_email)               AS email_address,
-      NULL                                                   AS phone_number,
-      UPPER(IFNULL(NULLIF(preferred_language, 'nan'), 'en')) AS language,
+      IFNULL(notification_email, public_email)                                         AS email_address,
+      NULL                                                                             AS phone_number,
+      UPPER(IFNULL(NULLIF(preferred_language, 'nan'), 'en'))                           AS language,
       DECODE(highest_paid_subscription_plan_id::VARCHAR, 
         '2',  'Bronze',
         '3',  'Silver',
         '4',  'Gold',
         '34', 'Free',
         'Free'
-      )                                                      AS plan                    
+      )                                                                                 AS plan,
+      highest_subscription_plan.highest_paid_subscription_namespace_id                  AS namespace_id,                    
     FROM users
     LEFT JOIN highest_subscription_plan
       ON users.user_id = highest_subscription_plan.user_id
