3d86815bf35254ce12c46fc358d5dddc39356ffd
Michael Walker <mwalker@gitlab.com>
2021-05-18 08:04:38 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_map_usage_ping_subscription_id.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_map_usage_ping_subscription_id.sql
@@ -14,8 +14,8 @@
 
     SELECT 
       *,
-      SPLIT_PART(subscription_lineage, ',', -1)  AS latest_subscription_in_lineage,
-      STRTOK_TO_ARRAY(subscription_lineage, ',') AS subscription_lineage_array
+      STRTOK_TO_ARRAY(subscription_lineage, ',') AS subscription_lineage_array,
+      GET(subscription_lineage_array, -1)::VARCHAR  AS latest_subscription_in_lineage,
     FROM dim_subscription
     WHERE subscription_status IN ('Active', 'Cancelled')
       AND subscription_start_date < subscription_end_date
