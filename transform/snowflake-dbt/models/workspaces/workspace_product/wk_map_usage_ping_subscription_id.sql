52b36feecaffd9bcbef334323dc625d550c156af
mpeychet <mpeychet@gitlab.com>
2021-05-18 17:40:48 +0200

    work now

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_map_usage_ping_subscription_id.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_map_usage_ping_subscription_id.sql
@@ -15,7 +15,7 @@
     SELECT 
       *,
       STRTOK_TO_ARRAY(subscription_lineage, ',') AS subscription_lineage_array,
-      GET(subscription_lineage_array, -1)::VARCHAR  AS latest_subscription_in_lineage
+      ARRAY_SLICE(subscription_lineage_array, -2, -1)::VARCHAR  AS latest_subscription_in_lineage
     FROM dim_subscription
     WHERE subscription_status IN ('Active', 'Cancelled')
       AND subscription_start_date < subscription_end_date
