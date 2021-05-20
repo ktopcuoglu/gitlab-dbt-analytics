b1bfb47d75995d68bd69d9bd77b54368a3941669
mpeychet <mpeychet@gitlab.com>
2021-05-21 00:13:43 +0200

    remove

--- transform/snowflake-dbt/models/legacy/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
+++ transform/snowflake-dbt/models/legacy/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
@@ -637,7 +637,7 @@
 {% for event_cte in event_ctes %}
 
     SELECT
-      MD5({{ event_cte.event_name}}.{{ event_cte.primary_key }} || '-' || {{ event_cte.event_name }})     sAS event_primary_key,
+      MD5({{ event_cte.event_name}}.{{ event_cte.primary_key }} || '-' || '{{ event_cte.event_name }}')   AS event_primary_key,
       '{{ event_cte.event_name }}'                                                                        AS event_name,
       ultimate_namespace.namespace_id,
       ultimate_namespace.namespace_created_at,
