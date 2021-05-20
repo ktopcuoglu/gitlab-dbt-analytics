b81ab65ef533c8585ccc18edfba4db9f67b18994
mpeychet <mpeychet@gitlab.com>
2021-05-20 09:22:30 +0200

    wk spo: be explicit in the  union

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_spo.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_spo.sql
@@ -9,10 +9,44 @@
 ])
 }}
 
-SELECT *
+SELECT 
+  reporting_month,
+  organization_id,
+  delivery,
+  organization_type,
+  product_tier,
+  is_paid_product_tier,
+  umau_value,
+  release_stage,
+  manage_stage,
+  secure_stage,
+  verify_stage,
+  monitor_stage,
+  protect_stage,
+  create_stage,
+  package_stage,
+  configure_stage,
+  plan_stage
 FROM wk_self_managed_spo
 
 UNION
 
-SELECT *
+SELECT 
+  reporting_month,
+  organization_id,
+  delivery,
+  organization_type,
+  product_tier,
+  is_paid_product_tier,
+  umau_value,
+  release_stage,
+  manage_stage,
+  secure_stage,
+  verify_stage,
+  monitor_stage,
+  protect_stage,
+  create_stage,
+  package_stage,
+  configure_stage,
+  plan_stage
 FROM wk_saas_spo
