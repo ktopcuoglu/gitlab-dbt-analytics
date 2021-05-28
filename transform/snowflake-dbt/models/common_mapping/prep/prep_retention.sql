866a9dbba0a43847832b240b1db0ca5712356905
Paul Armstrong <parmstrong@gitlab.com>
2021-05-28 14:29:28 +0200

    Corrected indents, cleaned up naming, moved column order.

--- transform/snowflake-dbt/models/common_mapping/prep/prep_retention.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_retention.sql
@@ -1,64 +1,72 @@
-WITH sfdc_account AS (
+WITH crm_account AS (
 
     SELECT
-        arr_segmentation                                as arr_segmentation,
-        churn_type                                      as churn_type,
-        gross_retention_mrr                             as gross_retention_mrr,
-        net_retention_mrr                               as net_retention_mrr,
-        original_mrr                                    as original_mrr,
-        retention_month                                 as retention_month,
-        salesforce_account_id                           as dim_crm_account_id,
-        months_since_sfdc_account_cohort_start          as months_since_cohort_start,
-        sfdc_account_cohort_month                       as cohort_month,
-        sfdc_account_cohort_quarter                     as cohort_quarter,
-        ''                                              as dim_subscription_id,
-        quarters_since_sfdc_account_cohort_start        as quarters_since_cohort_start,
-        'crm_account'                                   as retention_type
+      salesforce_account_id                           as dim_crm_account_id,
+      NULL                                            as dim_subscription_id,
+      'crm_account'                                   as retention_type,
+      arr_segmentation                                as arr_segmentation,
+      churn_type                                      as churn_type,
+      gross_retention_mrr                             as gross_retention_mrr,
+      net_retention_mrr                               as net_retention_mrr,
+      original_mrr                                    as original_mrr,
+      retention_month                                 as retention_month,
+      months_since_sfdc_account_cohort_start          as months_since_cohort_start,
+      sfdc_account_cohort_month                       as cohort_month,
+      sfdc_account_cohort_quarter                     as cohort_quarter,
+      quarters_since_sfdc_account_cohort_start        as quarters_since_cohort_start
     FROM {{ ref ('retention_sfdc_account_') }}
 
-), zuora_subscription AS (
+), subscription AS (
 
     SELECT
-        arr_segmentation                                as arr_segmentation,
-        churn_type                                      as churn_type,
-        gross_retention_mrr                             as gross_retention_mrr,
-        net_retention_mrr                               as net_retention_mrr,
-        original_mrr                                    as original_mrr,
-        retention_month                                 as retention_month,
-        salesforce_account_id                           as dim_crm_account_id,
-        months_since_zuora_subscription_cohort_start    as months_since_cohort_start,
-        zuora_subscription_cohort_month                 as cohort_month,
-        zuora_subscription_cohort_quarter               as cohort_quarter,
-        zuora_subscription_id                           as dim_subscription_id,
-        quarters_since_zuora_subscription_cohort_start  as quarters_since_cohort_start,
-        'dim_subscription'                              as retention_type
+      salesforce_account_id                           as dim_crm_account_id,
+      zuora_subscription_id                           as dim_subscription_id,
+      'dim_subscription'                              as retention_type,
+      arr_segmentation                                as arr_segmentation,
+      churn_type                                      as churn_type,
+      gross_retention_mrr                             as gross_retention_mrr,
+      net_retention_mrr                               as net_retention_mrr,
+      original_mrr                                    as original_mrr,
+      retention_month                                 as retention_month,
+      months_since_zuora_subscription_cohort_start    as months_since_cohort_start,
+      zuora_subscription_cohort_month                 as cohort_month,
+      zuora_subscription_cohort_quarter               as cohort_quarter,
+      quarters_since_zuora_subscription_cohort_start  as quarters_since_cohort_start
     FROM {{ ref('retention_zuora_subscription_') }}
 
-), parent_account AS (
+), parent_crm_account AS (
 
     SELECT
-        arr_segmentation                                as arr_segmentation,
-        churn_type                                      as churn_type,
-        gross_retention_mrr                             as gross_retention_mrr,
-        net_retention_mrr                               as net_retention_mrr,
-        original_mrr                                    as original_mrr,
-        retention_month                                 as retention_month,
-        salesforce_account_id                           as dim_crm_account_id,
-        months_since_parent_account_cohort_start        as months_since_cohort_start,
-        parent_account_cohort_month                     as cohort_month,
-        parent_account_cohort_quarter                   as cohort_quarter,
-        ''                                              as dim_subscription_id,
-        quarters_since_parent_account_cohort_start      as quarters_since_cohort_start,
-        'parent_crm_account'                            as retention_type
+      salesforce_account_id                           as dim_crm_account_id,
+      NULL                                            as dim_subscription_id,
+      'parent_crm_account'                            as retention_type,
+      arr_segmentation                                as arr_segmentation,
+      churn_type                                      as churn_type,
+      gross_retention_mrr                             as gross_retention_mrr,
+      net_retention_mrr                               as net_retention_mrr,
+      original_mrr                                    as original_mrr,
+      retention_month                                 as retention_month,
+      months_since_parent_account_cohort_start        as months_since_cohort_start,
+      parent_account_cohort_month                     as cohort_month,
+      parent_account_cohort_quarter                   as cohort_quarter,
+      quarters_since_parent_account_cohort_start      as quarters_since_cohort_start
     FROM {{ ref('retention_parent_account_') }}
 
 ), final AS (
 
-SELECT * FROM parent_account
-UNION
-SELECT * FROM sfdc_account
-UNION
-SELECT * FROM zuora_subscription
+    SELECT *
+    FROM parent_account
+
+    UNION
+
+    SELECT *
+    FROM sfdc_account
+
+    UNION
+
+    SELECT *
+    FROM zuora_subscription
+
 )
 
 
