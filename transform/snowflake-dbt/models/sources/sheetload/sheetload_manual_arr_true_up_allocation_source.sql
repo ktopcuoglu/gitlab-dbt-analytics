7ba9c7bb8f6d0dff5ac9a3ae475f52de779cc70c
iweeks <iweeks@gitlab.com>
2021-06-02 19:23:07 -0400

    remove duplicate column name

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_manual_arr_true_up_allocation_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_manual_arr_true_up_allocation_source.sql
@@ -1,11 +1,10 @@
 WITH source AS (
 
-    SELECT 
+    SELECT
       account_id::VARCHAR             AS account_id,
       crm_account_id::VARCHAR         AS crm_account_id,
       rate_plan_charge_id::VARCHAR    AS rate_plan_charge_id,
       dim_subscription_id::VARCHAR    AS dim_subcription_id,
-      subscription_name::VARCHAR      AS subscription_name,
       subscription_name::VARCHAR      AS subscription_name,
       subscription_status::VARCHAR    AS subscription_status,
       dim_product_detail_id::VARCHAR  AS dim_product_detail_id,
@@ -18,7 +17,6 @@ WITH source AS (
     FROM {{ source('sheetload','manual_arr_true_up_allocation') }}
 
 )
-  
-SELECT * 
-FROM source
 
+SELECT *
+FROM source
