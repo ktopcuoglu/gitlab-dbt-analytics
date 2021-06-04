3baf13209cf10135db3f88567411e04e11ca110b
Paul Armstrong <parmstrong@gitlab.com>
2021-06-04 22:33:47 +0200

    Changing around logic structure

--- transform/snowflake-dbt/models/common/fct_retention.sql
+++ transform/snowflake-dbt/models/common/fct_retention.sql
@@ -29,8 +29,10 @@ WITH dim_date AS (
 ), next_renewal_month AS (
 
     SELECT DISTINCT
-      merged_accounts.dim_parent_crm_account_id,
-      MIN(subscription_end_month) OVER (PARTITION BY merged_accounts.dim_parent_crm_account_id)    AS next_renewal_month
+      merged_accounts.dim_parent_crm_account_id                                                                       AS dim_parent_crm_account_id,
+      prep_product_detail.product_tier_name                                                                           AS product_tier_name,
+      MIN(subscription_end_month) OVER (PARTITION BY merged_accounts.dim_parent_crm_account_id)                       AS next_renewal_month,
+      MIN(subscription_end_month) OVER (PARTITION BY merged_accounts.dim_parent_crm_account_id, product_tier_name)    AS next_renewal_month_product
     FROM prep_recurring_charge
     INNER JOIN dim_date
       ON dim_date.date_id = prep_recurring_charge.dim_date_id
@@ -40,14 +42,18 @@ WITH dim_date AS (
       ON merged_accounts.dim_crm_account_id = COALESCE(crm_accounts.merged_to_account_id, crm_accounts.dim_crm_account_id)
     LEFT JOIN prep_subscription
       ON prep_subscription.dim_subscription_id = prep_recurring_charge.dim_subscription_id
-      AND subscription_end_month <= DATEADD('year', 1, date_actual)
+    INNER JOIN prep_product_detail
+      ON prep_product_detail.dim_product_detail_id = prep_recurring_charge.dim_product_detail_id
     WHERE subscription_end_month >= DATE_TRUNC('month',CURRENT_DATE)
+      AND subscription_end_month <= DATEADD('year', 1, date_actual)
 
 ), last_renewal_month AS (
 
     SELECT DISTINCT
-      merged_accounts.dim_parent_crm_account_id,
-      MAX(subscription_end_month) OVER (PARTITION BY merged_accounts.dim_parent_crm_account_id)    AS last_renewal_month
+      merged_accounts.dim_parent_crm_account_id                                                                       AS dim_parent_crm_account_id,
+      prep_product_detail.product_tier_name                                                                           AS product_tier_name,
+      MAX(subscription_end_month) OVER (PARTITION BY merged_accounts.dim_parent_crm_account_id)                       AS last_renewal_month,
+      MAX(subscription_end_month) OVER (PARTITION BY merged_accounts.dim_parent_crm_account_id, product_tier_name)    AS last_renewal_month_product
     FROM prep_recurring_charge
     INNER JOIN dim_date
       ON dim_date.date_id = prep_recurring_charge.dim_date_id
@@ -57,24 +63,26 @@ WITH dim_date AS (
       ON merged_accounts.dim_crm_account_id = COALESCE(crm_accounts.merged_to_account_id, crm_accounts.dim_crm_account_id)
     LEFT JOIN prep_subscription
       ON prep_subscription.dim_subscription_id = prep_recurring_charge.dim_subscription_id
-      AND subscription_end_month <= DATEADD('year', 1, date_actual)
+    INNER JOIN prep_product_detail
+      ON prep_product_detail.dim_product_detail_id = prep_recurring_charge.dim_product_detail_id
     WHERE subscription_end_month < DATE_TRUNC('month',CURRENT_DATE)
+      AND subscription_end_month <= DATEADD('year', 1, date_actual)
 
-), parent_account_mrrs AS (
+), final AS (
 
     SELECT
-      prep_crm_account.dim_parent_crm_account_id,
+      prep_crm_account.dim_parent_crm_account_id                AS dim_parent_crm_account_id,
       dim_date.date_actual                                      AS mrr_month,
       dateadd('year', 1, date_actual)                           AS retention_month,
-      next_renewal_month,
-      last_renewal_month,
-      COUNT(DISTINCT prep_crm_account.dim_parent_crm_account_id)
-                                                                AS parent_customer_count,
-      SUM(ZEROIFNULL(mrr))                                      AS mrr_total,
-      SUM(ZEROIFNULL(arr))                                      AS arr_total,
-      SUM(ZEROIFNULL(quantity))                                 AS quantity_total,
-      ARRAY_AGG(product_tier_name)                              AS product_category,
-      MAX(product_ranking)                                      AS product_ranking
+      next_renewal_month_product                                AS next_renewal_month_product,
+      last_renewal_month_product                                AS last_renewal_month_product,
+      next_renewal_month                                        AS next_renewal_month,
+      last_renewal_month                                        AS last_renewal_month,
+      ZEROIFNULL(mrr)                                           AS mrr,
+      ZEROIFNULL(arr)                                           AS arr,
+      ZEROIFNULL(quantity)                                      AS quantity,
+      prep_product_detail.product_tier_name                     AS product_tier_name,
+      prep_product_detail.product_ranking                       AS product_ranking
     FROM prep_recurring_charge
     INNER JOIN prep_product_detail
       ON prep_product_detail.dim_product_detail_id = prep_recurring_charge.dim_product_detail_id
@@ -86,77 +94,6 @@ WITH dim_date AS (
       ON next_renewal_month.dim_parent_crm_account_id = prep_crm_account.dim_parent_crm_account_id
     LEFT JOIN last_renewal_month
       ON last_renewal_month.dim_parent_crm_account_id = prep_crm_account.dim_parent_crm_account_id
-    {{ dbt_utils.group_by(n=5) }}
-
-), retention_subs AS (
-
-    SELECT
-      current_mrr.dim_parent_crm_account_id,
-      current_mrr.mrr_month             AS current_mrr_month,
-      current_mrr.retention_month,
-      current_mrr.mrr_total             AS current_mrr,
-      future_mrr.mrr_total              AS future_mrr,
-      current_mrr.arr_total             AS current_arr,
-      future_mrr.arr_total              AS future_arr,
-      current_mrr.parent_customer_count AS current_parent_customer_count,
-      future_mrr.parent_customer_count  AS future_parent_customer_count,
-      current_mrr.quantity_total        AS current_quantity,
-      future_mrr.quantity_total         AS future_quantity,
-      current_mrr.product_category      AS current_product_category,
-      future_mrr.product_category       AS future_product_category,
-      current_mrr.product_ranking       AS current_product_ranking,
-      future_mrr.product_ranking        AS future_product_ranking,
-      current_mrr.last_renewal_month,
-      current_mrr.next_renewal_month,
-      --The type of arr change requires a row_number. Row_number = 1 indicates new in the macro; however, for retention, new is not a valid option since retention starts in month 12, well after the First Order transaction.
-      2                              AS row_number
-    FROM parent_account_mrrs AS current_mrr
-    LEFT JOIN parent_account_mrrs AS future_mrr
-      ON current_mrr.dim_parent_crm_account_id = future_mrr.dim_parent_crm_account_id
-        AND current_mrr.retention_month = future_mrr.mrr_month
-
-), final AS (
-
-    SELECT
-    {{ dbt_utils.surrogate_key(['retention_subs.dim_parent_crm_account_id','retention_month']) }}
-                                                AS fct_retention_id,
-      retention_subs.dim_parent_crm_account_id  AS dim_parent_crm_account_id,
-      prep_crm_account.crm_account_name         AS parent_crm_account_name,
-      retention_month,
-      dim_date.fiscal_year                      AS retention_fiscal_year,
-      dim_date.fiscal_quarter                   AS retention_fiscal_quarter,
-      retention_subs.last_renewal_month,
-      retention_subs.next_renewal_month,
-      current_mrr                               AS prior_year_mrr,
-      COALESCE(future_mrr, 0)                   AS net_retention_mrr,
-      CASE WHEN net_retention_mrr > 0
-        THEN least(net_retention_mrr, current_mrr)
-        ELSE 0 END                              AS gross_retention_mrr,
-      current_arr                               AS prior_year_arr,
-      COALESCE(future_arr, 0)                   AS net_retention_arr,
-      CASE WHEN net_retention_arr > 0
-        THEN least(net_retention_arr, current_arr)
-        ELSE 0 END                              AS gross_retention_arr,
-      current_quantity                          AS prior_year_quantity,
-      COALESCE(future_quantity, 0)              AS net_retention_quantity,
-      current_parent_customer_count             AS prior_year_parent_customer_count,
-      COALESCE(future_parent_customer_count, 0) AS net_retention_parent_customer_count,
-      {{ reason_for_quantity_change_seat_change('net_retention_quantity', 'prior_year_quantity') }},
-      future_product_category                   AS net_retention_product_category,
-      current_product_category                  AS prior_year_product_category,
-      future_product_ranking                    AS net_retention_product_ranking,
-      current_product_ranking                   AS prior_year_product_ranking,
-      {{ type_of_arr_change('net_retention_arr', 'prior_year_arr','row_number') }},
-      {{ reason_for_arr_change_seat_change('net_retention_quantity', 'prior_year_quantity', 'net_retention_arr', 'prior_year_arr') }},
-      {{ reason_for_arr_change_price_change('net_retention_product_category', 'prior_year_product_category', 'net_retention_quantity', 'prior_year_quantity', 'net_retention_arr', 'prior_year_arr', 'net_retention_product_ranking','prior_year_product_ranking') }},
-      {{ reason_for_arr_change_tier_change('net_retention_product_ranking', 'prior_year_product_ranking', 'net_retention_quantity', 'prior_year_quantity', 'net_retention_arr', 'prior_year_arr') }},
-      {{ annual_price_per_seat_change('net_retention_quantity', 'prior_year_quantity', 'net_retention_arr', 'prior_year_arr') }}
-    FROM retention_subs
-    INNER JOIN dim_date
-      ON dim_date.date_actual = retention_subs.retention_month
-    LEFT JOIN prep_crm_account
-      ON prep_crm_account.dim_crm_account_id = retention_subs.dim_parent_crm_account_id
-    WHERE retention_month <= dateadd(month, -1, CURRENT_DATE)
 
 )
 
