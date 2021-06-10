3a154396dff60c119bdbdf8eeaa8c0177ed132ac
Ian Schweickart <ian.schweickart@decisivedata.net>
2021-06-10 13:56:04 -0700

    Rebase: updated grain to include uuid and hostname, and added billable user and max historical user counts from usage ping in self-managed tables

--- transform/snowflake-dbt/macros/version/usage_ping_over_ping_smoothed.sql
+++ transform/snowflake-dbt/macros/version/usage_ping_over_ping_smoothed.sql
@@ -1,4 +1,4 @@
-{%- macro usage_ping_over_ping_smoothed(all_time_event_metric_column, partition_by_column = 'dim_subscription_id', order_by_column = 'snapshot_month', days_since_last_ping = 'days_since_last_ping', days_in_month = 'days_in_month_count') -%} 
+{%- macro usage_ping_over_ping_smoothed(all_time_event_metric_column, partion_by_columns = ['dim_subscription_id' ,'uuid', 'hostname'], order_by_column = 'snapshot_month', days_since_last_ping = 'days_since_last_ping', days_in_month = 'days_in_month_count') -%} 
 
     {%- set events_since_last_ping =  all_time_event_metric_column ~ '_since_last_ping' -%}
     {%- set events_per_day_alias =  all_time_event_metric_column ~ '_estimated_daily' -%}
@@ -8,7 +8,10 @@
     {{ events_since_last_ping }},
     FIRST_VALUE({{ events_since_last_ping }} / {{ days_since_last_ping }})
         IGNORE NULLS OVER (
-          PARTITION BY {{ partition_by_column }}
+          PARTITION BY
+            {{ partion_by_columns[0] }},
+            {{ partion_by_columns[1] }},
+            {{ partion_by_columns[2] }}
           ORDER BY {{ order_by_column }}
           ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
         )                                                                   AS {{ events_per_day_alias }},
--- transform/snowflake-dbt/models/marts/pumps/pumps.md
+++ transform/snowflake-dbt/models/marts/pumps/pumps.md
@@ -36,6 +36,15 @@ Information on the Enterprise Dimensional Model can be found in the [handbook](h
 
 {% enddocs %}
 
+{% docs mart_product_usage_free_user_metrics_monthly %}
+This table unions the sets of all Self-Managed and SaaS **free users**. The data from this table will be used for Salesforce Customer Product Insights.
+
+The grain of this table is namespace || uuid-hostname per month.
+
+Information on the Enterprise Dimensional Model can be found in the [handbook](https://about.gitlab.com/handbook/business-ops/data-team/platform/edw/)
+
+{% enddocs %}
+
 {% docs pump_hash_marketing_contact %}
 
 a copy of mart_marketing_contact with the email hashed
