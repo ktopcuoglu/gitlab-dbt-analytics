6c38d87ee5d3d7ebd69fab8c729e0d7f3c05af3e
mpeychet <mpeychet@gitlab.com>
2021-06-10 17:09:13 +0200

    working version

--- transform/snowflake-dbt/models/common_mapping/prep/prep_ci_pipeline.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_ci_pipeline.sql
@@ -17,7 +17,7 @@
       ci_pipeline_id AS dim_ci_pipeline_id, 
       
       -- FOREIGN KEYS
-      gitlab_dotcom_events_source dim_project_id,
+      gitlab_dotcom_ci_pipelines_source.project_id      AS dim_project_id,
       prep_project.dim_namespace_id,
       prep_project.ultimate_parent_namespace_id,
       prep_user.dim_user_id,
@@ -25,31 +25,31 @@
       IFNULL(dim_namespace_plan_hist.dim_plan_id, 34)   AS dim_plan_id,
       merge_request_id,
 
-      created_at, 
-      started_at, 
-      committed_at,
-      finished_at, 
-      ci_pipeline_duration, 
+      gitlab_dotcom_ci_pipelines_source.created_at, 
+      gitlab_dotcom_ci_pipelines_source.started_at, 
+      gitlab_dotcom_ci_pipelines_source.committed_at,
+      gitlab_dotcom_ci_pipelines_source.finished_at, 
+      gitlab_dotcom_ci_pipelines_source.ci_pipeline_duration, 
 
-      status, 
-      ref,
-      has_tag, 
-      yaml_errors, 
-      lock_version, 
-      auto_canceled_by_id, 
-      pipeline_schedule_id, 
-      ci_pipeline_source, 
-      config_source, 
-      is_protected, 
-      failure_reason, 
-      ci_pipeline_iid
-    FROM gitlab_dotcom_ci_pipelines_dedupe_source
-    LEFT JOIN prep_project ON gitlab_dotcom_ci_pipelines_dedupe_source.project_id = prep_project.dim_project_id
+      gitlab_dotcom_ci_pipelines_source.status, 
+      gitlab_dotcom_ci_pipelines_source.ref,
+      gitlab_dotcom_ci_pipelines_source.has_tag, 
+      gitlab_dotcom_ci_pipelines_source.yaml_errors, 
+      gitlab_dotcom_ci_pipelines_source.lock_version, 
+      gitlab_dotcom_ci_pipelines_source.auto_canceled_by_id, 
+      gitlab_dotcom_ci_pipelines_source.pipeline_schedule_id, 
+      gitlab_dotcom_ci_pipelines_source.ci_pipeline_source, 
+      gitlab_dotcom_ci_pipelines_source.config_source, 
+      gitlab_dotcom_ci_pipelines_source.is_protected, 
+      gitlab_dotcom_ci_pipelines_source.failure_reason, 
+      gitlab_dotcom_ci_pipelines_source.ci_pipeline_iid
+    FROM gitlab_dotcom_ci_pipelines_source
+    LEFT JOIN prep_project ON gitlab_dotcom_ci_pipelines_source.project_id = prep_project.dim_project_id
     LEFT JOIN dim_namespace_plan_hist ON prep_project.ultimate_parent_namespace_id = dim_namespace_plan_hist.dim_namespace_id
-        AND gitlab_dotcom_ci_pipelines_dedupe_source.created_at >= dim_namespace_plan_hist.valid_from
-        AND gitlab_dotcom_ci_pipelines_dedupe_source.created_at < dim_namespace_plan_hist.valid_to
-    LEFT JOIN prep_user ON gitlab_dotcom_ci_pipelines_dedupe_source.user_id = prep_user.dim_user_id
-    LEFT JOIN dim_date ON TO_DATE(gitlab_dotcom_ci_pipelines_dedupe_source.created_at) = dim_date.date_day
+        AND gitlab_dotcom_ci_pipelines_source.created_at >= dim_namespace_plan_hist.valid_from
+        AND gitlab_dotcom_ci_pipelines_source.created_at < dim_namespace_plan_hist.valid_to
+    LEFT JOIN prep_user ON gitlab_dotcom_ci_pipelines_source.user_id = prep_user.dim_user_id
+    LEFT JOIN dim_date ON TO_DATE(gitlab_dotcom_ci_pipelines_source.created_at) = dim_date.date_day
 
 )
 
