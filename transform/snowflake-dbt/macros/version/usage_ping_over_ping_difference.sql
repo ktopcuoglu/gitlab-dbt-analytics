29eee3960132dd1459e37ce9223fe117fa438af8
Ian Schweickart <ian.schweickart@decisivedata.net>
2021-06-15 14:15:32 -0400

    replaced list of partition by columns with for loop in diff table macros

--- transform/snowflake-dbt/macros/version/usage_ping_over_ping_difference.sql
+++ transform/snowflake-dbt/macros/version/usage_ping_over_ping_difference.sql
@@ -1,4 +1,4 @@
-{%- macro usage_ping_over_ping_difference(all_time_event_metric_column, partion_by_columns = ['dim_subscription_id' ,'uuid', 'hostname'], order_by_column = 'snapshot_month') -%} 
+{%- macro usage_ping_over_ping_difference(all_time_event_metric_column, partition_by_columns = ['dim_subscription_id' ,'uuid', 'hostname'], order_by_column = 'snapshot_month') -%} 
 
     {%- set ping_over_ping_alias =  all_time_event_metric_column ~ '_since_last_ping' -%}
 
@@ -6,9 +6,10 @@
     {{ all_time_event_metric_column }} - LAG({{ all_time_event_metric_column }})
       IGNORE NULLS OVER (
         PARTITION BY
-          {{ partion_by_columns[0] }},
-          {{ partion_by_columns[1] }},
-          {{ partion_by_columns[2] }}
+          {%- for column in partition_by_columns %}
+          {{ column }}
+          {%- if not loop.last -%},{% endif %}
+          {%- endfor %}
         ORDER BY {{ order_by_column }}
       )                                                                         AS {{ ping_over_ping_alias }}
 
