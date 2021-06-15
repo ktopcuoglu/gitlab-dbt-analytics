3c65a42b08c08de30f87824688da08f338f56bfb
Ian Schweickart <ian.schweickart@decisivedata.net>
2021-06-15 14:15:32 -0400

    replaced list of partition by columns with for loop in diff table macros

--- transform/snowflake-dbt/macros/version/usage_ping_month_range.sql
+++ transform/snowflake-dbt/macros/version/usage_ping_month_range.sql
@@ -1,4 +1,4 @@
-{%- macro usage_ping_month_range(all_time_event_metric_column, month_column = 'snapshot_month', partion_by_columns = ['dim_subscription_id' ,'uuid', 'hostname']) -%} 
+{%- macro usage_ping_month_range(all_time_event_metric_column, month_column = 'snapshot_month', partition_by_columns = ['dim_subscription_id', 'uuid', 'hostname']) -%} 
 
     {%- set first_month_alias =  all_time_event_metric_column ~ '_first_ping_month' -%}
     {%- set last_month_alias =  all_time_event_metric_column ~ '_last_ping_month' -%}
@@ -8,18 +8,20 @@
             NULL)
         ) OVER (
             PARTITION BY
-              {{ partion_by_columns[0] }},
-              {{ partion_by_columns[1] }},
-              {{ partion_by_columns[2] }}
+              {%- for column in partition_by_columns %}
+              {{ column }}
+              {%- if not loop.last -%},{% endif %}
+              {%- endfor %}
         )                                                       AS {{ first_month_alias }},
     MAX(IFF({{ all_time_event_metric_column }} IS NOT NULL,
             {{ month_column }},
             NULL)
         ) OVER (
             PARTITION BY
-              {{ partion_by_columns[0] }},
-              {{ partion_by_columns[1] }},
-              {{ partion_by_columns[2] }}
+              {%- for column in partition_by_columns %}
+              {{ column }}
+              {%- if not loop.last -%},{% endif %}
+              {%- endfor %}
         )                                                       AS {{ last_month_alias }}
 
 {%- endmacro -%}
--- transform/snowflake-dbt/macros/version/usage_ping_over_ping_smoothed.sql
+++ transform/snowflake-dbt/macros/version/usage_ping_over_ping_smoothed.sql
@@ -1,4 +1,4 @@
-{%- macro usage_ping_over_ping_smoothed(all_time_event_metric_column, partion_by_columns = ['dim_subscription_id' ,'uuid', 'hostname'], order_by_column = 'snapshot_month', days_since_last_ping = 'days_since_last_ping', days_in_month = 'days_in_month_count') -%} 
+{%- macro usage_ping_over_ping_smoothed(all_time_event_metric_column, partition_by_columns = ['dim_subscription_id' ,'uuid', 'hostname'], order_by_column = 'snapshot_month', days_since_last_ping = 'days_since_last_ping', days_in_month = 'days_in_month_count') -%} 
 
     {%- set events_since_last_ping =  all_time_event_metric_column ~ '_since_last_ping' -%}
     {%- set events_per_day_alias =  all_time_event_metric_column ~ '_estimated_daily' -%}
@@ -9,9 +9,10 @@
     FIRST_VALUE({{ events_since_last_ping }} / {{ days_since_last_ping }})
         IGNORE NULLS OVER (
           PARTITION BY
-            {{ partion_by_columns[0] }},
-            {{ partion_by_columns[1] }},
-            {{ partion_by_columns[2] }}
+            {%- for column in partition_by_columns %}
+            {{ column }}
+            {%- if not loop.last -%},{% endif %}
+            {%- endfor %}
           ORDER BY {{ order_by_column }}
           ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
         )                                                                   AS {{ events_per_day_alias }},
