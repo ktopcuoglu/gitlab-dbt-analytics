7dbfc3b0f43794cd03c6197901e6d400de3bcb08
Jean Del Rosario <jpeguero@gitlab.com>
2021-06-15 16:37:02 -0400

    add 18 character length user_id

--- transform/snowflake-dbt/models/legacy/sheetload/sheetload_mapping_sdr_sfdc_bamboohr.sql
+++ transform/snowflake-dbt/models/legacy/sheetload/sheetload_mapping_sdr_sfdc_bamboohr.sql
@@ -1,9 +1,27 @@
-WITH source AS (
+WITH mapping_table AS (
 
     SELECT *
     FROM {{ ref('sheetload_mapping_sdr_sfdc_bamboohr_source') }}
 
+), sfdc_users AS (
+
+    SELECT
+      *,
+      substr(user_id, 1, 15)         AS trim_mapping
+    FROM {{ ref('sfdc_users_source')}}
+
 )
 
-SELECT *
-FROM source
+SELECT
+  sfdc_users.user_id,
+  mapping_table.user_id              AS fifteen_length_user_id,
+  mapping_table.first_name,
+  mapping_table.last_name,
+  mapping_table.username,
+  mapping_table.active,
+  mapping_table.profile,
+  mapping_table.eeid
+
+FROM mapping_table
+LEFT JOIN sfdc_users
+  ON mapping_table.user_id = sfdc_users.trim_mapping
