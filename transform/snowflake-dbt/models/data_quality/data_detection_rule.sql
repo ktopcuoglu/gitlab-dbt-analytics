8bbd8d2fd3f37bf3a7f1fac27f272dbbb36682e4
snalamaru <snalamaru@gitlab.com>
2021-06-15 17:09:18 -0500

    Updating models

--- transform/snowflake-dbt/models/data_quality/data_detection_rule.sql
+++ transform/snowflake-dbt/models/data_quality/data_detection_rule.sql
@@ -9,7 +9,8 @@ WITH detection_rule AS (
           1 as rule_id,
           'Missing instance types' as rule_name,
           'Missing instance types for UUID/hostname' as rule_description,
-          'Product' as type_of_data
+          'Product' as type_of_data,
+          50 AS threshold
 
    UNION
 
@@ -26,7 +27,8 @@ WITH detection_rule AS (
           3 as rule_id,
           'Subscription with missing Licenses' as rule_name,
           'Subscription IDs that do not have an associated License ID' as rule_description,
-          'Product' as type_of_data
+          'Product' as type_of_data,
+          50 AS threshold
 
     UNION
 
@@ -34,7 +36,8 @@ WITH detection_rule AS (
           4 as rule_id,
           'Subscriptions with Self-Managed Plans having License Start dates in the future' as rule_name,
           'Subscription IDs with Self-Managed Plans having license_start_date in the future' as rule_description,
-          'Product' as type_of_data
+          'Product' as type_of_data,
+          50 AS threshold
 
     UNION
 
@@ -42,7 +45,8 @@ WITH detection_rule AS (
           5                                                                                                 AS rule_id,
           'Subscriptions with Self-Managed Plans having License Start Date greater than Licese Expire date' AS rule_name,
           'Subscriptions with Self-Managed Plans having license_start_date greater than licese_expire_date' AS rule_description,
-          'Product'                                                                                         AS type_of_data
+          'Product'                                                                                         AS type_of_data,
+          50 AS threshold
 
     UNION
 
@@ -50,7 +54,8 @@ WITH detection_rule AS (
            6 as rule_id,
           'Expired Licenses' as rule_name,
           'License IDs with Subscription End Dates in the Past' as rule_description,
-          'Product' as type_of_data
+          'Product' as type_of_data,
+          50 AS threshold
 
     UNION 
 
@@ -58,7 +63,8 @@ WITH detection_rule AS (
           7 as rule_id,
           'SaaS Subscriptions Not Mapped to Namespaces' as rule_name,
           'SaaS Subscription IDs that do not have any associated Namespace IDs' as rule_description,
-          'Product' as type_of_data
+          'Product' as type_of_data,
+          50 AS threshold
               
 )
 
--- transform/snowflake-dbt/models/data_quality/product_data_detection_rule_run_detail.sql
+++ transform/snowflake-dbt/models/data_quality/product_data_detection_rule_run_detail.sql
@@ -36,6 +36,26 @@ WITH dim_date AS (
     SELECT *
     FROM {{ ref('bdg_namespace_order_subscription') }}
 
+) , map_subscription_license_all AS (
+
+    SELECT DISTINCT
+        dim_subscription.dim_subscription_id, 
+        dim_license.dim_license_id 
+    FROM dim_subscription 
+    LEFT OUTER JOIN dim_license 
+    ON s.dim_subscription_id = l.dim_subscription_id
+
+) , map_license_all AS (
+
+    SELECT DISTINCT
+        map_license_subscription_account.dim_license_id, 
+        map_license_subscription_account.license_md5, 
+        map_license_subscription_account.dim_subscription_id, 
+        dim_license.license_start_date
+    FROM map_license_subscription_account 
+    LEFT OUTER JOIN dim_license
+    ON map_license_subscription_account.dim_license_id = dim_license.dim_license_id
+
 ), rule_run_date AS (
 
    SELECT distinct
@@ -43,9 +63,10 @@ WITH dim_date AS (
           'Product' as type_of_data
     FROM dim_date
 
+
 ), processed_record_count AS (
 
-  SELECT 
+    SELECT 
         1 as rule_id,
         count(DISTINCT(instance_uuid))                                        AS processed_record_count,
         (SELECT count(DISTINCT(instance_uuid)) FROM dim_host_instance_type
@@ -53,52 +74,61 @@ WITH dim_date AS (
         (SELECT count(DISTINCT(instance_uuid)) FROM dim_host_instance_type
          WHERE INSTANCE_TYPE in ('Unknown', NULL, ''))                        AS failed_record_count,
          dbt_updated_at
-  FROM dim_host_instance_type
+    FROM dim_host_instance_type
 
   UNION 
 
-  SELECT 
+    SELECT 
         2                                                                                                AS rule_id,
         count(DISTINCT(dim_license_id)) AS processed_record_count,
         (SELECT COUNT(DISTINCT(dim_license_id)) FROM dim_license WHERE dim_subscription_id IS NOT NULL)  AS passed_record_count,
         (SELECT COUNT(DISTINCT(dim_license_id)) FROM dim_license WHERE dim_subscription_id IS NULL)      AS failed_record_count,
         updated_date
-  FROM dim_license
+    FROM dim_license
 
   UNION
 
- /* SELECT 3 as rule_id,
-        count(DISTINCT(dim_subscription_id)) AS processed_record_count,
-        (SELECT COUNT(DISTINCT(dim_subscription_id)) FROM dim_subscription WHERE dim_license_id IS NOT NULL)  AS passed_record_count,
-        (SELECT COUNT(DISTINCT(dim_susbcription_id)) FROM dim_subscription WHERE dim_license_id IS NULL)      AS failed_record_count
-  FROM dim_subscription
+    SELECT 3                                                                                                          AS rule_id,
+        count(DISTINCT(dim_subscription_id))                                                                        AS processed_record_count,
+        (SELECT COUNT(DISTINCT(dim_subscription_id)) FROM dim_subscription WHERE dim_license_id IS NOT NULL)        AS passed_record_count,
+        (SELECT COUNT(DISTINCT(dim_susbcription_id)) FROM map_subscription_license_all WHERE dim_license_id is null) AS failed_record_count                                                                     AS failed_record_count
+    FROM dim_subscription
+  
+  UNION 
 
-  UNION */
-
-  SELECT 
+    SELECT 
         4 as rule_id,
         count(DISTINCT(dim_subscription_id)) AS processed_record_count,
-        (SELECT COUNT(DISTINCT(dim_license_id)) FROM dim_license WHERE license_start_date <= license_expire_date)  AS passed_record_count,
-        (SELECT COUNT(DISTINCT(dim_license_id)) FROM dim_license WHERE license_start_date > license_expire_date)   AS failed_record_count,
+        (SELECT COUNT(DISTINCT(dim_license_id)) FROM dim_license WHERE starts_at <= CURRENT_DATE)  AS passed_record_count,
+        (SELECT COUNT(DISTINCT(dim_license_id)) FROM dim_license WHERE starts_at > CURRENT_DATE)   AS failed_record_count,
         dbt_updated_at
-  FROM map_license_subscription_account ---join with SM table to get SM plans only -  either with bdg_subscription_product_rate_plan or final SM table
+    FROM map_license_subscription_account 
 
-UNION 
+  UNION 
 
-  SELECT 
+    SELECT 
         5 as rule_id,
         count(DISTINCT(dim_license_id)) AS processed_record_count
 
-  FROM dim_license 
+    FROM dim_license 
 
-UNION 
+  UNION 
 
-  SELECT 
-        6 as rule_id,
+    SELECT 
+        7 as rule_id,
         count(DISTINCT(dim_subscription_id)) AS processed_record_count,
-        (SELECT COUNT(DISTINCT(dim_subscription_id) FROM bdg_namespace_order_subscription WHERE dim_subscription_id IS NOT NULL AND dim_namespace_id IS NOT NULL)  AS  passed_record_count,
-        (SELECT COUNT(DISTINCT(dim_subscription_id) FROM bdg_namespace_order_subscription WHERE dim_subscription_id IS NOT NULL AND dim_namespace_id IS NULL)      AS  failed_record_count,
-  FROM bdg_namespace_order_subscription 
+        (SELECT COUNT(DISTINCT(dim_subscription_id)) FROM bdg_namespace_order_subscription WHERE dim_subscription_id IS NOT NULL AND dim_namespace_id IS NOT NULL)  AS  passed_record_count,
+        (SELECT COUNT(DISTINCT(dim_subscription_id)) FROM bdg_namespace_order_subscription WHERE dim_subscription_id IS NOT NULL AND dim_namespace_id IS NULL)      AS  failed_record_count,
+   FROM bdg_namespace_order_subscription 
+
+  UNION 
+
+    SELECT 
+       7 as rule_id,
+       count(DISTINCT(dim_subscription_id)) AS processed_record_count,
+       (SELECT COUNT(DISTINCT(dim_subscription_id)) FROM bdg_namespace_order_subscription WHERE dim_subscription_id IS NOT NULL AND dim_namespace_id IS NOT NULL)  AS  passed_record_count,
+       (SELECT COUNT(DISTINCT(dim_subscription_id)) FROM bdg_namespace_order_subscription WHERE dim_subscription_id IS NOT NULL AND dim_namespace_id IS NULL)      AS  failed_record_count,
+    FROM bdg_namespace_order_subscription 
 
 )
 
