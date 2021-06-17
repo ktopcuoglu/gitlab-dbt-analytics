1ad7f26235d7ff2b507a7a39ec7c65cbae1f8df6
snalamaru <snalamaru@gitlab.com>
2021-06-17 14:29:19 -0500

    New model for run_result

--- transform/snowflake-dbt/models/data_quality/product_data_detection_run_result.sql
+++ transform/snowflake-dbt/models/data_quality/product_data_detection_run_result.sql
@@ -1,30 +1,49 @@
-{{config({ 
-    "schema": "data_quality" 
- })
+{{config({
+   "schema": "data_quality"
+})
 }}
-
-
+ 
+ 
 WITH detection_rule AS (
-
- SELECT * 
- FROM {{ ref('product_data_detection_run_result') }}
-
-), rule_run_detail AS ( 
-    
-    SELECT * 
-    FROM {{ ref('product_data_detection_run_detail') }}
-
+ 
+    SELECT *
+    FROM {{ ref('data_detection_rule') }}
+ 
+), rule_run_detail AS (
+  
+   SELECT 
+        rule_id,
+        processed_record_count,
+        passed_record_count,
+        failed_record_count,
+        ((passed_record_count/processed_record_count)*100) AS percent_of_records_passed,
+        ((failed_record_count/processed_record_count)*100) AS percent_of_records_failed,
+        rule_run_date,
+        type_of_data
+   FROM {{ ref('product_data_detection_run_detail') }}
+ 
+), final
+ 
+    SELECT DISTINCT
+        detection_rule.rule_id,
+        detection_rule.rule_name,
+        detection_rule.rule_description,
+        rule_run_detail.rule_run_date,
+        rule_run_detail.percent_of_records_passed,
+        rule_run_detail.percent_of_records_failed,
+        CASE WHEN percent_of_records_passed > threshold THEN 'PASS' ELSE 'FAIL' END AS pass_fail_flag,
+        rule_run_detail.type_of_data
+    FROM rule_run_detail
+    LEFT OUTER JOIN ON detection_rule
+    rule_run_detail.rule_id = detection_rule.rule_id
+ 
 )
+ 
+{{ dbt_audit(
+   cte_ref="detection_rule",
+   created_by="@snalamaru",
+   updated_by="@snalamaru",
+   created_date="2021-06-16",
+   updated_date="2021-06-16"
+) }}
 
-
-
-SELECT 
-    detection_rule.rule_id, 
-    detection_rule.rule_name, 
-    detection_rule.rule_description, 
-    rule_run_detail.rule_run_date, 
-    rule_run_detail.processed_record_count,
-    rule_run_detail.passed_record_count, 
-    rule_run_detail.failed_record_count,
-
- rule_run_detail.type_of_dataFROM rule_run_detailLEFT OUTER JOIN detection_rule ONrule_id = rule_id
