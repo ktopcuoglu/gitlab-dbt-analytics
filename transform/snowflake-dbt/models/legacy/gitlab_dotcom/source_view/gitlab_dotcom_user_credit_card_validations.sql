db206ee2d0e9660014037792605a5a4b324221ce
@dvanrooijen2 <dvanrooijen@gitlab.com>
2021-06-18 11:51:33 +0200

    commit

---
+++ transform/snowflake-dbt/models/legacy/gitlab_dotcom/source_view/gitlab_dotcom_user_credit_card_validations.sql
@@ -0,0 +1,14 @@
+{{ config({
+    "schema": "legacy"
+    })
+}}
+
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_user_credit_card_validations_source') }}
+
+)
+
+SELECT *
+FROM source
