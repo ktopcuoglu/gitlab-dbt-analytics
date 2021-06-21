f65aba00e28fefd44e1fae8fd10ac4c238773e05
Paul Armstrong <parmstrong@gitlab.com>
2021-06-21 16:11:42 +0200

    Added jihu account exclusion.

--- transform/snowflake-dbt/models/marts/arr/incr_snapshots/mart_retention_parent_account_snapshot_model.sql
+++ transform/snowflake-dbt/models/marts/arr/incr_snapshots/mart_retention_parent_account_snapshot_model.sql
@@ -19,21 +19,34 @@ WITH snapshot_dates AS (
 
    {% endif %}
 
+), dim_crm_account AS (
+
+    SELECT *
+    FROM {{ ref('dim_crm_account') }}
+
 ), mart_retention_parent_account AS (
 
-    SELECT
-      *
+    SELECT *
     FROM {{ ref('mart_retention_parent_account_snapshot_base') }}
+
+), filtered_retention AS (
+
+    SELECT
+      mart_retention_parent_account.*
+    FROM mart_retention_parent_account
+    JOIN dim_crm_account
+    ON dim_crm_account.dim_parent_crm_account_ID = mart_retention_parent_account.dim_crm_account_id
+    WHERE is_jihu_account = FALSE
 
 ), mart_retention_parent_account_spined AS (
 
     SELECT
       snapshot_dates.date_id AS snapshot_id,
-      mart_retention_parent_account.*
-    FROM mart_retention_parent_account
+      filtered_retention.*
+    FROM filtered_retention
     INNER JOIN snapshot_dates
-      ON snapshot_dates.date_actual >= mart_retention_parent_account.dbt_valid_from
-      AND snapshot_dates.date_actual < {{ coalesce_to_infinity('mart_retention_parent_account.dbt_valid_to') }}
+      ON snapshot_dates.date_actual >= filtered_retention.dbt_valid_from
+      AND snapshot_dates.date_actual < {{ coalesce_to_infinity('filtered_retention.dbt_valid_to') }}
 
 ), final AS (
 
