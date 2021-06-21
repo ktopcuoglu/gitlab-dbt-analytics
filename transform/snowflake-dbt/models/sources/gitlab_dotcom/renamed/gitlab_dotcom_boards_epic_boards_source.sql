5820a78ee5e2f4263099b9a51e080c632ae4e8de
@dvanrooijen2 <dvanrooijen@gitlab.com>
2021-06-21 12:05:55 +0200

    renamed

---
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_boards_epic_boards_source.sql
@@ -0,0 +1,21 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_boards_epic_boards') }}
+
+), renamed AS (
+
+    SELECT
+      id::NUMBER                  as id
+      hide_backlog_list::BOOLEAN  as hide_backlog_list
+      hide_closed_list::BOOLEAN   as hide_closed_list
+      group_id::NUMBER            as group_id
+      created_at::TIMESTAMP       as created_at
+      updated_at::TIMESTAMP       as updated_at
+      name::VARCHAR               as name
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
