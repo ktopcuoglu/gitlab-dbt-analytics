8e346da9cb68c54b253ffe0734b64e23a9602047
Paul Armstrong <parmstrong@gitlab.com>
2021-06-22 13:43:14 +0200

    Corrected column being selected.

--- transform/snowflake-dbt/models/marts/arr/incr_snapshots/mart_retention_parent_account_snapshot_model.sql
+++ transform/snowflake-dbt/models/marts/arr/incr_snapshots/mart_retention_parent_account_snapshot_model.sql
@@ -38,7 +38,7 @@ WITH snapshot_dates AS (
 ), final AS (
 
     SELECT
-     {{ dbt_utils.surrogate_key(['snapshot_id', 'primary_key']) }} AS mart_retention_parent_account_snapshot_id,
+     {{ dbt_utils.surrogate_key(['snapshot_id', 'fct_retention_id']) }} AS mart_retention_parent_account_snapshot_id,
        *
     FROM mart_retention_parent_account_spined
 
