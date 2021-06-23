3cf7e74254334b55a98f8330541c06468a6fd99a
snalamaru <snalamaru@gitlab.com>
2021-06-23 12:26:22 -0500

    Cosmetic changes

--- transform/snowflake-dbt/models/data_quality/data_detection_rule.sql
+++ transform/snowflake-dbt/models/data_quality/data_detection_rule.sql
@@ -6,65 +6,65 @@
 WITH detection_rule AS (
 
     SELECT 
-          1 as rule_id,
-          'Missing instance types' as rule_name,
-          'Missing instance types for UUID/hostname' as rule_description,
-          'Product' as type_of_data,
+          1                                          AS rule_id,
+          'Missing instance types'                   AS rule_name,
+          'Missing instance types for UUID/hostname' AS rule_description,
+          'Product'                                  AS type_of_data,
           50 AS threshold
 
    UNION
 
     SELECT 
-          2 as rule_id,
-          'Licenses with missing Subscriptions' as rule_name,
-          'License IDs that do not have an associated Subscription ID' as rule_description,
-          'Product' AS type_of_data,
+          2                                                             AS rule_id,
+          'Licenses with missing Subscriptions'                         AS rule_name,
+          'License IDs that do not have an associated Subscription ID'  AS rule_description,
+          'Product'                                                     AS type_of_data,
            50 AS threshold
 
     UNION
 
       SELECT 
-          3 as rule_id,
-          'Subscription with missing Licenses' as rule_name,
-          'Subscription IDs that do not have an associated License ID' as rule_description,
-          'Product' as type_of_data,
-          50 AS threshold
+          3                                                             AS rule_id,
+          'Subscription with missing Licenses'                          AS rule_name,
+          'Subscription IDs that do not have an associated License ID'  AS rule_description,
+          'Product'                                                     AS type_of_data,
+          50                                                            AS threshold
 
     UNION
 
       SELECT 
-          4 as rule_id,
-          'Subscriptions with Self-Managed Plans having License Start dates in the future' as rule_name,
-          'Subscription IDs with Self-Managed Plans having license_start_date in the future' as rule_description,
-          'Product' as type_of_data,
-          50 AS threshold
+          4                                                                                  AS rule_id,
+          'Subscriptions with Self-Managed Plans having License Start dates in the future'   AS rule_name,
+          'Subscription IDs with Self-Managed Plans having license_start_date in the future' AS rule_description,
+          'Product'                                                                          AS type_of_data,
+          50                                                                                 AS threshold
 
     UNION
 
       SELECT 
-          5                                                                                                 AS rule_id,
+          5                                                                                                  AS rule_id,
           'Subscriptions with Self-Managed Plans having License Start Date greater than License Expire date' AS rule_name,
           'Subscriptions with Self-Managed Plans having license_start_date greater than license_expire_date' AS rule_description,
-          'Product'                                                                                         AS type_of_data,
-          50 AS threshold
+          'Product'                                                                                          AS type_of_data,
+          50                                                                                                 AS threshold
 
     UNION
 
       SELECT 
-           6 as rule_id,
-          'Expired Licenses with Subscription End Dates in the Past' as rule_name,
-          'Expired License IDs with Subscription End Dates in the Past' as rule_description,
-          'Product' as type_of_data,
-          50 AS threshold
+           6                                                            AS rule_id,
+          'Expired Licenses with Subscription End Dates in the Past'    AS rule_name,
+          'Expired License IDs with Subscription End Dates in the Past' AS rule_description,
+          'Product'                                                     AS type_of_data,
+          50                                                            AS threshold
 
     UNION 
 
       SELECT 
-          7 as rule_id,
-          'SaaS Subscriptions Not Mapped to Namespaces' as rule_name,
-          'SaaS Subscription IDs that do not have any associated Namespace IDs' as rule_description,
-          'Product' as type_of_data,
-          50 AS threshold
+          7                                                                     AS rule_id,
+          'SaaS Subscriptions Not Mapped to Namespaces'                         AS rule_name,
+          'SaaS Subscription IDs that do not have any associated Namespace IDs' AS rule_description,
+          'Product'                                                             AS type_of_data,
+          50                                                                    AS threshold
               
 )
 
