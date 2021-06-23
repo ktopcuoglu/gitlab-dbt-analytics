e1d145c7c47b6459b91090f3a20b14ed338dff0a
Michael Walker <mwalker@gitlab.com>
2021-06-23 19:47:28 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/data_quality/product_data_detection_run_detail.sql
+++ transform/snowflake-dbt/models/data_quality/product_data_detection_run_detail.sql
@@ -10,7 +10,7 @@
 WITH rule_run_date AS (
 
    SELECT distinct
-           date_day as rule_run_date,
+           date_day AS rule_run_date,
           'Product' AS type_of_data
     FROM {{ ref('dim_date') }}
     WHERE rule_run_date BETWEEN '2021-06-23' AND CURRENT_DATE --date when the code would be pushed to Production,we would be joining this with the dbt updated data for the models.
