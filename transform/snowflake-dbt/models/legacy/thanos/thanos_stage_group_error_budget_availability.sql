e2a0cdd1d2802f37ecdfbf6ec0cf09ae85504da5
Michael Walker <mwalker@gitlab.com>
2021-07-02 14:40:27 +0000

    Apply 3 suggestion(s) to 3 file(s)

--- transform/snowflake-dbt/models/legacy/thanos/thanos_stage_group_error_budget_availability.sql
+++ transform/snowflake-dbt/models/legacy/thanos/thanos_stage_group_error_budget_availability.sql
@@ -2,7 +2,7 @@ WITH source AS (
 
     SELECT *
     FROM {{ ref('thanos_stage_group_error_budget_availability_source') }}
-    WHERE is_success IS true
+    WHERE is_success = true
 )
 SELECT *
 FROM source
