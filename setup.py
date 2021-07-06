b9508f74fb2cadbcfcbe1888663d61a012d11215
Michael Walker <mwalker@gitlab.com>
2021-07-06 15:11:57 +0000

    Apply 2 suggestion(s) to 1 file(s)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="GitLab dbt dev",
     version="1.0",
     description="dbt module for development",
-    author="Taylor Murphy, Michael Walker",
-    author_email="mwalker@gitlab.com", # I don't know what this file does
+    author="GitLab Data",
+    author_email="analyticsapi@gitlab.com",
     install_requires=requires,
 )
