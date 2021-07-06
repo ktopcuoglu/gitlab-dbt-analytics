45fabe287f4ad6d2d9246023c9d24aa9518f9153
Ian Schweickart <ian.schweickart@decisivedata.net>
2021-07-06 16:28:17 -0700

    finalized historical orders logic, added changes for MRs 9564 and 9589

--- transform/snowflake-dbt/models/common_mapping/prep/prep_order_hist.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_order_hist.sql
@@ -4,6 +4,8 @@
 }}
 
 {{ simple_cte([
+    ('versions', 'customers_db_versions_source'),
+    ('current_orders', 'customers_db_orders_source'),
     ('dim_date', 'dim_date'),
     ('namespaces', 'prep_namespace'),
     ('subscriptions', 'dim_subscription'),
@@ -13,83 +15,126 @@
 WITH customers_db_versions AS (
 
     SELECT *
-    FROM {{ref('customers_db_versions_source')}}
+    FROM versions
     -- selecting only orders
     WHERE item_type = 'Order'
+      AND object IS NOT NULL
 
 ), flattened_object AS (
 
-    SELECT 
-      *, 
-      SUBSTR(value, 1, POSITION(':' IN value) -1) AS column_key, 
-      TRIM(SUBSTR(value, POSITION(':' IN value) + 1, LEN(value))) AS column_value
+    SELECT
+      *,
+      SPLIT_PART(value, ': ', 1)                                                          AS column_key,
+      NULLIF(TRIM(LTRIM(SPLIT_PART(value, ': ', 2), '&1 '), ''''),'')                     AS column_value
     FROM customers_db_versions,
       -- objects look like a yaml table, splitting the object into rows at each linebreak 
-      lateral split_to_table(customers_db_versions.object, '\n')
-    WHERE item_type = 'Order'
+    LATERAL SPLIT_TO_TABLE(object, '\n')
 
-), old_changes AS (
+), cleaned AS (
+
+    SELECT
+      version_id,
+      item_id                                                                             AS order_id,
+      created_at                                                                          AS valid_to,
+      IFF(column_key = 'customer_id', column_value::NUMBER, NULL)                         AS customer_id,
+      IFF(column_key = 'product_rate_plan_id', column_value, NULL)                        AS product_rate_plan_id,
+      IFF(column_key = 'subscription_id', column_value, NULL)                             AS subscription_id,
+      IFF(column_key = 'subscription_name', column_value, NULL)                           AS subscription_name,
+      IFF(column_key = 'start_date', column_value::DATE, NULL)                            AS order_start_date,
+      IFF(column_key = 'end_date', column_value::DATE, NULL)                              AS order_end_date,
+      IFF(column_key = 'quantity', column_value::NUMBER, NULL)                            AS order_quantity,
+      IFF(column_key = 'created_at',
+          COALESCE(TRY_TO_TIMESTAMP(column_value),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 3)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC)),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 4)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC))),
+          NULL)                                                                           AS order_created_at,
+      IFF(column_key = 'updated_at',
+          COALESCE(TRY_TO_TIMESTAMP(column_value),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 3)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC)),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 4)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC))),
+          NULL)                                                                           AS order_updated_at,
+      IFF(column_key = 'gl_namespace_id', TRY_TO_NUMBER(TRIM(column_value, '''')), NULL)  AS gitlab_namespace_id,
+      IFF(column_key = 'gl_namespace_name', column_value, NULL)                           AS gitlab_namespace_name,
+      IFF(column_key = 'amendment_type', column_value, NULL)                              AS amendment_type,
+      IFF(column_key = 'trial', IFNULL(column_value, FALSE)::BOOLEAN, NULL)               AS order_is_trial,
+      IFF(column_key = 'last_extra_ci_minutes_sync_at',
+          COALESCE(TRY_TO_TIMESTAMP(column_value),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 3)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC)),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 4)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC))),
+          NULL)                                                                           AS last_extra_ci_minutes_sync_at,
+      IFF(column_key = 'zuora_account_id', column_value, NULL)                            AS zuora_account_id,
+      IFF(column_key = 'increased_billing_rate_notified_at',
+          COALESCE(TRY_TO_TIMESTAMP(column_value),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 3)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC)),
+                   TRY_TO_TIMESTAMP(LAG(column_value, 4)
+                     OVER (PARTITION BY id, item_id, seq ORDER BY index DESC))),
+          NULL)                                                                           AS increased_billing_rate_notified_at
+    FROM 
+  
+), pivoted AS (
 
     SELECT 
       version_id, 
-      item_id,
-      created_at AS valid_to,
-      MAX(CASE WHEN column_key = 'id' THEN TRY_TO_NUMBER(column_value) END)                                                 AS order_id,
-      MAX(CASE WHEN column_key = 'customer_id' THEN TRY_TO_NUMBER(column_value) END)                                        AS customer_id,
-      MAX(CASE WHEN column_key = 'subscription_name' THEN column_value END)                                                 AS subscription_name,
-      MAX(CASE WHEN column_key = 'subscription_id' THEN column_value END)                                                   AS subscription_id,
-      MAX(CASE WHEN column_key = 'start_date' THEN TRY_TO_DATE(column_value) END)::DATE                                     AS order_start_date,
-      MAX(CASE WHEN column_key = 'created_at' THEN TRY_TO_TIMESTAMP(column_value) END)::TIMESTAMP                           AS order_created_at,
-      MAX(CASE WHEN column_key = 'product_rate_plan_id' THEN column_value END)                                              AS product_rate_plan_id,
-      MAX(CASE WHEN column_key = 'quantity' THEN TRY_TO_NUMBER(column_value) END)::NUMBER                                   AS order_quantity,
-      MAX(CASE WHEN column_key = 'end_date' THEN TRY_TO_DATE(column_value) END)::DATE                                       AS order_end_date,
-      MAX(CASE WHEN column_key = 'amendment_type' THEN column_value END)                                                    AS amendment_type,
-      MAX(CASE WHEN column_key = 'gl_namespace_id' AND column_value <> '' THEN TRY_TO_NUMBER(TRIM(column_value, '\'')) END) AS dim_namespace_id,
-      MAX(CASE WHEN column_key = 'gl_namespace_name' THEN column_value END)                                                 AS dim_namespace_name,
-      MAX(CASE WHEN column_key = 'trial' THEN column_value END)::BOOLEAN                                                    AS order_is_trial,
-      MAX(CASE WHEN column_key = 'last_extra_ci_minutes_sync_at' THEN TRY_TO_TIMESTAMP(column_value) END)                   AS last_extra_ci_minutes_sync_at,
-      MAX(CASE WHEN column_key = 'zuora_account_id' THEN column_value END)                                                  AS zuora_account_id,
-      MAX(CASE WHEN column_key = 'increased_billing_rate_notified_at' THEN TRY_TO_TIMESTAMP(column_value) END)              AS increased_billing_rate_notified_at,
-      MAX(CASE WHEN column_key = 'reconciliation_accepted' THEN column_value END)                                           AS reconciliation_accepted,
-      MAX(CASE WHEN column_key = 'billing_rate_last_action' THEN column_value END)                                          AS billing_rate_last_action,
-      MAX(CASE WHEN column_key = 'billing_rate_adjusted_at' THEN TRY_TO_TIMESTAMP(column_value) END)::TIMESTAMP             AS billing_rate_adjusted_at
-    FROM flattened_object
-    GROUP BY 1,2,3
+      order_id,
+      valid_to,
+      MAX(customer_id)                                                                    AS customer_id,
+      MAX(product_rate_plan_id)                                                           AS product_rate_plan_id,
+      MAX(subscription_id)                                                                AS subscription_id,
+      MAX(subscription_name)                                                              AS subscription_name,
+      MAX(order_start_date)                                                               AS order_start_date,
+      MAX(order_end_date)                                                                 AS order_end_date,
+      MAX(order_quantity)                                                                 AS order_quantity,
+      MAX(order_created_at)                                                               AS order_created_at,
+      MAX(order_updated_at)                                                               AS order_updated_at,
+      MAX(gitlab_namespace_id)                                                            AS gitlab_namespace_id,
+      MAX(gitlab_namespace_name)                                                          AS gitlab_namespace_name,
+      MAX(amendment_type)                                                                 AS amendment_type,
+      MAX(order_is_trial)                                                                 AS order_is_trial,
+      MAX(last_extra_ci_minutes_sync_at)                                                  AS last_extra_ci_minutes_sync_at,
+      MAX(zuora_account_id)                                                               AS zuora_account_id,
+      MAX(increased_billing_rate_notified_at)                                             AS increased_billing_rate_notified_at
+    FROM cleaned
+    {{ dbt_utils.group_by(n=3) }}
 
 ), transformed AS (
 
-    SELECT 
-      customers_db_versions.item_id AS order_from_source, 
-      old_changes.*, 
-      COALESCE(LAG(customers_db_versions.created_at) OVER (PARTITION BY old_changes.order_id ORDER BY customers_db_versions.version_id ASC), old_changes.order_created_at) AS valid_from
-    FROM customers_db_versions
-    LEFT JOIN old_changes ON customers_db_versions.version_id = old_changes.version_id
+    SELECT *,
+      IFNULL(LAG(valid_to) OVER (PARTITION BY order_id ORDER BY id),
+             order_created_at)                                                            AS valid_from
+    FROM pivoted
 
 ), unioned AS (
 
     SELECT 
-      order_id,
+      order_id                                                                            AS dim_order_id,
       customer_id,
       product_rate_plan_id,
-      subscription_id,
+      subscription_id                                                                     AS dim_subscription_id,
       subscription_name,
       order_start_date,
       order_end_date,
       order_quantity,
       order_created_at,
-      dim_namespace_id::NUMBER            AS dim_namespace_id,
-      dim_namespace_name,
+      gitlab_namespace_id::NUMBER                                                         AS dim_namespace_id,
+      gitlab_namespace_name                                                               AS namespace_name,
       amendment_type,
       order_is_trial,
       last_extra_ci_minutes_sync_at,
-      zuora_account_id,
+      zuora_account_id                                                                    AS dim_billing_account_id,
       increased_billing_rate_notified_at,
       valid_from,
       valid_to
     FROM transformed
     WHERE order_created_at IS NOT NULL
 
-    UNION 
+    UNION ALL
 
     SELECT 
       order_id,
@@ -100,19 +145,50 @@ WITH customers_db_versions AS (
       order_start_date,
       order_end_date,
       order_quantity,
-      order_created_at AS valid_to,
-      gitlab_namespace_id::NUMBER         AS dim_namespace_id,
+      order_created_at
+      gitlab_namespace_id::NUMBER,
       gitlab_namespace_name,
       amendment_type,
       order_is_trial,
       last_extra_ci_minutes_sync_at,
       zuora_account_id,
       increased_billing_rate_notified_at,
-      order_updated_at AS valid_from,
-      NULL AS valid_to
-    FROM {{ref('customers_db_orders_source')}}
+      order_updated_at                                                                    AS valid_from,
+      NULL                                                                                AS valid_to
+    FROM current_orders
 
 ), joined AS (
+
+    SELECT 
+      unioned.dim_order_id,
+      unioned.customer_id,
+      unioned.product_rate_plan_id,
+      unioned.order_created_at,
+      start_dates.date_day                                                                AS order_start_date,
+      end_dates.date_day                                                                  AS order_end_date,
+      unioned.order_quantity,
+      subscriptions.dim_subscription_id,
+      subscriptions.subscription_name,
+      namespaces.dim_namespace_id,
+      namespaces.namespace_name,
+      billing_accounts.dim_billing_account_id,
+      unioned.amendment_type,
+      unioned.order_is_trial,
+      unioned.last_extra_ci_minutes_sync_at,
+      unioned.increased_billing_rate_notified_at,
+      unioned.valid_from,
+      unioned.valid_to
+    FROM unioned
+    LEFT JOIN subscriptions
+      ON unioned.dim_subscription_id = subscriptions.dim_subscription_id
+    LEFT JOIN namespaces
+      ON unioned.dim_namespace_id = namespaces.dim_namespace_id
+    LEFT JOIN billing_accounts
+      ON unioned.dim_billing_account_id = billing_accounts.dim_billing_account_id
+    LEFT JOIN dim_date AS start_dates
+      ON unioned.order_start_date = start_dates.date_day
+    LEFT JOIN dim_date AS end_dates
+      ON unioned.order_end_date = end_dates.date_day
 
 )
 
@@ -120,6 +196,6 @@ WITH customers_db_versions AS (
     cte_ref="joined",
     created_by="@ischweickartDD",
     updated_by="@ischweickartDD",
-    created_date="2021-06-30",
-    updated_date="2021-06-30"
+    created_date="2021-07-06",
+    updated_date="2021-07-06"
 ) }}
