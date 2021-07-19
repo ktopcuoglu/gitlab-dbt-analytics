09c6cc17d6f0db8570fcefc2f3aa4f05421a9e85
Davis Townsend <dtownsend@gitlab.com>
2021-07-19 12:04:47 -0500

    correct identifier for issue_severity table

--- transform/snowflake-dbt/models/common_mapping/prep/prep_issue_severity.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_issue_severity.sql
@@ -10,11 +10,11 @@ With gitlab_dotcom_issue_severity_source AS (
 ), renamed AS (
   
     SELECT
-      gitlab_dotcom_issuable_severities_source.issue_severity_id     AS dim_issue_severity_id,
+      gitlab_dotcom_issue_severity_source.issue_severity_id     AS dim_issue_severity_id,
       -- FOREIGN KEYS
-      gitlab_dotcom_issuable_severities_source.issue_id     AS dim_issue_id,
+      gitlab_dotcom_issue_severity_source.issue_id     AS dim_issue_id,
       --
-      gitlab_dotcom_issuable_severities_source.severity     AS severity      
+      gitlab_dotcom_issue_severity_source.severity     AS severity      
 
     FROM gitlab_dotcom_issue_severity_source
 )
