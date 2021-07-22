ab0b639d2bbbb96818c25496ad1274edf4dd098e
@dvanrooijen2 <dvanrooijen@gitlab.com>
2021-07-22 12:39:25 +0200

    commit

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/scd/gitlab_dotcom_label_links_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/scd/gitlab_dotcom_label_links_source.sql
@@ -11,8 +11,7 @@ WITH
       target_type::VARCHAR                           AS target_type,
       created_at::TIMESTAMP                          AS label_link_created_at,
       updated_at::TIMESTAMP                          AS label_link_updated_at,
-       -- Columns were added in distinct_source CTE
-      valid_from
+      valid_from -- Columns were added in distinct_source CTE
 
     FROM distinct_source
 
