c90e53522fdd90c880e55f4a4a5b9234e3b80e5c
Davis Townsend <dtownsend@gitlab.com>
2021-07-22 12:26:33 -0500

    correct field name

--- transform/snowflake-dbt/models/common_mapping/prep/prep_label_links.sql
+++ transform/snowflake-dbt/models/common_mapping/prep/prep_label_links.sql
@@ -31,15 +31,15 @@ WITH gitlab_dotcom_label_links_source AS (
       END AS dim_epic_id,
       --
       gitlab_dotcom_label_links_source.target_type,
-      gitlab_dotcom_label_links_source.created_at       AS label_added_at,
-      gitlab_dotcom_label_links_source.updated_at       AS label_updated_at
+      gitlab_dotcom_label_links_source.label_link_created_at       AS label_added_at,
+      gitlab_dotcom_label_links_source.label_link_updated_at       AS label_updated_at
       --
 
     FROM gitlab_dotcom_label_links_source
     -- exclude broken links (deleted labels)
     WHERE gitlab_dotcom_label_links_source.label_id IS NOT NULL
     -- only include currently active labels to avoid duplicate label_link_ids
-      AND gitlab_dotcom_label_links_source.is_currently_valid = TRUE
+      AND is_currently_valid = TRUE
 
 
 )
