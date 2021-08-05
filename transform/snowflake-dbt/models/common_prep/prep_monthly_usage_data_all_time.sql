8bf142e5f667d6c5719f72812d2e28921c9010b0
Mathieu Peychet <mpeychet@gitlab.com>
2021-08-05 07:12:44 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/common_prep/prep_monthly_usage_data_all_time.sql
+++ transform/snowflake-dbt/models/common_prep/prep_monthly_usage_data_all_time.sql
@@ -27,7 +27,7 @@
         has_timed_out,
         DATE_TRUNC('month', ping_created_at) AS ping_created_month
     FROM data
-    LEFT JOIN fct_usage_ping_payload
+    LEFT JOIN prep_usage_ping_payload
       ON data.dim_usage_ping_id = fct_usage_ping_payload.dim_usage_ping_id
     -- need host_name in the QUALIFY statement
     QUALIFY ROW_NUMBER() OVER (PARTITION BY dim_instance_id, metrics_path, ping_created_month ORDER BY ping_created_at DESC) = 1
