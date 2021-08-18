aebc601865565cdcd63b0133c0527536142a7531
Paul Armstrong <parmstrong@gitlab.com>
2021-08-18 20:18:48 +0200

    Cleanup, logging.

--- extract/rollup_table_clones/src/query_utils.py
+++ extract/rollup_table_clones/src/query_utils.py
@@ -1,23 +1,13 @@
 from gitlabdata.orchestration_utils import (
     data_science_engine_factory,
     query_dataframe,
-   # snowflake_stage_load_copy_remove,
+    query_executor,
 )
-from googleapiclient.discovery import build
-from yaml import load, safe_load, YAMLError
-from os import environ as env
 import pandas as pd
-from io import BytesIO
-from apiclient.http import MediaIoBaseDownload
-from sqlalchemy.engine.base import Engine
-import numpy as np
-import time
 import logging
-import sys
-import os
 
 
-def get_table_column_names(engine, table_name):
+def get_table_column_names(engine, db_name, table_name):
     # TODO: Correct table reference.
     query = f"SELECT " \
             f"ordinal_position," \
@@ -26,14 +16,14 @@ def get_table_column_names(engine, table_name):
             f"data_type," \
             f"character_maximum_length," \
             f"column_name || data_type as compare_column " \
-            f"FROM RAW.INFORMATION_SCHEMA.COLUMNS " \
+            f"FROM {db_name}.INFORMATION_SCHEMA.COLUMNS " \
             f"WHERE TABLE_NAME = '{table_name}' order by 1"
     return query_dataframe(engine, query)
 
 
-def get_tables_to_roll_up(engine, table_name):
+def get_tables_to_roll_up(engine, db_name, table_name):
     schema_check = f"SELECT table_name " \
-                   f"FROM RAW.INFORMATION_SCHEMA.TABLES" \
+                   f"FROM {db_name}.INFORMATION_SCHEMA.TABLES" \
                    f"WHERE RIGHT(TABLE_NAME, 2) = '08'" \
                    f"AND LEFT(TABLE_NAME, {len(table_name)}) = '{table_name}' " \
                    f"ORDER BY 1"
@@ -60,12 +50,12 @@ def process_row(row):
         return f"{row['column_name']} {row['data_type']} ,"
 
 
-def rollup_table_clone(engine, table_name):
-    roll_up_table_info = get_table_column_names(engine, f"{table_name}_ROLLUP")
-    tables_to_roll_up = get_tables_to_roll_up(engine, table_name)
+def rollup_table_clone(engine, db_name, schema_name, table_name):
+    roll_up_table_info = get_table_column_names(engine, db_name, f"{table_name}_ROLLUP")
+    tables_to_roll_up = get_tables_to_roll_up(engine, db_name, table_name)
     for items in tables_to_roll_up.iteritems():
-        print(f"Processing {items[1]}")
-        column_info = get_table_column_names(engine, items[1])
+        logging.info(f"Processing {items[1]}")
+        column_info = get_table_column_names(engine, db_name, items[1])
 
         column_string = ", ".join((column_info)['column_name'].unique())
         # Probably break this down, bit ridiculous as a one liner.
@@ -77,27 +67,32 @@ def rollup_table_clone(engine, table_name):
             if processed_row:
                 select_string = select_string + processed_row
 
-        insert_stmt = f"INSERT INTO {table_name}_ROLLUP ({column_string}, ORIGINAL_TABLE_NAME) SELECT {select_string} '{items[1]}' as ORIGINAL_TABLE_NAME FROM RAW.FULL_TABLE_CLONES.{items[1]}"
-        query_dataframe(engine, insert_stmt)
+        insert_stmt = f"INSERT INTO {db_name}.{schema_name}.{table_name}_ROLLUP " \
+                      f"({column_string}, ORIGINAL_TABLE_NAME) " \
+                      f"SELECT {select_string} '{items[1]}' as ORIGINAL_TABLE_NAME " \
+                      f"FROM {db_name}.{schema_name}.{items[1]}"
+        logging.info("Running query")
+        query_executor(engine, insert_stmt)
+        logging.info("Successfully rolled up table clones")
 
 
-def create_rollup_table(engine, db_name, schema_name, table_name):
-    tables_to_roll_up = get_tables_to_roll_up(engine, table_name)
-    latest_table_name = f"{db_name}.{schema_name}.{max(tables_to_roll_up.iteritems())[1]}"
+def recreate_rollup_table(engine, db_name, schema_name, table_name):
+    tables_to_roll_up = get_tables_to_roll_up(engine, db_name, table_name)
+    latest_table_name = max(tables_to_roll_up.iteritems())[1]
     rollup_table_name = f"{db_name}.{schema_name}.{table_name}_ROLLUP"
 
-    latest_table_columns = get_table_column_names(engine, latest_table_name)
+    latest_table_columns = get_table_column_names(engine, db_name, latest_table_name)
 
     big_df = latest_table_columns
 
     for items in tables_to_roll_up.iteritems():
-        print(f"Processing {items[1]}")
-        other_data = get_table_column_names(engine, items[1])
+        logging.info(f"Processing {items[1]}")
+        other_data = get_table_column_names(engine, db_name, items[1])
         big_df = big_df.append(other_data[~other_data['compare_column'].isin(big_df['compare_column'])])
 
     big_df = big_df.groupby(['column_name']).max().reset_index()
 
-    query_dataframe(engine, f"DROP TABLE IF EXISTS {rollup_table_name}")
+    query_executor(engine, f"DROP TABLE IF EXISTS {rollup_table_name}")
 
     create_table_statement = f"CREATE TABLE {rollup_table_name} ("
 
@@ -106,4 +101,5 @@ def create_rollup_table(engine, db_name, schema_name, table_name):
 
     create_table_statement = create_table_statement[:-1] + ', ORIGINAL_TABLE_NAME TEXT)'
 
-    query_dataframe(engine, create_table_statement)
+    query_executor(engine, create_table_statement)
+
