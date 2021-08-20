46c6e8a2ff69490a8083f4de8eea3e7f654d9767
Paul Armstrong <parmstrong@gitlab.com>
2021-08-20 14:05:41 +0200

    dbt model rename, correct schema reference.

--- transform/snowflake-dbt/models/sources/full_table_clones/mart_arr_clone_rollup_source.sql
+++ transform/snowflake-dbt/models/sources/full_table_clones/mart_arr_clone_rollup_source.sql
@@ -3,13 +3,7 @@ WITH source AS (
     SELECT *
     FROM {{ source('full_table_clones','mart_arr_rollup') }}
 
-), intermediate AS (
-
-    SELECT
-      *
-    FROM source
-
 )
 
 SELECT *
-FROM intermediate
+FROM source
