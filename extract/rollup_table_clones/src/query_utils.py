52c2a644e92f232d5529e30e88b7932ef86e92d3
Paul Armstrong <parmstrong@gitlab.com>
2021-08-23 10:41:48 +0200

    Return empty dfs

--- extract/rollup_table_clones/src/query_utils.py
+++ extract/rollup_table_clones/src/query_utils.py
@@ -69,8 +69,11 @@ def get_latest_tables_to_roll_up(
         )
 
         query_results = query_dataframe(engine, schema_check)
+
         if query_results and not query_results.empty:
             return query_results["table_name"]
+        else:
+            return query_results
 
 
 def get_latest_rolled_up_table_name(
