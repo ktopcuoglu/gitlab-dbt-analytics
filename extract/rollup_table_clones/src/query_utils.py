76eb1700e65a77825bc10fb4b16bc76458165d24
Paul Armstrong <parmstrong@gitlab.com>
2021-08-23 10:14:52 +0200

    NoneType check, empty df check.

--- extract/rollup_table_clones/src/query_utils.py
+++ extract/rollup_table_clones/src/query_utils.py
@@ -69,8 +69,7 @@ def get_latest_tables_to_roll_up(
         )
 
         query_results = query_dataframe(engine, schema_check)
-
-        if not query_results.empty:
+        if query_results and not query_results.empty:
             return query_results["table_name"]
 
 
