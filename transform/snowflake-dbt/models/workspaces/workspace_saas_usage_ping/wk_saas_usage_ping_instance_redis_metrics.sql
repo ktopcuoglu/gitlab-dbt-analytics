a1cd3f7d335604f0df1aa87786947aa914574243
mpeychet <mpeychet@gitlab.com>
2021-08-31 09:59:50 +0200

    improve query

--- transform/snowflake-dbt/models/workspaces/workspace_saas_usage_ping/wk_saas_usage_ping_instance_redis_metrics.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_saas_usage_ping/wk_saas_usage_ping_instance_redis_metrics.sql
@@ -13,10 +13,12 @@
     SELECT
       COALESCE(TRY_PARSE_JSON(path)[0]::TEXT, path::TEXT)         AS metric_path,
       value::TEXT                                                 AS metric_value,
-      response['recording_ce_finished_at'] AS recorded_at
+      response['recording_ce_finished_at']::TIMESTAMP             AS recorded_at
     FROM instance_redis_metrics,
     LATERAL FLATTEN(INPUT => response,
     RECURSIVE => TRUE) AS r
+    -- -3 is for non redis metrics so we exclude them from the model
+    WHERE TRY_TO_NUMBER(value::TEXT) <> -3
 
 )
 SELECT *
