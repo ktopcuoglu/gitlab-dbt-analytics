d659363272c4a86957e3512064c44cd682957893
Radovan Bacovic <rbacovic@gitlab.com>
2021-09-06 14:49:04 +0200

    Test passed via pipeline.

--- extract/test/test_saas_usage_ping_extract.py
+++ extract/test/test_saas_usage_ping_extract.py
@@ -8,6 +8,8 @@ input_timestamps = [
     datetime(2021, 9, 1, 23, 10, 21).timestamp(),
     datetime(2020, 8, 1, 23, 10, 22).timestamp(),
     datetime(2021, 7, 1, 23, 10, 23).timestamp(),
+    "test_string",
+    "",
     None,
 ]
 
@@ -16,10 +18,10 @@ Know testing the private method is not aligned with best praxis, but found it is
 in this implementation.
 """
 for i, check_time in enumerate(input_timestamps):
-    res = usage_ping_test._get_md5(usage_ping_test, check_time)
+    res = usage_ping_test._get_md5(None, check_time)
     # Check output data type
     assert isinstance(res, str)
     # Check is len 32 as it is expected length
     assert len(res) == 32  # bytes in hex representation
     # As this is one-way function, can't test it with many things - let see to we have all details with various inputs
-    assert not res is None
+    assert res is not None
