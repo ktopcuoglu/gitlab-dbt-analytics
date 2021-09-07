4c3ab4d3f5a078f497fcd3f11d37aa4db25fe234
jjstark <jstark@gitlab.com>
2021-09-07 08:56:52 -0700

    sales mart

--- transform/snowflake-dbt/models/marts/sales_funnel/restricted_safe/mart_sales_funnel_target_daily.sql
+++ transform/snowflake-dbt/models/marts/sales_funnel/restricted_safe/mart_sales_funnel_target_daily.sql
@@ -1,8 +1,3 @@
-{{config({
-    "schema": "common_mart_sales"
-  })
-}}
-
 {{ simple_cte([
     ('dim_crm_user_hierarchy_live','dim_crm_user_hierarchy_live'),
     ('dim_sales_qualified_source','dim_sales_qualified_source'),
