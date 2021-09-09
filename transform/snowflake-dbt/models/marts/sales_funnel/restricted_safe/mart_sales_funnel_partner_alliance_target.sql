e8537643836f1fdfeda9c249fe27ca336f3bc0bd
Jean Peguero <jpeguero@gitlab.com>
2021-09-09 18:32:48 -0400

    Update mart_sales target alliance and channel models to account for new channel reporting methods

--- transform/snowflake-dbt/models/marts/sales_funnel/restricted_safe/mart_sales_funnel_partner_alliance_target.sql
+++ transform/snowflake-dbt/models/marts/sales_funnel/restricted_safe/mart_sales_funnel_partner_alliance_target.sql
@@ -21,16 +21,13 @@
       dim_crm_user_hierarchy_live.crm_user_sales_segment_region_grouped,
       dim_order_type.order_type_name,
       dim_order_type.order_type_grouped,
-      dim_dr_partner_engagement.dr_partner_engagement_name,
       dim_sales_qualified_source.sales_qualified_source_name,
       dim_sales_qualified_source.sqs_bucket_engagement_name,
       dim_channel_type.channel_type_name,
       dim_alliance_type.alliance_type_name,
       dim_alliance_type.alliance_type_short_name,
       fct_sales_funnel_target.allocated_target
-    FROM fct_sales_funnel_target
-    LEFT JOIN dim_dr_partner_engagement
-      ON fct_sales_funnel_target.dim_dr_partner_engagement_id = dim_dr_partner_engagement.dim_dr_partner_engagement_id
+    FROM fct_sales_funnel_target=
     LEFT JOIN dim_alliance_type
       ON fct_sales_funnel_target.dim_alliance_type_id = dim_alliance_type.dim_alliance_type_id
     LEFT JOIN dim_sales_qualified_source
@@ -52,5 +49,5 @@
     created_by="@jpeguero",
     updated_by="@jpeguero",
     created_date="2021-04-08",
-    updated_date="2021-04-28",
+    updated_date="2021-09-09",
   ) }}
