8fa831298f953ecc99515d91076ceee0bddda8fe
Jean Peguero <jpeguero@gitlab.com>
2021-09-09 18:05:45 -0400

    Fix boolean in channel_type macro

--- transform/snowflake-dbt/macros/common/channel_type.sql
+++ transform/snowflake-dbt/macros/common/channel_type.sql
@@ -11,7 +11,7 @@ CASE
     AND {{ order_type }} = '1. New - First Order'
     THEN 'Co-sell - New'
   WHEN {{ sqs_bucket_engagement }} = 'Co-sell'
-    (AND {{ order_type }} != '1. New - First Order' OR {{ order_type }} IS NULL)
+    AND ( {{ order_type }} != '1. New - First Order' OR {{ order_type }} IS NULL)
     THEN 'Co-sell - Growth'
 END
 
