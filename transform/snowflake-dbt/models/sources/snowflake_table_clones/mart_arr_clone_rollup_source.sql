5c70204f382cbcd800282e4579605d2307068689
Paul Armstrong <parmstrong@gitlab.com>
2021-09-15 14:01:58 +0200

    Remove uneccessary CTE.

--- transform/snowflake-dbt/models/sources/snowflake_table_clones/mart_arr_clone_rollup_source.sql
+++ transform/snowflake-dbt/models/sources/snowflake_table_clones/mart_arr_clone_rollup_source.sql
@@ -3,12 +3,7 @@ WITH source AS (
     SELECT *
     FROM {{ source('full_table_clones','mart_arr_rollup') }}
 
-), final AS (
-
-    SELECT *
-    FROM source
-
 )
 
 SELECT *
-FROM final
+FROM source
