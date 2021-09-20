d56e57b94c13255d1734d8b205cb12be6e679e50
Paul Armstrong <parmstrong@gitlab.com>
2021-09-20 21:12:29 +0200

    Added revenue invoice accounting.

---
+++ transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_invoice_accounting_summary.sql
@@ -0,0 +1,16 @@
+{% snapshot zuora_rateplan_snapshots %}
+
+    {{
+        config(
+          strategy='timestamp',
+          unique_key='prd_id || ''-'' || line_id || ''-'' || root_line_id || ''-'' || rc_id || ''-'' || acct_type_id',
+          updated_at='incr_updt_dt',
+        )
+    }}
+    
+    SELECT
+        prd_id || '-' || line_id || '-' || root_line_id || '-' || rc_id || '-' || acct_type_id AS revenue_snapshot_id,
+        *
+    FROM {{ source('zuora_revenue','zuora_revenue_organization') }}
+    
+{% endsnapshot %}
