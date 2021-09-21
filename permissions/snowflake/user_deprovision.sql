25ebd8305cf539ea3e9e515c743a03b9e9112895
laddula <laddula@gitlab.com>
2021-09-21 13:46:08 -0400

    updated comment

--- permissions/snowflake/user_deprovision.sql
+++ permissions/snowflake/user_deprovision.sql
@@ -1,3 +1,3 @@
--- using the SECURITYADMIN role run the following by setting 'USER_NAME' provided in Offboarding or Deprovisioning template.
+-- using the SECURITYADMIN role run the following by setting 'USER_NAME' provided in Offboarding or Deprovisioning issue request.
 
 ALTER USER IF EXISTS USER_NAME SET DISABLED = TRUE;
