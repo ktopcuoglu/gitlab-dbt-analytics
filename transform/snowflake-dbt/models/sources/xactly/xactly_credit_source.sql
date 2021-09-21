03ba567ecdda22fdb4276d8aad66934526fba8ea
ken_aguilar <kaguilar@gitlab.com>
2021-09-21 09:34:27 +0800

    add xactly_credit_source columns

---
+++ transform/snowflake-dbt/models/sources/xactly/xactly_credit_source.sql
@@ -0,0 +1,74 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('tap_xactly', 'credit') }}
+
+), renamed AS (
+
+    SELECT
+      amount,
+      amount_display_symbol,
+      amount_unit_type_id,
+      batch_number,
+      business_group_id,
+      created_by_id,
+      created_by_name,
+      created_date,
+      credit_id,
+      credit_type_id,
+      credit_type_name,
+      customer_id,
+      customer_name,
+      eff_participant_id,
+      eff_position_id,
+      estimated_rel_date,
+      ever_on_hold,
+      geography_id,
+      geography_name,
+      incentive_date,
+      is_active,
+      is_held,
+      is_processed,
+      is_rollable,
+      item_code,
+      mgr_eff_part_id,
+      mgr_pos_id,
+      mgr_master_pos_id,
+      modified_by_id,
+      modified_by_name,
+      modified_date,
+      name,
+      order_code,
+      order_item_id,
+      participant_id,
+      md5(participant_name) AS participant_uuid,
+      period_id,
+      period_name,
+      plan_id,
+      plan_name,
+      position_id,
+      md5(position_name) AS position_uuid,
+      product_id,
+      product_name,
+      reason_code_id,
+      reason_code_name,
+      release_date,
+      release_group_id,
+      rollable_on_reporting,
+      rule_id,
+      rule_name,
+      run_id,
+      source_credit_id,
+      source_position_id,
+      src_pos_relation_type_id,
+      sub_batch_number,
+      sub_part_key,
+      trans_id,
+      version,
+      _loaded_at
+
+    FROM source
+)
+
+SELECT *
+FROM renamed
