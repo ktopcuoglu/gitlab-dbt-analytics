95e5c29267f8eef89b322a6d147a1178d05783d7
Jean Peguero <jpeguero@gitlab.com>
2021-09-22 16:25:56 -0400

    Add customers db licenses to dim_license. And move prep models referenced in prep_license to dim_license

--- transform/snowflake-dbt/models/common/dim_license.sql
+++ transform/snowflake-dbt/models/common/dim_license.sql
@@ -1,31 +1,45 @@
-WITH license AS (
+WITH tiers AS (
 
-     SELECT
-       dim_license_id,
-       license_md5,
-       dim_subscription_id,
-       subscription_name,
-       dim_subscription_id_original,
-       dim_subscription_id_previous,
-       license_user_count,
-       license_plan,
-       dim_product_tier_id,
-       is_trial,
-       is_internal,
-       company,
-       license_start_date,
-       license_expire_date,
-       created_at,
-       updated_at
+    SELECT *
+    FROM {{ ref('prep_product_tier') }}
+
+), license AS (
+
+    SELECT *
     FROM {{ ref('prep_license') }}
 
+), final AS (
+
+    SELECT
+      dim_license_id,
+
+      dim_subscription_id,
+      dim_subscription_id_original,
+      dim_subscription_id_previous,
+      tiers.dim_product_tier_id,
+
+      license_md5,
+      subscription_name,
+      environment,
+      license_user_count,
+      license_plan,
+      is_trial,
+      is_internal,
+      company,
+      license_start_date,
+      license_expire_date,
+      created_at,
+      updated_at
+    FROM license
+    LEFT JOIN tiers
+      ON LOWER(tiers.product_tier_name) = license.license_plan 
 )
 
 
 {{ dbt_audit(
-    cte_ref="license",
+    cte_ref="final",
     created_by="@snalamaru",
-    updated_by="@snalamaru",
+    updated_by="@jpeguero",
     created_date="2021-01-08",
-    updated_date="2021-01-08"
+    updated_date="2021-09-22"
 ) }}
