65ff12d245407a5ed981b6476339f1ee34d0d9b4
laddula <laddula@gitlab.com>
2021-09-27 11:06:12 -0400

    Adding sources snowplow, demandbase, bamboohr, qualtrics, netsuite

---
+++ transform/snowflake-dbt/models/workspaces/workspace_data/tdf/qualtrics_tables_date.sql
@@ -0,0 +1,29 @@
+{{ config({
+        "materialized": "table"
+    })
+}}
+
+WITH qualtrics_date AS (
+
+    {% set tables = [
+'contact','distribution','nps_survey_responses','post_purchase_survey_responses','survey','questions' ] %}
+        
+                                                          
+    {% for table in tables %} 
+    SELECT '{{table}}'                                                     AS table_name,
+        MAX(uploaded_at)                                                   AS max_date 
+    FROM {{source('qualtrics', table)}}  
+  
+  
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+
+{% endfor %} 
+  
+)
+
+
+  SELECT *
+  FROM qualtrics_date 
+  
