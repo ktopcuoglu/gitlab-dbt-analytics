2ec8ad07f071e50b719b19f05ebdfd1a0531aa12
Davis Townsend <dtownsend@gitlab.com>
2021-09-27 15:40:19 -0500

    fix spacing

--- transform/snowflake-dbt/models/common/fct_issue_metrics.sql
+++ transform/snowflake-dbt/models/common/fct_issue_metrics.sql
@@ -18,30 +18,29 @@
 ) , joined AS (
 
     SELECT
-    -- grain is age of one issue over time
-    prep_issue_labels.date_id,
-    prep_issue_labels.dim_issue_id,
-    prep_issue_labels.date_actual,
-    -- calculate age since issue was opened
-    CASE
-      -- date at point in time is after issue close date, don't show this in results
-      WHEN prep_issue_labels.date_actual > dim_issue.issue_closed_at THEN NULL 
-      -- issue still open, calc age from issue creation to point in time
-      WHEN dim_issue.issue_closed_at IS NULL THEN DATEDIFF('DAY',dim_issue.created_at,prep_issue_labels.date_actual)
-      -- issue is closed, but date at point in time is before issue close date age is from creation date to point in time
-      ELSE DATEDIFF('DAY',dim_issue.created_at,prep_issue_labels.date_actual)
-    END AS open_age_in_days, 
-    -- calculate age since severity label was added 
-    CASE
-      -- date at point in time is after issue close date, don't show this in results
-      WHEN prep_issue_labels.date_actual > dim_issue.issue_closed_at THEN NULL 
-      -- issue still open, calc age from issue creation to point in time
-      WHEN dim_issue.issue_closed_at IS NULL THEN DATEDIFF('DAY',sev_labels.label_added_at,prep_issue_labels.date_actual)
-      -- issue is closed, but date at point in time is before issue close date age is from creation date to point in time
-      ELSE DATEDIFF('DAY',sev_labels.label_added_at,prep_issue_labels.date_actual)
-    END AS open_age_after_sev_label_added,
-    sev_labels.label_name AS severity,
-    dim_issue.labels
+      prep_issue_labels.date_id,
+      prep_issue_labels.dim_issue_id,
+      prep_issue_labels.date_actual,
+      -- calculate age since issue was opened
+      CASE
+        -- date at point in time is after issue close date, don't show this in results
+        WHEN prep_issue_labels.date_actual > dim_issue.issue_closed_at THEN NULL 
+        -- issue still open, calc age from issue creation to point in time
+        WHEN dim_issue.issue_closed_at IS NULL THEN DATEDIFF('DAY',dim_issue.created_at,prep_issue_labels.date_actual)
+        -- issue is closed, but date at point in time is before issue close date age is from creation date to point in time
+        ELSE DATEDIFF('DAY',dim_issue.created_at,prep_issue_labels.date_actual)
+        END AS open_age_in_days, 
+        -- calculate age since severity label was added 
+      CASE
+        -- date at point in time is after issue close date, don't show this in results
+        WHEN prep_issue_labels.date_actual > dim_issue.issue_closed_at THEN NULL 
+        -- issue still open, calc age from issue creation to point in time
+        WHEN dim_issue.issue_closed_at IS NULL THEN DATEDIFF('DAY',sev_labels.label_added_at,prep_issue_labels.date_actual)
+        -- issue is closed, but date at point in time is before issue close date age is from creation date to point in time
+        ELSE DATEDIFF('DAY',sev_labels.label_added_at,prep_issue_labels.date_actual)
+      END AS open_age_after_sev_label_added,
+      sev_labels.label_name AS severity,
+      dim_issue.labels
     FROM dim_issue
     LEFT JOIN prep_issue_labels
       ON prep_issue_labels.dim_issue_id = dim_issue.dim_issue_id
