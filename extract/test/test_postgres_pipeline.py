88a876b12f2b484148bbdcf08a476ebebeb90b17
Radovan Bacovic <rbacovic@gitlab.com>
2021-10-01 11:33:59 +0200

    Test case added.

--- extract/test/test_postgres_pipeline.py
+++ extract/test/test_postgres_pipeline.py
@@ -8,14 +8,15 @@ with open(
     implemented_tables = el_gitlab_com_db_manifest["tables"].keys()
 
 # check are new tables are implemented
-tables = ["bulk_import_entities", "geo_nodes"]
-
-for table in tables:
-    assert table in implemented_tables
+table = "bulk_import_entities"
+assert table in implemented_tables
 
 
-# number of columns implemented for table "GEO_NODES#
-assert (
-    el_gitlab_com_db_manifest["tables"]["geo_nodes"]["import_query"].count(" NULL AS")
-    == 21
-)
+with open(
+    "../postgres_pipeline/manifests_decomposed/el_gitlab_com_trusted_data_extract_load_db_manifest.yaml",
+    "r",
+) as db_trusted_data:
+    el_gitlab_com_db_trusted = yaml.safe_load(db_trusted_data)
+
+
+assert table in el_gitlab_com_db_trusted["tables"]["cluster_1"]["import_query"]
