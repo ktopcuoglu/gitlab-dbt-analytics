427a72d50e80ee7a3cb6a590d5ccf0ea404649cf
Jean Peguero <jpeguero@gitlab.com>
2021-10-12 12:32:38 -0400

    Create mapping table for issues that have been moved to others

---
+++ transform/snowflake-dbt/models/common_mapping/map_moved_issue.sql
@@ -0,0 +1,39 @@
+WITH RECURSIVE issues AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_issues_source') }}
+
+), recursive_cte(issue_id, moved_to_id, issue_lineage) AS (
+
+    SELECT
+      issue_id,
+      moved_to_id,
+      TO_ARRAY(issue_id) AS issue_lineage
+    FROM issues
+    WHERE moved_to_id IS NULL
+
+    UNION ALL
+
+    SELECT
+      iter.issue_id,
+      iter.moved_to_id,
+      ARRAY_INSERT(anchor.issue_lineage, 0, iter.issue_id) AS issue_lineage
+    FROM recursive_cte AS anchor
+    INNER JOIN issues AS iter
+      ON iter.moved_to_id = anchor.issue_id
+
+), final AS (
+
+    SELECT
+      issue_id                                                  AS issue_id,
+      issue_lineage,
+      issue_lineage[ARRAY_SIZE(issue_lineage) - 1]::VARCHAR     AS last_moved_issue_id,
+      IFF(last_moved_issue_id != issue_id, TRUE, FALSE)         AS is_issue_moved,
+      --return final common dimension mapping,
+      last_moved_issue_id                                       AS dim_issue_id
+    FROM recursive_cte
+
+)
+
+select *
+from final
