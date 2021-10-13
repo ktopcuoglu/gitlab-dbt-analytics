538f8d407c259ccba4cde54d457f1e8cfafe4304
snalamaru <snalamaru@gitlab.com>
2021-10-13 17:30:41 -0500

    Added instance type to fact table

--- transform/snowflake-dbt/models/common/fct_saas_product_usage_metrics_monthly.sql
+++ transform/snowflake-dbt/models/common/fct_saas_product_usage_metrics_monthly.sql
@@ -48,6 +48,7 @@
       {{ get_date_id('saas_subscriptions.snapshot_month') }}                            AS snapshot_date_id,
       saas_usage_ping.ping_date                                                         AS ping_created_at,
       {{ get_date_id('saas_usage_ping.ping_date') }}                                    AS ping_created_date_id,
+      saas_usage_ping.instance_type,
       -- Wave 1
       gitlab_seats.seats                                                                AS subscription_seats,
       gitlab_seats.seats_in_use                                                         AS billable_user_count,
