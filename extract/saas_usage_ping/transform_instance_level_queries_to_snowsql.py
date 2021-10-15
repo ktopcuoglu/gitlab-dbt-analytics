dc17df8fe8f24fa72155cc11699657365f9d8a11
Radovan Bacovic <rbacovic@gitlab.com>
2021-10-15 10:09:25 +0200

    Refactored file make more readable and stick with SOLID principles. It is testable and ready for the unit testing.

--- extract/saas_usage_ping/transform_instance_level_queries_to_snowsql.py
+++ extract/saas_usage_ping/transform_instance_level_queries_to_snowsql.py
@@ -12,13 +12,12 @@ from sqlparse.sql import (
     Token,
     TokenList,
     Where,
+    Parenthesis,
+    Comparison,
 )
 from sqlparse.tokens import Keyword, Name, Punctuation, String, Whitespace
 from os import environ as env
 import requests
-
-## Workflow
-### Read and transform JSON file
 
 
 def get_sql_query_map(private_token=None) -> str:
@@ -29,72 +28,53 @@ def get_sql_query_map(private_token=None) -> str:
     response = requests.get(
         "https://gitlab.com/api/v4/usage_data/queries", headers=headers
     )
-    json_data = json.loads(response.text)
+    source_json_data = json.loads(response.text)
 
-    return json_data
+    return source_json_data
 
 
-def sql_queries_dict(json_data: str) -> Dict[Any, Any]:
+def optimize_token_size(input_token: str) -> str:
     """
-    function that transforms the given json file into a Python dict with only SQL batch counters
+    Function reduce and optimize the size of the token length
+    Primary goal is to:
+    - reduce multiple whitespaces: from ' ',' ',' ' to ' ' and decrease the size (which will speed up the process)
+    - remove '\n' (new line) characters as they have no any value for a query execution
     """
+    if not input_token:
+        return ""
 
-    full_payload_dict = flatten(json_data, reducer=make_reducer(delimiter="."))
+    optimized_token = []
+    current_pos = 0
+    previous_input = " "
 
-    sql_queries_dict = {}
+    for current_input in input_token:
+        if (
+            current_input != previous_input or previous_input != " "
+        ) and current_input != "\n":
+            optimized_token.append(input_token[current_pos])
+        previous_input = current_input
+        current_pos += 1
 
-    for key, value in full_payload_dict.items():
-        # Check if key is even then add pair to new dictionary
-        if isinstance(value, str) and value.startswith("SELECT"):
-            sql_queries_dict[key] = value
-
-    return sql_queries_dict
-
-
-def add_counter_name_as_column(sql_metrics_name: str, sql_query: str) -> str:
-    """
-    A query like
-
-    SELECT COUNT(issues.id)
-    FROM issues
-
-    will be changed to
-
-    SELECT
-      "counts.issues" AS counter_name,
-      COUNT(issues.id) AS counter_value,
-      TO_DATE(CURRENT_DATE) AS run_day
-    FROM issues
-    """
-
-    # removing extra " to have an easier query to parse
-    sql_query = sql_query.replace('"', "")
-
-    # using here the sqlparse library: https://pypi.org/project/sqlparse/
-    sql_query_parsed = sqlparse.parse(sql_query)
-
-    # split the query in tokens
-    # get a list of tokens
-    token_list = sql_query_parsed[0].tokens
-
-    select_index = 0
-    for index, token in enumerate(token_list):
-
-        # identify if it is a select statement
-        if token.is_keyword and str(token) == "SELECT":
-            # set the select_index
-            select_index = index
+    # Remove empty spaces from the end
+    for reverse_optimized in optimized_token[::-1]:
+        if reverse_optimized != " ":
             break
+        else:
+            optimized_token.pop()
+    return "".join(optimized_token)
 
-    from_index = 0
-    for index, token in enumerate(token_list):
-        if token.is_keyword and str(token) == "FROM":
-            from_index = index
-            break
 
-    # TODO: rbacovic - translate count(xx.xx.xx) into COUNT(xx.xx) make this more readable
-    # Use case fixed: translate COUNT(xx.xx.xx) into COUNT(xx.xx)
-    for index, token in enumerate(token_list):
+def translate_postgres_snowflake_count(input_token_list: list) -> list:
+    """
+    Function to support translation of COUNT syntax from Postgres to Snowflake.
+    Example:
+         - translate count(xx.xx.xx) -> COUNT(xx.xx)
+         - translate COUNT(DISTINCT xx.xx.xx) -> COUNT(DISTINCT xx.xx)
+    """
+    if not input_token_list:
+        return []
+
+    for index, token in enumerate(input_token_list):
         token = str(token).replace("COUNT( ", "COUNT(")
         check_postgres_count = str(token).upper()
         if (
@@ -113,33 +93,115 @@ def add_counter_name_as_column(sql_metrics_name: str, sql_query: str) -> str:
                 str(token)[: index_from + 1] + str(token)[token.index(".") + 1 :]
             )
 
-            token_list[index] = fixed_count
+            input_token_list[index] = fixed_count
+    return input_token_list
 
-    token_list_with_counter_name = token_list[:]
 
-    # TODO: rbacovic determinate if we have subquery (without FROM clause), make this a little bit more readable
-    # Use case fixed: put scalar sub query properly as a column value
-    # if yes, fit it as a scalar value in counter_value column
+def prepare_sql_statement(input_token_list: list) -> str:
+    """
+    Transform token list in a prepared, executable SQL statement
+    ready for the execution in Snowflake.
+    """
+    if not input_token_list:
+        return ""
+
+    # transform token list in list of strings
+    prepared_list = [str(token) for token in input_token_list]
+
+    # recreate from the list the SQL query
+    prepared_query = "".join(prepared_list)
+
+    return prepared_query
+
+
+def find_keyword_index(input_token_list: list, defined_keyword: str) -> int:
+    """
+    Find the index, if any exist for keyword defined
+    If no index is found, return 0 as a value
+    """
+    if not input_token_list:
+        return 0
+
+    for index, keyword_token in enumerate(input_token_list):
+        if (
+            keyword_token.is_keyword
+            and str(keyword_token).upper() == defined_keyword.upper()
+        ):
+            return index
+    return 0
+
+
+def sql_queries_dict(input_json_data: str) -> Dict[Any, Any]:
+    """
+    function that transforms the given json file into a Python dict with only SQL batch counters
+    """
+
+    full_payload_dict = flatten(input_json_data, reducer=make_reducer(delimiter="."))
+
+    sql_dict = {}
+
+    for key, value in full_payload_dict.items():
+        # Check if key is even then add pair to new dictionary
+        if isinstance(value, str) and value.startswith("SELECT"):
+            sql_dict[key] = value
+
+    return sql_dict
+
+
+def add_counter_name_as_column(sql_metrics_name: str, sql_query: str) -> str:
+    """
+    Transform query from Postgres to Snowflake syntax.
+    For this purpose using the sqlparse library: https://pypi.org/project/sqlparse/
+
+    Example:
+    A query like:
+
+        SELECT COUNT(issues.id)
+        FROM issues
+
+    will be changed to:
+
+        SELECT
+          "counts.issues" AS counter_name,
+          COUNT(issues.id) AS counter_value,
+          TO_DATE(CURRENT_DATE) AS run_day
+        FROM issues
+    """
+
+    # removing extra " to have an easier query to parse
+    sql_query = sql_query.replace('"', "")
+    sql_query_optimized = optimize_token_size(sql_query)
+
+    sql_query_parsed = sqlparse.parse(sql_query_optimized)
+
+    # split the query in tokens
+    # get a list of tokens
+    token_list = sql_query_parsed[0].tokens
+
+    # optimize token_list size
+    select_index = find_keyword_index(token_list, "SELECT")
+    from_index = find_keyword_index(token_list, "FROM")
+
+    token_list_with_counter_name = translate_postgres_snowflake_count(token_list)
+
+    """
+    Determinate if we have subquery (without FROM clause in the main query),
+    in the format SELECT (SELECT xx FROM);
+    If yes, fit it as a scalar value in counter_value column
+    """
     if from_index == 0:
         from_index = len(token_list_with_counter_name) + 1
-
-    # add a name for the count columns and add the date column
 
     # add the counter name column
     token_list_with_counter_name.insert(
         from_index - 1, " AS counter_value, TO_DATE(CURRENT_DATE) AS run_day  "
     )
+
     token_list_with_counter_name.insert(
-        select_index + 1, " '" + sql_metrics_name + "' AS counter_name, "
+        select_index + 1, f" '{sql_metrics_name}' AS counter_name, "
     )
 
-    # transform token list in list of strings
-    enhanced_query_list = [str(token) for token in token_list_with_counter_name]
-
-    # recreate from the list the SQL query
-    enhanced_query = "".join(enhanced_query_list)
-
-    return enhanced_query
+    return prepare_sql_statement(token_list_with_counter_name)
 
 
 def rename_table_name(
@@ -152,12 +214,13 @@ def rename_table_name(
     """
     Replaces the table name in the query -- represented as the list of tokens -- to make it able to run in Snowflake
 
-    Does this by prepending `prep.gitlab_dotcom.gitlab_dotcom_`
-    to the table name in the query and then appending `_dedupe_source`.
+    Does this by prepending `prep.gitlab_dotcom.gitlab_dotcom_` to the table name in the query and then
+    appending `_dedupe_source`
     """
 
     if any(token_word in keywords_to_look_at for token_word in str(token).split(" ")):
         i = 1
+
         # Whitespaces are considered as tokens and should be skipped
         while tokens[index + i].ttype is Whitespace:
             i += 1
@@ -166,10 +229,8 @@ def rename_table_name(
         if not str(next_token).startswith("prep") and not str(next_token).startswith(
             "prod"
         ):
-
             # insert, token list to string list, create the SQL query, reparse it
             # there is FOR sure a better way to do that
-
             token_string_list[
                 index + i
             ] = f"prep.gitlab_dotcom.gitlab_dotcom_{str(next_token)}_dedupe_source AS {str(next_token)}"
@@ -180,7 +241,7 @@ def rename_query_tables(sql_query: str) -> str:
     function to rename the table in the sql query
     """
 
-    ### comprehensive list of all the keywords that are followed by a table name
+    # comprehensive list of all the keywords that are followed by a table name
     keywords_to_look_at = [
         "FROM",
         "JOIN",
@@ -188,38 +249,23 @@ def rename_query_tables(sql_query: str) -> str:
 
     # start parsing the query and get the token_list
     parsed = sqlparse.parse(sql_query)
-    # TODO: rbacovic clean up token
-    # tokens = list(TokenList(parsed[0].tokens).flatten())
-    tokens = list(
-        token_flatten
-        for token_flatten in TokenList(parsed[0].tokens).flatten()
-        if token_flatten.value != "\n"
-    )
     tokens = list(TokenList(parsed[0].tokens).flatten())
-
     token_string_list = list(map(str, tokens))
 
     # go through the tokens to find the tables that should be renamed
     for index in range(len(tokens)):
         token = tokens[index]
         rename_table_name(keywords_to_look_at, token, tokens, index, token_string_list)
-
     return "".join(token_string_list)
 
 
-if __name__ == "__main__":
-    ## Files imported
+def main(json_query_list: dict) -> dict:
+    """
+    Main input point to transform queries
+    """
 
-    ## counter queries (sql+redis): this is generated manually by the product intelligence team
-    ## available here
+    sql_queries_dictionary = sql_queries_dict(json_query_list)
 
-    config_dict = env.copy()
-    json_data = get_sql_query_map(
-        private_token=config_dict["GITLAB_ANALYTICS_PRIVATE_TOKEN"]
-    )
-
-    sql_queries_dictionary = sql_queries_dict(json_data)
-    info("Processed sql queries")
     sql_queries_dict_with_new_column = {
         metric_name: add_counter_name_as_column(
             metric_name, sql_queries_dictionary[metric_name]
@@ -227,10 +273,20 @@ if __name__ == "__main__":
         for metric_name in sql_queries_dictionary
     }
 
-    final_sql_query_dict = {
+    transformed_sql_query_dict = {
         metric_name: rename_query_tables(sql_queries_dict_with_new_column[metric_name])
         for metric_name in sql_queries_dict_with_new_column
     }
+
+    return transformed_sql_query_dict
+
+
+if __name__ == "__main__":
+    config_dict = env.copy()
+    json_data = get_sql_query_map(private_token=config_dict["GITLAB_ANALYTICS_PRIVATE_TOKEN"])
+
+    info("Processed sql queries")
+    final_sql_query_dict = main(json_query_list=json_data)
     info("Processed final sql queries")
 
     with open("transformed_instance_queries.json", "w") as f:
