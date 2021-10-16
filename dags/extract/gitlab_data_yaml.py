6059025ea92cad0b0ca8daefd28dbd2bc406e3a9
Paul Armstrong <parmstrong@gitlab.com>
2021-10-16 19:54:53 +0200

    Removed hardcoded references, separated tasks out for better debugging.

--- dags/extract/gitlab_data_yaml.py
+++ dags/extract/gitlab_data_yaml.py
@@ -8,10 +8,10 @@ from airflow_utils import (
     clone_and_setup_extraction_cmd,
     gitlab_defaults,
     slack_failed_task,
+    gitlab_pod_env_vars,
 )
 from kube_secrets import (
     SNOWFLAKE_ACCOUNT,
-    SNOWFLAKE_LOAD_DATABASE,
     SNOWFLAKE_LOAD_PASSWORD,
     SNOWFLAKE_LOAD_ROLE,
     SNOWFLAKE_LOAD_USER,
@@ -23,7 +23,7 @@ from kubernetes_helpers import get_affinity, get_toleration
 
 # Load the env vars into a dict and set Secrets
 env = os.environ.copy()
-pod_env_vars = {"CI_PROJECT_DIR": "/analytics"}
+pod_env_vars = gitlab_pod_env_vars
 
 # Default arguments for the DAG
 default_args = {
@@ -47,20 +47,14 @@ dag = DAG(
 )
 
 # YAML Extract
-data_yaml_extract_cmd = f"""
-    {clone_and_setup_extraction_cmd} &&
-    python gitlab_data_yaml/upload.py &&
-    python gitlab_feature_flags_yaml/upload.py &&
-    python gitlab_flaky_tests/upload.py
-"""
+data_yaml_extract_cmd = f"""{clone_and_setup_extraction_cmd} && python gitlab_data_yaml/upload.py"""
 data_yaml_extract = KubernetesPodOperator(
     **gitlab_defaults,
-    image="registry.gitlab.com/gitlab-data/data-image/data-image:v0.0.13",
+    image=DATA_IMAGE,
     task_id="data-yaml-extract",
     name="data-yaml-extract",
     secrets=[
         SNOWFLAKE_ACCOUNT,
-        SNOWFLAKE_LOAD_DATABASE,
         SNOWFLAKE_LOAD_ROLE,
         SNOWFLAKE_LOAD_USER,
         SNOWFLAKE_LOAD_WAREHOUSE,
@@ -74,3 +68,53 @@ data_yaml_extract = KubernetesPodOperator(
     arguments=[data_yaml_extract_cmd],
     dag=dag,
 )
+
+# Flaky tests extract
+flaky_tests_extract_cmd = f"""{clone_and_setup_extraction_cmd} && python gitlab_flaky_tests/upload.py"""
+
+flaky_tests_extract = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DATA_IMAGE,
+    task_id="flaky-tests-extract",
+    name="flaky-tests-extract",
+    secrets=[
+        SNOWFLAKE_ACCOUNT,
+        SNOWFLAKE_LOAD_ROLE,
+        SNOWFLAKE_LOAD_USER,
+        SNOWFLAKE_LOAD_WAREHOUSE,
+        SNOWFLAKE_LOAD_PASSWORD,
+        GITLAB_ANALYTICS_PRIVATE_TOKEN,
+        GITLAB_COM_API_TOKEN,
+    ],
+    affinity=get_affinity(False),
+    tolerations=get_toleration(False),
+    env_vars=pod_env_vars,
+    arguments=[flaky_tests_extract_cmd],
+    dag=dag,
+)
+
+# Feature flags extract
+feature_flags_extract_cmd = f"""{clone_and_setup_extraction_cmd} && python gitlab_feature_flags_yaml/upload.py"""
+
+feature_flags_extract = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DATA_IMAGE,
+    task_id="feature-flags-extract",
+    name="feature-flags-extract",
+    secrets=[
+        SNOWFLAKE_ACCOUNT,
+        SNOWFLAKE_LOAD_ROLE,
+        SNOWFLAKE_LOAD_USER,
+        SNOWFLAKE_LOAD_WAREHOUSE,
+        SNOWFLAKE_LOAD_PASSWORD,
+        GITLAB_ANALYTICS_PRIVATE_TOKEN,
+        GITLAB_COM_API_TOKEN,
+    ],
+    affinity=get_affinity(False),
+    tolerations=get_toleration(False),
+    env_vars=pod_env_vars,
+    arguments=[feature_flags_extract_cmd],
+    dag=dag,
+)
+
+data_yaml_extract >> flaky_tests_extract >> feature_flags_extract
