25f9683a2af425d41c4ab30fdf6c981e4f2eb020
jjstark <jstark@gitlab.com>
2021-10-20 12:42:52 -0700

    use dbt config in ls

--- transform/snowflake-dbt/safe_model_check.py
+++ transform/snowflake-dbt/safe_model_check.py
@@ -5,21 +5,18 @@ from pandas.io.json import json_normalize
 
 
 def check_safe_models(file):
-    # select data
-    df = json_normalize(pd.Series(open(file).readlines()).apply(json.loads))
-    df = df[["name", "tags", "config.schema"]]
-    # filter data
-    is_restricted = df["config.schema"].str.contains("restricted_safe")
-    ## results
-    results = df[~is_restricted]
-    results_count = results.count()["name"]
-    error_message = "⚠️ The following models are not SAFE ⚠️:\r\n" + results.to_csv(
-        index=False
-    )
-    if results_count == 0:
-        logging.info("All models are safe 🥂")
-    else:
-        raise ValueError(error_message)
+
+    with open(file) as json_file:
+        first_char = json_file.read(1)
+        if not first_char:
+            logging.info("All models are safe 🥂")
+        else:
+            df = json_normalize(pd.Series(open(file).readlines()).apply(json.loads))
+            df = df[["name", "tags", "config.schema"]]
+            error_message = "⚠️ The following models are not SAFE ⚠️:\r\n" + df.to_csv(
+                index=False
+            )
+            raise ValueError(error_message)
 
 
 if __name__ == "__main__":
