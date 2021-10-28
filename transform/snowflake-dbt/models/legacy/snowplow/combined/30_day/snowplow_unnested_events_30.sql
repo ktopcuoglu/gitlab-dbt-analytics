e8b2befc6c971312a23aa989c40c6a0e2d4b80bd
Peter Empey <pempey@gitlab.com>
2021-10-28 11:51:05 -0600

    cleaned relationships and typed fields in unstructured events

--- transform/snowflake-dbt/models/legacy/snowplow/combined/30_day/snowplow_unnested_events_30.sql
+++ transform/snowflake-dbt/models/legacy/snowplow/combined/30_day/snowplow_unnested_events_30.sql
@@ -1,3 +1,3 @@
--- depends on: {{ ref('snowplow_sessions') }}
+-- depends on: {{ ref('snowplow_unnested_events') }}
 
 {{ schema_union_limit('snowplow_', 'snowplow_unnested_events', 'derived_tstamp', 30, database_name=env_var('SNOWFLAKE_PREP_DATABASE')) }}
--- transform/snowflake-dbt/models/legacy/snowplow/combined/all/snowplow_page_views_all.sql
+++ transform/snowflake-dbt/models/legacy/snowplow/combined/all/snowplow_page_views_all.sql
@@ -3,6 +3,6 @@
   })
 }}
 
--- depends on: {{ ref('snowplow_sessions') }}
+-- depends on: {{ ref('snowplow_page_views') }}
 
 {{ schema_union_all('snowplow_', 'snowplow_page_views', database_name=env_var('SNOWFLAKE_PREP_DATABASE')) }}
