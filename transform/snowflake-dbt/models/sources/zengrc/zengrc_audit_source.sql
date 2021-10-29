a06830e28cd8ae6d4933c733ca58900812d27847
Peter Empey <pempey@gitlab.com>
2021-10-29 10:34:57 -0600

    correct style and spelling errors

--- transform/snowflake-dbt/models/sources/zengrc/zengrc_audit_source.sql
+++ transform/snowflake-dbt/models/sources/zengrc/zengrc_audit_source.sql
@@ -8,21 +8,21 @@ WITH source AS (
 renamed AS (
 
     SELECT
-        code::VARCHAR                      AS audit_code,
-        created_at::TIMESTAMP              AS audit_created_at,
-        description::VARCHAR               AS audidt_description,
-        end_date::DATE                     AS audit_end_date,
-        id::NUMBER                         AS audit_id,
-        report_period_end_date::DATE       AS audit_report_period_end_date,
-        report_period_start_date::DATE     AS audit_report_period_start_date,
-        start_date::DATE                   AS audit_start_date,
-        status::VARCHAR                    AS audit_status,
-        sync_external_attachments::BOOLEAN AS has_external_attachments,
-        sync_external_comments::BOOLEAN    AS has_external_coments,
-        title::VARCHAR                     AS audit_title,
-        type::VARCHAR                      AS zengrc_object_type,
-        updated_at::TIMESTAMP              AS audit_uploaded_at,
-        __loaded_at::TIMESTAMP             AS audit_loaded_at
+      code::VARCHAR                      AS audit_code,
+      created_at::TIMESTAMP              AS audit_created_at,
+      description::VARCHAR               AS audidt_description,
+      end_date::DATE                     AS audit_end_date,
+      id::NUMBER                         AS audit_id,
+      report_period_end_date::DATE       AS audit_report_period_end_date,
+      report_period_start_date::DATE     AS audit_report_period_start_date,
+      start_date::DATE                   AS audit_start_date,
+      status::VARCHAR                    AS audit_status,
+      sync_external_attachments::BOOLEAN AS has_external_attachments,
+      sync_external_comments::BOOLEAN    AS has_external_comments,
+      title::VARCHAR                     AS audit_title,
+      type::VARCHAR                      AS zengrc_object_type,
+      updated_at::TIMESTAMP              AS audit_uploaded_at,
+      __loaded_at::TIMESTAMP             AS audit_loaded_at
     FROM source
 
 )
--- transform/snowflake-dbt/models/sources/zengrc/zengrc_objective_source.sql
+++ transform/snowflake-dbt/models/sources/zengrc/zengrc_objective_source.sql
@@ -8,16 +8,16 @@ WITH source AS (
 renamed AS (
 
     SELECT
-        code::VARCHAR          AS objective_code,
-        created_at::TIMESTAMP  AS objective_creates_at,
-        description::VARCHAR   AS objective_description,
-        id::NUMBER             AS objective_id,
-        os_state::VARCHAR      AS objective_os_state,
-        status::VARCHAR        AS objective_satus,
-        title::VARCHAR         AS objective_title,
-        type::VARCHAR          AS zengrc_object_type,
-        updated_at::TIMESTAMP  AS objective_updated_at,
-        __loaded_at::TIMESTAMP AS objective_loaded_at
+      code::VARCHAR          AS objective_code,
+      created_at::TIMESTAMP  AS objective_created_at,
+      description::VARCHAR   AS objective_description,
+      id::NUMBER             AS objective_id,
+      os_state::VARCHAR      AS objective_os_state,
+      status::VARCHAR        AS objective_status,
+      title::VARCHAR         AS objective_title,
+      type::VARCHAR          AS zengrc_object_type,
+      updated_at::TIMESTAMP  AS objective_updated_at,
+      __loaded_at::TIMESTAMP AS objective_loaded_at
     FROM source
 
 )
