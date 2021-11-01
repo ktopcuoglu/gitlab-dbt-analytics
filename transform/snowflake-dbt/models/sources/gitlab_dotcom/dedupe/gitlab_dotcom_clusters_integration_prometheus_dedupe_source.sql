ed68ba5768ca2b208b03531546bd0a77d1e44975
Radovan Bacovic <rbacovic@gitlab.com>
2021-11-01 11:35:39 +0100

    Alter schema sources description.

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_clusters_integration_prometheus_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_clusters_integration_prometheus_dedupe_source.sql
@@ -11,4 +11,4 @@ FROM {{ source('gitlab_dotcom', 'clusters_integration_prometheus') }}
 WHERE _uploaded_at >= (SELECT MAX(_uploaded_at) FROM {{this}})
 
 {% endif %}
-QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
+QUALIFY ROW_NUMBER() OVER (PARTITION BY cluster_id ORDER BY _uploaded_at DESC) = 1
--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_milestone_releases_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_milestone_releases_dedupe_source.sql
@@ -11,4 +11,4 @@ FROM {{ source('gitlab_dotcom', 'milestone_releases') }}
 WHERE _uploaded_at >= (SELECT MAX(_uploaded_at) FROM {{this}})
 
 {% endif %}
-QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
+QUALIFY ROW_NUMBER() OVER (PARTITION BY milestone_id, release_id ORDER BY _uploaded_at DESC) = 1
