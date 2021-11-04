a8fd64704831efc99b8d23e52cad8efd9c2acfa9
Paul Armstrong <parmstrong@gitlab.com>
2021-11-04 20:35:12 +0200

    Corrected all account column names to match up with FiveTran output.

--- transform/snowflake-dbt/models/sources/zuora_central_sandbox/zuora_central_sandbox_account_source.sql
+++ transform/snowflake-dbt/models/sources/zuora_central_sandbox/zuora_central_sandbox_account_source.sql
@@ -8,56 +8,54 @@ WITH source AS (
     SELECT
       id                                                     AS account_id,
       -- keys
-      communicationprofileid                                 AS communication_profile_id,
-      nullif("{{this.database}}".{{target.schema}}.id15to18(crmid), '')          
+      communication_profile_id                               AS communication_profile_id,
+      nullif("{{this.database}}".{{target.schema}}.id15to18(crm_id), '')
                                                              AS crm_id,
-      defaultpaymentmethodid                                 AS default_payment_method_id,
-      invoicetemplateid                                      AS invoice_template_id,
-      parentid                                               AS parent_id,
-      soldtocontactid                                        AS sold_to_contact_id,
-      billtocontactid                                        AS bill_to_contact_id,
-      taxexemptcertificateid                                 AS tax_exempt_certificate_id,
-      taxexemptcertificatetype                               AS tax_exempt_certificate_type,
+      default_payment_method_id                              AS default_payment_method_id,
+      invoice_template_id                                    AS invoice_template_id,
+      parent_id                                              AS parent_id,
+      sold_to_contact_id                                     AS sold_to_contact_id,
+      bill_to_contact_id                                     AS bill_to_contact_id,
+      tax_exempt_certificate_id                              AS tax_exempt_certificate_id,
+      tax_exempt_certificate_type                            AS tax_exempt_certificate_type,
 
       -- account info
-      accountnumber                                          AS account_number,
+      account_number                                         AS account_number,
       name                                                   AS account_name,
       notes                                                  AS account_notes,
-      purchaseordernumber                                    AS purchase_order_number,
-      accountcode__c                                         AS sfdc_account_code,
-      status,
-      entity__c                                              AS sfdc_entity,
+      purchase_order_number                                  AS purchase_order_number,
+      account_code_c                                          AS sfdc_account_code,
+      status                                                 AS status,
+      entity_c                                               AS sfdc_entity,
 
-      autopay                                                AS auto_pay,
+      auto_pay                                               AS auto_pay,
       balance                                                AS balance,
-      creditbalance                                          AS credit_balance,
-      billcycleday                                           AS bill_cycle_day,
+      credit_balance                                         AS credit_balance,
+      bill_cycle_day                                         AS bill_cycle_day,
       currency                                               AS currency,
-      conversionrate__c                                      AS sfdc_conversion_rate,
-      paymentterm                                            AS payment_term,
+      conversionrate_c                                       AS sfdc_conversion_rate,
+      payment_term                                           AS payment_term,
 
-      allowinvoiceedit                                       AS allow_invoice_edit,
-      batch,
-      invoicedeliveryprefsemail                              AS invoice_delivery_prefs_email,
-      invoicedeliveryprefsprint                              AS invoice_delivery_prefs_print,
-      paymentgateway                                         AS payment_gateway,
+      allow_invoice_edit                                     AS allow_invoice_edit,
+      batch                                                  AS batch,
+      invoice_delivery_prefs_email                           AS invoice_delivery_prefs_email,
+      invoice_delivery_prefs_print                           AS invoice_delivery_prefs_print,
+      payment_gateway                                        AS payment_gateway,
 
-      customerservicerepname                                 AS customer_service_rep_name,
-      salesrepname                                           AS sales_rep_name,
-      additionalemailaddresses                               AS additional_email_addresses,
-      --billtocontact                   as bill_to_contact,
-      parent__c                                              AS sfdc_parent,
-
+      customer_service_rep_name                              AS customer_service_rep_name,
+      sales_rep_name                                         AS sales_rep_name,
+      additional_email_addresses                             AS additional_email_addresses,
+      parent_c                                               AS sfdc_parent,
 
       -- financial info
-      lastinvoicedate                                        AS last_invoice_date,
+      last_invoice_date                                      AS last_invoice_date,
 
       -- metadata
-      createdbyid                                            AS created_by_id,
-      createddate                                            AS created_date,
-      updatedbyid                                            AS updated_by_id,
-      updateddate                                            AS updated_date,
-      deleted                                                AS is_deleted
+      created_by_id                                          AS created_by_id,
+      created_date                                           AS created_date,
+      updated_by_id                                          AS updated_by_id,
+      updated_date                                           AS updated_date,
+      _FIVETRAN_DELETED                                      AS is_deleted
 
     FROM source
 
