d38ee22aaa6ccf3960b11d513734d76745c0b8e1
Paul Armstrong <parmstrong@gitlab.com>
2021-11-05 11:11:48 +0200

    Column name corrections.

--- transform/snowflake-dbt/models/sources/zuora_central_sandbox/zuora_central_sandbox_rate_plan_charge_tier_source.sql
+++ transform/snowflake-dbt/models/sources/zuora_central_sandbox/zuora_central_sandbox_rate_plan_charge_tier_source.sql
@@ -6,8 +6,8 @@ WITH source AS (
 ), renamed AS (
 
     SELECT 
-      rateplanchargeid        AS rate_plan_charge_id,
-      productrateplanchargeid AS product_rate_plan_charge_id,
+      rate_plan_charge_id         AS rate_plan_charge_id,
+      product_rate_plan_charge_id AS product_rate_plan_charge_id,
       price,
       currency
     FROM source
