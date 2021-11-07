e3643b9aa36c5553e944e89ce2da825f24947b22
iweeks <iweeks@gitlab.com>
2021-11-06 20:39:13 -0400

    updated primary key

--- transform/snowflake-dbt/snapshots/marts/mart_charge_snapshot.sql
+++ transform/snowflake-dbt/snapshots/marts/mart_charge_snapshot.sql
@@ -2,7 +2,7 @@
     -- Using dbt updated at field as we want a new set of data everyday.
     {{
         config(
-          unique_key='primary_key',
+          unique_key='dim_charge_id',
           strategy='timestamp',
           updated_at='dbt_created_at',
           invalidate_hard_deletes=True
