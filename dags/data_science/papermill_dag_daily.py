941724b66bbef63d5853adfac3e558151448a948
Paul Armstrong <parmstrong@gitlab.com>
2021-11-08 15:49:51 +0200

    Correct syntax.

--- dags/data_science/papermill_dag_daily.py
+++ dags/data_science/papermill_dag_daily.py
@@ -45,7 +45,7 @@ dag = DAG("papermill_daily", default_args=default_args, schedule_interval="0 5 *
 drop_clones_cmd = f"""
     {clone_datascience_repo_cmd} &&
     cd data-science/deployments/daily && 
-    papermill scoring_code.ipynb && 
+    papermill scoring_code.ipynb
 """
 KubernetesPodOperator(
         **gitlab_defaults,
