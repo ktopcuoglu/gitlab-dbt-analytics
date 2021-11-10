bfdc1bf4a24521dce73252e1b8f60444ca3a9050
@dvanrooijen2 <dvanrooijen@gitlab.com>
2021-11-10 10:45:27 +0100

    adds tables

---
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_dast_profiles_source.sql
@@ -0,0 +1,24 @@
+WITH source AS (
+
+  SELECT *
+  FROM {{ ref('gitlab_dotcom_dast_profiles_dedupe_source') }}
+  
+), renamed AS (
+
+  SELECT
+    id::NUMBER                        AS dast_profile_id,
+    project_id::NUMBER                AS project_id,
+    dast_site_profile_id::NUMBER      AS dast_site_profile_id,
+    dast_scanner_profile_id::NUMBER   AS dast_scanner_profile_id,
+    created_at::TIMESTAMP             AS created_at, 
+    updated_at::TIMESTAMP             AS updated_at,
+    name::VARCHAR                     AS name,
+    description::VARCHAR              AS description,
+    branch_name::VARCHAR              AS branch_name
+  FROM source
+
+)
+
+SELECT *
+FROM renamed
+ORDER BY created_at
