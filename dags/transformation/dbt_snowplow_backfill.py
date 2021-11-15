8dd154d98492e9cddca3905f4078320adc236079
Paul Armstrong <parmstrong@gitlab.com>
2021-11-15 20:35:25 +0200

    Corrected git branch & target mapping.

--- dags/transformation/dbt_snowplow_backfill.py
+++ dags/transformation/dbt_snowplow_backfill.py
@@ -38,6 +38,11 @@ env = os.environ.copy()
 GIT_BRANCH = env["GIT_BRANCH"]
 pod_env_vars = {**gitlab_pod_env_vars, **{}}
 
+if GIT_BRANCH in ['master', 'main']:
+    target = 'prod'
+else:
+    target = 'ci'
+
 task_secrets = [
     GIT_DATA_TESTS_PRIVATE_KEY,
     GIT_DATA_TESTS_CONFIG,
@@ -73,7 +78,7 @@ dag = DAG(
     "dbt_snowplow_backfill",
     default_args=default_args,
     schedule_interval=None,
-    concurrency=4,
+    concurrency=2,
 )
 
 
@@ -83,7 +88,7 @@ def generate_dbt_command(vars_dict):
     dbt_generate_command = f""" 
         {dbt_install_deps_nosha_cmd} &&
         export SNOWFLAKE_TRANSFORM_WAREHOUSE="TRANSFORMING_4XL" &&
-        dbt run --profiles-dir profile --target prod --models +snowplow --full-refresh --vars '{json_dict}'; ret=$?;
+        dbt run --profiles-dir profile --target {target} --models +snowplow --full-refresh --vars '{json_dict}'; ret=$?;
         python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
         """
 
@@ -101,7 +106,7 @@ def generate_dbt_command(vars_dict):
 
 dbt_snowplow_combined_cmd = f"""
         {dbt_install_deps_nosha_cmd} &&
-        dbt run --profiles-dir profile --target prod --models staging.snowplow.combined; ret=$?;
+        dbt run --profiles-dir profile --target {target} --models staging.snowplow.combined; ret=$?;
         python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
         """
 
