7c12acbe93003d224c89620a861c48a3d6e475c9
Radovan Bacovic <rbacovic@gitlab.com>
2021-11-17 12:05:53 +0100

    Formatted code properly.

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_milestone_releases_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_milestone_releases_dedupe_source.sql
@@ -6,23 +6,21 @@
 
 WITH base AS (
 
-SELECT *
-FROM {{ source('gitlab_dotcom', 'milestone_releases') }}
-{% if is_incremental() %}
+    SELECT *
+    FROM {{ source('gitlab_dotcom', 'milestone_releases') }}
+    {% if is_incremental() %}
 
-WHERE _uploaded_at >= (SELECT MAX(_uploaded_at) FROM {{this}})
+    WHERE _uploaded_at >= (SELECT MAX(_uploaded_at) FROM {{this}})
 
-{% endif %}
-
-QUALIFY ROW_NUMBER() OVER (PARTITION BY milestone_id, release_id ORDER BY _uploaded_at DESC) = 1
+    {% endif %}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY milestone_id, release_id ORDER BY _uploaded_at DESC) = 1
 
 ), renamed AS (
 
-    SELECT
-      {{ dbt_utils.surrogate_key( ['milestone_id', 'release_id'])}} AS primary_key,
-      milestone_id::INT                                             AS milestone_id,
-      release_id::INT                                               AS release_id,
-      _uploaded_at                                                  AS _uploaded_at
+    SELECT {{ dbt_utils.surrogate_key( ['milestone_id', 'release_id'])}} AS primary_key,
+           milestone_id::INT                                             AS milestone_id,
+           release_id::INT                                               AS release_id,
+           _uploaded_at                                                  AS _uploaded_at
     FROM base
 
 )
