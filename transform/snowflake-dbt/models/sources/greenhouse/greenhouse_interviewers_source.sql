0255a0491c6997ee5ecb1eb5bbcea37ba5fa9045
Radovan Bacovic <rbacovic@gitlab.com>
2021-11-17 16:46:21 +0100

    WHERE clause to exclude user added (1 record).

--- transform/snowflake-dbt/models/sources/greenhouse/greenhouse_interviewers_source.sql
+++ transform/snowflake-dbt/models/sources/greenhouse/greenhouse_interviewers_source.sql
@@ -1,7 +1,12 @@
 WITH source as (
 
-  SELECT *
-  FROM {{ source('greenhouse', 'interviewers') }}
+    SELECT *
+    FROM {{ source('greenhouse', 'interviewers') }}
+    -- intentionally excluded record, details in https://gitlab.com/gitlab-data/analytics/-/issues/10211
+    WHERE NOT (user_id      = 4932640002
+    AND interview_id        = 101187575002
+    AND scorecard_id        IS NULL
+    AND user                IS NULL)
 
 ), renamed as (
 
