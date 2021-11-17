16a8e554ea45af84bc9d3713cb638104e071feb9
Radovan Bacovic <rbacovic@gitlab.com>
2021-11-17 11:38:33 +0100

    Model fixed and PK added (composite primary key as per definition from prod DB).

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_milestone_releases_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_milestone_releases_dedupe_source.sql
@@ -1,8 +1,10 @@
 {{ config({
     "materialized": "incremental",
-    "unique_key": "milestone_id"
+    "unique_key": "primary_key"
     })
 }}
+
+WITH base AS (
 
 SELECT *
 FROM {{ source('gitlab_dotcom', 'milestone_releases') }}
@@ -11,4 +13,19 @@ FROM {{ source('gitlab_dotcom', 'milestone_releases') }}
 WHERE _uploaded_at >= (SELECT MAX(_uploaded_at) FROM {{this}})
 
 {% endif %}
+
 QUALIFY ROW_NUMBER() OVER (PARTITION BY milestone_id, release_id ORDER BY _uploaded_at DESC) = 1
+
+), renamed AS (
+
+    SELECT
+      {{ dbt_utils.surrogate_key( ['milestone_id', 'release_id'])}} AS primary_key,
+      milestone_id::INT                                             AS milestone_id,
+      release_id::INT                                               AS release_id,
+      _uploaded_at                                                  AS _uploaded_at
+    FROM base
+
+)
+
+SELECT *
+FROM renamed
