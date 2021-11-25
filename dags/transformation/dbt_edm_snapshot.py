92ed140c11d2328f8f3399f2c432de635171aefc
Paul Armstrong <parmstrong@gitlab.com>
2021-11-25 19:58:33 +0200

    Added edm snapshot DAG to run through the entire edm snapshot process separately.

---
+++ dags/transformation/dbt_edm_snapshot.py
@@ -0,0 +1,143 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+from airflow_utils import (
+    DBT_IMAGE,
+    dbt_install_deps_cmd,
+    dbt_install_deps_nosha_cmd,
+    gitlab_defaults,
+    gitlab_pod_env_vars,
+    slack_failed_task,
+    dbt_install_deps_and_seed_cmd,
+    clone_repo_cmd,
+)
+from kube_secrets import (
+    GIT_DATA_TESTS_PRIVATE_KEY,
+    GIT_DATA_TESTS_CONFIG,
+    SALT,
+    SALT_EMAIL,
+    SALT_IP,
+    SALT_NAME,
+    SALT_PASSWORD,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+    SNOWFLAKE_USER,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+)
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+GIT_BRANCH = env["GIT_BRANCH"]
+pod_env_vars = {**gitlab_pod_env_vars, **{}}
+task_secrets = [
+    GIT_DATA_TESTS_PRIVATE_KEY,
+    GIT_DATA_TESTS_CONFIG,
+    SALT,
+    SALT_EMAIL,
+    SALT_IP,
+    SALT_NAME,
+    SALT_PASSWORD,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+    SNOWFLAKE_USER,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+]
+
+pull_commit_hash = """export GIT_COMMIT="{{ var.value.dbt_hash }}" """
+
+# Default arguments for the DAG
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "sla": timedelta(hours=12),
+    "sla_miss_callback": slack_failed_task,
+    "start_date": datetime(2019, 1, 1, 0, 0, 0),
+    "dagrun_timeout": timedelta(hours=6),
+}
+
+# Create the DAG
+dag = DAG("dbt_edm_snapshots", default_args=default_args, schedule_interval="0 8 * * *")
+
+# dbt-snapshot for daily tag
+dbt_snapshot_cmd = f"""
+    {dbt_install_deps_nosha_cmd} &&
+    dbt snapshot -s tag:edm_snapshot --profiles-dir profile; ret=$?;
+    python ../../orchestration/upload_dbt_file_to_snowflake.py snapshots; exit $ret
+"""
+
+dbt_snapshot = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DBT_IMAGE,
+    task_id="dbt-edm-snapshots",
+    name="dbt-edm-snapshots",
+    secrets=task_secrets,
+    env_vars=pod_env_vars,
+    arguments=[dbt_snapshot_cmd],
+    dag=dag,
+)
+
+
+# run snapshots on large warehouse
+dbt_snapshot_models_command = f"""
+    {pull_commit_hash} &&
+    {dbt_install_deps_and_seed_cmd} &&
+    export SNOWFLAKE_TRANSFORM_WAREHOUSE="TRANSFORMING_L" &&
+    dbt run --profiles-dir profile --target prod --models tags:edm_snapshot; ret=$?;
+    python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
+"""
+
+dbt_snapshot_models_run = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DBT_IMAGE,
+    task_id="dbt-run-edm-model-snapshots",
+    name="dbt-run-edm-model-snapshots",
+    trigger_rule="all_done",
+    secrets=task_secrets,
+    env_vars=pod_env_vars,
+    arguments=[dbt_snapshot_models_command],
+    dag=dag,
+)
+
+# dbt-test
+dbt_test_snapshots_cmd = f"""
+    {pull_commit_hash} &&
+    {dbt_install_deps_cmd} &&
+    dbt test --profiles-dir profile --target prod --models tags:edm_snapshot; ret=$?;
+    python ../../orchestration/upload_dbt_file_to_snowflake.py test; exit $ret
+"""
+
+dbt_test_snapshot_models = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DBT_IMAGE,
+    task_id="dbt-test-edm-snapshots",
+    name="dbt-test-edm-snapshots",
+    trigger_rule="all_done",
+    secrets=task_secrets,
+    env_vars=pod_env_vars,
+    arguments=[dbt_test_snapshots_cmd],
+    dag=dag,
+)
+
+
+(
+    dbt_snapshot
+    >> dbt_snapshot_models_run
+    >> dbt_test_snapshot_models
+)
