b41846ac069ec4fc7cf40ef96c02c8211d7ccbb7
Paul Armstrong <parmstrong@gitlab.com>
2021-11-25 19:59:01 +0200

    Remove edm snapshot from normal snapshot run.

--- dags/transformation/dbt_snapshot.py
+++ dags/transformation/dbt_snapshot.py
@@ -140,7 +140,7 @@ dbt_snapshot_models_command = f"""
     {pull_commit_hash} &&
     {dbt_install_deps_and_seed_cmd} &&
     export SNOWFLAKE_TRANSFORM_WAREHOUSE="TRANSFORMING_L" &&
-    dbt run --profiles-dir profile --target prod --models +legacy.snapshots; ret=$?;
+    dbt run --profiles-dir profile --target prod --models +legacy.snapshots --exclude tags:edm_snapshot; ret=$?;
     python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
 """
 
@@ -160,7 +160,7 @@ dbt_snapshot_models_run = KubernetesPodOperator(
 dbt_test_snapshots_cmd = f"""
     {pull_commit_hash} &&
     {dbt_install_deps_cmd} &&
-    dbt test --profiles-dir profile --target prod --models +legacy.snapshots; ret=$?;
+    dbt test --profiles-dir profile --target prod --models +legacy.snapshots --exclude tags:edm_snapshot; ret=$?;
     python ../../orchestration/upload_dbt_file_to_snowflake.py test; exit $ret
 """
 
