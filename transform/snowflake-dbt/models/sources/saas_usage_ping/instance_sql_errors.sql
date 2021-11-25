110fec160cfea53fc366babb318aaee18b530ef6
Paul Armstrong <parmstrong@gitlab.com>
2021-11-25 12:47:28 +0000

    Rename output columns to updated_at

--- transform/snowflake-dbt/models/sources/saas_usage_ping/instance_sql_errors.sql
+++ transform/snowflake-dbt/models/sources/saas_usage_ping/instance_sql_errors.sql
@@ -9,7 +9,7 @@ WITH base AS (
       run_id       AS run_id,
       sql_errors   AS sql_errors,
       ping_date    AS ping_date,
-      _uploaded_at AS _uploaded_at
+      _uploaded_at AS uploaded_at
     FROM base
 
 ), renamed AS (
@@ -18,7 +18,7 @@ WITH base AS (
       run_id                                              AS run_id,
       TRY_PARSE_JSON(sql_errors)                          AS sql_errors,
       ping_date::TIMESTAMP                                AS ping_date,
-      DATEADD('s', _uploaded_at, '1970-01-01')::TIMESTAMP AS _uploaded_at
+      DATEADD('s', uploaded_at, '1970-01-01')::TIMESTAMP AS uploaded_at
     FROM partitioned
 
 )
