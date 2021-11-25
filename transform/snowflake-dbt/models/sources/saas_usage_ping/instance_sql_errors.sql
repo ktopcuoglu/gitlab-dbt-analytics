857481d23066db1025788d9e090a29b0bc22173c
Paul Armstrong <parmstrong@gitlab.com>
2021-11-25 12:48:54 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/sources/saas_usage_ping/instance_sql_errors.sql
+++ transform/snowflake-dbt/models/sources/saas_usage_ping/instance_sql_errors.sql
@@ -18,7 +18,7 @@ WITH base AS (
       run_id                                              AS run_id,
       TRY_PARSE_JSON(sql_errors)                          AS sql_errors,
       ping_date::TIMESTAMP                                AS ping_date,
-      DATEADD('s', uploaded_at, '1970-01-01')::TIMESTAMP AS uploaded_at
+      DATEADD('s', uploaded_at, '1970-01-01')::TIMESTAMP  AS uploaded_at
     FROM partitioned
 
 )
