ec98638949188c95112d5e4515764a4bb04e6f4a
Paul Armstrong <parmstrong@gitlab.com>
2021-11-26 18:04:34 +0200

    Add success logs.

--- orchestration/upload_dbt_file_to_snowflake.py
+++ orchestration/upload_dbt_file_to_snowflake.py
@@ -46,9 +46,9 @@ if __name__ == "__main__":
             get_table_name(config_name, snowflake_database),
             snowflake_engine,
         )
-        sys.exit(0)
+        logging.info(f"{file_name} uploaded successfully")
     else:
         logging.error(
             f"Dbt File {file_name} is missing. Check if dbt run completed successfully"
         )
-        sys.exit(1)
+
