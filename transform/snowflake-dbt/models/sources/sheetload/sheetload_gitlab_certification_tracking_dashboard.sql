234352f012fc7364184db99cd9c0ff8098633434
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-08 09:21:53 +0100

    Add definition for dbt jobs.

---
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_gitlab_certification_tracking_dashboard.sql
@@ -0,0 +1,26 @@
+WITH source AS (
+
+  SELECT *
+  FROM {{ source('sheetload', 'gitlab_certification_tracking_dashboard') }}
+
+), renamed AS (
+
+  SELECT
+    user::VARCHAR              AS user,
+    account::VARCHAR           AS account,
+    passed_datetime::TIMESTAMP AS passed_datetime,
+    certification::VARCHAR     AS certification,
+    cert_date::DATE            AS cert_date,
+    partner_sfdc_id::VARCHAR   AS partner_sfdc_id,
+    account_owner::VARCHAR     AS account_owner,
+    region::VARCHAR            AS region,
+    track::VARCHAR             AS track,
+    pubsec_partner::BOOLEAN    AS pubsec_partner,
+    cert_month::VARCHAR        AS cert_month,
+    cert_quarter::VARCHAR      AS cert_quarter
+  FROM source  
+
+)
+
+SELECT *
+FROM renamed
