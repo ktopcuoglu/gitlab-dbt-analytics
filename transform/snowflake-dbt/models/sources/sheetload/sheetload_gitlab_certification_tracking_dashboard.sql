7a0472bbf4172868b2ab870f43a1246028ead251
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-10 13:14:54 +0100

    Column _updated_at added in the dbt job.

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_gitlab_certification_tracking_dashboard.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_gitlab_certification_tracking_dashboard.sql
@@ -6,19 +6,20 @@ WITH source AS (
 ), renamed AS (
 
   SELECT
-    user::VARCHAR              AS user,
-    account::VARCHAR           AS account,
-    passed_datetime::TIMESTAMP AS passed_datetime,
-    certification::VARCHAR     AS certification,
-    cert_date::DATE            AS cert_date,
-    partner_sfdc_id::VARCHAR   AS partner_sfdc_id,
-    account_owner::VARCHAR     AS account_owner,
-    region::VARCHAR            AS region,
-    track::VARCHAR             AS track,
-    pubsec_partner::BOOLEAN    AS pubsec_partner,
-    cert_month::VARCHAR        AS cert_month,
-    cert_quarter::VARCHAR      AS cert_quarter
-  FROM source  
+    user::VARCHAR                                       AS user,
+    account::VARCHAR                                    AS account,
+    passed_datetime::TIMESTAMP                          AS passed_datetime,
+    certification::VARCHAR                              AS certification,
+    cert_date::DATE                                     AS cert_date,
+    partner_sfdc_id::VARCHAR                            AS partner_sfdc_id,
+    account_owner::VARCHAR                              AS account_owner,
+    region::VARCHAR                                     AS region,
+    track::VARCHAR                                      AS track,
+    pubsec_partner::BOOLEAN                             AS pubsec_partner,
+    cert_month::VARCHAR                                 AS cert_month,
+    cert_quarter::VARCHAR                               AS cert_quarter,
+    DATEADD('s', _uploaded_at, '1970-01-01')::TIMESTAMP AS _uploaded_at
+  FROM source
 
 )
 
