f319408f35b36f12feafcca0867402ce1c7beff5
@dvanrooijen2 <dvanrooijen@gitlab.com>
2021-12-10 15:08:56 +0100

    model change

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_capacity_kpi_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_sales_capacity_kpi_source.sql
@@ -3,9 +3,16 @@ WITH source AS (
     SELECT *
     FROM {{ source('sheetload', 'sales_capacity_kpi') }}
 
+), renamed AS (
+
+    SELECT
+        month::DATE                                         AS month,
+        NULLIF(target, 0)                                   AS target,
+        NULLIF(actual, 0)                                   AS actual,      
+        TO_TIMESTAMP(TO_NUMERIC("_UPDATED_AT"))::TIMESTAMP  AS last_updated_at
+    FROM source
+
 )
 
 SELECT *
-FROM source
-
-
+FROM renamed
