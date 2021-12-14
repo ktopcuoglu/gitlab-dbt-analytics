1390b684189402ca2a9c4a1225c5b2e969a970f8
Peter Empey <pempey@gitlab.com>
2021-12-14 13:33:11 -0700

    adding departments and sources first pass

--- transform/snowflake-dbt/models/workspaces/workspace_people/rpt_hires.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_people/rpt_hires.sql
@@ -1,8 +1,8 @@
-WITH hires AS (
-    
-    SELECT *
-    FROM {{ ref('greenhouse_hires') }} 
-)
+{{ simple_cte([
+    ('hires','greenhouse_hires'),
+    ('job_departments','greenhouse_jobs_departments_source'),
+    ('departments','wk_prep_greenhouse_departments')
+])}}
 
 SELECT
   hires.unique_key,
@@ -13,6 +13,14 @@ SELECT
   hires.hired_in_bamboohr,
   hires.candidate_id,
   hires.job_opening_type,
-  hires.hire_type
+  hires.hire_type,
+  departments.department_name       AS greenhouse_department_name,
+  departments.level_1               AS greenhouse_department_level_1,
+  departments.level_2               AS greenhouse_department_level_2,
+  departments.level_3               AS greenhouse_department_level_3
 FROM hires
+LEFT JOIN job_departments
+  ON hires.job_id = job_departments.job_id
+LEFT JOIN departments
+  ON job_departments.department_id = departments.department_id
 
