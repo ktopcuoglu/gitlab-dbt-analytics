3f35a5c95a7e183be18e381456978ba98c3bb812
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-15 16:38:56 +0100

    Remove comment from dbt jobs.

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -11,9 +11,6 @@ SELECT DISTINCT
 FROM version_usage_data,
   lateral flatten(input => version_usage_data.stats_used, recursive => True) f
 WHERE IS_OBJECT(f.value) = FALSE
--- error when 'license_scanning_jobs' pops up.
--- more details in the issue https://gitlab.com/gitlab-data/analytics/-/issues/10749
--- This is a temporary solution and this hard-coded values should be removed.
 AND full_ping_name NOT IN ('groups_bugdb_active',
                            'license_scanning_jobs',
                            'groups_shimo_active',
