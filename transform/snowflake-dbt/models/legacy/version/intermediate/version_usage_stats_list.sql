82556d47dab3e84dddeab7cbd90509a234cd9355
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-15 16:43:23 +0100

    Remove comment from dbt jobs.

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -11,8 +11,4 @@ SELECT DISTINCT
 FROM version_usage_data,
   lateral flatten(input => version_usage_data.stats_used, recursive => True) f
 WHERE IS_OBJECT(f.value) = FALSE
-AND full_ping_name NOT IN ('groups_bugdb_active',
-                           'license_scanning_jobs',
-                           'groups_shimo_active',
-                           'projects_inheriting_shimo_active',
-                           'projects_inheriting_bugdb_active')
+AND full_ping_name NOT IN ('groups_bugdb_active', 'license_scanning_jobs', 'groups_shimo_active','projects_inheriting_shimo_active', 'projects_inheriting_bugdb_active')
