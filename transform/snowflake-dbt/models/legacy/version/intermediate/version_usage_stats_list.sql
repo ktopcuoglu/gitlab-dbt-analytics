b3faa3059c5b1d2bf04f79f5938218b6982d9436
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-15 13:29:21 +0100

    Final version.

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -13,6 +13,7 @@ FROM version_usage_data,
 WHERE IS_OBJECT(f.value) = FALSE
 -- error when 'license_scanning_jobs' pops up.
 -- more details in the issue https://gitlab.com/gitlab-data/analytics/-/issues/10749
+-- This is a temporary solution and this hard-coded values should be removed.
 AND full_ping_name NOT IN ('groups_bugdb_active',
                            'license_scanning_jobs',
                            'groups_shimo_active',
