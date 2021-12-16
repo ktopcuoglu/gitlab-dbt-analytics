aa8aecae65ae62dd9b6457a56b247a2d35f41387
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-16 10:43:59 +0100

    Final version.

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -3,17 +3,15 @@ WITH version_usage_data AS (
   SELECT * 
   FROM {{ ref('version_usage_data') }}
 
-), final AS (
-
-    SELECT DISTINCT
-      f.path                     AS ping_name,
-      REPLACE(f.path, '.','_')   AS full_ping_name
-    FROM version_usage_data,
-    lateral flatten(input => version_usage_data.stats_used, recursive => True) f
-    WHERE IS_OBJECT(f.value) = FALSE
-    AND full_ping_name NOT IN ('groups_bugdb_active', 'license_scanning_jobs', 'groups_shimo_active','projects_inheriting_shimo_active', 'projects_inheriting_bugdb_active')
-
 )
 
-SELECT *
-FROM final
+SELECT DISTINCT
+  f.path                     AS ping_name,
+  REPLACE(f.path, '.','_')   AS full_ping_name
+FROM version_usage_data,
+lateral flatten(input => version_usage_data.stats_used, recursive => True) f
+WHERE IS_OBJECT(f.value) = FALSE
+-- error when 5 items pops up.
+-- more details in the issue https://gitlab.com/gitlab-data/analytics/-/issues/10749
+-- This is a temporary solution and this hard-coded values should be removed.
+AND full_ping_name NOT IN ('groups_bugdb_active', 'license_scanning_jobs', 'groups_shimo_active','projects_inheriting_shimo_active', 'projects_inheriting_bugdb_active')
