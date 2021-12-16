c08d84a8f610146b39af3926f4b524a189b0aa59
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-16 09:28:12 +0100

    Reformat code to have final level

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -3,12 +3,17 @@ WITH version_usage_data AS (
   SELECT * 
   FROM {{ ref('version_usage_data') }}
 
+), final AS (
+
+    SELECT DISTINCT
+      f.path                     AS ping_name,
+      REPLACE(f.path, '.','_')   AS full_ping_name
+    FROM version_usage_data,
+      lateral flatten(input => version_usage_data.stats_used, recursive => True) f
+    WHERE IS_OBJECT(f.value) = FALSE
+    AND full_ping_name NOT IN ('groups_bugdb_active', 'license_scanning_jobs', 'groups_shimo_active','projects_inheriting_shimo_active', 'projects_inheriting_bugdb_active')
+
 )
 
-SELECT DISTINCT
-  f.path                     AS ping_name, 
-  REPLACE(f.path, '.','_')   AS full_ping_name
-FROM version_usage_data,
-  lateral flatten(input => version_usage_data.stats_used, recursive => True) f
-WHERE IS_OBJECT(f.value) = FALSE
-AND full_ping_name NOT IN ('groups_bugdb_active', 'license_scanning_jobs', 'groups_shimo_active','projects_inheriting_shimo_active', 'projects_inheriting_bugdb_active')
+SELECT *
+FROM final
