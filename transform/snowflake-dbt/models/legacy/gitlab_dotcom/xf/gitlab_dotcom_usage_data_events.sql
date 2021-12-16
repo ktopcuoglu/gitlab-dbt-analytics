ff83939fe7ab703db094420ac3ecef6a1099022c
Chris Sharp <csharp@gitlab.com>
2021-12-16 17:01:50 +0000

    add incremental config back in

--- transform/snowflake-dbt/models/legacy/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
+++ transform/snowflake-dbt/models/legacy/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
@@ -2,6 +2,12 @@
     tags=["mnpi_exception"]
 ) }}
 
+{{ config({
+        "materialized": "incremental",
+        "unique_key": "primary_key"
+        }) 
+}}
+
 /*
   Each dict must have ALL of the following:
     * event_name
@@ -624,7 +630,9 @@
       FROM {{ event_cte.source_cte_name }}
     {% endif %}
     WHERE created_at IS NOT NULL
-      AND created_at >= DATEADD(YEAR, -2, CURRENT_DATE)
+      {% if is_incremental() %}
+        AND created_at >= (SELECT MAX(event_created_at) FROM {{this}} WHERE event_name = '{{ event_cte.event_name }}')
+      {% endif %}
 
 )
 
