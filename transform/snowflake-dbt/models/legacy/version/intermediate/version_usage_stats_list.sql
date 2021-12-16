747e18d7a625033b66c136e333d226baf220639c
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-16 11:03:26 +0100

    Fake commit to run pipeline.

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -11,7 +11,7 @@ SELECT DISTINCT
 FROM version_usage_data,
 lateral flatten(input => version_usage_data.stats_used, recursive => True) f
 WHERE IS_OBJECT(f.value) = FALSE
--- error when 5 items pops up. 
+-- error when 5 items pops up.
 -- more details in the issue https://gitlab.com/gitlab-data/analytics/-/issues/10749
 -- This is a temporary solution and this hard-coded values should be removed.
 AND full_ping_name NOT IN ('groups_bugdb_active', 'license_scanning_jobs', 'groups_shimo_active','projects_inheriting_shimo_active', 'projects_inheriting_bugdb_active')
