aca147fe1f858043bf0c4c5cc4666ccfeb3d8ccd
Peter Empey <pempey@gitlab.com>
2021-12-20 12:21:25 -0700

    use enddate if exists

--- transform/snowflake-dbt/models/legacy/sheetload/sheetload_engineering_speciality_prior_to_capture.sql
+++ transform/snowflake-dbt/models/legacy/sheetload/sheetload_engineering_speciality_prior_to_capture.sql
@@ -9,5 +9,6 @@ SELECT
   employee_id, 
   speciality, 
   start_date                                                                           AS speciality_start_date,
-  LEAD(DATEADD(day,-1,start_date)) OVER (PARTITION BY employee_id ORDER BY start_date) AS speciality_end_date
+  COALESCE(DATEADD('day',-1,end_date),
+  LEAD(DATEADD(day,-1,start_date)) OVER (PARTITION BY employee_id ORDER BY start_date)) AS speciality_end_date
 FROM source
