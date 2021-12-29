07c56016b899f1d811341ba4f62e3cc26247fea6
Radovan Bacovic <rbacovic@gitlab.com>
2021-12-29 13:01:02 +0100

    Tested code working fine.

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -11,13 +11,18 @@ SELECT DISTINCT
 FROM version_usage_data,
 lateral flatten(input => version_usage_data.stats_used, recursive => True) f
 WHERE IS_OBJECT(f.value) = FALSE
--- error when 7 items pops up.
+-- error when listed items pops up.
 -- more details in the issue https://gitlab.com/gitlab-data/analytics/-/issues/10749
 -- This is a temporary solution and this hard-coded values should be removed.
 AND full_ping_name NOT IN ('groups_bugdb_active',
-                           'license_scanning_jobs',
                            'groups_shimo_active',
+                           'groups_inheriting_bugdb_active',
+                           'projects_bugdb_active',
                            'projects_inheriting_shimo_active',
                            'projects_inheriting_bugdb_active',
-                           'templates_bugdb_active',
-                           'instances_shimo_active')
+                           'projects_shimo_active',
+                           'instances_shimo_active',
+                           'license_scanning_jobs',
+                           'templates_bugdb_active')
+AND full_ping_name NOT ILIKE ('%_BUGDB_%')
+AND full_ping_name NOT ILIKE ('%_SHIMO_%')
