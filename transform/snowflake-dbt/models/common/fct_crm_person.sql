7c4ae4a2719e3c225eb658953257d2febdc8d192
Israel Weeks <iweeks@gitlab.com>
2022-01-07 21:57:02 +0000

    format

--- transform/snowflake-dbt/models/common/fct_crm_person.sql
+++ transform/snowflake-dbt/models/common/fct_crm_person.sql
@@ -176,11 +176,11 @@ WITH account_dims_mapping AS (
       COALESCE(sfdc_contacts.inquiry_datetime_inferred, sfdc_leads.inquiry_datetime_inferred)::DATE             AS inquiry_inferred_datetime,
       {{ get_date_id('inquiry_inferred_datetime') }}                                                            AS inquiry_inferred_datetime_id,
       {{ get_date_pt_id('inquiry_inferred_datetime') }}                                                         AS inquiry_inferred_datetime_pt_id,
-      LEAST(COALESCE(inquiry_date,'9999-01-01'),COALESCE(inquiry_inferred_datetime,'9999-01-01')) AS prep_true_inquiry_date,
+      LEAST(COALESCE(inquiry_date,'9999-01-01'),COALESCE(inquiry_inferred_datetime,'9999-01-01'))               AS prep_true_inquiry_date,
       CASE 
         WHEN prep_true_inquiry_date != '9999-01-01'
-           THEN prep_true_inquiry_date
-           END                                                                                                  AS true_inquiry_date,
+        THEN prep_true_inquiry_date
+      END                                                                                                       AS true_inquiry_date,
       mqls.first_mql_date::DATE                                                                                 AS mql_date_first,
       mqls.first_mql_date                                                                                       AS mql_datetime_first,
       CONVERT_TIMEZONE('America/Los_Angeles', mqls.first_mql_date)                                              AS mql_datetime_first_pt,
