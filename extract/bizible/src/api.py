177d668df99629740a04fdf64c7e48b04b005b5b
Paul Armstrong <parmstrong@gitlab.com>
2022-01-07 14:52:59 +0200

    Added bizible extractors.

--- extract/bizible/src/api.py
+++ extract/bizible/src/api.py
@@ -1,122 +1,107 @@
+import json
+import logging
 import os
 import sys
-from typing import Dict, Any
 
-import requests
+from pathlib import Path
+from time import time
+from typing import Any, Dict, List, Tuple
 
+import pandas as pd
+import yaml
+from snowflake.sqlalchemy import URL as snowflake_URL
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+from yaml.loader import SafeLoader
 
-class BambooAPI:
-    """
-    Initialized with subdomain and an API key. Currently just allows for fetching from the directory
-    and from tabular data. JSON is the only accepted return data type currently. Borrowed heavily from
-    https://github.com/smeggingsmegger/PyBambooHR/blob/master/PyBambooHR/PyBambooHR.py
-    """
+from gitlabdata.orchestration_utils import (
+    data_science_engine_factory,
+    query_dataframe,
+    query_executor,
+    snowflake_stage_load_copy_remove,
+    snowflake_engine_factory,
+)
+from typing import Dict
 
-    def __init__(self, subdomain="", api_token=None, datatype="JSON", timeout=120):
-        self.subdomain = subdomain
-
-        self.base_url = f"https://api.bamboohr.com/api/gateway.php/{subdomain}/v1/"
-
-        if api_token is not None:
-            self.api_token = api_token
-        else:
-            self.api_token = os.environ.get("BAMBOOHR_API_TOKEN")
-        if self.api_token is None:
-            raise Exception("BambooHR API token not configured")
-
-        # JSON or XML
-        self.datatype = datatype
-
-        # Global headers
-        self.headers = {}
-
-        self.timeout = timeout
-
-        if self.datatype == "JSON":
-            self.headers.update({"Accept": "application/json"})
-
-    def get_employee_directory(self) -> Dict[Any, Any]:
+class BizibleSnowFlakeExtractor:
+    def __init__(self, config_dict: Dict):
         """
-        API method for returning a globally shared company directory.
-        http://www.bamboohr.com/api/documentation/employees.php
-        @return: A list of employee dictionaries which is a list of employees in the directory.
+
+        :param config_dict: To be passed from the execute.py, should be ENV.
+        :type config_dict:
         """
-        url = self.base_url + "employees/directory"
-        r = requests.get(
-            url, timeout=self.timeout, headers=self.headers, auth=(self.api_token, ".")
-        )
-        r.raise_for_status()
+        self.bizible_engine = snowflake_engine_factory(config_dict, "BIZIBLE_USER")
+        self.snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
 
-        data = r.json()
 
-        self.quality_check(data)
-
-        employees = data["employees"]
-
-        return employees
-
-    def get_tabular_data(
-        self, table_name: str, employee_id: str = "all"
-    ) -> Dict[Any, Any]:
+    def get_latest_bizible_tables(self) -> object:
         """
-        API method to retrieve tabular data for an employee, or all employees if employee_id argument is 'all' (the default).
-        See http://www.bamboohr.com/api/documentation/tables.php for a list of available tables.
-        @return A list of dictionaries with the default return data.
+
+        :param bizible_engine:
+        :type bizible_engine:
+        :return:
+        :rtype:
         """
-        url = self.base_url + f"employees/{employee_id}/tables/{table_name}/"
-        r = requests.get(
-            url, timeout=self.timeout, headers=self.headers, auth=(self.api_token, ".")
-        )
-        r.raise_for_status()
-
-        data = r.json()
-
-        self.quality_check(data)
-
-        return data
-
-    def get_metadata(self, meta_name: str) -> Dict[Any, Any]:
+        bizible_tables_query = f"""
+            SELECT TABLE_CATALOG || '.' || TABLE_SCHEMA || '.' || TABLE_NAME as FULL_TABLE_NAME
+            FROM BIZIBLE_ROI_V3.INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = 'GITLAB'
         """
-        API method to The Metadata API allows you to retrieve "data about data" for a given BambooHR account.
-        See https://www.bamboohr.com/api/documentation/metadata.php for a list of available metadata types.
-        @return A list of dictionaries with the data returned from the API.
+
+        df = query_dataframe(self.bizible_engine, bizible_tables_query)
+        tables_to_download = df['full_table_name'].to_list()
+        return tables_to_download
+
+    def get_bizible_queries(self, bizible_tables: object) -> object:
         """
-        url = self.base_url + f"meta/{meta_name}"
-        r = requests.get(
-            url, timeout=self.timeout, headers=self.headers, auth=(self.api_token, ".")
-        )
-        r.raise_for_status()
 
-        data = r.json()
-
-        self.quality_check(data)
-
-        return data
-
-    def get_report(
-        self, report_number: int, report_format: str = "JSON"
-    ) -> Dict[Any, Any]:
+        :param bizible_tables:
+        :type bizible_tables:
+        :return:
+        :rtype:
         """
-        API method to retrieve a specific report that already exists
-        return A JSON response object
+        bizible_dict = {}
+        for t in bizible_tables:
+            table_name = t.split('.')[-1]
+
+            snowflake_query_max_date = f"""
+                SELECT 
+                    max(to_timestamp(d.value['_modified_date']::INT / 1000)) as last_modified_date
+                FROM "BIZIBLE_WAREHOUSE_IMPORT_RAW"."BIZIBLE".{table_name}, 
+                LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext), outer => true) d
+            """
+            df = query_dataframe(self.snowflake_engine, snowflake_query_max_date)
+
+            last_modified_date_list = df['last_modified_date'].to_list()
+
+            snowflake_last_modified_date = None
+
+            if len(last_modified_date_list) > 0 and last_modified_date_list[0]:
+                snowflake_last_modified_date = last_modified_date_list[0]
+
+            if snowflake_last_modified_date:
+                bizible_dict[table_name] = f"SELECT * FROM {t} WHERE _last_modified_date > '{snowflake_last_modified_date}'"
+            else:
+                bizible_dict[table_name] = f"SELECT * FROM {t}"
+        return bizible_dict
+
+    def extract_latest_bizible_files(self, bizible_queries: Dict):
         """
-        url = self.base_url + f"reports/{report_number}?format={report_format}"
-        r = requests.get(
-            url, timeout=self.timeout, headers=self.headers, auth=(self.api_token, ".")
-        )
-        r.raise_for_status()
 
-        data = r.json()
-
-        self.quality_check(data)
-
-        return data
-
-    def quality_check(self, json_response: Dict[Any, Any]) -> None:
+        :param bizible_queries:
+        :type bizible_queries:
         """
-        Sanity check on JSON response object for data integrity.
-        """
-        record_count = len(json_response)
+        for table_name in bizible_queries.keys():
+            file_name = f"{table_name}.json"
+            logging.info(f"Running {table_name} query")
+            df = query_dataframe(self.bizible_engine, bizible_queries[table_name])
+            logging.info(f"Creating {file_name}")
+            df.to_json(file_name, orient="records")
 
-        if record_count < 2:
-            sys.exit(1)
+            logging.info("Writing to db")
+            snowflake_stage_load_copy_remove(
+                    file_name,
+                    f"BIZIBLE_WAREHOUSE_IMPORT_RAW.BIZIBLE.BIZIBLE_LOAD",
+                    f"BIZIBLE_WAREHOUSE_IMPORT_RAW.BIZIBLE.{table_name}",
+                    self.snowflake_engine,
+            )
