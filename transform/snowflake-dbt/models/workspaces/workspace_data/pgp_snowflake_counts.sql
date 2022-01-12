f11e26a74dfbc309ab48a0b429d58a694d433a0c
laddula <laddula@gitlab.com>
2022-01-12 13:54:59 -0500

    added ci_builds and notes

--- transform/snowflake-dbt/models/workspaces/workspace_data/pgp_snowflake_counts.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_data/pgp_snowflake_counts.sql
@@ -9,7 +9,8 @@ WITH postgres_counts AS (
     SELECT table_name,
         created_date,
         updated_date,
-        number_of_records
+        number_of_records,
+        parse_json(_updated_at)::int::timestamp_ntz           AS updated_at
     FROM {{source('gitlab_dotcom','gitlab_pgp_export')}} 
     WHERE table_name NOT IN (
         'gitlab_db_operations_feature_flags',
@@ -25,8 +26,8 @@ WITH postgres_counts AS (
         'gitlab_db_ci_platform_metrics',
         'gitlab_db_namespace_root_storage_statistics',
         'gitlab_ops_db_ci_stages')
-    GROUP BY 1,2,3,4
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY table_name,created_date,updated_date ORDER BY updated_date DESC) = 1
+    GROUP BY 1,2,3,4,5
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY table_name,created_date,updated_date ORDER BY updated_at) = 1
                   ORDER BY table_name, updated_date DESC
 ),  date_check AS (
          
@@ -36,18 +37,19 @@ WITH postgres_counts AS (
     GROUP BY 1
 ),  sub_group AS (
 
-    {% set tables = ['label_priorities', 'labels', 'ldap_group_links', 'namespaces','cluster_providers_gcp', 'packages_packages', 'ci_runner_projects', 'push_rules', 'requirements', 'todos', 'project_auto_devops', 'application_settings', 'ci_triggers', 'clusters_applications_cilium', 'clusters_applications_elastic_stacks', 'users', 'zoom_meetings', 'alert_management_http_integrations', 'approval_project_rules', 'clusters', 'issue_metrics', 'jira_tracker_data', 'lists', 'sprints', 'users_ops_dashboard_projects', 'bulk_imports', 'cluster_agent_tokens',  'experiment_users', 'protected_branches', 'timelogs', 'project_features', 'milestones', 'alert_management_alerts', 'ci_group_variables', 'cluster_agents', 'emails', 'user_custom_attributes', 'grafana_integrations', 'security_scans', 'lfs_objects_projects' , 'merge_request_metrics', 'merge_requests_closing_issues', 'path_locks', 'approval_merge_request_rules' , 'csv_issue_imports', 'cluster_projects', 'vulnerabilities', 'releases', 'subscriptions', 'terraform_states', 'project_tracing_settings', 'notification_settings', 'environments', 'epics', 'in_product_marketing_emails', 'jira_imports', 'services', 'onboarding_progresses', 'project_custom_attributes', 'analytics_cycle_analytics_group_stages', 'approvals', 'ci_pipeline_schedule_variables', 'ci_runners', 'ci_trigger_requests', 'cluster_providers_aws', 'boards', 'projects', 'identities', 'lfs_objects', 'prometheus_alerts', 'snippets', 'system_note_metadata', 'merge_request_blocks', 'merge_request_diffs', 'experiment_subjects', 'deployments', 'merge_requests', 'remote_mirrors', 'integrations', 'events', 'ci_stages', 'ci_pipelines', 'ci_job_artifacts', 'ci_pipeline_schedules','approver_groups' , 'boards_epic_boards' ,'web_hooks', 'routes' ,'geo_nodes','notes' ,'issues','status_page_published_incidents','epic_metrics', 'dast_profiles'] %}																																															
+    {% set tables = ['label_priorities', 'labels', 'ldap_group_links', 'namespaces','cluster_providers_gcp', 'packages_packages', 'ci_runner_projects', 'push_rules', 'requirements', 'todos', 'project_auto_devops', 'application_settings', 'ci_triggers', 'clusters_applications_cilium', 'clusters_applications_elastic_stacks', 'users', 'zoom_meetings', 'alert_management_http_integrations', 'approval_project_rules', 'clusters', 'issue_metrics', 'jira_tracker_data', 'lists', 'sprints', 'users_ops_dashboard_projects', 'bulk_imports', 'cluster_agent_tokens',  'experiment_users', 'protected_branches', 'timelogs', 'project_features', 'milestones', 'alert_management_alerts', 'ci_group_variables', 'cluster_agents', 'emails', 'user_custom_attributes', 'grafana_integrations', 'security_scans', 'lfs_objects_projects' , 'merge_request_metrics', 'merge_requests_closing_issues', 'path_locks', 'approval_merge_request_rules' , 'csv_issue_imports', 'cluster_projects', 'vulnerabilities', 'releases', 'subscriptions', 'terraform_states', 'project_tracing_settings', 'notification_settings', 'environments', 'epics', 'in_product_marketing_emails', 'jira_imports', 'services', 'onboarding_progresses', 'project_custom_attributes', 'analytics_cycle_analytics_group_stages', 'approvals', 'ci_pipeline_schedule_variables', 'ci_runners', 'ci_trigger_requests', 'cluster_providers_aws', 'boards', 'projects', 'identities', 'lfs_objects', 'prometheus_alerts', 'snippets', 'system_note_metadata', 'merge_request_blocks', 'merge_request_diffs', 'experiment_subjects', 'deployments', 'merge_requests', 'remote_mirrors', 'integrations', 'events', 'ci_stages', 'ci_pipelines', 'ci_job_artifacts', 'ci_pipeline_schedules','approver_groups' , 'boards_epic_boards' ,'web_hooks', 'routes' ,'geo_nodes','notes' ,'issues','status_page_published_incidents','epic_metrics', 'dast_profiles', 'notes', 'ci_builds'] %}																																															
     
     {% for table in tables %}
     SELECT snowflake.id,
         'gitlab_db_{{table}}'                                  AS table_name,
         DATE(snowflake.created_at)                             AS created_date,
-        DATE(snowflake.updated_at)                             AS updated_date    
+        DATE(snowflake.updated_at)                             AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)     AS uploaded_at   
     FROM {{source('gitlab_dotcom', table)}}                    AS snowflake
     INNER JOIN date_check
     ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_{{table}}' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_date DESC) = 1 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id,table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
 
       
     {% if not loop.last %}
@@ -64,12 +66,13 @@ UNION ALL
     SELECT snowflake.id,
         'gitlab_ops_db_{{table}}'                                 AS table_name,
         DATE(snowflake.created_at)                                AS created_date,
-        DATE(snowflake.updated_at)                                AS updated_date    
+        DATE(snowflake.updated_at)                                AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)        AS uploaded_at     
     FROM {{source('gitlab_ops', table)}}                          AS snowflake
     INNER JOIN date_check
     ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_{{table}}' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_date DESC) = 1 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id,table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
 
       
     {% if not loop.last %}
@@ -86,81 +89,90 @@ UNION ALL
     SELECT snowflake.issue_id,
         'gitlab_db_issues_prometheus_alert_events'                                          AS table_name,
         DATE(snowflake.created_at)                                                          AS created_date,
-        DATE(snowflake.updated_at)                                                          AS updated_date    
-    FROM {{source('gitlab_dotcom','issues_prometheus_alert_events')}}                       AS snowflake,
-        date_check
-    WHERE DATE(snowflake.updated_at)  >= date_check.updated_date
+        DATE(snowflake.updated_at)                                                          AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)                                  AS uploaded_at    
+    FROM {{source('gitlab_dotcom','issues_prometheus_alert_events')}}                       AS snowflake
+    INNER JOIN date_check
+    ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_issues_prometheus_alert_events' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY issue_id ORDER BY updated_date DESC) = 1  
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY issue_id, table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
     UNION ALL
     SELECT snowflake.group_id,
         'gitlab_db_group_import_states'                                                       AS table_name,
         DATE(snowflake.created_at)                                                            AS created_date,
-        DATE(snowflake.updated_at)                                                            AS updated_date    
-    FROM {{source('gitlab_dotcom','group_import_states')}}                                    AS snowflake,
-        date_check
-    WHERE DATE(snowflake.updated_at)  >= date_check.updated_date
+        DATE(snowflake.updated_at)                                                            AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)                                    AS uploaded_at    
+    FROM {{source('gitlab_dotcom','group_import_states')}}                                    AS snowflake
+    INNER JOIN date_check
+    ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_group_import_states' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY group_id ORDER BY updated_date DESC) = 1 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY group_id, table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
     UNION ALL
     SELECT snowflake.issue_id,
         'gitlab_db_issues_self_managed_prometheus_alert_events'                                AS table_name,
         DATE(snowflake.created_at)                                                             AS created_date,
-        DATE(snowflake.updated_at)                                                             AS updated_date    
-    FROM {{source('gitlab_dotcom','issues_self_managed_prometheus_alert_events')}}             AS snowflake,
-        date_check
-    WHERE DATE(snowflake.updated_at)  >= date_check.updated_date
+        DATE(snowflake.updated_at)                                                             AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)                                     AS uploaded_at     
+    FROM {{source('gitlab_dotcom','issues_self_managed_prometheus_alert_events')}}             AS snowflake
+    INNER JOIN date_check
+    ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_issues_self_managed_prometheus_alert_events' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY issue_id ORDER BY updated_date DESC) = 1 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY issue_id, table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
     UNION ALL
     SELECT snowflake.project_id,
         'gitlab_db_status_page_settings'                                                       AS table_name,
         DATE(snowflake.created_at)                                                             AS created_date,
-        DATE(snowflake.updated_at)                                                             AS updated_date    
-    FROM {{source('gitlab_dotcom','status_page_settings')}}                                    AS snowflake,
-        date_check
-    WHERE DATE(snowflake.updated_at)  >= date_check.updated_date
+        DATE(snowflake.updated_at)                                                             AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)                                     AS uploaded_at    
+    FROM {{source('gitlab_dotcom','status_page_settings')}}                                    AS snowflake
+    INNER JOIN date_check
+    ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_status_page_settings' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY project_id ORDER BY updated_date DESC) = 1 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY project_id, table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
     UNION ALL
     SELECT snowflake.user_id,
         'gitlab_db_user_preferences'                                                           AS table_name,
         DATE(snowflake.created_at)                                                             AS created_date,
-        DATE(snowflake.updated_at)                                                             AS updated_date    
-    FROM {{source('gitlab_dotcom','user_preferences')}}                                        AS snowflake,
-        date_check
-    WHERE DATE(snowflake.updated_at)  >= date_check.updated_date
+        DATE(snowflake.updated_at)                                                             AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)                                     AS uploaded_at    
+    FROM {{source('gitlab_dotcom','user_preferences')}}                                        AS snowflake
+    INNER JOIN date_check
+    ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_user_preferences' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY updated_date DESC) = 1 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY user_id, table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
     UNION ALL
     SELECT snowflake.project_id,
         'gitlab_db_container_expiration_policies'                                              AS table_name,
         DATE(snowflake.created_at)                                                             AS created_date,
-        DATE(snowflake.updated_at)                                                             AS updated_date    
-    FROM {{source('gitlab_dotcom','container_expiration_policies')}}                           AS snowflake,
-        date_check
-    WHERE DATE(snowflake.updated_at)  >= date_check.updated_date
+        DATE(snowflake.updated_at)                                                             AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)                                     AS uploaded_at     
+    FROM {{source('gitlab_dotcom','container_expiration_policies')}}                           AS snowflake
+    INNER JOIN date_check
+    ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_container_expiration_policies' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY project_id ORDER BY updated_date DESC) = 1
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY project_id, table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
     UNION ALL
     SELECT snowflake.namespace_id,
         'gitlab_db_namespace_settings'                                                         AS table_name,
         DATE(snowflake.created_at)                                                             AS created_date,
-        DATE(snowflake.updated_at)                                                             AS updated_date    
-    FROM {{source('gitlab_dotcom','namespace_settings')}}                                      AS snowflake,
-        date_check
-    WHERE DATE(snowflake.updated_at)  >= date_check.updated_date
+        DATE(snowflake.updated_at)                                                             AS updated_date,
+        DATE(parse_json(_uploaded_at)::int::timestamp_ntz)                                     AS uploaded_at    
+    FROM {{source('gitlab_dotcom','namespace_settings')}}                                      AS snowflake
+    INNER JOIN date_check
+    ON DATE(snowflake.updated_at) >= date_check.updated_date
     AND date_check.table_name = 'gitlab_db_namespace_settings' 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY namespace_id ORDER BY updated_date DESC) = 1   
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY namespace_id, table_name,created_date, updated_date, uploaded_at ORDER BY uploaded_at DESC) = 1 
 
 ), snowflake_counts AS (
 
     SELECT table_name,
         created_date,
         updated_date,
+        uploaded_at,
         COUNT(*) AS number_of_records
     FROM final_group
-    GROUP BY 1,2,3
+    WHERE uploaded_at = created_date+1
+    GROUP BY 1,2,3,4
 
 ), comparision AS (
 
