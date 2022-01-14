1dab89a698cf172a7e81a3a2e4edeed677f23dac
Paul Armstrong <parmstrong@gitlab.com>
2022-01-14 14:31:35 +0200

    Separate MR, only for the bizible source models.

---
+++ transform/snowflake-dbt/models/sources/bizible/bizible_accounts_source.sql
@@ -0,0 +1,24 @@
+WITH source AS (
+
+    SELECT
+      id                      AS id,
+      created_date            AS created_date,
+      modified_date           AS modified_date,
+      name                    AS name,
+      web_site                AS web_site,
+      engagement_rating       AS engagement_rating,
+      engagement_score        AS engagement_score,
+      domain                  AS domain,
+      is_deleted              AS is_deleted,
+      custom_properties       AS custom_properties,
+      _created_date           AS _created_date,
+      _modified_date          AS _modified_date,
+      _deleted_date           AS _deleted_date,
+      _uploaded_at            AS _uploaded_at
+    FROM {{ source('bizible', 'biz_accounts') }}
+    ORDER BY uploaded_at DESC
+
+)
+
+SELECT *
+FROM source
---
+++ transform/snowflake-dbt/models/sources/bizible/bizible_currencies_source.sql
@@ -0,0 +1,25 @@
+WITH source AS (
+
+    SELECT
+      id                    AS id,
+      is_corporate          AS is_corporate,
+      is_enabled            AS is_enabled,
+      modified_date         AS modified_date,
+      modified_date_crm     AS modified_date_crm,
+      created_date          AS created_date,
+      created_date_crm      AS created_date_crm,
+      iso_code              AS iso_code,
+      iso_numeric           AS iso_numeric,
+      exponent              AS exponent,
+      name                  AS name,
+      _created_date         AS _created_date,
+      _modified_date        AS _modified_date,
+      _deleted_date         AS _deleted_date
+    FROM {{ source('bizible', 'biz_currencies') }}
+    ORDER BY uploaded_at DESC
+
+)
+
+SELECT *
+FROM renamed
+
