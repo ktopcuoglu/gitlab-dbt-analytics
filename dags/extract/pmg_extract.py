762996b88b65ce6c5a4bd195bc4b22bfb8c12d55
Paul Armstrong <parmstrong@gitlab.com>
2022-01-19 15:16:14 +0200

    Env mapping correction.

--- dags/extract/pmg_extract.py
+++ dags/extract/pmg_extract.py
@@ -22,8 +22,6 @@ from kube_secrets import (
 
 from kubernetes_helpers import get_affinity, get_toleration
 
-env = os.environ.copy()
-GIT_BRANCH = env["GIT_BRANCH"]
 pod_env_vars = gitlab_pod_env_vars
 
 default_args = {
