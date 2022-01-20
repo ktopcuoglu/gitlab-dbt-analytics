98476126b2673611b32abb34ac939d91a6b8d938
Peter Empey <pempey@gitlab.com>
2022-01-20 09:39:57 -0700

    added customer fileds and new tables

--- transform/snowflake-dbt/models/sources/zengrc/zengrc_risk_source.sql
+++ transform/snowflake-dbt/models/sources/zengrc/zengrc_risk_source.sql
@@ -8,17 +8,32 @@ WITH source AS (
 renamed AS (
 
     SELECT
-      code::VARCHAR                     AS risk_code,
-      created_at::TIMESTAMP             AS risk_created_at,
-      custom_attributes::VARIANT        AS risk_custom_attributes,
-      description::VARCHAR              AS risk_description,
-      id::NUMBER                        AS risk_id,
-      risk_vector_score_values::VARIANT AS risk_vector_score_values,
-      status::VARCHAR                   AS risk_status,
-      title::VARCHAR                    AS risk_title,
-      type::VARCHAR                     AS zengrc_object_type,
-      updated_at::TIMESTAMP             AS risk_updated_at,
-      __loaded_at::TIMESTAMP            AS risk_loaded_at
+      code::VARCHAR                                          AS risk_code,
+      created_at::TIMESTAMP                                  AS risk_created_at,
+      custom_attributes::VARIANT                             AS risk_custom_attributes,
+      description::VARCHAR                                   AS risk_description,
+      id::NUMBER                                             AS risk_id,
+      risk_vector_score_values::VARIANT                      AS risk_vector_score_values,
+      status::VARCHAR                                        AS risk_status,
+      title::VARCHAR                                         AS risk_title,
+      type::VARCHAR                                          AS zengrc_object_type,
+      updated_at::TIMESTAMP                                  AS risk_updated_at,
+      __loaded_at::TIMESTAMP                                 AS risk_loaded_at,
+      PARSE_JSON(custom_attributes)['156']['value']::VARCHAR AS acceptance_of_risk_ownership,
+      PARSE_JSON(custom_attributes)['44']['value']::VARCHAR  AS cia_impact,
+      PARSE_JSON(custom_attributes)['79']['value']::DATE     AS risk_identified_date,
+      PARSE_JSON(custom_attributes)['81']['value']::VARCHAR  AS existing_mitigations,
+      PARSE_JSON(custom_attributes)['57']['value']::VARCHAR  AS interested_parties,
+      PARSE_JSON(custom_attributes)['158']['value']::BOOLEAN AS is_risk_ready_for_review_and_closure,
+      PARSE_JSON(custom_attributes)['46']['value']::VARCHAR  AS risk_owner,
+      PARSE_JSON(custom_attributes)['74']['value']::VARCHAR  AS risk_tier,
+      PARSE_JSON(custom_attributes)['160']['value']::DATE    AS risk_treatment_completion_date,
+      PARSE_JSON(custom_attributes)['159']['value']::VARCHAR AS risk_treatment_option_selected,
+      PARSE_JSON(custom_attributes)['45']['value']::VARCHAR  AS root_cause,
+      PARSE_JSON(custom_attributes)['58']['value']::VARCHAR  AS threat_source,
+      PARSE_JSON(custom_attributes)['148']['value']::BOOLEAN AS is_tprm_related,
+      PARSE_JSON(custom_attributes)['75']['value']::BOOLEAN  AS is_within_risk_appetite
+
     FROM source
 
 )
--- transform/snowflake-dbt/models/workspaces/workspace_engineering/zengrc_person.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/zengrc_person.sql
@@ -1,42 +1,66 @@
-WITH audits AS (
-  SELECT
-    *
-  FROM {{ ref('zengrc_audit_source') }}
-),
+{{ simple_cte([
+    ('audits','zengrc_audit_source'),
+    ('assessments','zengrc_assessment_source'),
+    ('requests','zengrc_request_source')
+])}}
 
-  assessments AS (
-    SELECT
-      *
-    FROM {{ ref('zengrc_assessment_source') }}
-  ),
+, audit_managers AS (
 
-  audit_managers AS (
     SELECT DISTINCT
       audut_manager.value['id']::NUMBER    AS person_id,
       audut_manager.value['name']::VARCHAR AS person_name,
       audut_manager.value['type']::VARCHAR AS zengrc_type
     FROM audits
     INNER JOIN LATERAL FLATTEN(INPUT => TRY_PARSE_JSON(audits.audit_managers)) audut_manager
-  ),
 
-  assessors AS (
+), assessors AS (
+
     SELECT DISTINCT
       assessors.value['id']::NUMBER    AS person_id,
       assessors.value['name']::VARCHAR AS person_name,
       assessors.value['type']::VARCHAR AS zengrc_type
     FROM assessments
     INNER JOIN LATERAL FLATTEN(INPUT => TRY_PARSE_JSON(assessments.assessors)) assessors
-  ),
 
-  unioned AS (
-    SELECT
-      *
+), assignees AS (
+
+    SELECT DISTINCT
+      assignees.value['id']::NUMBER    AS person_id,
+      assignees.value['name']::VARCHAR AS person_name,
+      assignees.value['type']::VARCHAR AS zengrc_type
+    FROM requests
+    INNER JOIN LATERAL FLATTEN(INPUT => TRY_PARSE_JSON(requests.assignees)) assignees
+
+), requestors AS (
+
+    SELECT DISTINCT
+      requestors.value['id']::NUMBER    AS person_id,
+      requestors.value['name']::VARCHAR AS person_name,
+      requestors.value['type']::VARCHAR AS zengrc_type
+    FROM requests
+    INNER JOIN LATERAL FLATTEN(INPUT => TRY_PARSE_JSON(requests.requestors)) requestors
+
+), unioned AS (
+
+    SELECT *
     FROM audit_managers
+
     UNION
-    SELECT
-      *
+
+    SELECT *
     FROM assessors
-  )
+
+    UNION
+
+    SELECT *
+    FROM assignees
+
+    UNION
+
+    SELECT *
+    FROM requestors
+)
+
 SELECT
   *
 FROM unioned
