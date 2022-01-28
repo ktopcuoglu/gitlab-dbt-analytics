e2ee31f0516e2efd4c8250bf16641385fd210f81
ken_aguilar <kaguilar@gitlab.com>
2022-01-28 09:22:44 +0800

    Update column names

--- transform/snowflake-dbt/models/workspaces/workspace_finance/dim_order_action_api_sandbox.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_finance/dim_order_action_api_sandbox.sql
@@ -8,25 +8,25 @@ WITH base AS (
 
     SELECT
       
-      id                                 AS dim_order_action_id,
-      orderid                            AS dim_order_id,
-      subscriptionid                     AS dim_subscription_id,
-      subscriptionversionamendmentid     AS dim_amendment_id,
-      type                               AS order_action_type,
-      sequence                           AS order_action_sequence,
-      autorenew                          AS is_auto_renew,
-      cancellationpolicy                 AS cancellation_policy,
-      termtype                           AS term_type,
-      createddate                        AS order_action_created_date,
-      customeracceptancedate             AS customer_acceptance_date,
-      contracteffectivedate              AS contract_effective_date,
-      serviceactivationdate              AS service_activation_date,
-      currentterm                        AS current_term,
-      currenttermperiodtype              AS current_term_period_type,
-      renewalterm                        AS renewal_term,
-      renewaltermperiodtype              AS renewal_term_period_type,
-      renewsetting                       AS renewal_setting,
-      termstartdate                      AS term_start_date
+      dim_order_action_id               AS dim_order_action_id,
+      dim_order_id                      AS dim_order_id,
+      dim_subscription_id               AS dim_subscription_id,
+      dim_amendment_id                  AS dim_amendment_id,
+      order_action_type                 AS order_action_type,
+      order_action_sequence             AS order_action_sequence,
+      is_auto_renew                     AS is_auto_renew,
+      cancellation_policy               AS cancellation_policy,
+      term_type                         AS term_type,
+      order_action_created_date         AS order_action_created_date,
+      customer_acceptance_date          AS customer_acceptance_date,
+      contract_effective_date           AS contract_effective_date,
+      service_activation_date           AS service_activation_date,
+      current_term                      AS current_term,
+      current_term_period_type          AS current_term_period_type,
+      renewal_term                      AS renewal_term,
+      renewal_term_period_type          AS renewal_term_period_type,
+      renewal_setting                   AS renewal_setting,
+      term_start_date                   AS term_start_date
 
     FROM base
 
