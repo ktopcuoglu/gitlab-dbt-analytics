0a8b5a2b4553704ec8fd36c6f5d8b94c1442ff54
nfiguera <nfiguera@gitlab.com>
2022-02-01 09:57:35 +0100

    fixed comflict at account source

--- transform/snowflake-dbt/models/sources/sfdc/sfdc_account_source.sql
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_account_source.sql
@@ -87,20 +87,6 @@ WITH source AS (
       atam_address_postal_code__c                   AS tsp_address_postal_code,
 
       -- account demographics fields
-<<<<<<< HEAD
-      account_demographics_sales_segment__c        AS account_demographics_sales_segment,
-      account_demographics_geo__c                  AS account_demographics_geo,
-      account_demographics_region__c               AS account_demographics_region,
-      account_demographics_area__c                 AS account_demographics_area,
-      account_demographics_territory__c            AS account_demographics_territory,
-      account_demographics_employee_count__c       AS account_demographics_employee_count,
-      account_demographic_max_family_employees__c  AS account_demographics_max_family_employee,
-      account_demographics_upa_country__c          AS account_demographics_upa_country,
-      account_demographics_upa_state__c            AS account_demographics_upa_state,
-      account_demographics_upa_city__c             AS account_demographics_upa_city,
-      account_demographics_upa_street__c           AS account_demographics_upa_street,
-      account_demographics_upa_postal_code__c      AS account_demographics_upa_postal_code,
-=======
       account_demographics_sales_segment__c         AS account_demographics_sales_segment,
       account_demographics_geo__c                   AS account_demographics_geo,
       account_demographics_region__c                AS account_demographics_region,
@@ -113,7 +99,6 @@ WITH source AS (
       account_demographics_upa_city__c              AS account_demographics_upa_city,
       account_demographics_upa_street__c            AS account_demographics_upa_street,
       account_demographics_upa_postal_code__c       AS account_demographics_upa_postal_code,
->>>>>>> 724c6ec5c99cacffc69d36fa2ebaca0f26837d2c
 
       -- present state info
       health__c                                     AS health_score,
