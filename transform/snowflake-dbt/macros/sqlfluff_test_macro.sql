dd8851e2c612cdf4b138db7967d5ca010c9c9572
Peter Empey <pempey@gitlab.com>
2022-02-03 07:55:46 -0700

    remove test files

--- transform/snowflake-dbt/macros/sqlfluff_test_macro.sql
+++
@@ -1,3 +0,0 @@
-{% macro sqlfluff_test_macro(columns) %}
-COALESCE({{columns}}, MD5(-1))
-{% endmacro %}
--- transform/snowflake-dbt/models/sqlfuff_test.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    table_id,
-    {{ sqlfluff_test_macro('other_id') }} AS other_id,
-    {{ dbt_utils.surrogate_key(['other_id']) }} AS an_other_id
-FROM my_table
