3cc5014122af18dc95c7f23214646411fb110a54
Radovan Bacovic <rbacovic@gitlab.com>
2022-02-08 14:33:25 +0100

    Revert to the old version without filter.

--- transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
+++ transform/snowflake-dbt/models/legacy/version/intermediate/version_usage_stats_list.sql
@@ -6,28 +6,8 @@ WITH version_usage_data AS (
 )
 
 SELECT DISTINCT
-  f.path                     AS ping_name,
+  f.path                     AS ping_name, 
   REPLACE(f.path, '.','_')   AS full_ping_name
 FROM version_usage_data,
-lateral flatten(input => version_usage_data.stats_used, recursive => True) f
+  lateral flatten(input => version_usage_data.stats_used, recursive => True) f
 WHERE IS_OBJECT(f.value) = FALSE
--- error when listed items pops up. 
--- more details in the issues:
---   https://gitlab.com/gitlab-data/analytics/-/issues/10749
---   https://gitlab.com/gitlab-data/analytics/-/issues/11524 
--- This is a temporary solution and this hard-coded values should be removed.
-AND full_ping_name NOT IN ('groups_bugdb_active',
-                           'groups_shimo_active',
-                           'groups_inheriting_bugdb_active',
-                           'projects_bugdb_active',
-                           'projects_inheriting_shimo_active',
-                           'projects_inheriting_bugdb_active',
-                           'projects_shimo_active',
-                           'instances_shimo_active',
-                           'license_scanning_jobs',
-                           'templates_bugdb_active',
-                           'users_visiting_testing_license_compliance_full_report',
-                           'users_clicking_license_testing_visiting_external_website',
-                           'web_ide_previews_success')
-AND full_ping_name NOT ILIKE ('%_BUGDB_%')
-AND full_ping_name NOT ILIKE ('%_SHIMO_%')
