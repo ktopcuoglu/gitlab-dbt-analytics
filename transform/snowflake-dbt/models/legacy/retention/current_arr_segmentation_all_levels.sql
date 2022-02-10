02b91ae43783f8de854d0bda730e1e91dfda4d4e
ken_aguilar <kaguilar@gitlab.com>
2022-02-10 20:48:45 +0800

    Update model

--- transform/snowflake-dbt/models/legacy/retention/current_arr_segmentation_all_levels.sql
+++ transform/snowflake-dbt/models/legacy/retention/current_arr_segmentation_all_levels.sql
@@ -5,9 +5,9 @@
 WITH base AS (
     SELECT dim_subscription_id as zuora_subscription_id,
             dim_parent_crm_account_id as parent_account_id,
-            dim_crm_account_id as sfdc_account_id
+            dim_crm_account_id as sfdc_account_id,
           {{ dbt_utils.star(from=ref('mart_arr'), 
-            except=["dim_subscription_id", "dim_parent_crm_account_id", "dim_crm_account_id"]) }}
+            except=["dim_subscription_id", "dim_parent_crm_account_id"]) }}
     FROM {{ref('mart_arr')}}
 
 {% for level in levels -%} 
