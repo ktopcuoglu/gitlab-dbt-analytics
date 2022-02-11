b89df01f419f122d46fa4e871a98741a04787e50
Chris Sharp <csharp@gitlab.com>
2022-02-11 11:35:57 +0000

    update dbt_audit details

--- transform/snowflake-dbt/models/common_prep/prep_gitlab_dotcom_plan.sql
+++ transform/snowflake-dbt/models/common_prep/prep_gitlab_dotcom_plan.sql
@@ -22,7 +22,7 @@ WITH source AS (
 {{ dbt_audit(
     cte_ref="renamed",
     created_by="@mpeychet_",
-    updated_by="@mpeychet_",
+    updated_by="@chrissharp",
     created_date="2021-05-30",
-    updated_date="2021-05-30"
+    updated_date="2022-02-10"
 ) }}
