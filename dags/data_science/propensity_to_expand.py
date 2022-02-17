7358937879ec1782ecdedbd0c9175e0794ff20a3
Paul Armstrong <parmstrong@gitlab.com>
2022-02-17 14:33:48 +0200

    Temp pip install.

--- dags/data_science/propensity_to_expand.py
+++ dags/data_science/propensity_to_expand.py
@@ -47,6 +47,7 @@ dag = DAG(
 pte_scoring_command = f"""
     {clone_data_science_repo_cmd} &&
     cd data-science/deployments/pte && 
+    pip install papermill
     papermill scoring_code.ipynb -p is_local_development False
 """
 KubernetesPodOperator(
