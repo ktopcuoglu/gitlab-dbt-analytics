e235fe760b52659bf888a7d15ee838cb13dd689a
Radovan Bacovic <rbacovic@gitlab.com>
2022-02-18 13:33:05 +0100

    Change data type for columns: [USER_ID, MERGE_REQUEST_ID]

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_ci_pipelines_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_ci_pipelines_dedupe_source.sql
@@ -4,11 +4,51 @@
     })
 }}
 
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('gitlab_dotcom', 'ci_pipelines') }}
+
+), renamed AS (
+
+    SELECT
+      id                       AS id,
+      created_at               AS created_at,
+      updated_at               AS updated_at,
+      ref                      AS ref,
+      tag                      AS tag,
+      yaml_errors              AS yaml_errors,
+      committed_at             AS committed_at,
+      project_id               AS project_id,
+      status                   AS status,
+      started_at               AS started_at,
+      finished_at              AS finished_at,
+      duration                 AS duration,
+      user_id::NUMBER          AS user_id,
+      lock_version             AS lock_version,
+      auto_canceled_by_id      AS auto_canceled_by_id,
+      pipeline_schedule_id     AS pipeline_schedule_id,
+      source                   AS source,
+      config_source            AS config_source,
+      protected                AS protected,
+      failure_reason           AS failure_reason,
+      iid                      AS iid,
+      merge_request_id::NUMBER AS merge_request_id,
+      _uploaded_at             AS _uploaded_at
+    FROM source
+
+), partitioned AS (
+
+    SELECT *
+    FROM renamed
+    {% if is_incremental() %}
+
+    WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
+
+    {% endif %}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+
+)
+
 SELECT *
-FROM {{ source('gitlab_dotcom', 'ci_pipelines') }}
-{% if is_incremental() %}
-
-WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
-
-{% endif %}
-QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+FROM partitioned
