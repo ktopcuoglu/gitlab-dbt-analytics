f862c4de5e7dad4e6d5a98311cc227a5598cdc8a
Radovan Bacovic <rbacovic@gitlab.com>
2022-02-22 14:26:30 +0100

    Linter code for test file.

--- extract/postgres_pipeline/test/test_manifest_decomposed.py
+++ extract/postgres_pipeline/test/test_manifest_decomposed.py
@@ -1,12 +1,21 @@
-import pytest
+"""
+Testing routine for manifest decomposition
+"""
 import os
 import sys
+
+import pytest
+
 import yaml
 from yaml.loader import SafeLoader
 
-# Tweak path as due to script execution way in Airflow, can't touch the original code
+# Tweak path as due to script execution way in Airflow,
+# can't touch the original code
 abs_path = os.path.dirname(os.path.realpath(__file__))
-abs_path = abs_path[: abs_path.find("extract")] + "extract/postgres_pipeline/manifests_decomposed/"
+abs_path = (
+    abs_path[: abs_path.find("extract")]
+    + "extract/postgres_pipeline/manifests_decomposed/"
+)
 sys.path.append(abs_path)
 
 TABLES_LIST = [
@@ -19,26 +28,40 @@ TABLES_LIST = [
 
 
 def load_yaml_file(file_name: str) -> dict:
-    with open(file=file_name) as f:
-        data = yaml.load(f, Loader=SafeLoader)
+    """
+    Load yaml file to ensure changes didn't break anything
+
+    param file_name: input file name
+    return: dict
+    """
+    with open(file=file_name, encoding="utf-8") as file:
+        data = yaml.load(file, Loader=SafeLoader)
     return data
 
 
-def test_remove_incremental_tables():
-    loaded_file = load_yaml_file(
-        abs_path + "el_gitlab_com_db_manifest.yaml"
-    )
+def test_remove_incremental_tables() -> None:
+    """
+    Check are tables from incremental load are removed properly
+    return: None
+    """
+    full_path = f"{abs_path}el_gitlab_com_db_manifest.yaml"
+    loaded_file = load_yaml_file(full_path)
     for table in TABLES_LIST:
-        assert loaded_file.get("tables").get(table, None) is None
+        with pytest.raises(KeyError):
+            assert loaded_file["tables"][table] is None
 
 
-def test_add_scd_tables():
-    loaded_file = load_yaml_file(
-        abs_path + "el_gitlab_com_scd_db_manifest.yaml"
-    )
+def test_add_scd_tables() -> None:
+    """
+    Check are tables from incremental load are
+    added properly to SCD load
+    return: None
+    """
+    full_path = f"{abs_path}el_gitlab_com_scd_db_manifest.yaml"
+    loaded_file = load_yaml_file(full_path)
 
     for table in TABLES_LIST:
-        table_definition = loaded_file.get("tables").get(table, None)
+        table_definition = loaded_file["tables"][table]
         assert table_definition is not None
         assert table_definition.get("export_table", None) == table
 
