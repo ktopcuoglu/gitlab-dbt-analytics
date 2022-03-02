8149b7e22ac7f74b9a0513b5e548ae4df25aa9cc
Paul Armstrong <parmstrong@gitlab.com>
2022-03-02 14:15:34 +0200

    Different string formatting.

--- orchestration/manage_snowflake.py
+++ orchestration/manage_snowflake.py
@@ -251,10 +251,10 @@ class SnowflakeManager:
         for stage in stages:
 
             output_stage_name = (
-                f"""{create_db}"".""{stage['stage_schema']}"".""{stage['stage_name']}"""
+                f""""{create_db}"."{stage['stage_schema']}"."{stage['stage_name']}"""
             )
             from_stage_name = (
-                f"""{database.upper()}"".""{stage['stage_schema']}"".""{stage['stage_name']}"""
+                f""""{database.upper()}"."{stage['stage_schema']}"."{stage['stage_name']}"""
             )
 
             if stage["stage_type"] == "External Named":
