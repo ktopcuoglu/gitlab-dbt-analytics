01d348589aa067fc7d9dc6d55955672afe26156f
Brian Rhea <brhea@gitlab.com>
2022-03-09 15:28:20 -0700

    Add account_id from Qualtrics to Sisense

--- transform/snowflake-dbt/models/legacy/qualtrics/qualtrics_post_purchase_survey_responses.sql
+++ transform/snowflake-dbt/models/legacy/qualtrics/qualtrics_post_purchase_survey_responses.sql
@@ -8,6 +8,7 @@
     SELECT
       IFF(response_values['finished']::NUMBER = 1, TRUE, FALSE)::BOOLEAN AS is_finished,
       response_values['plan']::VARCHAR                                   AS user_plan,
+      response_values['account_id']::VARCHAR                             AS account_id,
       response_values['recordedDate']::TIMESTAMP                         AS recorded_at,
       response_values['startDate']::TIMESTAMP                            AS started_at,
       response_id
