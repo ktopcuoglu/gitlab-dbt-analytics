b2aac49c327c2192f1dcf68168ac9bd24bd0b5b8
Jean Peguero <jpeguero@gitlab.com>
2022-03-09 18:16:55 +0100

    Add region check for AMER Geo and Pubsec

--- transform/snowflake-dbt/macros/common/sales_segment_region_grouped.sql
+++ transform/snowflake-dbt/macros/common/sales_segment_region_grouped.sql
@@ -1,12 +1,14 @@
-{%- macro sales_segment_region_grouped(segment, sales_geo) -%}
+{%- macro sales_segment_region_grouped(segment, sales_geo, sales_region) -%}
 
 CASE 
-  WHEN {{ segment }} IN ('Large', 'PubSec') AND {{ sales_geo }} = 'West'
+  WHEN {{ segment }} IN ('Large', 'PubSec') AND {{ sales_geo }} = 'AMER' AND {{ sales_region }} = 'West'
     THEN 'US West'
-  WHEN {{ segment }} IN ('Large', 'PubSec') AND {{ sales_geo }} IN ('East', 'LATAM')
+  WHEN {{ segment }} IN ('Large', 'PubSec') AND {{ sales_geo }} IN ('AMER', 'LATAM') AND {{ sales_region }} IN ('East', 'LATAM')
     THEN 'US East'
   WHEN {{ segment }} IN ('Large', 'PubSec') AND {{ sales_geo }} IN ('APAC', 'PubSec','EMEA', 'Global')
     THEN {{ sales_geo }}
+  WHEN {{ segment }} IN ('Large', 'PubSec') AND {{ sales_region }} = 'PubSec'
+    THEN 'PubSec'
   WHEN {{ segment }} IN ('Large', 'PubSec') AND {{ sales_geo }} NOT IN ('West', 'East', 'APAC', 'PubSec','EMEA', 'Global')
     THEN 'Large Other'
   WHEN {{ segment }} NOT IN ('Large', 'PubSec')
