44d29d3a33f245df5abcf67289331755fac8509f
Rigerta Demiri <rdemiri@gitlab.com>
2022-03-13 18:21:18 +0100

    Add db name dynamically on macro and mask multiple models

--- transform/snowflake-dbt/macros/dbt_snowflake_masking/apply_dynamic_data_masking.sql
+++ transform/snowflake-dbt/macros/dbt_snowflake_masking/apply_dynamic_data_masking.sql
@@ -1,4 +1,4 @@
-{%- macro apply_dynamic_data_masking(database, columns) -%}
+{%- macro apply_dynamic_data_masking(columns) -%}
 
 {% set materialization = 'view' if model.config.materialized == 'view' else 'table' %}
 {% set database = generate_database_name(model.config.database) %}
@@ -11,44 +11,10 @@
 
     {% for column_name, column_type in column.items() %}
 
-        {% if column_type == "float" %}
-
             alter {{materialization}} {{database}}.{{schema}}.{{alias}} 
             modify column {{column_name}} 
-            set masking policy {{database}}.{{schema}}.hide_float_column_values;
-
-        {% elif column_type == "array" %}
-
-            alter {{materialization}} {{database}}.{{schema}}.{{alias}} 
-            modify column {{column_name}} 
-            set masking policy {{database}}.{{schema}}.hide_array_column_values;
-
-        {% elif column_type == "boolean" %}
-
-            alter {{materialization}} {{database}}.{{schema}}.{{model}} 
-            modify column {{column_name}} 
-            set masking policy {{database}}.{{schema}}.hide_boolean_column_values;
-
-        {% elif column_type == "number" %}
-
-            alter {{materialization}} {{database}}.{{schema}}.{{alias}} 
-            modify column {{column_name}} 
-            set masking policy {{database}}.{{schema}}.hide_number_column_values;
-
-        {% elif column_type == "string" %}
-
-            alter {{materialization}} {{database}}.{{schema}}.{{alias}} 
-            modify column {{column_name}} 
-            set masking policy {{database}}.{{schema}}.hide_string_column_values;
-       
-        {% elif column_type == "variant" %}
-
-            alter {{materialization}} {{database}}.{{schema}}.{{alias}} 
-            modify column {{column_name}} 
-            set masking policy {{database}}.{{schema}}.hide_variant_column_values;
+            set masking policy {{database}}.{{schema}}.hide_{{column_type}}_column_values;
         
-        {% endif %}
-
     {% endfor %}
 
 {% endfor %}
