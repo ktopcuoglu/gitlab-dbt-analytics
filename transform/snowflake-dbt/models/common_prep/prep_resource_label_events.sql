e2b46d1e80cb228e016909e74f7270bafcc473d2
Chris Sharp <csharp@gitlab.com>
2022-03-17 11:28:32 +0000

    update to use dim tables

--- transform/snowflake-dbt/models/common_prep/prep_resource_label_events.sql
+++ transform/snowflake-dbt/models/common_prep/prep_resource_label_events.sql
@@ -10,9 +10,9 @@
 
 {{ simple_cte([
     ('dim_date', 'dim_date'),
-    ('prep_epic', 'prep_epic'),
-    ('prep_issue', 'prep_issue'),
-    ('prep_merge_request', 'prep_merge_request'),
+    ('dim_epic', 'dim_epic'),
+    ('dim_issue', 'dim_issue'),
+    ('dim_merge_request', 'dim_merge_request'),
 ]) }}
 
 , resource_label_events AS (
@@ -29,14 +29,14 @@
 
     SELECT 
       resource_label_events.resource_label_event_id::NUMBER                 AS dim_issue_label_id,
-      COALESCE(prep_issue.dim_project_id,
-                prep_merge_request.dim_project_id)::NUMBER                  AS dim_project_id,
-      COALESCE(prep_epic.dim_plan_id,
-                prep_issue.dim_plan_id,
-                prep_merge_request.dim_plan_id)::NUMBER                     AS dim_plan_id,
-      COALESCE(prep_epic.group_id,
-                prep_issue.ultimate_parent_namespace_id,
-                prep_merge_request.ultimate_parent_namespace_id)::NUMBER    AS ultimate_parent_namespace_id,
+      COALESCE(dim_issue.dim_project_id,
+                dim_merge_request.dim_project_id)::NUMBER                   AS dim_project_id,
+      COALESCE(dim_epic.dim_plan_id,
+                dim_issue.dim_plan_id,
+                dim_merge_request.dim_plan_id)::NUMBER                      AS dim_plan_id,
+      COALESCE(dim_epic.group_id,
+                dim_issue.ultimate_parent_namespace_id,
+                dim_merge_request.ultimate_parent_namespace_id)::NUMBER     AS ultimate_parent_namespace_id,
       user_id::NUMBER                                                       AS dim_user_id,
       issue_id::NUMBER                                                      AS dim_issue_id,
       merge_request_id::NUMBER                                              AS dim_merge_request_id,
@@ -44,12 +44,12 @@
       resource_label_events.created_at::TIMESTAMP                           AS created_at,
       dim_date.date_id::NUMBER                                              AS created_date_id
     FROM resource_label_events
-    LEFT JOIN prep_epic
-      ON resource_label_events.epic_id = prep_epic.dim_epic_id
-    LEFT JOIN prep_issue
-      ON resource_label_events.issue_id = prep_issue.dim_issue_id
-    LEFT JOIN prep_merge_request
-      ON resource_label_events.merge_request_id = prep_merge_request.dim_merge_request_id
+    LEFT JOIN dim_epic
+      ON resource_label_events.epic_id = dim_epic.dim_epic_id
+    LEFT JOIN dim_issue
+      ON resource_label_events.issue_id = dim_issue.dim_issue_id
+    LEFT JOIN dim_merge_request
+      ON resource_label_events.merge_request_id = dim_merge_request.dim_merge_request_id
     LEFT JOIN dim_date 
       ON TO_DATE(resource_label_events.created_at) = dim_date.date_day
 
