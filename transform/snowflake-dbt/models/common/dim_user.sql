c08f1d9dd0d5386ac2fb6e5767656b6b24f75928
iweeks <iweeks@gitlab.com>
2022-03-18 15:53:18 -0400

    update user logic

--- transform/snowflake-dbt/models/common/dim_user.sql
+++ transform/snowflake-dbt/models/common/dim_user.sql
@@ -1,15 +1,6 @@
 WITH prep_user AS (
 
-    SELECT 
-      dim_user_id,
-      remember_created_at,
-      sign_in_count,
-      current_sign_in_at,
-      last_sign_in_at
-      created_at,
-      updated_at,
-      is_admin,
-      is_blocked_user
+    SELECT *
     FROM {{ ref('prep_user') }}
 
 )
@@ -17,7 +8,7 @@ WITH prep_user AS (
 {{ dbt_audit(
     cte_ref="prep_user",
     created_by="@mpeychet_",
-    updated_by="@chrissharp",
+    updated_by="@iweeks",
     created_date="2021-06-28",
-    updated_date="2022-02-16"
+    updated_date="2022-03-18"
 ) }}
