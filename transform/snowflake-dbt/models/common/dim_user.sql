6461c2aa1c0c6700968b261d5f3bc2dd8f49536c
iweeks <iweeks@gitlab.com>
2022-03-18 16:28:51 -0400

    update prep table

--- transform/snowflake-dbt/models/common/dim_user.sql
+++ transform/snowflake-dbt/models/common/dim_user.sql
@@ -1,6 +1,15 @@
 WITH prep_user AS (
 
-    SELECT *
+    SELECT 
+      dim_user_id,
+      remember_created_at,
+      sign_in_count,
+      current_sign_in_at,
+      last_sign_in_at,
+      created_at,
+      updated_at,
+      is_admin,
+      is_blocked_user
     FROM {{ ref('prep_user') }}
 
 )
