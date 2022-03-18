230cbc6b0b9b20b5b95baab14589dfd056bdac6c
icooper-acp <ian@amplifycp.com>
2022-03-18 08:41:58 -0700

    Adding in last 24 months filter to speed up prep table

--- transform/snowflake-dbt/models/workspaces/workspace_product/prep_service_ping_instance.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/prep_service_ping_instance.sql
@@ -15,7 +15,7 @@
 
 , source AS (
 
-    SELECT top 1000
+    SELECT
       id                                                                        AS dim_service_ping_instance_id,
       created_at::TIMESTAMP(0)                                                  AS ping_created_at,
       *,
