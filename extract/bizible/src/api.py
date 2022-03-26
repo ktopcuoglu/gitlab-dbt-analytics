09ceb803f93ff66c14a8e5dc2d706473ead139b5
Paul Armstrong <parmstrong@gitlab.com>
2022-03-26 20:08:16 +0200

    Black.

--- extract/bizible/src/api.py
+++ extract/bizible/src/api.py
@@ -103,7 +103,9 @@ class BizibleSnowFlakeExtractor:
         :type date_column:
         """
         end_date = datetime.now()
-        for dt in rrule.rrule(rrule.HOURLY, dtstart=last_modified_date, until=end_date, interval=2):
+        for dt in rrule.rrule(
+            rrule.HOURLY, dtstart=last_modified_date, until=end_date, interval=2
+        ):
             query_start_date = dt
             query_end_date = dt + timedelta(hours=2)
 
