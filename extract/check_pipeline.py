bd27414d8ea1af846ae5bacd832704dac49e800b
Radovan Bacovic <rbacovic@gitlab.com>
2022-04-04 15:40:22 +0200

    Refine flake8

--- extract/check_pipeline.py
+++ extract/check_pipeline.py
@@ -1,8 +1,10 @@
-def test_routine(input_string:str):
-    return F"This is result: {input_string}"
+def test_routine(input_string: str):
+    return f"This is result: {input_string}"
+
 
 def WRONG_NAMING():
     return "THIS IS WRONG NAMING "
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_routine("This is TEST")
