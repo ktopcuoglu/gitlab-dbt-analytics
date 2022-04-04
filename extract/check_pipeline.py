58cf4d091beb7417a85fad1e359c20d7b891b1ee
Radovan Bacovic <rbacovic@gitlab.com>
2022-04-04 15:26:52 +0200

    Flake8 test added.

--- extract/check_pipeline.py
+++ extract/check_pipeline.py
@@ -1,6 +1,8 @@
 def test_routine(input_string:str):
     return F"This is result: {input_string}"
 
+def WRONG_NAMING():
+    return "THIS IS WRONG NAMING "
 
 if __name__ == '__main__':
     test_routine("This is TEST")
