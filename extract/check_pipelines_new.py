107094211f5288bbd02b4912544b21017ee94e7c
Radovan Bacovic <rbacovic@gitlab.com>
2022-04-05 15:41:08 +0200

    Dummy file added.

---
+++ extract/check_pipelines_new.py
@@ -0,0 +1,12 @@
+def print_something(input_str: str):
+    return f"This is result {input_str}"
+
+
+if __name__ == "__main__":
+    print_something("Dummy string")
+    print(
+        "HAVE SOME Looooooong string xxxx x x xx "
+        "x x x x x x "
+        "xxxx "
+        "x x x x x x x xx x xx xx x xx xx x x"
+    )
