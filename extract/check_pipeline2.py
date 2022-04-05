0df9e4732eac4598569e59a685afcd7b6daba9eb
Radovan Bacovic <rbacovic@gitlab.com>
2022-04-05 09:14:45 +0200

    Narrow pylint to run on the changed files only.

--- extract/check_pipeline2.py
+++ extract/check_pipeline2.py
@@ -1,12 +1,15 @@
-def test_routine(input_string: str):
+'''
+Test file no2
+'''
+def test_routine2(input_string: str):
     return f"This is result: {input_string}"
 
 
-def WRONG_NAMING():
+def WRONG_NAMING2():
     return "THIS IS WRONG NAMING "
 
 
 if __name__ == "__main__":
-    test_routine("This is TEST")
-    print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+    test_routine2("This is TEST")
     # something new like a comment
+    print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
