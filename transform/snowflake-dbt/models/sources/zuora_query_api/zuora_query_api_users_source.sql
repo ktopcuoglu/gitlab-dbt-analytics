ed49a1142d1cec6ec3b9afe5cc398fe52faf641e
Paul Armstrong <parmstrong@gitlab.com>
2022-04-06 11:43:22 +0200

    Correct column select names

--- transform/snowflake-dbt/models/sources/zuora_query_api/zuora_query_api_users_source.sql
+++ transform/snowflake-dbt/models/sources/zuora_query_api/zuora_query_api_users_source.sql
@@ -6,12 +6,12 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
-      Id::TEXT                                                             AS zuora_user_id,
-      Email::TEXT                                                          AS email,
-      FirstName::TEXT                                                      AS first_name,
-      LastName::TEXT                                                       AS last_name,
-      Username::TEXT                                                       AS user_name,
-      TO_TIMESTAMP(CONVERT_TIMEZONE('UTC', CreatedDate))::TIMESTAMP        AS created_date,
+      "Id"::TEXT                                                           AS zuora_user_id,
+      "Email"::TEXT                                                        AS email,
+      "FirstName"::TEXT                                                    AS first_name,
+      "LastName"::TEXT                                                     AS last_name,
+      "Username"::TEXT                                                     AS user_name,
+      TO_TIMESTAMP(CONVERT_TIMEZONE('UTC', "CreatedDate"))::TIMESTAMP      AS created_date,
       TO_TIMESTAMP_NTZ(CAST(_uploaded_at AS INT))::TEXT                    AS uploaded_at
     FROM source
 
