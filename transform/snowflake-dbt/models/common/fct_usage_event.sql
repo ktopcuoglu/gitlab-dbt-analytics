f646a2df67bf46354ba5b071ff57632a61cc2650
iweeks <iweeks@gitlab.com>
2022-04-15 19:05:10 -0400

    add back user field

--- transform/snowflake-dbt/models/common/fct_usage_event.sql
+++ transform/snowflake-dbt/models/common/fct_usage_event.sql
@@ -32,6 +32,7 @@ fct_events AS (
     prep_event_24_months.parent_id,
     prep_event_24_months.dim_project_id,
     prep_event_24_months.event_created_at,
+    prep_event_24_months.days_since_user_creation_at_event_date,
     xmau_metrics.group_name,
     xmau_metrics.section_name,
     COALESCE(xmau_metrics.smau, FALSE) AS is_smau,
@@ -153,6 +154,7 @@ gitlab_dotcom_fact AS (
     final.plan_id_at_event_date,
     final.plan_name_at_event_date,
     final.plan_was_paid_at_event_date,
+    final.days_since_user_creation_at_event_date,
     'GITLAB_DOTCOM' AS data_source
   FROM final
   LEFT JOIN dim_date
