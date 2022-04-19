d3419a1342a6bce611c4b1ece22e48344fd76417
Radovan Bacovic <rbacovic@gitlab.com>
2022-04-19 10:19:46 +0200

    Revert usage ping test case.

--- extract/test/test_saas_usage_ping_extract.py
+++ extract/test/test_saas_usage_ping_extract.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import pytest
 import sys
 import os
@@ -12,12 +11,8 @@ sys.path.append(abs_path)
 from extract.saas_usage_ping.usage_ping import UsagePing
 
 
-@pytest.fixture
-def usage_ping_object():
-    return UsagePing
-
-
-def test_get_md5(usage_ping_object):
+def test_get_md5():
+    usage_ping_test = UsagePing
 
     input_timestamps = [
         datetime(2021, 9, 1, 23, 10, 21).timestamp(),
@@ -33,37 +28,14 @@ def test_get_md5(usage_ping_object):
     in this implementation.
     """
     for i, check_time in enumerate(input_timestamps):
-        res = usage_ping_object._get_md5(None, check_time)
+        res = usage_ping_test._get_md5(None, check_time)
         # Check output data type
         assert isinstance(res, str)
         # Check is len 32 as it is expected length
         assert len(res) == 32  # bytes in hex representation
-        # As this is one-way function, can't test it with many things
-        # let see do we have all details with various inputs
+        # As this is one-way function, can't test it with many things - let see to we have all details with various inputs
         assert res is not None
 
 
-def test_load_json_file_not_existing_file(usage_ping_object) -> None:
-    """
-    return: None
-    """
-
-    with pytest.raises(FileNotFoundError):
-        _ = usage_ping_object._get_json_data_from_file(
-            self=None, file_name="THIS_DOES_NOT_EXITS.json"
-        )
-
-
-def test_prepare_dataframe_to_upload(usage_ping_object):
-    test_dataframe = usage_ping_object._prepare_dataframe_to_upload(
-        self=None, columns=[0, "a", "b", "c"], content="x"
-    )
-    assert test_dataframe is not None
-    assert type(test_dataframe) == pd.DataFrame
-    assert test_dataframe.loc[0][0] == "x"
-
-
 if __name__ == "__main__":
-    test_get_md5(usage_ping_object)
-    test_load_json_file_not_existing_file(usage_ping_object)
-    test_prepare_dataframe_to_upload(usage_ping_object)
+    test_get_md5()
