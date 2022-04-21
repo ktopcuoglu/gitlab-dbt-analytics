5d59a23832e0bfb2740c38bc9ac3cc9b048e25ee
Rigerta Demiri <rdemiri@gitlab.com>
2022-04-21 22:44:36 +0200

    Apply sqlfluff fix on the models

--- transform/snowflake-dbt/models/sources/xactly/xactly_period_type_source.sql
+++ transform/snowflake-dbt/models/sources/xactly/xactly_period_type_source.sql
@@ -1,25 +1,27 @@
 WITH source AS (
 
-    SELECT *
-    FROM {{ source('xactly', 'xc_period_type') }}
+  SELECT *
+  FROM {{ source('xactly', 'xc_period_type') }}
 
-), renamed AS (
+),
 
-    SELECT
+renamed AS (
 
-      period_type_id::FLOAT         AS period_type_id,
-      version::FLOAT                AS version,
-      name::VARCHAR                 AS name,
-      is_active::VARCHAR            AS is_active,
-      created_date::VARCHAR         AS created_date,
-      created_by_id::FLOAT          AS created_by_id,
-      created_by_name::VARCHAR      AS created_by_name,
-      modified_date::VARCHAR        AS modified_date,
-      modified_by_id::FLOAT         AS modified_by_id,
-      modified_by_name::VARCHAR     AS modified_by_name 
+  SELECT
 
-    FROM source
-    
+    period_type_id::FLOAT AS period_type_id,
+    version::FLOAT AS version,
+    name::VARCHAR AS name,
+    is_active::VARCHAR AS is_active,
+    created_date::VARCHAR AS created_date,
+    created_by_id::FLOAT AS created_by_id,
+    created_by_name::VARCHAR AS created_by_name,
+    modified_date::VARCHAR AS modified_date,
+    modified_by_id::FLOAT AS modified_by_id,
+    modified_by_name::VARCHAR AS modified_by_name
+
+  FROM source
+
 )
 
 SELECT *
