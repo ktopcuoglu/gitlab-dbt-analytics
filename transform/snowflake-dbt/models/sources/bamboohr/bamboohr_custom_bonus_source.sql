e8eeb5ec17c5fe9de9909e84d5b575b901ca2ad1
Peter Empey <pempey@gitlab.com>
2022-04-27 07:36:55 -0600

    remove second unnest step

--- transform/snowflake-dbt/models/sources/bamboohr/bamboohr_custom_bonus_source.sql
+++ transform/snowflake-dbt/models/sources/bamboohr/bamboohr_custom_bonus_source.sql
@@ -11,12 +11,6 @@ WITH source AS (
       FROM source,
       LATERAL FLATTEN(INPUT => parse_json(jsontext), outer => true) d
 
-), unnest_again AS (
-
-      SELECT d.value as data_by_row
-      FROM intermediate,
-      LATERAL FLATTEN(INPUT => parse_json(data_by_row), outer => true) d
-
 ), renamed AS (
 
       SELECT
@@ -25,7 +19,7 @@ WITH source AS (
            data_by_row['customBonustype']::varchar   AS bonus_type,
            data_by_row['customBonusdate']::date      AS bonus_date,
            data_by_row['customNominatedBy']::varchar AS bonus_nominator_type
-      FROM unnest_again
+      FROM intermediate
       WHERE bonus_date IS NOT NULL
 
 )
