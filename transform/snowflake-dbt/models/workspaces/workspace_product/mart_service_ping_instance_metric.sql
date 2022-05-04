115bf3e46d7f98ad5f5eeafa6a498a9c5e86d15a
icooper-acp <ian@amplifycp.com>
2022-05-04 08:04:33 -0700

    Changed mart_service_ping_instance_metric to derive from the macro like the others

--- transform/snowflake-dbt/models/workspaces/workspace_product/mart_service_ping_instance_metric.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/mart_service_ping_instance_metric.sql
@@ -4,39 +4,4 @@
     unique_key = "service_ping_instance_metric_id"
 ) }}
 
-{{ simple_cte([
-    ('mart_service_ping_instance_metric_7_day', 'mart_service_ping_instance_metric_7_day'),
-    ('mart_service_ping_instance_metric_28_day', 'mart_service_ping_instance_metric_28_day'),
-    ('mart_service_ping_instance_metric_all_time', 'mart_service_ping_instance_metric_all_time')
-    ])
-
-}}
-
-, mart_7_day AS (
-
-SELECT * FROM mart_service_ping_instance_metric_7_day
-{% if is_incremental() %}
-            AND ping_created_at >= (SELECT MAX(ping_created_at) FROM {{this}})
-{% endif %}
-
-), mart_28_day AS (
-
-SELECT * FROM mart_service_ping_instance_metric_28_day
-{% if is_incremental() %}
-            AND ping_created_at >= (SELECT MAX(ping_created_at) FROM {{this}})
-{% endif %}
-
-), mart_all_time AS (
-
-SELECT * FROM mart_service_ping_instance_metric_all_time
-{% if is_incremental() %}
-            AND ping_created_at >= (SELECT MAX(ping_created_at) FROM {{this}})
-{% endif %}
-
-)
-
-SELECT * FROM mart_7_day
-  UNION ALL
-SELECT * FROM mart_28_day
-  UNION ALL
-SELECT * FROM mart_all_time
+{{ macro_mart_service_ping_instance_metric('fct_service_ping_instance_metric') }}
--- transform/snowflake-dbt/models/workspaces/workspace_product/test_macro.sql
+++
@@ -1,6 +0,0 @@
-{{ config(
-    tags=["product", "mnpi_exception"],
-    materialized = "table"
-) }}
-
-{{ macro_mrt_srvc_ping_instance_metric('dim_subscription') }}
