b197485b21532e69124643c3602291e153a3fa5c
Paul Armstrong <parmstrong@gitlab.com>
2022-05-09 15:25:56 +0200

    Add ticket tags to simplify querying.

---
+++ transform/snowflake-dbt/models/sources/zendesk/zendesk_ticket_tags_source.sql
@@ -0,0 +1,19 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('zendesk', 'tickets') }}
+
+), renamed AS (
+
+    -- Parse lists to values
+    SELECT
+      ID            AS ticket_id,
+      f.VALUE       AS ticket_tag
+    FROM
+      source,
+      table(FLATTEN(INPUT => STRTOK_TO_ARRAY(tags, '[], "'))) f;
+
+)
+
+SELECT *
+FROM renamed
