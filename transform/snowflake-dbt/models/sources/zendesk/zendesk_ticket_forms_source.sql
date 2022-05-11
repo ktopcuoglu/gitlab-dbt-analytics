0b1d58216a4145c2e66ab8e82ed6e629ef560229
Paul Armstrong <parmstrong@gitlab.com>
2022-05-11 15:00:43 +0200

    Syntax fix.

--- transform/snowflake-dbt/models/sources/zendesk/zendesk_ticket_forms_source.sql
+++ transform/snowflake-dbt/models/sources/zendesk/zendesk_ticket_forms_source.sql
@@ -9,16 +9,16 @@ WITH source AS (
 
       id::NUMBER                                AS ticket_form_id,
       active::BOOLEAN                           AS is_form_active,
-      default::BOOLEAN                          AS is_default,
-      display_name:VARCHAR                      AS display_name,
+      "DEFAULT"::BOOLEAN                        AS is_default,
+      display_name::VARCHAR                     AS display_name,
       end_user_visible::BOOLEAN                 AS is_end_user_visible,
       in_all_brands::BOOLEAN                    AS is_in_all_brands,
-      name:VARCHAR                              AS name,
+      name::VARCHAR                             AS name,
       position::NUMBER                          AS position,
-      raw_display_name:VARCHAR                  AS raw_display_name,
-      raw_name:VARCHAR                          AS raw_name,
-      restricted_brand_ids:VARCHAR              AS restricted_brand_ids,
-      ticket_field_ids:VARCHAR                  AS ticket_field_ids,
+      raw_display_name::VARCHAR                 AS raw_display_name,
+      raw_name::VARCHAR                         AS raw_name,
+      restricted_brand_ids::VARCHAR             AS restricted_brand_ids,
+      ticket_field_ids::VARCHAR                 AS ticket_field_ids,
       updated_at::TIMESTAMP                     AS updated_at,
       created_at::TIMESTAMP                     AS created_at
 
