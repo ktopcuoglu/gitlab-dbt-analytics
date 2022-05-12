175aa81254f48a691b38e7ebf9576e17fd079e0c
iweeks <iweeks@gitlab.com>
2022-05-12 12:06:58 -0400

    final updates

--- transform/snowflake-dbt/models/common_mapping/map_gitlab_dotcom_xmau_metrics.sql
+++ transform/snowflake-dbt/models/common_mapping/map_gitlab_dotcom_xmau_metrics.sql
@@ -1,11 +1,22 @@
 {{ simple_cte([
-    ('gitlab_dotcom_xmau_metrics', 'gitlab_dotcom_xmau_metrics_common')
+    ('gitlab_dotcom_xmau_metrics', 'gitlab_dotcom_xmau_metrics_common'),
+    ('dotcom_event_to_edm_event', 'legacy_dotcom_event_name_to_edm_event_name_mapping')
 ]) }},
 
 mapping_table AS (
 
-  SELECT *
-  FROM gitlab_dotcom_xmau_metrics
+    SELECT
+      gitlab_dotcom_xmau_metrics.common_events_to_include,
+      dotcom_event_to_edm_event.legacy_dotcom_event_name AS legacy_events_to_include,
+      gitlab_dotcom_xmau_metrics.stage_name,
+      gitlab_dotcom_xmau_metrics.smau,
+      gitlab_dotcom_xmau_metrics.group_name,
+      gitlab_dotcom_xmau_metrics.gmau,
+      gitlab_dotcom_xmau_metrics.section_name,
+      gitlab_dotcom_xmau_metrics.is_umau
+    FROM gitlab_dotcom_xmau_metrics
+    LEFT JOIN dotcom_event_to_edm_event
+      ON gitlab_dotcom_xmau_metrics.common_events_to_include = dotcom_event_to_edm_event.prep_event_name
 
 )
 
