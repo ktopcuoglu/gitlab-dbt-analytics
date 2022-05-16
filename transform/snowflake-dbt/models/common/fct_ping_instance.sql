7afa3c43687cd131c7274d5b8cbd65fc549c55c4
snalamaru <snalamaru@gitlab.com>
2022-05-16 14:04:26 -0500

    updated code for fct tables

--- transform/snowflake-dbt/models/common/fct_ping_instance.sql
+++ transform/snowflake-dbt/models/common/fct_ping_instance.sql
@@ -13,139 +13,97 @@
     ('dim_date', 'dim_date'),
     ('map_ip_to_country', 'map_ip_to_country'),
     ('locations', 'prep_location_country'),
-    ('prep_ping_instance', 'prep_ping_instance'),
-    ('dim_product_tier', 'dim_product_tier')
+    ('dim_product_tier', 'dim_product_tier'),
+    ('prep_ping_instance', 'prep_ping_instance')
     ])
 
-}},
+}}
 
-map_ip_location AS (
+, map_ip_location AS (
 
-  SELECT
-    map_ip_to_country.ip_address_hash AS ip_address_hash,
-    map_ip_to_country.dim_location_country_id AS dim_location_country_id
-  FROM map_ip_to_country
-  INNER JOIN locations
-  WHERE map_ip_to_country.dim_location_country_id = locations.dim_location_country_id
+    SELECT
+      map_ip_to_country.ip_address_hash                 AS ip_address_hash,
+      map_ip_to_country.dim_location_country_id         AS dim_location_country_id
+    FROM map_ip_to_country
+    INNER JOIN locations
+      WHERE map_ip_to_country.dim_location_country_id = locations.dim_location_country_id
 
-),
+), source AS (
 
-source AS (
+    SELECT
+      prep_ping_instance.*,
+      --(prep_ping_instance.raw_usage_data_payload:license_md5::TEXT)                                       AS license_md5,
+      TO_DATE(prep_ping_instance.raw_usage_data_payload:license_trial_ends_on::TEXT)                      AS license_trial_ends_on,
+      (prep_ping_instance.raw_usage_data_payload:license_subscription_id::TEXT)                           AS license_subscription_id,
+      prep_ping_instance.raw_usage_data_payload:usage_activity_by_stage_monthly.manage.events::NUMBER     AS umau_value
+    FROM prep_ping_instance
+      {% if is_incremental() %}
+                  WHERE ping_created_at >= (SELECT MAX(ping_created_at) FROM {{this}})
+      {% endif %}
 
-  SELECT
-    prep_ping_instance.*,
-    --(prep_ping_instance.raw_usage_data_payload:license_md5::TEXT)                                       AS license_md5,
-    TO_DATE(
-      prep_ping_instance.raw_usage_data_payload:license_trial_ends_on::TEXT
-    ) AS license_trial_ends_on,
-    (
-      prep_ping_instance.raw_usage_data_payload:license_subscription_id::TEXT
-    ) AS license_subscription_id,
-    prep_ping_instance.raw_usage_data_payload:usage_activity_by_stage_monthly.manage.events::NUMBER AS umau_value
-  FROM prep_ping_instance
-  {% if is_incremental() %}
-        
-    WHERE ping_created_at >= (SELECT MAX(ping_created_at) FROM {{this}})
-  {% endif %}
+), add_country_info_to_usage_ping AS (
 
-),
+    SELECT
+      source.*,
+      map_ip_location.dim_location_country_id     AS dim_location_country_id
+    FROM source
+    LEFT JOIN map_ip_location
+      ON source.ip_address_hash = map_ip_location.ip_address_hash
 
-add_country_info_to_usage_ping AS (
+), prep_usage_ping_cte AS (
 
-  SELECT
-    source.*,
-    map_ip_location.dim_location_country_id AS dim_location_country_id
-  FROM source
-  LEFT JOIN map_ip_location
-    ON source.ip_address_hash = map_ip_location.ip_address_hash
-
-),
-
-dim_product_tier AS (
-
-  SELECT *
-  FROM dim_product_tier
-
-),
-
-prep_usage_ping_cte AS (
-
-  SELECT
-    dim_ping_instance_id AS dim_ping_instance_id,
-    dim_host_id AS dim_host_id,
-    dim_instance_id AS dim_instance_id,
-    dim_installation_id AS dim_installation_id,
-    dim_product_tier.dim_product_tier_id AS dim_product_tier_id,
-    ping_created_at AS ping_created_at,
-    license_md5 AS license_md5,
-    dim_location_country_id AS dim_location_country_id,
-    license_trial_ends_on AS license_trial_ends_on,
-    license_subscription_id AS license_subscription_id,
-    umau_value AS umau_value,
-    product_tier AS product_tier,
-    main_edition AS main_edition
-  FROM add_country_info_to_usage_ping
-  LEFT JOIN dim_product_tier
-    ON
-      TRIM(
-        LOWER(add_country_info_to_usage_ping.product_tier)
-      ) = TRIM(LOWER(dim_product_tier.product_tier_historical_short))
-      AND IFF(
-        add_country_info_to_usage_ping.dim_instance_id = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f',
-        'SaaS',
-        'Self-Managed'
-      ) = dim_product_tier.product_delivery_type
+    SELECT
+      dim_ping_instance_id                                AS dim_ping_instance_id,
+      dim_host_id                                         AS dim_host_id,
+      dim_instance_id                                     AS dim_instance_id,
+      dim_installation_id                                 AS dim_installation_id,
+      dim_product_tier.dim_product_tier_id                AS dim_product_tier_id,
+      ping_created_at                                     AS ping_created_at,
+      license_md5                                         AS license_md5,
+      dim_location_country_id                             AS dim_location_country_id,
+      license_trial_ends_on                               AS license_trial_ends_on,
+      license_subscription_id                             AS license_subscription_id,
+      umau_value                                          AS umau_value,
+      product_tier                                        AS product_tier,
+      main_edition                                        AS main_edition
+    FROM add_country_info_to_usage_ping
+    LEFT JOIN dim_product_tier
+    ON TRIM(LOWER(add_country_info_to_usage_ping.product_tier)) = TRIM(LOWER(dim_product_tier.product_tier_historical_short))
+    AND IFF( add_country_info_to_usage_ping.dim_instance_id = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f','SaaS','Self-Managed') = dim_product_tier.product_delivery_type
     --AND main_edition = 'EE'
 
-),
+), joined_payload AS (
 
-joined_payload AS (
-
-  SELECT
-    prep_usage_ping_cte.*,
-    prep_license.dim_license_id AS dim_license_id,
-    dim_date.date_id AS dim_ping_date_id,
-    COALESCE(
-      license_subscription_id, prep_subscription.dim_subscription_id
-    ) AS dim_subscription_id,
-    IFF(
-      prep_usage_ping_cte.ping_created_at < license_trial_ends_on, TRUE, FALSE
-    ) AS is_trial
-  FROM prep_usage_ping_cte
-  LEFT JOIN prep_license
-    ON prep_usage_ping_cte.license_md5 = prep_license.license_md5
-  LEFT JOIN prep_subscription
-    ON prep_license.dim_subscription_id = prep_subscription.dim_subscription_id
-  LEFT JOIN dim_date
-    ON TO_DATE(prep_usage_ping_cte.ping_created_at) = dim_date.date_day
-
-),
-
-final AS (
-  
-  SELECT
-    {{ dbt_utils.surrogate_key(['dim_ping_instance_id']) }} AS ping_instance_id,
-    dim_ping_instance_id AS dim_ping_instance_id,
-    dim_product_tier_id AS dim_product_tier_id,
-    dim_subscription_id AS dim_subscription_id,
-    dim_location_country_id AS dim_location_country_id,
-    dim_ping_date_id AS dim_ping_date_id,
-    dim_instance_id AS dim_instance_id,
-    dim_host_id AS dim_host_id,
-    dim_installation_id AS dim_installation_id,
-    dim_license_id AS dim_license_id,
-    ping_created_at AS ping_created_at,
-    umau_value AS umau_value,
-    license_subscription_id AS dim_subscription_license_id,
-    'VERSION_DB' AS data_source
-  FROM joined_payload
+    SELECT
+      {{ dbt_utils.surrogate_key(['dim_ping_instance_id']) }}                         AS ping_instance_id,
+      dim_ping_instance_id                                                            AS dim_ping_instance_id,
+      dim_product_tier_id                                                             AS dim_product_tier_id,
+      COALESCE(license_subscription_id, prep_subscription.dim_subscription_id)        AS dim_subscription_id,
+      dim_location_country_id                                                         AS dim_location_country_id,
+      dim_date.date_id                                                                AS dim_ping_date_id,
+      dim_instance_id                                                                 AS dim_instance_id,
+      dim_host_id                                                                     AS dim_host_id,
+      dim_installation_id                                                             AS dim_installation_id,
+      prep_license.dim_license_id                                                     AS dim_license_id,
+      ping_created_at                                                                 AS ping_created_at,
+      umau_value                                                                      AS umau_value,
+      license_subscription_id                                                         AS dim_subscription_license_id,
+      IFF(prep_usage_ping_cte.ping_created_at < license_trial_ends_on, TRUE, FALSE)   AS is_trial,
+      'VERSION_DB'                                                                    AS data_source
+    FROM prep_usage_ping_cte
+    LEFT JOIN prep_license
+      ON prep_usage_ping_cte.license_md5 = prep_license.license_md5
+    LEFT JOIN prep_subscription
+      ON prep_license.dim_subscription_id = prep_subscription.dim_subscription_id
+    LEFT JOIN dim_date
+      ON TO_DATE(prep_usage_ping_cte.ping_created_at) = dim_date.date_day
 
 )
 
 {{ dbt_audit(
-    cte_ref="final",
+    cte_ref="joined_payload",
     created_by="@icooper-acp",
-    updated_by="@iweeks",
+    updated_by="@snalamaru",
     created_date="2022-03-08",
-    updated_date="2022-05-12"
+    updated_date="2022-05-16"
 ) }}
--- transform/snowflake-dbt/models/common/fct_ping_instance_metric.sql
+++ transform/snowflake-dbt/models/common/fct_ping_instance_metric.sql
@@ -14,153 +14,112 @@
     ('map_ip_to_country', 'map_ip_to_country'),
     ('locations', 'prep_location_country'),
     ('prep_ping_instance', 'prep_ping_instance_flattened'),
-    ('dim_ping_metric', 'dim_ping_metric'),
-    ('dim_product_tier', 'dim_product_tier')
+    ('dim_product_tier', 'dim_product_tier'),
+    ('dim_ping_metric', 'dim_ping_metric')
     ])
 
-}},
+}}
 
-map_ip_location AS (
+, map_ip_location AS (
 
-  SELECT
-    map_ip_to_country.ip_address_hash AS ip_address_hash,
-    map_ip_to_country.dim_location_country_id AS dim_location_country_id
-  FROM map_ip_to_country
-  INNER JOIN locations
-  WHERE map_ip_to_country.dim_location_country_id = locations.dim_location_country_id
+    SELECT
+      map_ip_to_country.ip_address_hash                 AS ip_address_hash,
+      map_ip_to_country.dim_location_country_id         AS dim_location_country_id
+    FROM map_ip_to_country
+    INNER JOIN locations
+      WHERE map_ip_to_country.dim_location_country_id = locations.dim_location_country_id
 
-),
+), source AS (
 
-source AS (
+    SELECT
+      prep_ping_instance.*
+    FROM prep_ping_instance
+      {% if is_incremental() %}
+                  WHERE ping_created_at >= (SELECT MAX(ping_created_at) FROM {{this}})
+      {% endif %}
 
-  SELECT
-    prep_ping_instance.*
-  FROM prep_ping_instance
-  {% if is_incremental() %}
- 
-     WHERE ping_created_at > (SELECT MAX(ping_created_at) FROM {{this}})
-      
-  {% endif %}
+), add_country_info_to_usage_ping AS (
 
-),
+    SELECT
+      source.*,
+      map_ip_location.dim_location_country_id     AS dim_location_country_id
+    FROM source
+    LEFT JOIN map_ip_location
+      ON source.ip_address_hash = map_ip_location.ip_address_hash
 
-add_country_info_to_usage_ping AS (
+), prep_usage_ping_cte AS (
 
-  SELECT
-    source.*,
-    map_ip_location.dim_location_country_id AS dim_location_country_id
-  FROM source
-  LEFT JOIN map_ip_location
-    ON source.ip_address_hash = map_ip_location.ip_address_hash
+    SELECT
+      dim_ping_instance_id                                AS dim_ping_instance_id,
+      dim_host_id                                         AS dim_host_id,
+      dim_instance_id                                     AS dim_instance_id,
+      dim_installation_id                                 AS dim_installation_id,
+      dim_product_tier.dim_product_tier_id                AS dim_product_tier_id,
+      ping_created_at                                     AS ping_created_at,
+      license_md5                                         AS license_md5,
+      dim_location_country_id                             AS dim_location_country_id,
+      license_trial_ends_on                               AS license_trial_ends_on,
+      license_subscription_id                             AS license_subscription_id,
+      umau_value                                          AS umau_value,
+      product_tier                                        AS product_tier,
+      main_edition                                        AS main_edition,
+      metrics_path                                        AS metrics_path,
+      metric_value                                        AS metric_value,
+      has_timed_out                                       AS has_timed_out
+    FROM add_country_info_to_usage_ping
+    LEFT JOIN dim_product_tier
+    ON TRIM(LOWER(add_country_info_to_usage_ping.product_tier)) = TRIM(LOWER(dim_product_tier.product_tier_historical_short))
+    AND IFF( add_country_info_to_usage_ping.dim_instance_id = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f','SaaS','Self-Managed') = dim_product_tier.product_delivery_type
+    --AND main_edition = 'EE'
 
-),
+), joined_payload AS (
 
-dim_product_tier AS (
+    SELECT
+      prep_usage_ping_cte.*,
+      prep_license.dim_license_id                                                                          AS dim_license_id,
+      dim_date.date_id                                                                                     AS dim_ping_date_id,
+      COALESCE(license_subscription_id, prep_subscription.dim_subscription_id)                             AS dim_subscription_id,
+      IFF(prep_usage_ping_cte.ping_created_at < license_trial_ends_on, TRUE, FALSE)                        AS is_trial
+    FROM prep_usage_ping_cte
+    LEFT JOIN prep_license
+      ON prep_usage_ping_cte.license_md5 = prep_license.license_md5
+    LEFT JOIN prep_subscription
+      ON prep_license.dim_subscription_id = prep_subscription.dim_subscription_id
+    LEFT JOIN dim_date
+      ON TO_DATE(prep_usage_ping_cte.ping_created_at) = dim_date.date_day
 
-  SELECT *
-  FROM dim_product_tier
+), metric_attributes AS (
 
-),
+    SELECT * FROM dim_ping_metric
 
-prep_usage_ping_cte AS (
-
-  SELECT
-    dim_ping_instance_id AS dim_ping_instance_id,
-    dim_host_id AS dim_host_id,
-    dim_instance_id AS dim_instance_id,
-    dim_installation_id AS dim_installation_id,
-    dim_product_tier.dim_product_tier_id AS dim_product_tier_id,
-    ping_created_at AS ping_created_at,
-    license_md5 AS license_md5,
-    dim_location_country_id AS dim_location_country_id,
-    license_trial_ends_on AS license_trial_ends_on,
-    license_subscription_id AS license_subscription_id,
-    umau_value AS umau_value,
-    product_tier AS product_tier,
-    main_edition AS main_edition,
-    metrics_path AS metrics_path,
-    metric_value AS metric_value,
-    has_timed_out AS has_timed_out
-  FROM add_country_info_to_usage_ping
-  LEFT JOIN dim_product_tier
-    ON
-      TRIM(
-        LOWER(add_country_info_to_usage_ping.product_tier)
-      ) = TRIM(LOWER(dim_product_tier.product_tier_historical_short))
-      AND IFF(
-        add_country_info_to_usage_ping.dim_instance_id = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f',
-        'SaaS',
-        'Self-Managed'
-      ) = dim_product_tier.product_delivery_type
-
-),
-
-joined_payload AS (
-
-  SELECT
-    prep_usage_ping_cte.*,
-    prep_license.dim_license_id AS dim_license_id,
-    dim_date.date_id AS dim_ping_date_id,
-    COALESCE(
-      license_subscription_id, prep_subscription.dim_subscription_id
-    ) AS dim_subscription_id,
-    IFF(
-      prep_usage_ping_cte.ping_created_at < license_trial_ends_on, TRUE, FALSE
-    ) AS is_trial
-  FROM prep_usage_ping_cte
-  LEFT JOIN prep_license
-    ON prep_usage_ping_cte.license_md5 = prep_license.license_md5
-  LEFT JOIN prep_subscription
-    ON prep_license.dim_subscription_id = prep_subscription.dim_subscription_id
-  LEFT JOIN dim_date
-    ON TO_DATE(prep_usage_ping_cte.ping_created_at) = dim_date.date_day
-
-),
-
-flattened_high_level AS (
-  
-  SELECT
-    dim_ping_instance_id AS dim_ping_instance_id,
-    metrics_path AS metrics_path,
-    metric_value AS metric_value,
-    has_timed_out AS has_timed_out,
-    dim_product_tier_id AS dim_product_tier_id,
-    dim_subscription_id AS dim_subscription_id,
-    dim_location_country_id AS dim_location_country_id,
-    dim_ping_date_id AS dim_ping_date_id,
-    dim_instance_id AS dim_instance_id,
-    dim_host_id AS dim_host_id,
-    dim_installation_id AS dim_installation_id,
-    dim_license_id AS dim_license_id,
-    ping_created_at AS ping_created_at,
-    umau_value AS umau_value,
-    license_subscription_id AS dim_subscription_license_id,
-    'VERSION_DB' AS data_source
+), flattened_high_level as (
+    SELECT
+      {{ dbt_utils.surrogate_key(['dim_ping_instance_id', 'joined_payload.metrics_path']) }}                      AS ping_instance_metric_id,
+      dim_ping_instance_id                                                                                        AS dim_ping_instance_id,
+      joined_payload.metrics_path                                                                                 AS metrics_path,
+      metric_value                                                                                                AS metric_value,
+      has_timed_out                                                                                               AS has_timed_out,
+      dim_product_tier_id                                                                                         AS dim_product_tier_id,
+      dim_subscription_id                                                                                         AS dim_subscription_id,
+      dim_location_country_id                                                                                     AS dim_location_country_id,
+      dim_ping_date_id                                                                                            AS dim_ping_date_id,
+      dim_instance_id                                                                                             AS dim_instance_id,
+      dim_host_id                                                                                                 AS dim_host_id,
+      dim_installation_id                                                                                         AS dim_installation_id,
+      dim_license_id                                                                                              AS dim_license_id,
+      ping_created_at                                                                                             AS ping_created_at,
+      umau_value                                                                                                  AS umau_value,
+      license_subscription_id                                                                                     AS dim_subscription_license_id,
+      'VERSION_DB'                                                                                                AS data_source,
+      metric_attributes.time_frame                                                                                AS time_frame
   FROM joined_payload
-
-),
-
-metric_attributes AS (
-
-  SELECT * 
-  FROM dim_ping_metric
-
-),
-
-final AS (
-
-  SELECT
-    {{ dbt_utils.surrogate_key(['dim_ping_instance_id', 'flattened_high_level.metrics_path']) }} AS ping_instance_metric_id,
-    flattened_high_level.*,
-    metric_attributes.time_frame AS time_frame
-  FROM flattened_high_level
-  LEFT JOIN metric_attributes
-    ON flattened_high_level.metrics_path = metric_attributes.metrics_path
+      LEFT JOIN metric_attributes
+  ON joined_payload.metrics_path = metric_attributes.metrics_path
 
 )
 
 {{ dbt_audit(
-    cte_ref="final",
+    cte_ref="flattened_high_level",
     created_by="@icooper-acp",
     updated_by="@snalamaru",
     created_date="2022-03-08",
