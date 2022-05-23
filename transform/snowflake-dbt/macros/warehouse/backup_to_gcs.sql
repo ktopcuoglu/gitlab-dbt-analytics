d81255ed1e78344a89c8316c1fcc9b74305ff9c4
Radovan Bacovic <rbacovic@gitlab.com>
2022-05-23 09:08:57 +0200

    Restructured the code, works fine.

--- transform/snowflake-dbt/macros/warehouse/backup_to_gcs.sql
+++ transform/snowflake-dbt/macros/warehouse/backup_to_gcs.sql
@@ -1,15 +1,34 @@
 {% macro backup_to_gcs() %}
 
-        {% set day_of_month = run_started_at.strftime("%d") %}
+        {% set backups =
+            {
+                "RAW":
+                    [
+                        "SNAPSHOTS"
+                    ]
+            }
+        %}
 
+        {% set day_of_month = run_started_at.strftime("%d") %}
+        
         {{ log('Backing up for Day ' ~ day_of_month, info = true) }}
 
-        {% set tables = dbt_utils.get_relations_by_prefix('SNAPSHOTS', '', exclude='FIVETRAN_%', database='RAW') %}
+        {% for database, schemas in backups.items() %}
+        
+            {% for schema in schemas %}
+        
+                {{ log('Getting tables in schema ' ~ schema ~ '...', info = true) }}
 
-        {% for table in tables %}
-            {{ log('Backing up ' ~ table.name ~ '...', info = true) }}
-            {% set backup_table_command = get_backup_table_command(table, day_of_month) %}
-            {{ backup_table_command }}
+                {% set tables = dbt_utils.get_relations_by_prefix(schema.upper(), '', exclude='FIVETRAN_%', database=database) %}
+
+                {% for table in tables %}
+                    {{ log('Backing up ' ~ table.name ~ '...', info = true) }}
+                    {% set backup_table_command = get_backup_table_command(table, day_of_month) %}
+                    {{ backup_table_command }}
+                {% endfor %}
+        
+            {% endfor %}
+        
         {% endfor %}
 
 {%- endmacro -%}
