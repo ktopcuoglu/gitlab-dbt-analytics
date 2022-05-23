624c156a707c3bd311752f0a5d82581a5d50b1c9
Radovan Bacovic <rbacovic@gitlab.com>
2022-05-23 08:57:40 +0200

    Check code flow only.

--- transform/snowflake-dbt/macros/warehouse/backup_to_gcs.sql
+++ transform/snowflake-dbt/macros/warehouse/backup_to_gcs.sql
@@ -1,7 +1,7 @@
 {% macro backup_to_gcs() %}
 
 
-    {% set tables = dbt_utils.get_relations_by_prefix('RAW', '', exclude='FIVETRAN_%', database='SNAPSHOTS') %}
+    {% set tables = dbt_utils.get_relations_by_prefix('SNAPSHOTS', '', exclude='FIVETRAN_%', database='RAW') %}
 
     {% for table in tables %}
         {{ log('Backing up ' ~ table.name ~ '...', info = true) }}
