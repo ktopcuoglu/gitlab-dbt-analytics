b02963badb1c16d4051ea4983322bf0bf5a54550
Radovan Bacovic <rbacovic@gitlab.com>
2022-05-23 08:31:26 +0200

    Check code flow only.

--- transform/snowflake-dbt/macros/warehouse/backup_to_gcs.sql
+++ transform/snowflake-dbt/macros/warehouse/backup_to_gcs.sql
@@ -15,30 +15,6 @@
         
         {{ log('Backing up for Day ' ~ day_of_month, info = true) }}
 
-        {% for database, schemas in backups.items() %}
-        
-            {% for schema in schemas %}
-        
-                {{ log('Getting tables in schema ' ~ schema ~ '...', info = true) }}
-
-                {% for table in tables %}
-
-                    {% if loop.last %}
-
-                        {{ log('THIS IS LAST ONE', info = true) }}
-
-                    {% else %}
-
-                        {{ log('THIS IS NOT THE LAST ONE', info = true) }}
-
-                    {% endif %}
-
-                {% endfor %}
-
-            {% endfor %}
-
-        {% endfor %}
-
     {%- endcall -%}
 
 {%- endmacro -%}
