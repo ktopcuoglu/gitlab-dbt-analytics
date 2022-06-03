92f87c89cccfeb8b44b170d43b7b421c8003afbd
Paul Armstrong <parmstrong@gitlab.com>
2022-06-03 14:48:19 +0200

    Later revision for cloning.

--- dags/transformation/dbt_1_1.py
+++ dags/transformation/dbt_1_1.py
@@ -43,7 +43,7 @@ GIT_BRANCH = env["GIT_BRANCH"]
 pod_env_vars = {**gitlab_pod_env_vars, **{}}
 
 # This value is set based on the commit hash setter task in dbt_snapshot
-pull_commit_hash = """export GIT_COMMIT="81f6e559ca936a7bb233a4ffbce74c049b77663b" """
+pull_commit_hash = """export GIT_COMMIT="fc7161faa1ce93c23cc0bf51a68f4772fc9adced" """
 
 
 # Default arguments for the DAG
