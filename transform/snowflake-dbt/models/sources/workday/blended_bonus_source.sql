c826df7dca40cce85e75a2f3890847a4bb490c25
Peter Empey <pempey@gitlab.com>
2022-06-03 06:40:07 -0600

    added bounus unique tracking

--- transform/snowflake-dbt/models/sources/workday/blended_bonus_source.sql
+++ transform/snowflake-dbt/models/sources/workday/blended_bonus_source.sql
@@ -23,6 +23,7 @@ SELECT
   bamboohr.bonus_date,
   bamboohr.bonus_type,
   bamboohr.uploaded_at,
+  ROW_NUMBER() OVER (PARTITION BY map.wk_employee_id, bamboohr.bonus_date ORDER BY bamboohr.bonus_id ASC) AS bonus_sequence,
   'bamboohr' AS source_system
 FROM bamboohr
 INNER JOIN map
@@ -35,13 +36,14 @@ SELECT
   bonus_date,
   bonus_type,
   uploaded_at,
+  ROW_NUMBER() OVER (PARTITION BY employee_id, bonus_date ORDER BY initiated_at ASC) AS bonus_sequence,
   'workday' AS source_system
 FROM workday
 ),
 
 filtered AS (
 
-  {{ gitlab_snowflake.workday_bamboohr_blending_filter('unioned', ['employee_id','bonus_date','bonus_type']) }}
+  {{ gitlab_snowflake.workday_bamboohr_blending_filter('unioned', ['employee_id','bonus_date','bonus_type','bonus_sequence']) }}
   
 )
 
