0cdcb6ab3bde30faa6a31e71bc9207c2cd529721
Paul Armstrong <parmstrong@gitlab.com>
2022-06-03 10:58:45 +0200

    Update to latest dbt image version.

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -12,7 +12,7 @@ from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperato
 SSH_REPO = "git@gitlab.com:gitlab-data/analytics.git"
 HTTP_REPO = "https://gitlab.com/gitlab-data/analytics.git"
 DATA_IMAGE = "registry.gitlab.com/gitlab-data/data-image/data-image:v0.0.27"
-DBT_IMAGE = "registry.gitlab.com/gitlab-data/data-image/dbt-image:v0.0.15"
+DBT_IMAGE = "registry.gitlab.com/gitlab-data/data-image/dbt-image:v1.0.2"
 PERMIFROST_IMAGE = "registry.gitlab.com/gitlab-data/permifrost:v0.13.1"
 ANALYST_IMAGE = "registry.gitlab.com/gitlab-data/data-image/analyst-image:v0.0.25"
 
