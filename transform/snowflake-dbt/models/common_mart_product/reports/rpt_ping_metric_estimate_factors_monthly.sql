b6245367e50410f0650476f154cfe6bb523e54e5
snalamaru <snalamaru@gitlab.com>
2022-06-08 13:58:48 -0500

    replaced old rpt names

--- transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_metric_estimate_factors_monthly.sql
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_metric_estimate_factors_monthly.sql
@@ -17,14 +17,14 @@
   SELECT
       {{ dbt_utils.surrogate_key(['ping_created_at_month', 'metrics_path', 'ping_edition', 'estimation_grain']) }}          AS ping_metric_estimate_factors_monthly_id,
       {{ dbt_utils.star(from=ref('rpt_ping_subscriptions_reported_estimate_factors_monthly'), except=['PING_SUBSCRIPTIONS_REPORTED_ESTIMATE_FACTORS_MONTHLY_ID', 'CREATED_BY', 'UPDATED_BY', 'MODEL_CREATED_DATE', 'MODEL_UPDATED_DATE', 'DBT_CREATED_AT', 'DBT_UPDATED_AT']) }}
-  FROM rpt_ping_instance_metric_adoption_subscription_monthly
+  FROM rpt_ping_subscriptions_reported_estimate_factors_monthly
 
   UNION ALL
 
   SELECT
       {{ dbt_utils.surrogate_key(['ping_created_at_month', 'metrics_path', 'ping_edition', 'estimation_grain']) }}          AS ping_metric_estimate_factors_monthly_id,
       {{ dbt_utils.star(from=ref('rpt_ping_subscriptions_on_versions_estimate_factors_monthly'), except=['PING_SUBSCRIPTIONS_ON_VERSIONS_ESTIMATE_FACTORS_MONTHLY_ID', 'CREATED_BY', 'UPDATED_BY', 'MODEL_CREATED_DATE', 'MODEL_UPDATED_DATE', 'DBT_CREATED_AT', 'DBT_UPDATED_AT']) }}
-  FROM rpt_ping_instance_metric_adoption_subscription_metric_monthly
+  FROM rpt_ping_subscriptions_on_versions_estimate_factors_monthly
 
 )
 
