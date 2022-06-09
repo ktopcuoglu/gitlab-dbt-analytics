3cc0f6ded0d82c90d7d45fd990d7a4c651d3c3f9
Jean Peguero <jpeguero@gitlab.com>
2022-06-09 12:46:08 +0200

    Update casing

--- transform/snowflake-dbt/models/sources/driveload/driveload_marketing_dnc_list_source.sql
+++ transform/snowflake-dbt/models/sources/driveload/driveload_marketing_dnc_list_source.sql
@@ -6,16 +6,16 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
-      address::VARCHAR                       AS email_address,
-      is_role_address::BOOLEAN               AS is_role_address,
-      is_disposable_address::BOOLEAN         AS is_disposable_address,
-      did_you_mean::VARCHAR                  AS did_you_mean,
-      result::VARCHAR                        AS result,
-      reason::VARIANT                        AS reason,
-      risk::VARCHAR                          AS risk,
-      root_address::VARCHAR                  AS root_address
+      "address"::VARCHAR                       AS email_address,
+      "is_role_address"::BOOLEAN               AS is_role_address,
+      "is_disposable_address"::BOOLEAN         AS is_disposable_address,
+      "did_you_mean"::VARCHAR                  AS did_you_mean,
+      "result"::VARCHAR                        AS result,
+      "reason"::VARIANT                        AS reason,
+      "risk"::VARCHAR                          AS risk,
+      "root_address"::VARCHAR                  AS root_address
     FROM source
-    QUALIFY ROW_NUMBER() OVER(PARTITION BY address ORDER BY _updated_at DESC) = 1
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY email_address ORDER BY _updated_at DESC) = 1
 
 )
 
