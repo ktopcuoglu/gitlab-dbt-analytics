de0399c63e9d3fa73b126be1ab255a38eba5ed86
Radovan Bacovic <rbacovic@gitlab.com>
2022-06-17 14:56:01 +0200

    DAG initial version.

---
+++ dags/extract/snowplow_posthog_backfilling.py
@@ -0,0 +1,105 @@
+import json
+import os
+from datetime import date, datetime
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow_utils import (
+    DBT_IMAGE,
+    dbt_install_deps_nosha_cmd,
+    gitlab_defaults,
+    gitlab_pod_env_vars,
+    partitions,
+    slack_failed_task,
+)
+from kube_secrets import (
+    GIT_DATA_TESTS_PRIVATE_KEY,
+    GIT_DATA_TESTS_CONFIG,
+    SALT,
+    SALT_EMAIL,
+    SALT_IP,
+    SALT_NAME,
+    SALT_PASSWORD,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+    SNOWFLAKE_USER,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+)
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+GIT_BRANCH = env["GIT_BRANCH"]
+pod_env_vars = {**gitlab_pod_env_vars, **{}}
+
+
+task_secrets = [
+    GIT_DATA_TESTS_PRIVATE_KEY,
+    GIT_DATA_TESTS_CONFIG,
+    SALT,
+    SALT_EMAIL,
+    SALT_IP,
+    SALT_NAME,
+    SALT_PASSWORD,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+    SNOWFLAKE_USER,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+]
+
+start_date = datetime(2022, 1, 1, 0, 0, 0)
+dag_name = "snowplow_posthog_backfilling"
+
+
+# Default arguments for the DAG
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "start_date": start_date,
+}
+
+# Create the DAG
+def generate_dbt_command(vars_dict):
+    json_dict = json.dumps(vars_dict)
+
+    dbt_generate_command = f"""echo 'test'"""
+
+    return KubernetesPodOperator(
+        **gitlab_defaults,
+        image=DBT_IMAGE,
+        task_id=f"{dag_name}-{vars_dict['year']}-{vars_dict['month']}",
+        name=f"{dag_name}-{vars_dict['year']}-{vars_dict['month']}",
+        secrets=task_secrets,
+        env_vars=pod_env_vars,
+        arguments=[dbt_generate_command],
+        dag=dag,
+    )
+)
+
+
+
+
+dag = DAG(
+    dag_name,
+    default_args=default_args,
+    schedule_interval=None,
+    concurrency=2,
+)
+
+for month in partitions(start_date.date(), date.today(), "month"
+):
+    generate_dbt_command(month)
