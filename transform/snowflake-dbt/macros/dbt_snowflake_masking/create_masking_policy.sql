69bec32a680b7132faba50fcfd1a362daa9e8be6
Peter Empey <pempey@gitlab.com>
2022-06-17 13:34:00 +0000

    correcting capitalization

--- transform/snowflake-dbt/macros/dbt_snowflake_masking/create_masking_policy.sql
+++ transform/snowflake-dbt/macros/dbt_snowflake_masking/create_masking_policy.sql
@@ -5,7 +5,7 @@
 CREATE OR REPLACE MASKING POLICY "{{ database }}".{{ schema }}.{{ policy }}_{{ data_type }} AS (val {{ data_type }}) 
   RETURNS {{ data_type }} ->
       CASE 
-        WHEN CURRENT_ROLE() IN ('transformer','loader') THEN val  -- Set for specific roles that should always have access
+        WHEN CURRENT_ROLE() IN ('TRANSFORMER','LOADER') THEN val  -- Set for specific roles that should always have access
         WHEN IS_ROLE_IN_SESSION('{{ policy }}') THEN val -- Set for the user to inherit access bases on there roles
         ELSE {{ mask }} 
       END; 
