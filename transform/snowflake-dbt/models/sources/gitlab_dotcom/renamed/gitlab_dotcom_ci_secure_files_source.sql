cfce505d02bcb0b84cfdbcd04761dea288221c59
@dvanrooijen2 <dvanrooijen@gitlab.com>
2022-06-22 10:49:37 +0200

    add ci_secure_files

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_ci_secure_files_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_ci_secure_files_source.sql
@@ -6,15 +6,15 @@ WITH source AS (
 ), renamed AS (
 
   SELECT
-    id::NUMBER              AS ci_secure_files_id
-    project_id::NUMBER      AS project_id
-    created_at::TIMESTAMP   AS created_at
-    updated_at::TIMESTAMP   AS updated_at
-    file_store::NUMBER      AS file_store
-    name::VARCHAR           AS name
-    file::VARCHAR           AS file
-    checksum::VARCHAR       AS checksum
-    key_date::VARCHAR       AS key_date
+    id::NUMBER              AS ci_secure_files_id,
+    project_id::NUMBER      AS project_id,
+    created_at::TIMESTAMP   AS created_at,
+    updated_at::TIMESTAMP   AS updated_at,
+    file_store::NUMBER      AS file_store,
+    name::VARCHAR           AS name,
+    file::VARCHAR           AS file,
+    checksum::VARCHAR       AS checksum,
+    key_data::VARCHAR       AS key_data
   FROM source
 
 )
