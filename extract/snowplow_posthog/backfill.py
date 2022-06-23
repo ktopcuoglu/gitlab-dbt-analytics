791f9b7aba352f2f3432eea7a8f1595b7b83a398
Radovan Bacovic <rbacovic@gitlab.com>
2022-06-23 15:04:10 +0200

    Ogranize and finished extraction part (working).

--- extract/snowplow_posthog/backfill.py
+++ extract/snowplow_posthog/backfill.py
@@ -4,7 +4,6 @@ For this task will use PostHog Python API library.
 Library URL: https://posthog.com/docs/integrate/server/python
 """
 
-
 import yaml
 import sys
 from os import environ as env
@@ -13,15 +12,17 @@ from fire import Fire
 from logging import info, basicConfig
 
 import boto3
+import gzip
 
 ENCODING = "utf-8"
 
 
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-" Extract things
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""
+Extract routines
+"""
 
-def get_s3_credentials -> tuple:
+
+def get_s3_credentials() -> tuple:
     """
     This function returns the set of aws_access_key_id,aws_secret_access_key
     based on the the schema name provided.
@@ -34,18 +35,18 @@ def get_s3_credentials -> tuple:
     return aws_access_key_id, aws_secret_access_key, aws_s3_posthog_bucket
 
 
-def s3_list_files(client, bucket, prefix='') -> str:
+def s3_list_files(client, bucket, prefix="") -> str:
     """
     List files in specific S3 URL using yield
     """
-    paginator = client.get_paginator('list_objects')
-    for result in paginator.paginate(Bucket=bucket,
-                                     Prefix=prefix,
-                                     Delimiter='/'):
-        for content in result.get('Contents', []):
-            yield content.get('Key')
+    paginator = client.get_paginator("list_objects")
+    for result in client.list_objects_v2(Bucket=bucket, Prefix=prefix).get("Contents"):
+        yield result["Key"]
 
-def s3_get_client(aws_access_key_id: str, aws_secret_access_key: str) -> boto3.resources.base.ServiceResource:
+
+def s3_get_client(
+    aws_access_key_id: str, aws_secret_access_key: str
+) -> boto3.resources.base.ServiceResource:
     """
     Get s3 client with the file list
     """
@@ -54,6 +55,19 @@ def s3_get_client(aws_access_key_id: str, aws_secret_access_key: str) -> boto3.r
         aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
     )
     return session.client("s3")
+
+
+def s3_load_source_file(client, bucket: str, file_name: str) -> list:
+    """
+    Load data from object storage (for now, it is a S3 bucket)
+    """
+    csv_obj = client.get_object(Bucket=bucket, Key=file_name)
+
+    body = csv_obj["Body"]
+
+    with gzip.open(body, "rt") as gz_file:
+        for line in gz_file.readlines():
+            yield line.split("\t")
 
 
 def s3_extraction(file_prefix: str) -> None:
@@ -65,22 +79,18 @@ def s3_extraction(file_prefix: str) -> None:
     """
     aws_access_key_id, aws_secret_access_key, aws_s3_posthog_bucket = get_s3_credentials
 
-    s3_client = s3_get_client
+    s3_client = s3_get_client(aws_access_key_id, aws_secret_access_key)
 
-    for file in s3_list_files(client=s3_client, bucket=aws_s3_posthog_bucket, prefix=file_prefix):
+    posthog_file_list = s3_list_files(s3_client, aws_s3_posthog_bucket, prefix="")
+    # for file in s3_list_files(
+    #     client=s3_client, bucket=aws_s3_posthog_bucket, prefix=file_prefix
+    # ):
         pass
 
 
-
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-" Load things
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-def load_source_data(object_storage: str) -> None:
-    """
-    Load data from object storage (for now, it is a S3 bucket)
-    """
-    pass
+"""
+Load routines
+"""
 
 
 def get_row() -> list:
@@ -102,8 +112,7 @@ def get_properties(schema_file: str, table_name: str, values: list) -> dict:
     """
     Get key-value pairs for properties for uploading
     """
-    properties_list = \
-        load_manifest_file(file_name=schema_file).get(table_name)
+    properties_list = load_manifest_file(file_name=schema_file).get(table_name)
 
     return dict(zip(properties_list, values))
 
