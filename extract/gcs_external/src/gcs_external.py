10263cf1780a0e2575f6d256f80145d7343cc383
jjstark <jstark@gitlab.com>
2022-06-27 15:11:39 -0700

    test error case

--- extract/gcs_external/src/gcs_external.py
+++ extract/gcs_external/src/gcs_external.py
@@ -38,7 +38,7 @@ def get_load_command(path_date: str) -> str:
           WHERE $1:msg='access'
           )
           
-          SELECP
+          SELECT
             blob_downloaded.correlation_id,
             blob_downloaded.timestamp,
             blob_downloaded.root_repo,
@@ -70,7 +70,7 @@ def load_data():
         results = connection.execute(load_command).fetchone()
         logging.info(results)
     except:
-        logging.error(f"Failed to load. Snowflake returned: {results}")
+        logging.error(f"Failed to load")
         raise
     finally:
         connection.close()
