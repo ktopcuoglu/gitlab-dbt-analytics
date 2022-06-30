e34a044a03a775a9f52a22817908407014682bbf
Rigerta Demiri <rdemiri@gitlab.com>
2022-06-30 10:04:41 +0200

    Remove leading zero from date

--- dags/extract/gcs_external.py
+++ dags/extract/gcs_external.py
@@ -35,7 +35,7 @@ default_args = {
     "owner": "airflow",
     "retries": 0,
     "retry_delay": timedelta(minutes=1),
-    "start_date": datetime(2021, 11, 01),
+    "start_date": datetime(2021, 11, 1),
     "dagrun_timeout": timedelta(hours=1),
 }
 
