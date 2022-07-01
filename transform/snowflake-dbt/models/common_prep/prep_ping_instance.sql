c287495ae053ef9973130eed2aabfc10d94a1136
tpoole1 <tpoole@gmail.com>
2022-07-01 18:42:26 -0400

    fix

--- transform/snowflake-dbt/models/common_prep/prep_ping_instance.sql
+++ transform/snowflake-dbt/models/common_prep/prep_ping_instance.sql
@@ -1,6 +1,6 @@
 {{ config(
     tags=["product", "mnpi_exception"],
-    full_refresh = false,
+    full_refresh = false,    
     materialized = "incremental",
     unique_key = "dim_ping_instance_id"
 ) }}
