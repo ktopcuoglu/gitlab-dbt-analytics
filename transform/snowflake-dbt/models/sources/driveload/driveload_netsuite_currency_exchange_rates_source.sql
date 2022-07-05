b2ed2dc4274bf5d9b6083c76ae21c3e35d34a24f
Michelle Cooper <mcooper@gitlab.com>
2022-07-05 12:42:29 -0700

    remove netsuite exchange rate models

--- transform/snowflake-dbt/models/sources/driveload/driveload_netsuite_currency_exchange_rates_source.sql
+++
@@ -1,8 +0,0 @@
-WITH source AS (
-
-  SELECT * 
-  FROM {{ source('driveload','netsuite_currency_exchange_rates') }}
-
-)
-SELECT * 
-FROM source
--- transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/driveload_netsuite_currency_exchange_rates.sql
+++
@@ -1,8 +0,0 @@
-WITH source AS (
-
-  SELECT * 
-  FROM {{ ref('driveload_netsuite_currency_exchange_rates_source') }}
-
-)
-SELECT * 
-FROM source
