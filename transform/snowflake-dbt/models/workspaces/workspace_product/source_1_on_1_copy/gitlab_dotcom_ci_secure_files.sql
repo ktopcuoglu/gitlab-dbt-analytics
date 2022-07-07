2074818c71d49a9c7bc72477e8a4e10c4ed711a7
@dvanrooijen2 <dvanrooijen@gitlab.com>
2022-07-07 14:45:24 +0200

    add ci_secure_files

--- transform/snowflake-dbt/models/workspaces/workspace_product/source_1_on_1_copy/gitlab_dotcom_ci_secure_files.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/source_1_on_1_copy/gitlab_dotcom_ci_secure_files.sql
@@ -1,9 +1,9 @@
 WITH source AS (
 
- SELECT *
-  FROM {{ ref('gitlab_dotcom_ci_secure_files_source') }}
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_ci_secure_files_source') }}
 
 )
 
 SELECT *
- FROM source
+FROM source
