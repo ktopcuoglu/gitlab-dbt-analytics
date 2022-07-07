4a16f615c4eaa4838f3d98543eafe4efb51375d4
ved prakash <vprakash@gitlab.com>
2022-07-07 15:37:15 +0200

    debug false and sync false

--- extract/snowplow_posthog/backfill.py
+++ extract/snowplow_posthog/backfill.py
@@ -275,11 +275,10 @@ def posthog_authorize() -> None:
     ) = posthog_get_credentials()
 
     posthog.project_api_key = posthog_project_api_key
-    posthog.personal_api_key = posthog_personal_api_key
+    #posthog.personal_api_key = posthog_personal_api_key
     posthog.host = posthog_host
     #posthog.sync_mode = True
-    
-    posthog.debug = True
+    #posthog.debug = True
 
 
 def posthog_push_json(data: dict) -> None:
