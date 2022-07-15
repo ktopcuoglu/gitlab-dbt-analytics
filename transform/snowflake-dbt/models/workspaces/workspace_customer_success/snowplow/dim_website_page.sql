f15d99cff8c7514038de5d42dd31b7aede6ff200
Chris Sharp <csharp@gitlab.com>
2022-07-15 15:52:19 +0100

    fix regex

--- transform/snowflake-dbt/models/workspaces/workspace_customer_success/snowplow/dim_website_page.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_customer_success/snowplow/dim_website_page.sql
@@ -16,11 +16,11 @@
     SELECT DISTINCT
       app_id,
       page_urlhost,
-      REGEXP_REPLACE(page_urlpath, '^[^a-zA-Z]*|[0-9.]|(\-\/+)', '')      AS clean_urlpath,
-      split_part(clean_urlpath, '/' ,1)                                   AS page_group,
-      split_part(clean_urlpath, '/' ,2)                                   AS page_type,
-      split_part(clean_urlpath, '/' ,3)                                   AS page_sub_type,
-      refr_medium                                                         AS referrer_medium
+      REGEXP_REPLACE(page_urlpath, '^[^a-zA-Z]*|[0-9]|(\-\/+)|\.html$', '') AS clean_urlpath,
+      split_part(clean_urlpath, '/' ,1)                                     AS page_group,
+      split_part(clean_urlpath, '/' ,2)                                     AS page_type,
+      split_part(clean_urlpath, '/' ,3)                                     AS page_sub_type,
+      refr_medium                                                           AS referrer_medium
     FROM events
     WHERE event IN ('struct', 'page_view', 'unstruct')
 
@@ -28,7 +28,7 @@
 
     SELECT DISTINCT
       --surrogate_key
-      {{ dbt_utils.surrogate_key(['clean_urlpath']) }}                    AS dim_website_page_sk,
+      {{ dbt_utils.surrogate_key(['page_urlhost','clean_urlpath']) }}     AS dim_website_page_sk,
       app_id,
       page_urlhost,
       clean_urlpath,
