2bd5757dd496bb7dcb2b80d552be4e7aa1964e55
Chris Sharp <csharp@gitlab.com>
2022-07-19 11:57:07 +0100

    dim_website_page to incremental model

--- transform/snowflake-dbt/models/workspaces/workspace_snowplow/dim_website_page.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_snowplow/dim_website_page.sql
@@ -1,11 +1,9 @@
-{{ 
-    config(
-        materialized='table'
-    ) 
-}}
+{{ config(
+        materialized = "incremental",
+        unique_key = "dim_website_page_sk",
+) }}
 
-{{ 
-    simple_cte([
+{{ simple_cte([
     ('events', 'snowplow_unnested_events_all')
     ])
 
@@ -13,30 +11,42 @@
 
 , page AS (
 
-    SELECT DISTINCT
+    SELECT
       app_id,
       page_urlhost,
-      REGEXP_REPLACE(page_urlpath, '^[^a-zA-Z]*|[0-9]|(\-\/+)|\.html$|\/$', '') AS clean_urlpath,
-      SPLIT_PART(clean_urlpath, '/' ,1)                                         AS page_group,
-      SPLIT_PART(clean_urlpath, '/' ,2)                                         AS page_type,
-      SPLIT_PART(clean_urlpath, '/' ,3)                                         AS page_sub_type,
-      refr_medium                                                               AS referrer_medium
+      RTRIM(
+        REGEXP_REPLACE(page_urlpath, '^[^a-zA-Z]*|[0-9]|(\-\/+)|\.html$|\/$', '')
+        , '/')                                                                      AS clean_urlpath,
+      SPLIT_PART(clean_urlpath, '/' ,1)                                             AS page_group,
+      SPLIT_PART(clean_urlpath, '/' ,2)                                             AS page_type,
+      SPLIT_PART(clean_urlpath, '/' ,3)                                             AS page_sub_type,
+      refr_medium                                                                   AS referrer_medium,
+      max(uploaded_at)                                                              AS max_event_timestamp
     FROM events
     WHERE event IN ('struct', 'page_view', 'unstruct')
     AND page_urlpath IS NOT NULL
+
+    {% if is_incremental() %}
+
+    AND uploaded_at > (SELECT max(max_event_timestamp)) FROM {{ this }})
+
+    {% endif %}
+
+    {{ dbt_utils.group_by(n=7) }}
 
 ), dim_with_pk AS (
 
     SELECT DISTINCT
       --surrogate_key
-      {{ dbt_utils.surrogate_key(['page_urlhost','clean_urlpath']) }}     AS dim_website_page_sk,
+      {{ dbt_utils.surrogate_key(['page_urlhost','clean_urlpath']) }}               AS dim_website_page_sk,
       app_id,
       page_urlhost,
-      RTRIM(clean_urlpath, '/')                                           AS clean_urlpath,
+      clean_urlpath,
       page_group,
       page_type,
       page_sub_type,
-      referrer_medium
+      referrer_medium,
+      max_event_timestamp
     FROM page
 
 )
