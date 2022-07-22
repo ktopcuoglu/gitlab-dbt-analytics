0cada7e2e96672b3856d1b6aa833e010bffca2f0
Chris Sharp <csharp@gitlab.com>
2022-07-22 17:46:29 +0100

    update regex to remove YYYY/MM/DD from url

--- transform/snowflake-dbt/macros/snowplow/clean_url.sql
+++ transform/snowflake-dbt/macros/snowplow/clean_url.sql
@@ -2,6 +2,6 @@
 
 {% macro clean_url(column_name) -%}
 
-RTRIM(REGEXP_REPLACE({{ column_name }}, '\d{4}\/\d{2}\/\d{2}\/|^[^a-zA-Z]*|[0-9]|(\-\/+)|\.html$|\/$', ''), '/')
+RTRIM(REGEXP_REPLACE({{ column_name }}, '[0-9]{4}/[0-9]{2}/[0-9]{2}/|^[^a-zA-Z]*|[0-9]|(\-\/+)|\.html$|\/$', ''), '/')
 
 {%- endmacro %}
