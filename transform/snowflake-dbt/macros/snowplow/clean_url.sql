0e20f00baae166d756b1d36f93ed82e0af2a241a
Chris Sharp <csharp@gitlab.com>
2022-07-25 15:27:12 +0100

    add documentation for clean_url macro

--- transform/snowflake-dbt/macros/snowplow/clean_url.sql
+++ transform/snowflake-dbt/macros/snowplow/clean_url.sql
@@ -1,5 +1,3 @@
--- regex explanation available at https://regex101.com/r/nSdGr7/1
-
 {% macro clean_url(column_name) -%}
 
 RTRIM(REGEXP_REPLACE({{ column_name }}, '[0-9]{4}/[0-9]{2}/[0-9]{2}/|^[^a-zA-Z]*|[0-9]|(\-\/+)|\.html$|\/$', ''), '/')
