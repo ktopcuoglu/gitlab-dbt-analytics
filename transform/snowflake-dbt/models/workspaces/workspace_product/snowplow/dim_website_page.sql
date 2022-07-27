24dee64201bbddef702860edb723e66205b10914
Chris Sharp <csharp@gitlab.com>
2022-07-27 11:19:56 +0100

    add app_id to sk and join

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/dim_website_page.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/dim_website_page.sql
@@ -36,7 +36,7 @@
 
     SELECT DISTINCT
       -- Surrogate Key
-      {{ dbt_utils.surrogate_key(['page_url_host','clean_url_path']) }}               AS dim_website_page_sk,
+      {{ dbt_utils.surrogate_key(['app_id','page_url_host','clean_url_path']) }}               AS dim_website_page_sk,
 
       -- Natural Keys
       app_id,
