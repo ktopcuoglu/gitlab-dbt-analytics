a0957e102472d2fa72ec2897ea5ac3acdfe59605
Paul Armstrong <parmstrong@gitlab.com>
2022-07-27 14:24:51 +0200

    Update source file, move date calculations.

--- transform/snowflake-dbt/models/sources/driveload/driveload_lam_corrections_source.sql
+++ transform/snowflake-dbt/models/sources/driveload/driveload_lam_corrections_source.sql
@@ -2,13 +2,12 @@ WITH source AS (
 
     SELECT
       dim_parent_crm_account_id                  AS dim_parent_crm_account_id,
+      parent_crm_account_sales_segment           AS dim_parent_crm_account_sales_segment,
       dev_count                                  AS dev_count,
       estimated_capped_lam                       AS estimated_capped_lam,
-      parent_crm_account_sales_segment           AS dim_parent_crm_account_sales_segment,
-      MIN(first_day_of_month)                    AS valid_from,
-      MAX(first_day_of_month)                    AS valid_to
+      valid_from                                 AS valid_from,
+      valid_to_final_date                        AS valid_to
     FROM {{ source('driveload', 'lam_corrections') }}
-    {{ dbt_utils.group_by(n=4) }}
 )
 
 SELECT *
