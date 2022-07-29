9d699cce12ab2ca3b766b844f9a993afbc6be496
Paul Armstrong <parmstrong@gitlab.com>
2022-07-29 18:48:11 +0200

    Correct data types

--- transform/snowflake-dbt/models/sources/driveload/driveload_lam_corrections_source.sql
+++ transform/snowflake-dbt/models/sources/driveload/driveload_lam_corrections_source.sql
@@ -1,12 +1,12 @@
 WITH source AS (
 
     SELECT
-      dim_parent_crm_account_id                  AS dim_parent_crm_account_id,
-      parent_crm_account_sales_segment           AS dim_parent_crm_account_sales_segment,
-      dev_count                                  AS dev_count,
-      estimated_capped_lam                       AS estimated_capped_lam,
-      valid_from                                 AS valid_from,
-      valid_to_final_date                        AS valid_to
+      dim_parent_crm_account_id::VARCHAR                AS dim_parent_crm_account_id,
+      parent_crm_account_sales_segment::VARCHAR         AS dim_parent_crm_account_sales_segment,
+      dev_count::FLOAT                                  AS dev_count,
+      estimated_capped_lam::FLOAT                       AS estimated_capped_lam,
+      valid_from::DATE                                  AS valid_from,
+      valid_to_final_date::DATETIME                     AS valid_to
     FROM {{ source('driveload', 'lam_corrections') }}
 )
 
