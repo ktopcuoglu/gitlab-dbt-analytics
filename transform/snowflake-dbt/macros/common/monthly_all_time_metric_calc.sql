48b17c7b9a88bd7e0967ca7addf13b6b1e6bb8c3
Israel Weeks <iweeks@gitlab.com>
2022-07-30 20:45:18 -0400

    update macro

--- transform/snowflake-dbt/macros/common/monthly_all_time_metric_calc.sql
+++ transform/snowflake-dbt/macros/common/monthly_all_time_metric_calc.sql
@@ -1,8 +1,8 @@
 {%- macro monthly_all_time_metric_calc(metric_value, dim_installation_id, metrics_path, ping_created_at) -%}
 
-ROUND({{ metric_value }} -
+{{ metric_value }} -
 COALESCE(LAG({{ metric_value }}) OVER
         (PARTITION BY {{ dim_installation_id }}, {{ metrics_path }}
-         ORDER BY {{ ping_created_at }} ASC), 0)) AS monthly_metric_value
+         ORDER BY {{ ping_created_at }} ASC), 0) AS monthly_metric_value
 
 {%- endmacro -%}
