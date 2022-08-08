95fbe4abe5d844b84971b441868bfe7582d42a1f
Peter Empey <pempey@gitlab.com>
2022-08-08 09:15:46 -0600

    add match on email

--- transform/snowflake-dbt/models/common_mapping/map_team_member_user.sql
+++ transform/snowflake-dbt/models/common_mapping/map_team_member_user.sql
@@ -2,18 +2,28 @@ WITH employees AS (
   -- Attempts to get the most recent username for a team member
   SELECT DISTINCT
     employee_id,
-    LAST_VALUE(gitlab_username)
+    LAST_VALUE(NULLIF(gitlab_username, ''))
     OVER (PARTITION BY employee_id ORDER BY uploaded_at ASC) AS gitlab_username
   FROM {{ ref('blended_employee_mapping_source') }}
   WHERE NULLIF(gitlab_username, '') IS NOT NULL
 
 ),
 
+emails AS (
+  -- Attempts to get the most recent user email for a team_member
+  SELECT DISTINCT
+    employee_id,
+    LAST_VALUE(work_email) IGNORE NULLS
+    OVER (PARTITION BY employee_id ORDER BY uploaded_at ASC) AS gitlab_email
+  FROM {{ ref('blended_directory_source') }}
+),
+
 users AS (
   -- Attempts to get the most recent user id for a username
   SELECT
     user_id,
-    user_name
+    user_name,
+    notification_email
   FROM {{ ref('gitlab_dotcom_users_source') }}
   QUALIFY ROW_NUMBER() OVER (PARTITION BY user_name ORDER BY last_activity_on DESC ) = 1
 
@@ -26,10 +36,14 @@ map AS (
     {{ dbt_utils.surrogate_key(['users.user_id']) }} AS dim_user_sk,
     employees.employee_id,
     users.user_id,
-    users.user_name AS gitlab_username
+    users.user_name AS gitlab_username,
+    users.notification_email
   FROM employees
+  LEFT JOIN emails
+    ON employees.employee_id = emails.employee_id
   LEFT JOIN users
     ON employees.gitlab_username = users.user_name
+      OR emails.gitlab_email = users.notification_email
   WHERE users.user_id IS NOT NULL
 )
 
@@ -38,5 +52,5 @@ map AS (
     created_by="@pempey",
     updated_by="@pempey",
     created_date="2022-07-22",
-    updated_date="2022-07-22"
+    updated_date="2022-08-08"
 ) }}
