f40537a2a60943867b6a253913e9103d46fded98
Jueun Kim <jkim@gitlab.com>
2022-08-16 18:24:03 +0200

    removing date casting

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_sales_sfdc_opportunity_xf_refactored.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_sales_sfdc_opportunity_xf_refactored.sql
@@ -635,7 +635,7 @@ WITH sfdc_opportunity AS (
       -- 20201021 NF: This should be replaced by a table that keeps track of excluded deals for forecasting purposes
       CASE
         WHEN account.ultimate_parent_id IN ('001610000111bA3','0016100001F4xla','0016100001CXGCs','00161000015O9Yn','0016100001b9Jsc')
-          AND sfdc_opportunity_xf.close_date < '2020-08-01'::DATE
+          AND sfdc_opportunity_xf.close_date < '2020-08-01'
             THEN 1
         -- NF 2021 - Pubsec extreme deals
         WHEN sfdc_opportunity_xf.opportunity_id IN ('0064M00000WtZKUQA3','0064M00000Xb975QAB')
